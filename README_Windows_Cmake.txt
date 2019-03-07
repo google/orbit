b259935e5478f7beab7ddd6efba061f1fabf0f07
Pierric-XPS <pierric.gimmig@gmail.com>
2019-03-06 23:37:31 -0800

    Cmake: Fixed DIA SDK directory.  Copy qt dlls to exe dir.

--- README_Windows_Cmake.txt
+++ README_Windows_Cmake.txt
@@ -1,8 +1,9 @@
 Requirements:
 - Make sure you have the DIA SDK installed, (install Visual Studio with Windows 10 SDK component)
+-- ex: C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\DIA SDK
 
 1. Install Qt (5.12.1 works).  Make sure you select the appropriate MSVC versions to install.
-2. Add C:\Qt\5.12.1\msvc2017_64\bin\ to your PATH.
+2. Create a QTDIR environment variable and set it to your qt installation directory (ex: C:\Qt\5.12.1\msvc2017_64\bin\).
 3. cd to root of orbitprofiler directory
 4. bootstrap-orbit.sh
 6. Open build/orbitProfiler.sln using a Visual Studio *started with administrator privileges*
--- bootstrap-orbit.bat
+++ bootstrap-orbit.bat
@@ -1,14 +1,12 @@
-::Qt is in C:\Qt\5.8\msvc2015_64\
-
 :: Build vcpkg
-git submodule init
-git submodule update
+call git submodule init
+call git submodule update
 cd external/vcpkg
 
 if exist "vcpkg.exe" (
     echo found vcpkg.exe
 ) else (
-    call .\bootstrap-vcpkg.bat
+    call ./bootstrap-vcpkg.bat
 )
 
 :: Build dynamic dependencies
@@ -20,12 +18,15 @@ vcpkg install freeglut glew freetype-gl curl breakpad capstone asio cereal imgui
 set VCPKG_DEFAULT_TRIPLET=x64-windows-static
 vcpkg install capstone freeglut imgui
 
+if not exist "installed/x64-windows/include/google_breakpad/processor/linked_ptr.h" (
+call copy "buildtrees/breakpad/src/9e12edba6d-12269dd01c/src/processor/linked_ptr.h" "installed/x64-windows/include/google_breakpad/processor/linked_ptr.h"
+)
+
 :: CMake
 cd ../..
+
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE='..\external\vcpkg\scripts\buildsystems\vcpkg.cmake' -DCMAKE_GENERATOR_PLATFORM=x64 ..
+cmake -DCMAKE_TOOLCHAIN_FILE='../external/vcpkg/scripts/buildsystems/vcpkg.cmake' -DCMAKE_GENERATOR_PLATFORM=x64 ..
 
 cd ..
-
-copy "external\vcpkg\buildtrees\breakpad\src\9e12edba6d-12269dd01c\src\processor\linked_ptr.h" "external\vcpkg\installed\x64-windows\include\google_breakpad\processor\linked_ptr.h"
