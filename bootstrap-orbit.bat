fddce6893f1770e3f3dc5b4e0ebe8609b7e75b8e
Pierric-XPS <pierric.gimmig@gmail.com>
2019-03-07 00:55:28 -0800

    fixed linked_ptr.h copy

--- bootstrap-orbit.bat
+++ bootstrap-orbit.bat
@@ -18,10 +18,6 @@ vcpkg install freeglut glew freetype-gl curl breakpad capstone asio cereal imgui
 set VCPKG_DEFAULT_TRIPLET=x64-windows-static
 vcpkg install capstone freeglut imgui
 
-if not exist "installed/x64-windows/include/google_breakpad/processor/linked_ptr.h" (
-call copy "buildtrees/breakpad/src/9e12edba6d-12269dd01c/src/processor/linked_ptr.h" "installed/x64-windows/include/google_breakpad/processor/linked_ptr.h"
-)
-
 :: CMake
 cd ../..
 
@@ -30,3 +26,5 @@ cd build
 cmake -DCMAKE_TOOLCHAIN_FILE='../external/vcpkg/scripts/buildsystems/vcpkg.cmake' -DCMAKE_GENERATOR_PLATFORM=x64 ..
 
 cd ..
+
+copy "external\vcpkg\buildtrees\breakpad\src\9e12edba6d-12269dd01c\src\processor\linked_ptr.h" "external\vcpkg\installed\x64-windows\include\google_breakpad\processor\linked_ptr.h" /y
