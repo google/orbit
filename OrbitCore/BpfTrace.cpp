bbf8c8a97611f63db05f9fedb23e7ce1f2e6294a
Pierric Gimmig <pierric.gimmig@gmail.com>
2020-01-27 21:12:17 -0800

    Remove spam in RunPerfEventOpen.

--- OrbitCore/BpfTrace.cpp
+++ OrbitCore/BpfTrace.cpp
@@ -363,7 +363,6 @@ void BpfTrace::RunPerfEventOpen(bool* a_ExitRequested)
 
     while( !(*a_ExitRequested) )
     {
-        SCOPE_TIMER_LOG("Perf events consume iteration");
         // Lets sleep a bit, such that we are not constantly reading from the buffers
         // and thus wasting cpu time. 10000 microseconds are still small enough to 
         // not have our buffers overflown and therefore losing events.
@@ -394,7 +393,7 @@ void BpfTrace::RunPerfEventOpen(bool* a_ExitRequested)
                 {
                 case PERF_RECORD_SAMPLE:
                     {
-                        PRINT("Consuming uprobe record...");
+                        SCOPE_TIMER_LOG("PERF_RECORD_SAMPLE");
                         if (GParams.m_BpftraceCallstacks)
                         {
                             auto record = ring_buffer.ConsumeRecord<LinuxUprobeEventWithStack>(header);
