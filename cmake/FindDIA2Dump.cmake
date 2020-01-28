feaf6c0edb2eca1ce089ee7756edbad990a5222c
Henning Becker <henning.becker@gmail.com>
2020-01-28 14:22:03 +0100

    Applied cmake formatting by cmake-format.

--- cmake/FindDIA2Dump.cmake
+++ cmake/FindDIA2Dump.cmake
@@ -2,23 +2,20 @@ project(DIA2Dump)
 set(DIR external/DIA2Dump)
 
 add_library(DIA2Dump OBJECT)
-target_sources(DIA2Dump PUBLIC
-    ${DIR}/Callback.h
-    ${DIR}/DIA2Dump.h
-    ${DIR}/PrintSymbol.h
-    ${DIR}/regs.h)
+target_sources(DIA2Dump PUBLIC ${DIR}/Callback.h ${DIR}/DIA2Dump.h
+                               ${DIR}/PrintSymbol.h ${DIR}/regs.h)
 
-target_sources(DIA2Dump PRIVATE
-    ${DIR}/dia2dump.cpp
-    ${DIR}/PrintSymbol.cpp
-    ${DIR}/regs.cpp)
+target_sources(DIA2Dump PRIVATE ${DIR}/dia2dump.cpp ${DIR}/PrintSymbol.cpp
+                                ${DIR}/regs.cpp)
 
 target_include_directories(DIA2Dump SYSTEM PUBLIC ${DIR})
 target_link_libraries(DIA2Dump PUBLIC DIASDK::DIASDK)
 
-# FIXME: DIA2Dump depends on OrbitCore and OrbitGl, hence
-# it should be pulled into OrbitGl.
-target_link_libraries(DIA2Dump PUBLIC multicore::multicore concurrentqueue::concurrentqueue oqpi::oqpi xxHash::xxHash cereal)
+# FIXME: DIA2Dump depends on OrbitCore and OrbitGl, hence it should be pulled
+# into OrbitGl.
+target_link_libraries(
+  DIA2Dump PUBLIC multicore::multicore concurrentqueue::concurrentqueue
+                  oqpi::oqpi xxHash::xxHash cereal)
 target_include_directories(DIA2Dump PRIVATE "OrbitCore/")
 
 add_library(DIA2Dump::DIA2Dump ALIAS DIA2Dump)
--- cmake/Findconcurrentqueue.cmake
+++ cmake/Findconcurrentqueue.cmake
@@ -1,7 +1,8 @@
 project(concurrentqueue CXX)
 
 add_library(concurrentqueue INTERFACE IMPORTED GLOBAL)
-target_include_directories(concurrentqueue SYSTEM INTERFACE external/concurrentqueue)
+target_include_directories(concurrentqueue SYSTEM
+                           INTERFACE external/concurrentqueue)
 target_compile_features(concurrentqueue INTERFACE cxx_std_11)
 
 add_library(concurrentqueue::concurrentqueue ALIAS concurrentqueue)
