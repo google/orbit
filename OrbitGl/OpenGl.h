2d4df7843ad0550f2a0e4fc8cb0f44dbc778d6c6
Henning Becker <hebecker@google.com>
2020-01-29 16:19:17 +0100

    Added one more change about opengl in the windows build.

--- OrbitGl/OpenGl.h
+++ OrbitGl/OpenGl.h
@@ -3,6 +3,14 @@
 //-----------------------------------
 #pragma once
 
+// clang-format off
+#ifdef WIN32
+// windows.h must be included BEFORE GL/glew.h
+// otherwise wglew.h undefs WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif
+// clang-format on
+
 #include <GL/glew.h>
 #include <GL/glu.h>
 #include <freetype-gl.h>
