0f4cdb0be94cb1b4a16b91ef4ba748695e680c0f
Henning Becker <hebecker@google.com>
2020-01-31 09:16:03 +0100

    Added some more checks in bootstrap-orbit.sh

--- bootstrap-orbit.sh
+++ bootstrap-orbit.sh
@@ -11,8 +11,19 @@ sudo apt-get install -y build-essential cmake ninja bison flex
 sudo apt-get install -y libglu1-mesa-dev mesa-common-dev libxmu-dev libxi-dev 
 sudo apt-get install -y linux-tools-common
 
+# Dev dependencies:
+# - cmake >= 3.15
+# - ninja (optional)
+# - bison (for compiling qt5)
+# - flex (for compiling qt5)
+
 # Load Submodules (vcpkg, libunwindstack)
 git submodule update --init --recursive
+
+if [ $? -ne 0 ]; then
+  echo "Orbit: Could not update/initialize all the submodules. Exiting..."
+  exit 1
+fi
 
 # Build vcpkg
 cd external/vcpkg
@@ -23,11 +34,20 @@ then
 else
     echo "Orbit: compiling vcpkg"
     ./bootstrap-vcpkg.sh
+    if [ $? -ne 0 ]; then
+      echo "Orbit: Could not bootstrap vcpkg. Exiting..."
+      exit 2
+    fi
 fi
 
 ## Build dependencies
 ./vcpkg install freetype freetype-gl breakpad \
   capstone asio cereal imgui freeglut glew curl qt5-base
+
+if [ $? -ne 0 ]; then
+  echo "Orbit: Could not install all the dependencies. Check for vcpkg error messages. Exiting..."
+  exit 3
+fi
 
 # CMake
 cd ../..
@@ -39,5 +59,6 @@ cd build
 if [ ! -f toolchain.cmake ]; then
   cp ../contrib/toolchains/toolchain-linux-default-release.cmake toolchain.cmake
 fi
-cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -G Ninja ..
+
+cmake -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake -G Ninja .. || exit 4
 cmake --build .
