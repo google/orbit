b7795350600c500b6019cb5d4f72bd13fa08d561
antonrohr <antonrohr@google.com>
2020-01-31 09:41:56 +0100

    Improved loading debug symbols on Linux
    OrbitModule.cpp:
    FindSymbols() checks now if the module already includes debugging symbols. If not, it searches at specified locations for symbol file with the same filename and .debug/elf.debug file endings and then compares their BuildId with the BuildId of the module.

--- OrbitCore/OrbitModuleTest.cpp
+++ OrbitCore/OrbitModuleTest.cpp
@@ -9,10 +9,14 @@
 TEST(OrbitModule, LoadPdb) {
   std::string executable_path = Path::GetExecutablePath();
   // This is a simple executable that prints "Hello Earh!"
-  std::string test_elf_file = executable_path + "/testdata/hello_world_elf";
+  std::string file_path = executable_path + "/testdata/hello_world_elf";
 
   Pdb pdb;
-  ASSERT_TRUE(pdb.LoadFunctions(test_elf_file.c_str()));
+  ASSERT_TRUE(pdb.LoadFunctions(file_path.c_str()));
+
+  EXPECT_EQ(pdb.GetLoadedModuleName(), file_path);
+  EXPECT_EQ(pdb.GetFileName(), file_path);
+  EXPECT_EQ(pdb.GetName(), "hello_world_elf");
 
   // Check functions
   const std::vector<Function>& functions = pdb.GetFunctions();
@@ -25,7 +29,7 @@ TEST(OrbitModule, LoadPdb) {
   EXPECT_EQ(function->Address(), 0x1080);
   EXPECT_EQ(function->Size(), 0);
   EXPECT_EQ(function->GetPdb(), &pdb);
-  EXPECT_EQ(function->Probe(), test_elf_file + ":deregister_tm_clones");
+  EXPECT_EQ(function->Probe(), file_path + ":deregister_tm_clones");
 
   // This function points to .init section and should not have a Probe
   function = &functions[4];
@@ -42,5 +46,20 @@ TEST(OrbitModule, LoadPdb) {
   EXPECT_EQ(function->Address(), 0x1135);
   EXPECT_EQ(function->Size(), 35);
   EXPECT_EQ(function->GetPdb(), &pdb);
-  EXPECT_EQ(function->Probe(), test_elf_file + ":main");
+  EXPECT_EQ(function->Probe(), file_path + ":main");
+}
+
+TEST(OrbitModule, LoadPdbSeparateSymbols) {
+  std::string executable_path = Path::GetExecutablePath();
+  std::string file_path = executable_path + "testdata/no_symbols_elf";
+
+  Pdb pdb;
+  ASSERT_TRUE(pdb.LoadFunctions(file_path.c_str()));
+
+  std::string symbols_file_name = "no_symbols_elf.debug";
+  std::string symbols_path = executable_path + "testdata/" + symbols_file_name;
+
+  EXPECT_EQ(pdb.GetLoadedModuleName(), file_path);
+  EXPECT_EQ(pdb.GetFileName(), symbols_path);
+  EXPECT_EQ(pdb.GetName(), symbols_file_name);
 }
