6bf75599d0dc477ff57dee1b335259da98e3d1d0
Henning Becker <henning.becker@gmail.com>
2020-02-03 13:20:31 +0100

    Removed xxsum.c from xxHash target.
    xxsum.c is an entry point (has a main) and should not be part of
    the library. This already lead to an ODR violation when running
    gtest.

--- cmake/FindxxHash.cmake
+++ cmake/FindxxHash.cmake
@@ -1,6 +1,6 @@
 set(DIR external/xxHash-r42)
 
-add_library(xxHash OBJECT ${DIR}/xxhash.c ${DIR}/xxhsum.c)
+add_library(xxHash OBJECT ${DIR}/xxhash.c)
 target_include_directories(xxHash SYSTEM PUBLIC ${DIR})
 
 add_library(xxHash::xxHash ALIAS xxHash)
