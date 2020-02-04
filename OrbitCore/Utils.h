google/orbit

commit 97bbe50e2127f329ed57a922a4e6643105b10c62

Switch string formatting to absl::StrFormat()
This way we do not have to maintain our own copies of these function.

OrbitCore/Utils.h
@@ -19,6 +19,8 @@
 #include <unordered_map>
 #include <vector>
 
+#include "absl/strings/str_format.h"
+
 #include "BaseTypes.h"
 #include "Platform.h"
 
@@ -93,21 +95,13 @@ inline void Fill(T& a_Array, U& a_Value) {
 
 //-----------------------------------------------------------------------------
 template <typename... Args>
-std::string Format(const char* format, Args... args) {
-  const size_t size = 4096;
-  char buf[size];
-  snprintf(buf, size, format, args...);
-  return std::string(buf);
-}
-
-//-----------------------------------------------------------------------------
-template <typename... Args>
 std::wstring Format(const wchar_t* format, Args... args) {
   const size_t size = 4096;
   wchar_t buf[size];
   swprintf(buf, size, format, args...);
   return std::wstring(buf);
 }
+
 
 //-----------------------------------------------------------------------------
 template <class T>
@@ -414,99 +408,66 @@ class CWindowsMessageToString {
 #endif
 
 //-----------------------------------------------------------------------------
-inline std::wstring GetPrettySize(ULONG64 a_Size) {
-  const double KB = 1024.0;
-  const double MB = 1024.0 * KB;
-  const double GB = 1024.0 * MB;
-  const double TB = 1024.0 * GB;
+inline std::string GetPrettySize(uint64_t size) {
+  constexpr double KB = 1024.0;
+  constexpr double MB = 1024.0 * KB;
+  constexpr double GB = 1024.0 * MB;
+  constexpr double TB = 1024.0 * GB;
 
-  double size = (double)a_Size;
+  if (size < KB) return absl::StrFormat("%" PRIu64" B", size);
+  if (size < MB) return absl::StrFormat("%.2f KB", size / KB);
+  if (size < GB) return absl::StrFormat("%.2f MB", size / MB);
+  if (size < TB) return absl::StrFormat("%.2f GB", size / GB);
 
-  if (size < KB) return Format(L"%I64u B", a_Size);
-  if (size < MB) return Format(L"%.2f KB", size / KB);
-  if (size < GB) return Format(L"%.2f MB", size / MB);
-  if (size < TB) return Format(L"%.2f GB", size / GB);
-
-  return Format(L"%.2f TB", size / TB);
+  return absl::StrFormat("%.2f TB", size / TB);
 }
 
 //-----------------------------------------------------------------------------
-inline std::string GetPrettyTime(double a_MilliSeconds) {
-  const double Day = 24 * 60 * 60 * 1000;
-  const double Hour = 60 * 60 * 1000;
-  const double Minute = 60 * 1000;
-  const double Second = 1000;
-  const double Milli = 1;
-  const double Micro = 0.001;
-  const double Nano = 0.000001;
+inline std::string GetPrettyTime(double milliseconds) {
+  constexpr double Day = 24 * 60 * 60 * 1000;
+  constexpr double Hour = 60 * 60 * 1000;
+  constexpr double Minute = 60 * 1000;
+  constexpr double Second = 1000;
+  constexpr double Milli = 1;
+  constexpr double Micro = 0.001;
+  constexpr double Nano = 0.000001;
 
   std::string res;
 
-  if (a_MilliSeconds < Micro)
-    res = Format("%.3f ns", a_MilliSeconds / Nano);
-  else if (a_MilliSeconds < Milli)
-    res = Format("%.3f us", a_MilliSeconds / Micro);
-  else if (a_MilliSeconds < Second)
-    res = Format("%.3f ms", a_MilliSeconds);
-  else if (a_MilliSeconds < Minute)
-    res = Format("%.3f s", a_MilliSeconds / Second);
-  else if (a_MilliSeconds < Hour)
-    res = Format("%.3f min", a_MilliSeconds / Minute);
-  else if (a_MilliSeconds < Day)
-    res = Format("%.3f h", a_MilliSeconds / Hour);
-  else
-    res = Format("%.3f days", a_MilliSeconds / Day);
+  if (milliseconds < Micro) {
+    res = absl::StrFormat("%.3f ns", milliseconds / Nano);
+  } else if (milliseconds < Milli) {
+    res = absl::StrFormat("%.3f us", milliseconds / Micro);
+  } else if (milliseconds < Second) {
+    res = absl::StrFormat("%.3f ms", milliseconds);
+  } else if (milliseconds < Minute) {
+    res = absl::StrFormat("%.3f s", milliseconds / Second);
+  } else if (milliseconds < Hour) {
+    res = absl::StrFormat("%.3f min", milliseconds / Minute);
+  } else if (milliseconds < Day) {
+    res = absl::StrFormat("%.3f h", milliseconds / Hour);
+  } else {
+    res = absl::StrFormat("%.3f days", milliseconds / Day);
+  }
 
   return res;
 }
 
 //-----------------------------------------------------------------------------
-inline std::wstring GetPrettyTimeW(double a_MilliSeconds) {
-  const double Day = 24 * 60 * 60 * 1000;
-  const double Hour = 60 * 60 * 1000;
-  const double Minute = 60 * 1000;
-  const double Second = 1000;
-  const double Milli = 1;
-  const double Micro = 0.001;
-  const double Nano = 0.000001;
+inline std::string GetPrettyBitRate(uint64_t size_in_bytes) {
+  uint64_t size = 8 * size_in_bytes;
 
-  std::wstring res;
+  constexpr double KB = 1024.0;
+  constexpr double MB = 1024.0 * KB;
+  constexpr double GB = 1024.0 * MB;
+  constexpr double TB = 1024.0 * GB;
 
-  if (a_MilliSeconds < Micro)
-    res = Format(L"%.3f ns", a_MilliSeconds / Nano);
-  else if (a_MilliSeconds < Milli)
-    res = Format(L"%.3f us", a_MilliSeconds / Micro);
-  else if (a_MilliSeconds < Second)
-    res = Format(L"%.3f ms", a_MilliSeconds);
-  else if (a_MilliSeconds < Minute)
-    res = Format(L"%.3f s", a_MilliSeconds / Second);
-  else if (a_MilliSeconds < Hour)
-    res = Format(L"%.3f min", a_MilliSeconds / Minute);
-  else if (a_MilliSeconds < Day)
-    res = Format(L"%.3f h", a_MilliSeconds / Hour);
-  else
-    res = Format(L"%.3f days", a_MilliSeconds / Day);
+  if (size < KB) return absl::StrFormat("%" PRIu64 " bit/s", size);
+  if (size < MB) return absl::StrFormat("%.2f kbit/s", size / KB);
+  if (size < GB) return absl::StrFormat("%.2f Mbit/s", size / MB);
+  if (size < TB) return absl::StrFormat("%.2f Gbit/s", size / GB);
 
-  return res;
-}
-
-//-----------------------------------------------------------------------------
-inline std::string GetPrettyBitRate(ULONG64 a_SizeInBytes) {
-  ULONG64 a_Size = 8 * a_SizeInBytes;
-
-  const double KB = 1024.0;
-  const double MB = 1024.0 * KB;
-  const double GB = 1024.0 * MB;
-  const double TB = 1024.0 * GB;
-
-  double size = (double)a_Size;
-
-  if (size < KB) return Format("%I64u bit/s", a_Size);
-  if (size < MB) return Format("%.2f kbit/s", size / KB);
-  if (size < GB) return Format("%.2f Mbit/s", size / MB);
-  if (size < TB) return Format("%.2f Gbit/s", size / GB);
-
-  return Format("%.2f Tbit/s", size / TB);
+  return absl::StrFormat("%.2f Tbit/s", size / TB);
 }
 
 #ifndef WIN32
@@ -590,7 +551,7 @@ std::vector<std::pair<Key, Val> > ValueSort(
               });
   else
     std::sort(vec.begin(), vec.end(),
-              [&a_SortFunc](const PairType& a, const PairType& b) {
+              [](const PairType& a, const PairType& b) {
                 return a.second < b.second;
               });
 

CMakeLists.txt
@@ -36,6 +36,7 @@ find_package(CURL REQUIRED)
 find_package(oqpi REQUIRED)
 find_package(capstone REQUIRED)
 find_package(GLUT REQUIRED)
+find_package(unofficial-abseil CONFIG REQUIRED)
 
 # Hack for freelgut:
 # freeglut accidentally links against libxrandr when the headers of

OrbitCore/CMakeLists.txt
@@ -176,7 +176,8 @@ target_include_directories(OrbitCore PUBLIC ${CMAKE_CURRENT_LIST_DIR})
 
 target_link_libraries(
   OrbitCore PUBLIC cereal xxHash::xxHash concurrentqueue::concurrentqueue
-                   multicore::multicore CURL::CURL oqpi::oqpi)
+                   multicore::multicore CURL::CURL oqpi::oqpi
+                   unofficial::abseil::strings)
 
 if(WIN32)
   target_link_libraries(

OrbitCore/Capture.cpp
@@ -7,6 +7,8 @@
 #include <fstream>
 #include <ostream>
 
+#include "absl/strings/str_format.h"
+
 #include "BpfTrace.h"
 #include "Core.h"
 #include "CoreApp.h"
@@ -94,7 +96,8 @@ bool Capture::Inject(bool a_WaitForConnection) {
 
   GInjected = inject.Inject(dllName.c_str(), *GTargetProcess, "OrbitInit");
   if (GInjected) {
-    ORBIT_LOG(Format("Injected in %s", GTargetProcess->GetName().c_str()));
+    ORBIT_LOG(absl::StrFormat("Injected in %s",
+                              ws2s(GTargetProcess->GetName()).c_str()));
     GInjectedProcessW = GTargetProcess->GetName();
     GInjectedProcess = ws2s(GInjectedProcessW);
   }
@@ -103,7 +106,8 @@ bool Capture::Inject(bool a_WaitForConnection) {
     int numTries = 50;
     while (!GTcpServer->HasConnection() && numTries-- > 0) {
       ORBIT_LOG(
-          Format("Waiting for connection on port %i", Capture::GCapturePort));
+          absl::StrFormat("Waiting for connection on port %i",
+                          Capture::GCapturePort));
       Sleep(100);
     }
 
@@ -123,7 +127,8 @@ bool Capture::InjectRemote() {
       inject.Inject(dllName.c_str(), *GTargetProcess, "OrbitInitRemote");
 
   if (GInjected) {
-    ORBIT_LOG(Format("Injected in %s", GTargetProcess->GetName().c_str()));
+    ORBIT_LOG(absl::StrFormat("Injected in %s",
+                              ws2s(GTargetProcess->GetName()).c_str()));
     GInjectedProcessW = GTargetProcess->GetName();
     GInjectedProcess = ws2s(GInjectedProcessW);
   }

OrbitCore/LinuxPerf.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <iostream>
 
+#include "absl/strings/str_format.h"
+
 #include "Callstack.h"
 #include "Capture.h"
 #include "ConnectionManager.h"
@@ -46,7 +48,7 @@
 LinuxPerf::LinuxPerf(uint32_t a_PID, uint32_t a_Freq)
     : m_PID(a_PID), m_Frequency(a_Freq) {
   m_Callback = [this](const std::string& a_Buffer) { HandleLine(a_Buffer); };
-  m_PerfCommand = Format(
+  m_PerfCommand = absl::StrFormat(
       "perf record -k monotonic -F %u -p %u -g --no-buffering -o - | perf "
       "script -i -",
       m_Frequency, m_PID);

OrbitCore/LinuxUtils.cpp
@@ -28,6 +28,8 @@
 #include <sstream>
 #include <string>
 
+#include "absl/strings/str_format.h"
+
 #include "Callstack.h"
 #include "Capture.h"
 #include "ConnectionManager.h"
@@ -48,7 +50,7 @@ std::vector<std::string> ListModules(pid_t a_PID) {
   std::vector<std::string> modules;
   // TODO: we should read the file directly instead or memory map it.
   std::string result =
-      ExecuteCommand(Format("cat /proc/%u/maps", a_PID).c_str());
+      ExecuteCommand(absl::StrFormat("cat /proc/%u/maps", a_PID).c_str());
 
   std::stringstream ss(result);
   std::string line;
@@ -63,7 +65,7 @@ std::vector<std::string> ListModules(pid_t a_PID) {
 std::vector<pid_t> ListThreads(pid_t a_PID) {
   std::vector<pid_t> threads;
   std::string result =
-      ExecuteCommand(Format("ls /proc/%u/task", a_PID).c_str());
+      ExecuteCommand(absl::StrFormat("ls /proc/%u/task", a_PID).c_str());
 
   std::stringstream ss(result);
   std::string line;
@@ -92,7 +94,7 @@ std::string ReadMaps(pid_t a_PID) {
 //-----------------------------------------------------------------------------
 uint64_t GetTracePointID(const char* a_Group, const char* a_Event) {
   std::string cmd =
-      Format("cat /sys/kernel/debug/tracing/events/%s/%s/id", a_Group, a_Event);
+      absl::StrFormat("cat /sys/kernel/debug/tracing/events/%s/%s/id", a_Group, a_Event);
   std::string result = ExecuteCommand(cmd.c_str());
   trim(result);
   return std::stoull(result);
@@ -154,7 +156,7 @@ void ListModules(pid_t a_PID,
 
 //-----------------------------------------------------------------------------
 uint32_t GetPID(const char* a_Name) {
-  std::string command = Format("ps -A | grep %s", a_Name);
+  std::string command = absl::StrFormat("ps -A | grep %s", a_Name);
   std::string result = ExecuteCommand(command.c_str());
   auto tokens = Tokenize(result);
   if (!tokens.empty()) return (uint32_t)atoi(tokens[0].c_str());
@@ -185,7 +187,7 @@ std::unordered_map<uint32_t, float> GetCpuUtilization() {
 //-----------------------------------------------------------------------------
 bool Is64Bit(pid_t a_PID) {
   std::string result =
-      ExecuteCommand(Format("file -L /proc/%u/exe", a_PID).c_str());
+      ExecuteCommand(absl::StrFormat("file -L /proc/%u/exe", a_PID).c_str());
   return Contains(result, "64-bit");
 }
 

OrbitCore/Log.h
@@ -71,10 +71,10 @@ class Logger {
     m_Logs[a_Type].Log(a_String);
   }
 
-  void LogError(const char* a_Function, int a_Line) {
+  void LogError(const char* function, int line) {
     std::string err = GetLastErrorAsString();
-    Log(OrbitLog::Global, Format("Error: %s (%i) LastError: %s", a_Function,
-                                 a_Line, err.c_str()));
+    Log(OrbitLog::Global, absl::StrFormat("Error: %s (%i) LastError: %s",
+                                          function, line, err.c_str()));
   }
 
   template <class T>

OrbitCore/ModuleManager.cpp
@@ -4,6 +4,7 @@
 
 #include "ModuleManager.h"
 
+#include "absl/strings/str_format.h"
 #include "Capture.h"
 #include "CoreApp.h"
 #include "OrbitModule.h"
@@ -47,8 +48,9 @@ void ModuleManager::OnReceiveMessage(const Message& a_Msg) {
     } else if (dataType == DataTransferHeader::Code) {
       Function* func = Capture::GTargetProcess->GetFunctionFromAddress(
           header.m_Address, true);
-      std::string name =
-          func ? func->PrettyName() : Format("%p", (void*)header.m_Address);
+      std::string name = func ? func->PrettyName() :
+          absl::StrFormat("0x%" PRIx64, header.m_Address);
+
       GCoreApp->Disassemble(name, header.m_Address, a_Msg.GetData(),
                             a_Msg.m_Size);
     }

commit 67d1c10a88c549269dc7a2bf8dafb8cedb51dc0e

Bring std::string Format back to fix windows build
This fixes windows build regression introduced by
97bbe50e2127f329ed57a922a4e6643105b10c62

OrbitCore/Utils.h
@@ -95,6 +95,15 @@ inline void Fill(T& a_Array, U& a_Value) {
 
 //-----------------------------------------------------------------------------
 template <typename... Args>
+std::string Format(const char* format, Args... args) {
+  const size_t size = 4096;
+  char buf[size];
+  snprintf(buf, size, format, args...);
+  return std::string(buf);
+}
+
+//-----------------------------------------------------------------------------
+template <typename... Args>
 std::wstring Format(const wchar_t* format, Args... args) {
   const size_t size = 4096;
   wchar_t buf[size];