4b0d01bfffc7d022273a47ad68d10c632ce20d03
Daniel Fenner <dfenner@google.com>
2020-02-07 10:25:34 +0100

    Undo changes used to debug CI.

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fail on any error.
-#set -e
+set -e
 
 # Display commands being run.
 # WARNING: please only enable 'set -x' if necessary for debugging, and be very
@@ -18,6 +18,12 @@
 # in the job configuration.
 cd ${KOKORO_ARTIFACTS_DIR}/github/orbitprofiler
 ./bootstrap-orbit.sh
-external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-echo "INSTANCE_EXTERNAL_IP=${external_ip}"
-sleep 7200;
+
+# Uncomment the three lines below to print the external ip into the log and
+# keep the vm alive for two hours. This is useful to debug build failures that
+# can not be resolved by looking into sponge alone. Also comment out the
+# "set -e" at the top of this file (otherwise a failed build will exit this
+# script immediately).
+# external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+# echo "INSTANCE_EXTERNAL_IP=${external_ip}"
+# sleep 7200;
