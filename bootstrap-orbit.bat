474ebe2adab4918b9d8f3738caed3f0fe8863fb6
danielfenner <56686285+danielfenner@users.noreply.github.com>
2020-02-18 18:32:47 +0100

    Require a specific version of WinSDK. (#164)

--- bootstrap-orbit.bat
+++ bootstrap-orbit.bat
@@ -2,6 +2,9 @@
 
 :: Possible values are 141 for Visual Studio 2017 or 142 for Visual Studio 2019
 set ORBIT_VS_VERSION=141
+:: We require a specific version of the Winsdk to avoid anything random being picked up.
+:: This is appended to the toolchain files for vcpkg and Orbit itself below.
+set ORBIT_WINSDK_VERSION="10.0.18362.0"
 
 :: Build vcpkg
 call git submodule update --init
@@ -10,21 +13,16 @@ cd external/vcpkg
 if exist "vcpkg.exe" (
     echo found vcpkg.exe
 ) else (
-    if %ORBIT_VS_VERSION% EQU 141 (
-        echo set^(VCPKG_PLATFORM_TOOLSET v141^) >> triplets\x86-windows.cmake
-        echo set^(VCPKG_PLATFORM_TOOLSET v141^) >> triplets\x64-windows.cmake
-    ) else (
-        echo set^(VCPKG_PLATFORM_TOOLSET v142^) >> triplets\x86-windows.cmake
-        echo set^(VCPKG_PLATFORM_TOOLSET v142^) >> triplets\x64-windows.cmake
-    )
+    echo set^(VCPKG_PLATFORM_TOOLSET v%ORBIT_VS_VERSION%^) >> triplets\x86-windows.cmake
+    echo set^(VCPKG_PLATFORM_TOOLSET v%ORBIT_VS_VERSION%^) >> triplets\x64-windows.cmake
+    echo set^(CMAKE_SYSTEM_VERSION %ORBIT_WINSDK_VERSION%^) >> triplets\x86-windows.cmake
+    echo set^(CMAKE_SYSTEM_VERSION %ORBIT_WINSDK_VERSION%^) >> triplets\x64-windows.cmake
     call ./bootstrap-vcpkg.bat
 )
 
-:: 32 bit
+:: Build dynamic dependencies
 set VCPKG_DEFAULT_TRIPLET=x86-windows
 vcpkg install abseil freeglut glew freetype freetype-gl curl breakpad capstone asio cereal imgui qt5-base gtest
-
-:: Build dynamic dependencies
 set VCPKG_DEFAULT_TRIPLET=x64-windows
 vcpkg install abseil freeglut glew freetype freetype-gl curl breakpad capstone asio cereal imgui qt5-base gtest
 
@@ -37,6 +35,7 @@ copy "external\vcpkg\buildtrees\breakpad\src\f427f61ed3-fe83a49e5d\src\processor
 :: CMake build
 mkdir build_release_x86
 copy "contrib\toolchains\toolchain-windows-32bit-msvc-release.cmake" "build_release_x86\toolchain.cmake" /y
+echo set^(CMAKE_SYSTEM_VERSION %ORBIT_WINSDK_VERSION%^) >> build_release_x86\toolchain.cmake
 cd build_release_x86
 if %ORBIT_VS_VERSION% EQU 141 (
     cmake -DCMAKE_TOOLCHAIN_FILE="toolchain.cmake" -G "Visual Studio 15 2017 Win32" ..
@@ -48,6 +47,7 @@ cd ..
 
 mkdir build_release_x64
 copy "contrib\toolchains\toolchain-windows-64bit-msvc-release.cmake" "build_release_x64\toolchain.cmake" /y
+echo set^(CMAKE_SYSTEM_VERSION %ORBIT_WINSDK_VERSION%^) >> build_release_x64\toolchain.cmake
 cd build_release_x64
 if %ORBIT_VS_VERSION% EQU 141 (
     cmake -DCMAKE_TOOLCHAIN_FILE="toolchain.cmake" -G "Visual Studio 15 2017 Win64" ..
