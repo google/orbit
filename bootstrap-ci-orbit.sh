96a60c28d3e68ba4ab521265ffc48fca21420526
Henning Becker <henning.becker@gmail.com>
2020-03-09 08:51:34 +0100

    Removed git-submodule initialization from scripts.
    git submodules are not needed anymore, since the
    last two submodules (lzma and libunwindstack) have been
    moved over to conan packages.

--- bootstrap-ci-orbit.sh
+++ bootstrap-ci-orbit.sh
@@ -2,14 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Loading submodules (libunwindstack, lzma)..."
-(cd $DIR && git submodule update --init --recursive)
-
-if [ $? -ne 0 ]; then
-  echo "Orbit: Could not update/initialize all the submodules. Exiting..."
-  exit 1
-fi
-
 echo "Installing conan configuration (profiles, settings, etc.)..."
 conan config install $DIR/contrib/conan/config || exit $?
 
--- bootstrap-orbit.sh
+++ bootstrap-orbit.sh
@@ -35,15 +35,6 @@ else
   echo "Found conan. Skipping installation..."
 fi
 
-
-echo "Loading submodules (libunwindstack, lzma)..."
-(cd $DIR && git submodule update --init --recursive)
-
-if [ $? -ne 0 ]; then
-  echo "Orbit: Could not update/initialize all the submodules. Exiting..."
-  exit 1
-fi
-
 echo "Installing conan configuration (profiles, settings, etc.)..."
 conan config install $DIR/contrib/conan/config || exit $?
 
