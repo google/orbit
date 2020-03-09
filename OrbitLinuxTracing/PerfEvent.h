332c8c89e8fa60d53f4b0e703ea22e6c6a22cae4
Davide Pallotti <dpallotti@google.com>
2020-03-09 13:39:37 +0100

    Un-templatize classes in PerfEvent.h

--- OrbitLinuxTracing/PerfEvent.h
+++ OrbitLinuxTracing/PerfEvent.h
@@ -131,10 +131,9 @@ class LostPerfEvent : public PerfEvent {
   uint64_t GetNumLost() const { return ring_buffer_record.lost; }
 };
 
-template <typename perf_record_t>
 class SamplePerfEvent : public PerfEvent {
  public:
-  perf_record_t ring_buffer_record;
+  perf_event_stack_sample ring_buffer_record;
 
   uint64_t GetTimestamp() const override {
     return ring_buffer_record.sample_id.time;
@@ -143,56 +142,55 @@ class SamplePerfEvent : public PerfEvent {
   pid_t GetPid() const { return ring_buffer_record.sample_id.pid; }
   pid_t GetTid() const { return ring_buffer_record.sample_id.tid; }
   uint32_t GetCpu() const { return ring_buffer_record.sample_id.cpu; }
-};
-
-namespace {
-std::array<uint64_t, PERF_REG_X86_64_MAX>
-perf_sample_regs_user_all_to_register_array(
-    const perf_event_sample_regs_user_all& regs) {
-  std::array<uint64_t, PERF_REG_X86_64_MAX> registers{};
-  registers[PERF_REG_X86_AX] = regs.ax;
-  registers[PERF_REG_X86_BX] = regs.bx;
-  registers[PERF_REG_X86_CX] = regs.cx;
-  registers[PERF_REG_X86_DX] = regs.dx;
-  registers[PERF_REG_X86_SI] = regs.si;
-  registers[PERF_REG_X86_DI] = regs.di;
-  registers[PERF_REG_X86_BP] = regs.bp;
-  registers[PERF_REG_X86_SP] = regs.sp;
-  registers[PERF_REG_X86_IP] = regs.ip;
-  registers[PERF_REG_X86_FLAGS] = regs.flags;
-  registers[PERF_REG_X86_CS] = regs.cs;
-  registers[PERF_REG_X86_SS] = regs.ss;
-  // Registers ds, es, fs, gs do not actually exist.
-  registers[PERF_REG_X86_DS] = 0ul;
-  registers[PERF_REG_X86_ES] = 0ul;
-  registers[PERF_REG_X86_FS] = 0ul;
-  registers[PERF_REG_X86_GS] = 0ul;
-  registers[PERF_REG_X86_R8] = regs.r8;
-  registers[PERF_REG_X86_R9] = regs.r9;
-  registers[PERF_REG_X86_R10] = regs.r10;
-  registers[PERF_REG_X86_R11] = regs.r11;
-  registers[PERF_REG_X86_R12] = regs.r12;
-  registers[PERF_REG_X86_R13] = regs.r13;
-  registers[PERF_REG_X86_R14] = regs.r14;
-  registers[PERF_REG_X86_R15] = regs.r15;
-  return registers;
-}
-}  // namespace
-
-class StackSamplePerfEvent : public SamplePerfEvent<perf_event_stack_sample> {
- public:
-  void Accept(PerfEventVisitor* visitor) override;
 
   std::array<uint64_t, PERF_REG_X86_64_MAX> GetRegisters() const {
-    return perf_sample_regs_user_all_to_register_array(ring_buffer_record.regs);
+    return perf_event_sample_regs_user_all_to_register_array(
+        ring_buffer_record.regs);
   }
 
   const char* GetStackData() const { return ring_buffer_record.stack.data; }
   uint64_t GetStackSize() const { return ring_buffer_record.stack.dyn_size; }
+
+ private:
+  static std::array<uint64_t, PERF_REG_X86_64_MAX>
+  perf_event_sample_regs_user_all_to_register_array(
+      const perf_event_sample_regs_user_all& regs) {
+    std::array<uint64_t, PERF_REG_X86_64_MAX> registers{};
+    registers[PERF_REG_X86_AX] = regs.ax;
+    registers[PERF_REG_X86_BX] = regs.bx;
+    registers[PERF_REG_X86_CX] = regs.cx;
+    registers[PERF_REG_X86_DX] = regs.dx;
+    registers[PERF_REG_X86_SI] = regs.si;
+    registers[PERF_REG_X86_DI] = regs.di;
+    registers[PERF_REG_X86_BP] = regs.bp;
+    registers[PERF_REG_X86_SP] = regs.sp;
+    registers[PERF_REG_X86_IP] = regs.ip;
+    registers[PERF_REG_X86_FLAGS] = regs.flags;
+    registers[PERF_REG_X86_CS] = regs.cs;
+    registers[PERF_REG_X86_SS] = regs.ss;
+    // Registers ds, es, fs, gs do not actually exist.
+    registers[PERF_REG_X86_DS] = 0ul;
+    registers[PERF_REG_X86_ES] = 0ul;
+    registers[PERF_REG_X86_FS] = 0ul;
+    registers[PERF_REG_X86_GS] = 0ul;
+    registers[PERF_REG_X86_R8] = regs.r8;
+    registers[PERF_REG_X86_R9] = regs.r9;
+    registers[PERF_REG_X86_R10] = regs.r10;
+    registers[PERF_REG_X86_R11] = regs.r11;
+    registers[PERF_REG_X86_R12] = regs.r12;
+    registers[PERF_REG_X86_R13] = regs.r13;
+    registers[PERF_REG_X86_R14] = regs.r14;
+    registers[PERF_REG_X86_R15] = regs.r15;
+    return registers;
+  }
 };
 
-template <typename perf_record_t>
-class AbstractUprobesPerfEvent : public SamplePerfEvent<perf_record_t> {
+class StackSamplePerfEvent : public SamplePerfEvent {
+ public:
+  void Accept(PerfEventVisitor* visitor) override;
+};
+
+class AbstractUprobesPerfEvent {
  public:
   const Function* GetFunction() const { return function_; }
   void SetFunction(const Function* function) { function_ = function; }
@@ -201,23 +199,25 @@ class AbstractUprobesPerfEvent : public SamplePerfEvent<perf_record_t> {
   const Function* function_ = nullptr;
 };
 
-class UprobesWithStackPerfEvent
-    : public AbstractUprobesPerfEvent<perf_event_stack_sample> {
+class UprobesWithStackPerfEvent : public SamplePerfEvent,
+                                  public AbstractUprobesPerfEvent {
  public:
   void Accept(PerfEventVisitor* visitor) override;
-
-  std::array<uint64_t, PERF_REG_X86_64_MAX> GetRegisters() const {
-    return perf_sample_regs_user_all_to_register_array(ring_buffer_record.regs);
-  }
-
-  const char* GetStackData() const { return ring_buffer_record.stack.data; }
-  uint64_t GetStackSize() const { return ring_buffer_record.stack.dyn_size; }
 };
 
-class UretprobesPerfEvent
-    : public AbstractUprobesPerfEvent<perf_event_empty_sample> {
+class UretprobesPerfEvent : public PerfEvent, public AbstractUprobesPerfEvent {
  public:
+  perf_event_empty_sample ring_buffer_record;
+
+  uint64_t GetTimestamp() const override {
+    return ring_buffer_record.sample_id.time;
+  }
+
   void Accept(PerfEventVisitor* visitor) override;
+
+  pid_t GetPid() const { return ring_buffer_record.sample_id.pid; }
+  pid_t GetTid() const { return ring_buffer_record.sample_id.tid; }
+  uint32_t GetCpu() const { return ring_buffer_record.sample_id.cpu; }
 };
 
 // This carries a snapshot of /proc/<pid>/maps and does not reflect a
