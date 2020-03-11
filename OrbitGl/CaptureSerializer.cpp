92ceb3bbe0e22629b8606d7e141cfdd97f4045a6
Daniel Fenner <dfenner@google.com>
2020-03-11 09:22:40 +0100

    Fix broken build.

--- OrbitGl/CaptureSerializer.cpp
+++ OrbitGl/CaptureSerializer.cpp
@@ -152,7 +152,7 @@ void CaptureSerializer::Load(const std::wstring a_FileName) {
     // Sampling profiler
     archive(Capture::GSamplingProfiler);
     Capture::GSamplingProfiler->SortByThreadUsage();
-    GOrbitApp->AddSamplingReport(Capture::GSamplingProfiler);
+    GOrbitApp->AddSamplingReport(Capture::GSamplingProfiler, GOrbitApp);
     Capture::GSamplingProfiler->SetLoadedFromFile(true);
 
     // Event buffer
