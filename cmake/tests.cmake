fd04dd4645f1253d1ff6fc1fcbbcd18e5d5b4e72
Henning Becker <henning.becker@gmail.com>
2020-03-18 11:46:51 +0100

    Suffixed test-xml-files for Sponge.

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -3,8 +3,9 @@ function(register_test)
   string(REGEX REPLACE "Tests$" "" TEST_NAME "${TEST_TARGET}")
 
   set(TESTRESULTS_DIRECTORY "${CMAKE_BINARY_DIR}/testresults")
-  add_test(NAME "${TEST_NAME}"
-          COMMAND "$<TARGET_FILE:${TEST_TARGET}>"
-          "--gtest_output=xml:${TESTRESULTS_DIRECTORY}/${TEST_NAME}.xml")
+  add_test(
+    NAME "${TEST_NAME}"
+    COMMAND
+      "$<TARGET_FILE:${TEST_TARGET}>"
+      "--gtest_output=xml:${TESTRESULTS_DIRECTORY}/${TEST_NAME}_sponge_log.xml")
 endfunction()
-
