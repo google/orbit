8050a91c1b1ebbec1869111d1a2df561cf1467b8
Henning Becker <henning.becker@gmail.com>
2020-03-19 10:21:30 +0100

    Added packaging and GCS bucket upload to kokoro config.

--- kokoro/gcp_windows/kokoro_build.bat
+++ kokoro/gcp_windows/kokoro_build.bat
@@ -1,7 +1,13 @@
 :: Code under repo is checked out to %KOKORO_ARTIFACTS_DIR%\github.
 :: The final directory name in this path is determined by the scm name specified
 :: in the job configuration
-cd %KOKORO_ARTIFACTS_DIR%\github\orbitprofiler
+SET REPO_ROOT=%~dp0
 
-call bootstrap-ci-orbit.bat
-exit %ERRORLEVEL%
+conan config install %REPO_ROOT%\contrib\conan\config
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cd %KOKORO_ARTIFACTS_DIR%\github\orbitprofiler
+call build.bat msvc2019_release_x64
+IF ERRORLEVEL 1 exit %ERRORLEVEL%
+
+call conan package -bf %REPO_ROOT%\build_msvc2019_release_x64 %REPO_ROOT%
