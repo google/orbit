f7d8abe05fed30c2a55319b56d1e41310918f77f
Henning Becker <henning.becker@gmail.com>
2020-03-20 09:55:18 +0100

    Removed OrbitNoGl hack.

--- OrbitGl/App.cpp
+++ OrbitGl/App.cpp
@@ -20,21 +20,15 @@
 #include "Callstack.h"
 #include "Capture.h"
 #include "CaptureSerializer.h"
-#ifndef NOGL
 #include "CaptureWindow.h"
-#endif
 #include "ConnectionManager.h"
 #include "Debugger.h"
 #include "DiaManager.h"
 #include "EventTracer.h"
 #include "FunctionDataView.h"
-#ifndef NOGL
 #include "GlCanvas.h"
-#endif
 #include "GlobalDataView.h"
-#ifndef NOGL
 #include "ImGuiOrbit.h"
-#endif
 #include "Injection.h"
 #include "Introspection.h"
 #include "KeyAndString.h"
@@ -52,9 +46,7 @@
 #include "PluginManager.h"
 #include "PrintVar.h"
 #include "ProcessDataView.h"
-#ifndef NOGL
 #include "RuleEditor.h"
-#endif
 #include "SamplingProfiler.h"
 #include "SamplingReport.h"
 #include "ScopeTimer.h"
@@ -66,19 +58,15 @@
 #include "TcpClient.h"
 #include "TcpServer.h"
 #include "TestRemoteMessages.h"
-#ifndef NOGL
 #include "TextRenderer.h"
-#endif
 #include "TimerManager.h"
 #include "TypeDataView.h"
 #include "Utils.h"
 #include "Version.h"
 #include "curl/curl.h"
 
-#ifndef NOGL
 #define GLUT_DISABLE_ATEXIT_HACK
 #include "GL/freeglut.h"
-#endif
 
 #ifdef _WIN32
 #include "Disassembler.h"
@@ -189,9 +177,7 @@ void GLoadPdbAsync(const std::vector<std::string>& a_Modules) {
 //-----------------------------------------------------------------------------
 void OrbitApp::ProcessTimer(const Timer& a_Timer, const std::string&) {
   CHECK(!ConnectionManager::Get().IsService());
-#ifndef NOGL
   GCurrentTimeGraph->ProcessTimer(a_Timer);
-#endif
   ++Capture::GFunctionCountMap[a_Timer.m_FunctionAddress];
 }
 
@@ -273,9 +259,7 @@ void OrbitApp::AddKeyAndString(uint64_t key, std::string_view str) {
 void OrbitApp::LoadSystrace(const std::string& a_FileName) {
   SystraceManager::Get().Clear();
   Capture::ClearCaptureData();
-#ifndef NOGL
   GCurrentTimeGraph->Clear();
-#endif
   if (Capture::GClearCaptureDataFunc) {
     Capture::GClearCaptureDataFunc();
   }
@@ -289,9 +273,7 @@ void OrbitApp::LoadSystrace(const std::string& a_FileName) {
   Capture::GVisibleFunctionsMap = Capture::GSelectedFunctionsMap;
 
   for (const auto& timer : systrace->GetTimers()) {
-#ifndef NOGL
     GCurrentTimeGraph->ProcessTimer(timer);
-#endif
     ++Capture::GFunctionCountMap[timer.m_FunctionAddress];
   }
 
@@ -317,9 +299,7 @@ void OrbitApp::AppendSystrace(const std::string& a_FileName,
   Capture::GVisibleFunctionsMap = Capture::GSelectedFunctionsMap;
 
   for (const auto& timer : systrace->GetTimers()) {
-#ifndef NOGL
     GCurrentTimeGraph->ProcessTimer(timer);
-#endif
     Capture::GFunctionCountMap[timer.m_FunctionAddress];
   }
 
@@ -370,9 +350,7 @@ bool OrbitApp::Init() {
 //-----------------------------------------------------------------------------
 void OrbitApp::PostInit() {
   string_manager_ = std::make_shared<StringManager>();
-#ifndef NOGL
   GCurrentTimeGraph->SetStringManager(string_manager_);
-#endif
 
   if (HasTcpServer()) {
     GTcpServer->AddCallback(Msg_MiniDump, [=](const Message& a_Msg) {
@@ -397,12 +375,10 @@ void OrbitApp::PostInit() {
   }
 
   if (!GOrbitApp->GetHeadless()) {
-#ifndef NOGL
     int my_argc = 0;
     glutInit(&my_argc, NULL);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     GetDesktopResolution(GOrbitApp->m_ScreenRes[0], GOrbitApp->m_ScreenRes[1]);
-#endif
   } else {
     ConnectionManager::Get().InitAsService();
   }
@@ -582,11 +558,7 @@ void OrbitApp::Disassemble(const std::string& a_FunctionName,
 //-----------------------------------------------------------------------------
 const std::unordered_map<DWORD64, std::shared_ptr<class Rule> >*
 OrbitApp::GetRules() {
-#ifndef NOGL
   return &m_RuleEditor->GetRules();
-#else
-  return nullptr;
-#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -606,9 +578,7 @@ int OrbitApp::OnExit() {
   }
   delete GTcpServer;
   delete GOrbitApp;
-#ifndef NOGL
   Orbit_ImGui_Shutdown();
-#endif
   return 0;
 }
 
@@ -650,11 +620,9 @@ void OrbitApp::MainTick() {
   ++GOrbitApp->m_NumTicks;
 
   if (DoZoom) {
-#ifndef NOGL
     GCurrentTimeGraph->UpdateThreadIds();
     GOrbitApp->m_CaptureWindow->ZoomAll();
     GOrbitApp->NeedsRedraw();
-#endif
     DoZoom = false;
   }
 }
@@ -737,17 +705,13 @@ void OrbitApp::RegisterOutputLog(LogDataView* a_Log) {
 
 //-----------------------------------------------------------------------------
 void OrbitApp::RegisterRuleEditor(RuleEditor* a_RuleEditor) {
-#ifndef NOGL
   assert(m_RuleEditor == nullptr);
   m_RuleEditor = a_RuleEditor;
-#endif
 }
 
 //-----------------------------------------------------------------------------
 void OrbitApp::NeedsRedraw() {
-#ifndef NOGL
   m_CaptureWindow->NeedsUpdate();
-#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -774,10 +738,8 @@ void OrbitApp::AddSelectionReport(
 
 //-----------------------------------------------------------------------------
 void OrbitApp::GoToCode(DWORD64 a_Address) {
-#ifndef NOGL
   m_CaptureWindow->FindCode(a_Address);
   SendToUiNow(L"gotocode");
-#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -905,16 +867,13 @@ void OrbitApp::OnLoadSession(const std::string& file_name) {
 
 //-----------------------------------------------------------------------------
 void OrbitApp::OnSaveCapture(const std::string& file_name) {
-#ifndef NOGL
   CaptureSerializer ar;
   ar.m_TimeGraph = GCurrentTimeGraph;
   ar.Save(s2ws(file_name));
-#endif
 }
 
 //-----------------------------------------------------------------------------
 void OrbitApp::OnLoadCapture(const std::string& file_name) {
-#ifndef NOGL
   StopCapture();
   Capture::ClearCaptureData();
   GCurrentTimeGraph->Clear();
@@ -928,7 +887,6 @@ void OrbitApp::OnLoadCapture(const std::string& file_name) {
   m_ModulesDataView->SetProcess(Capture::GTargetProcess);
   StopCapture();
   DoZoom = true;  // TODO: remove global, review logic
-#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -1231,8 +1189,6 @@ void OrbitApp::OnRemoteModuleDebugInfo(const Message& a_Message) {
 
 //-----------------------------------------------------------------------------
 void OrbitApp::LaunchRuleEditor(Function* a_Function) {
-#ifndef NOGL
   m_RuleEditor->m_Window.Launch(a_Function);
   SendToUiNow(TEXT("RuleEditor"));
-#endif
 }
