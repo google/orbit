c7f630e9665a1767cbd944b897f2652b84061e72
Henning Becker <henning.becker@gmail.com>
2020-03-21 11:03:02 +0100

    Replaced EXIT /B with GOTO :EOF in Windows Batch scripts.

--- bootstrap-orbit.bat
+++ bootstrap-orbit.bat
@@ -6,17 +6,17 @@ where /q conan
 if ERRORLEVEL 1 (
     echo "Conan not found. Trying to install it via python-pip..."
 
-	where /q pip3
+    where /q pip3
     if ERRORLEVEL 1 (
         echo "It seems you don't have Python3 installed (pip3.exe not found)."
         echo "Please install Python or make it available in the path."
         echo "Alternatively you could also just install conan manually and make"
         echo "it available in the path."
-        exit /B 1
+	GOTO :EOF
     )
 
     pip3 install conan
-    if ERRORLEVEL 1 exit /b 1
+    if ERRORLEVEL 1 GOTO :EOF
 
     where /q conan
     if ERRORLEVEL 1 (
@@ -24,13 +24,13 @@ if ERRORLEVEL 1 (
         echo "in the path. Please ensure that your Python user-executable folder is"
         echo "in the path and call this script again."
         echo "You can call 'pip3 show -f conan' to figure out where conan.exe was placed."
-        exit /B 2
+        GOTO :EOF
     )
 ) else (
     echo "Conan found. Installation skipped."
 )
 
 conan config install %REPO_ROOT%\contrib\conan\configs\windows
-if ERRORLEVEL 1 exit /b 1
+if ERRORLEVEL 1 GOTO :EOF
 
 call build.bat %*
