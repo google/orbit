5d33d025b1bd89efabdc35bc8b01b634cf8cc565
Henning Becker <henning.becker@gmail.com>
2020-03-21 11:03:02 +0100

    Replaced EXIT /B with GOTO :EOF in Windows Batch scripts.

--- bootstrap-orbit-ggp.bat
+++ bootstrap-orbit-ggp.bat
@@ -6,17 +6,17 @@ where /q conan
 if ERRORLEVEL 1 (
     echo "Conan not found. Trying to install it via python-pip..."
 
-	where /q pip3
+    where /q pip3
     if ERRORLEVEL 1 (
         echo "It seems you don't have Python3 installed (pip3.exe not found)."
         echo "Please install Python or make it available in the path."
         echo "Alternatively you could also just install conan manually and make"
         echo "it available in the path."
-        exit /B 1
+        GOTO :EOF
     )
 
     pip3 install conan
-    if ERRORLEVEL 1 exit /b 1
+    if ERRORLEVEL 1 GOTO :EOF
 
     where /q conan
     if ERRORLEVEL 1 (
@@ -24,25 +24,25 @@ if ERRORLEVEL 1 (
         echo "in the path. Please ensure that your Python user-executable folder is"
         echo "in the path and call this script again."
         echo "You can call 'pip3 show -f conan' to figure out where conan.exe was placed."
-        exit /B 2
+        GOTO :EOF
     )
 ) else (
     echo "Conan found. Installation skipped."
 )
 
 conan config install %REPO_ROOT%\contrib\conan\configs\windows
-if ERRORLEVEL 1 exit /b 1
+if ERRORLEVEL 1 GOTO :EOF
 
 REM Install Stadia GGP SDK
 conan search ggp_sdk | findstr "orbitdeps/stable" > NUL
 if ERRORLEVEL 1 (
   if not exist %REPO_ROOT%\contrib\conan\recipes\ggp_sdk\ (
     git clone sso://user/hebecker/conan-ggp_sdk %REPO_ROOT%\contrib\conan\recipes\ggp_sdk
-    if ERRORLEVEL 1 exit /b 1
+    if ERRORLEVEL 1 GOTO :EOF
   )
 
   conan export %REPO_ROOT%\contrib\conan\recipes\ggp_sdk orbitdeps/stable
-  if ERRORLEVEL 1 exit /b 1
+  if ERRORLEVEL 1 GOTO :EOF
 
 ) ELSE (
   echo "ggp_sdk seems to be installed already. Skipping installation step..."
