1d44dee12bf3d196eb17278dc7cb1edb7b42a53b
Henning Becker <henning.becker@gmail.com>
2020-03-24 08:29:25 +0100

    Enable debugging in gcp_ubuntu_ggp build.

--- kokoro/gcp_ubuntu_ggp/kokoro_build.sh
+++ kokoro/gcp_ubuntu_ggp/kokoro_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fail on any error.
-set -e
+# set -e
 
 # Display commands being run.
 # WARNING: please only enable 'set -x' if necessary for debugging, and be very
@@ -37,6 +37,6 @@ conan package -bf $DIR/build_$profile/ $DIR
 # can not be resolved by looking into sponge alone. Also comment out the
 # "set -e" at the top of this file (otherwise a failed build will exit this
 # script immediately).
-# external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-# echo "INSTANCE_EXTERNAL_IP=${external_ip}"
-# sleep 7200;
+external_ip=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+echo "INSTANCE_EXTERNAL_IP=${external_ip}"
+sleep 7200;
