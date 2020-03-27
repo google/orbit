0e5506afe5767b6e20debcb3902ef5908bce76b5
Ronald Wotzlaw <wotzlaw@google.com>
2020-03-27 09:21:54 +0100

    Clear string manager on remote after capture to make sure strings are sent again when restarting the ORBIT UI.

--- OrbitCore/StringManager.h
+++ OrbitCore/StringManager.h
@@ -14,6 +14,7 @@ class StringManager {
   void Add(uint64_t key, const std::string_view str);
   std::optional<std::string> Get(uint64_t key);
   bool Exists(uint64_t key);
+  void Clear();
 private:
   absl::flat_hash_map<uint64_t, std::string> key_to_string_;
   std::mutex mutex_;
