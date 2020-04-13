b82aa88fc4c4bae4c1bf8d4c156bf7102c661a5a
Pierric Gimmig <pierric@google.com>
2020-04-13 14:55:14 -0700

    Fix ContextSwitch member initialization order.

--- OrbitCore/ContextSwitch.cpp
+++ OrbitCore/ContextSwitch.cpp
@@ -12,12 +12,12 @@ static_assert(sizeof(ContextSwitch) == 20);
 
 //-----------------------------------------------------------------------------
 ContextSwitch::ContextSwitch(SwitchType a_Type)
-    : m_ProcessId(0),
+    : m_Time(0),
+      m_ProcessId(0),
       m_ThreadId(0),
-      m_Type(a_Type),
-      m_Time(0),
       m_ProcessorIndex(0xFF),
-      m_ProcessorNumber(0xFF) {}
+      m_ProcessorNumber(0xFF),
+      m_Type(a_Type) {}
 
 //-----------------------------------------------------------------------------
 ContextSwitch::~ContextSwitch() = default;
