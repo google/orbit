af380ccd6874bc6e005d9018170500786ca61958
Henning Becker <henning.becker@gmail.com>
2020-04-20 17:38:43 +0200

    Fixed paths in run{,_service}.sh

--- run_service.sh
+++ run_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-exec $DIR/build_default_release/package/bin/OrbitService
+exec $DIR/build_default_release/bin/OrbitService
