4e6ae8cc9811980caff0ffeee7203bd12120136d
antonrohr <antonrohr@google.com>
2020-04-22 11:19:23 +0200

    added conan file for outcome package

---
+++ contrib/conan/recipes/outcome/conanfile.py
@@ -0,0 +1,21 @@
+# from https://github.com/ned14/outcome/blob/develop/conan/conanfile.py
+from conans import ConanFile, tools
+
+class OutcomeConan(ConanFile):
+    name = "Outcome"
+    version = "3dae433e"
+    license = "Apache-2.0"
+    description = "Provides very lightweight outcome<T> and result<T>"
+    repo_url = "https://github.com/ned14/outcome"
+
+    def source(self):
+        file_url = "https://raw.githubusercontent.com/ned14/outcome/{}/".format(self.version)
+        tools.download(file_url + "single-header/outcome.hpp", filename="outcome.hpp")
+        tools.download(file_url + "Licence.txt", filename="LICENCE")
+
+    def package(self):
+        self.copy("outcome.hpp", dst="include")
+        self.copy("LICENCE", dst="licenses")
+
+    def package_id(self):
+        self.info.header_only()
