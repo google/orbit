2d45da9dbc1809bc828a92cfbae9c52bc02390f3
antonrohr <antonrohr@google.com>
2020-04-22 15:57:49 +0200

    fixed a problem with the outcome conanfile
    amended the outcome header file in a way that the
    namespace works automatically

--- contrib/conan/recipes/outcome/conanfile.py
+++ contrib/conan/recipes/outcome/conanfile.py
@@ -12,6 +12,13 @@ class OutcomeConan(ConanFile):
         file_url = "https://raw.githubusercontent.com/ned14/outcome/{}/".format(self.version)
         tools.download(file_url + "single-header/outcome.hpp", filename="outcome.hpp")
         tools.download(file_url + "Licence.txt", filename="LICENCE")
+        header_file = tools.load("outcome.hpp")
+        header_file += "\n"
+        header_file += "#ifndef OUTCOME_NAMESPACE_INCGUARD_\n"
+        header_file += "#define OUTCOME_NAMESPACE_INCGUARD_\n"
+        header_file += "namespace outcome = OUTCOME_V2_NAMESPACE;\n"
+        header_file += "#endif /* OUTCOME_NAMESPACE_INCGUARD_ */\n"
+        tools.save("outcome.hpp", header_file)
 
     def package(self):
         self.copy("outcome.hpp", dst="include")
