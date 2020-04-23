61fcb0c9fccf8f0e05711a11a2579e99384fd25f
dimitry <dimitry@google.com>
2020-04-23 17:18:10 +0200

    Move process list to grpc
    Use grpc to get process list and display it.
    
    Bug: http://b/154064901
    Test: build, run client and check that process list works as expected.

--- OrbitGl/App.h
+++ OrbitGl/App.h
@@ -23,6 +23,8 @@
 #include "LogDataView.h"
 #include "Message.h"
 #include "ModulesDataView.h"
+#include "OrbitBase/MainThreadExecutor.h"
+#include "ProcessListManager.h"
 #include "ProcessMemoryClient.h"
 #include "ProcessesDataView.h"
 #include "RuleEditor.h"
@@ -206,7 +208,6 @@ class OrbitApp final : public CoreApp, public DataViewFactory {
 
   void RequestThaw() { m_NeedsThawing = true; }
   void OnRemoteProcess(const Message& a_Message);
-  void OnRemoteProcessList(const Message& a_Message);
   void OnRemoteModuleDebugInfo(const Message& a_Message);
   void OnRemoteModuleDebugInfo(const std::vector<ModuleDebugInfo>&) override;
   void ApplySession(const Session& session) override;
@@ -286,6 +287,9 @@ class OrbitApp final : public CoreApp, public DataViewFactory {
 
   std::shared_ptr<StringManager> string_manager_;
   std::shared_ptr<grpc::Channel> grpc_channel_;
+
+  std::unique_ptr<MainThreadExecutor> main_thread_executor_;
+  std::unique_ptr<ProcessListManager> process_list_manager_;
 
   const SymbolHelper symbol_helper_;
 #if defined(_WIN32)
