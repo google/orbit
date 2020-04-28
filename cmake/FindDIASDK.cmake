4065b493b90b924d9c888d113878e3ccf38cfbaa
Henning Becker <henning.becker@gmail.com>
2020-04-28 10:52:14 +0200

    Added new copyright header to cmake files.

--- cmake/FindDIASDK.cmake
+++ cmake/FindDIASDK.cmake
@@ -1,3 +1,7 @@
+# Copyright (c) 2020 The Orbit Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 if(CMAKE_VS_PLATFORM_NAME STREQUAL "x64")
   set(DIA_PLATFORM_NAME "amd64")
 else()
