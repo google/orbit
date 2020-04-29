3f009d108dabcc0d2a6df0fe5ff8791d7eeb40ef
Davide Pallotti <dpallotti@google.com>
2020-04-29 15:42:14 +0200

    Simple line removals in OrbitService.cpp

--- OrbitService/OrbitService.cpp
+++ OrbitService/OrbitService.cpp
@@ -1,24 +1,18 @@
 #include "OrbitService.h"
 
-#include <iostream>
-
-#include "Capture.h"
 #include "ConnectionManager.h"
 #include "Core.h"
 #include "CoreApp.h"
 #include "TcpServer.h"
-#include "TimerManager.h"
 
 // TODO: we should probably make it configurable
 constexpr uint32_t kAsioServerPort = 44766;
 
 OrbitService::OrbitService() {
-  // TODO: these should be a private fields.
-  GTimerManager = std::make_unique<TimerManager>();
+  // TODO: These should be private fields.
   GTcpServer = std::make_unique<TcpServer>();
-  Capture::Init();
-
   GTcpServer->StartServer(kAsioServerPort);
+
   ConnectionManager::Get().InitAsService();
 
   core_app_ = std::make_unique<CoreApp>();
@@ -29,7 +23,6 @@ OrbitService::OrbitService() {
 void OrbitService::Run(std::atomic<bool>* exit_requested) {
   while (!(*exit_requested)) {
     GTcpServer->ProcessMainThreadCallbacks();
-    Capture::Update();
     Sleep(16);
   }
 
