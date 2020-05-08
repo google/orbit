8e6b7e542903d6a7d2b3fb9d4dcfafe163deaf38
antonrohr <antonrohr@google.com>
2020-05-08 09:48:04 +0200

    Added SshManager class
    The SshManager class abstracts all ssh capabilities Orbit
    needs. It needs to be constructed with the ssh credentials,
    tasks that need to be done before the main task (OrbitService)
    and ports that should be forwarded. Then the Tick() function
    need to be called periodically.

---
+++ OrbitSsh/include/OrbitSsh/SshManager.h
@@ -0,0 +1,74 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_SSH_SSH_HANDLER_H_
+#define ORBIT_SSH_SSH_HANDLER_H_
+
+#include <filesystem>
+#include <functional>
+#include <memory>
+#include <queue>
+#include <string>
+#include <vector>
+
+#include "OrbitSsh/ExecChannelManager.h"
+#include "OrbitSsh/ResultType.h"
+#include "OrbitSsh/SessionManager.h"
+#include "OrbitSsh/TunnelManager.h"
+
+namespace OrbitSsh {
+
+// The SshManager class abstracts all ssh capabilities Orbit uses. It needs to
+// be constructed with the ssh credentials, tasks that need to be done before
+// the main task (OrbitService) and ports that should be forwarded. Task here
+// refers to command line commands. Then the Tick() function needs to be called
+// periodically.
+class SshManager {
+  enum class State {
+    kNotInitialized,
+    kSessionRunning,
+    kPreTasksRunning,
+    kMainTaskStarting,
+    kMainAndTunnelsRunning,
+    kClosed,
+  };
+
+ public:
+  // Task refers to a shell command.
+  // * command is the shell command that will be executed.
+  // * output_callback will be called everytime the task produces stdout output
+  // * exit_callback will be called once with the exit code, when the task
+  // returns
+  struct Task {
+    std::string command;
+    std::function<void(std::string)> output_callback;
+    std::function<void(int)> exit_callback;
+  };
+  SshManager(std::string host, int port, std::string user,
+             std::filesystem::path known_hosts_path,
+             std::filesystem::path key_path, std::queue<Task> pre_tasks,
+             Task main_task, std::vector<int> tunnel_ports);
+  SshManager() = delete;
+  SshManager(const SshManager&) = delete;
+  SshManager& operator=(const SshManager&) = delete;
+  ~SshManager();
+  State Tick();
+  ResultType Close();
+
+ private:
+  void MainTaskExit(int exit_code);
+  void StartPortForwarding();
+  ResultType CloseTunnels();
+  State state_ = State::kNotInitialized;
+  SessionManager session_manager_;
+  std::optional<ExecChannelManager> exec_channel_;
+  std::vector<TunnelManager> tunnels_;
+  std::queue<Task> pre_tasks_;
+  Task main_task_;
+  std::vector<int> tunnel_ports_;
+};
+
+}  // namespace OrbitSsh
+
+#endif  // ORBIT_SSH_SSH_HANDLER_H_
