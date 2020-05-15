d0f34ae8efa6bad42050e7ad11edce118a87f5cb
Henning Becker <henning.becker@gmail.com>
2020-05-15 12:33:30 +0200

    Extend error class.

--- OrbitSsh/include/OrbitSsh/Error.h
+++ OrbitSsh/include/OrbitSsh/Error.h
@@ -11,21 +11,65 @@
 
 namespace OrbitSsh {
 
-enum class SftpError {
+enum class Error {
+  kBannerRecv = LIBSSH2_ERROR_BANNER_RECV,
+  kBannerSend = LIBSSH2_ERROR_BANNER_SEND,
+  kInvalidMac = LIBSSH2_ERROR_INVALID_MAC,
+  kKeyFailure = LIBSSH2_ERROR_KEX_FAILURE,
   kAlloc = LIBSSH2_ERROR_ALLOC,
   kSocketSend = LIBSSH2_ERROR_SOCKET_SEND,
+  kExchangeFailure = LIBSSH2_ERROR_KEY_EXCHANGE_FAILURE,
+  kTimeout = LIBSSH2_ERROR_TIMEOUT,
+  kHostkeyInit = LIBSSH2_ERROR_HOSTKEY_INIT,
+  kHostkeySign = LIBSSH2_ERROR_HOSTKEY_SIGN,
+  kDecrypt = LIBSSH2_ERROR_DECRYPT,
+  kSocketDisconnect = LIBSSH2_ERROR_SOCKET_DISCONNECT,
+  kProto = LIBSSH2_ERROR_PROTO,
+  kPasswordExpired = LIBSSH2_ERROR_PASSWORD_EXPIRED,
+  kFile = LIBSSH2_ERROR_FILE,
+  kMethodNone = LIBSSH2_ERROR_METHOD_NONE,
+  kAuthenticationFailed = LIBSSH2_ERROR_AUTHENTICATION_FAILED,
+  kPublicKeyUnrecognized = LIBSSH2_ERROR_PUBLICKEY_UNRECOGNIZED,
+  kPublickeyUnverified = LIBSSH2_ERROR_PUBLICKEY_UNVERIFIED,
+  kChannelOutOfOrder = LIBSSH2_ERROR_CHANNEL_OUTOFORDER,
+  kChannelFailure = LIBSSH2_ERROR_CHANNEL_FAILURE,
+  kChannelRequestDenied = LIBSSH2_ERROR_CHANNEL_REQUEST_DENIED,
+  kChannelUnknown = LIBSSH2_ERROR_CHANNEL_UNKNOWN,
+  kChannelWindowExceeded = LIBSSH2_ERROR_CHANNEL_WINDOW_EXCEEDED,
+  kChannelPacketExceeded = LIBSSH2_ERROR_CHANNEL_PACKET_EXCEEDED,
+  kChannelClosed = LIBSSH2_ERROR_CHANNEL_CLOSED,
+  kChannelEofSent = LIBSSH2_ERROR_CHANNEL_EOF_SENT,
+  kScpProtocol = LIBSSH2_ERROR_SCP_PROTOCOL,
+  kZlib = LIBSSH2_ERROR_ZLIB,
   kSocketTimeout = LIBSSH2_ERROR_SOCKET_TIMEOUT,
   kSftpProtocol = LIBSSH2_ERROR_SFTP_PROTOCOL,
-  kEagain = LIBSSH2_ERROR_EAGAIN
+  kRequestDenied = LIBSSH2_ERROR_REQUEST_DENIED,
+  kMethodNotSupported = LIBSSH2_ERROR_METHOD_NOT_SUPPORTED,
+  kInvalid = LIBSSH2_ERROR_INVAL,
+  kInvalidPollType = LIBSSH2_ERROR_INVALID_POLL_TYPE,
+  kPublicKeyProtocol = LIBSSH2_ERROR_PUBLICKEY_PROTOCOL,
+  kEagain = LIBSSH2_ERROR_EAGAIN,
+  kBufferToSmall = LIBSSH2_ERROR_BUFFER_TOO_SMALL,
+  kBadUse = LIBSSH2_ERROR_BAD_USE,
+  kCompress = LIBSSH2_ERROR_COMPRESS,
+  kOutOfBoundary = LIBSSH2_ERROR_OUT_OF_BOUNDARY,
+  kAgentProtocol = LIBSSH2_ERROR_AGENT_PROTOCOL,
+  kSocketRecv = LIBSSH2_ERROR_SOCKET_RECV,
+  kEncrypt = LIBSSH2_ERROR_ENCRYPT,
+  kBadSocket = LIBSSH2_ERROR_BAD_SOCKET,
+  kKnownHosts = LIBSSH2_ERROR_KNOWN_HOSTS,
+  kChannelWindowFull = LIBSSH2_ERROR_CHANNEL_WINDOW_FULL,
+  kKeyfileAuthFailed = LIBSSH2_ERROR_KEYFILE_AUTH_FAILED,
+  kSocketNone = LIBSSH2_ERROR_SOCKET_NONE
 };
 
-struct SftpErrorCategory : std::error_category {
+struct ErrorCategory : std::error_category {
   using std::error_category::error_category;
 
   const char* name() const noexcept override { return "libssh2_sftp"; }
   std::string message(int condition) const override;
   std::error_condition default_error_condition(int c) const noexcept override {
-    if (static_cast<SftpError>(c) == SftpError::kEagain) {
+    if (static_cast<Error>(c) == Error::kEagain) {
       return std::make_error_condition(
           std::errc::resource_unavailable_try_again);
     } else {
@@ -34,19 +78,19 @@ struct SftpErrorCategory : std::error_category {
   }
 };
 
-inline const SftpErrorCategory& GetSftpErrorCategory() {
-  static SftpErrorCategory category{};
+inline const ErrorCategory& GetErrorCategory() {
+  static ErrorCategory category{};
   return category;
 }
 
-inline std::error_code make_error_code(SftpError e) {
-  return std::error_code{static_cast<int>(e), GetSftpErrorCategory()};
+inline std::error_code make_error_code(Error e) {
+  return std::error_code{static_cast<int>(e), GetErrorCategory()};
 }
 
 }  // namespace OrbitSsh
 
 namespace std {
 template <>
-struct is_error_condition_enum<OrbitSsh::SftpError> : std::true_type {};
+struct is_error_condition_enum<OrbitSsh::Error> : std::true_type {};
 }  // namespace std
 #endif  // ORBIT_SSH_ERROR_H_
