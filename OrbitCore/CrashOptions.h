2533f78079b8588c79472b1a5130cc7e39ea2e60
IrinaShkviro <irinashkviro@google.com>
2020-05-15 16:36:06 +0200

    move crash2 collection server url to the option

---
+++ OrbitCore/CrashOptions.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_CORE_CRASH_OPTIONS_H_
+#define ORBIT_CORE_CRASH_OPTIONS_H_
+#include <string>
+
+namespace CrashServerOptions {
+std::string GetUrl();
+}  // namespace CrashServer
+
+#endif  // ORBIT_CORE_CRASH_OPTIONS_H_
