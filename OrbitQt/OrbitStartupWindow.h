fdb3dd5c82effe00ff1433e310f4ab8ad9b346e4
antonrohr <antonrohr@google.com>
2020-05-18 09:56:52 +0200

    StartupWindow execution of ggp ssh info
    This results in Startupwindow executing ggp ssh info
    before it returns (triggered by either double clicking
    on an entry ot clicking ok after choosing an entry)

--- OrbitQt/OrbitStartupWindow.h
+++ OrbitQt/OrbitStartupWindow.h
@@ -13,17 +13,32 @@
 #include "OrbitGgp/GgpClient.h"
 #include "OrbitGgp/GgpInstance.h"
 #include "OrbitGgp/GgpInstanceItemModel.h"
+#include "OrbitGgp/GgpSshInfo.h"
 
 class OrbitStartupWindow : public QDialog {
  public:
   explicit OrbitStartupWindow(QWidget* parent = nullptr);
-  int Run(std::string* ip_address);
+
+  template <typename Credentials>
+  int Run(Credentials* credentials) {
+    int dialog_result = exec();
+
+    if (dialog_result != 0) {
+      credentials->host = ssh_info_->host.toStdString();
+      credentials->key_path = ssh_info_->key_path.toStdString();
+      credentials->known_hosts_path = ssh_info_->known_hosts_path.toStdString();
+      credentials->user = ssh_info_->user.toStdString();
+      credentials->port = ssh_info_->port;
+    }
+    return dialog_result;
+  }
 
  private:
   void ReloadInstances();
 
   std::optional<GgpClient> ggp_client_;
   std::optional<GgpInstance> chosen_instance_;
+  std::optional<GgpSshInfo> ssh_info_;
   QPointer<GgpInstanceItemModel> model_;
 };
 
