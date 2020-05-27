f28419bbf041e6cc0e9e19083bea269152f42478
Davide Pallotti <dpallotti@google.com>
2020-05-27 13:44:39 +0200

    Catch exception thrown by asio::write
    Bug: http://b/155464095

--- OrbitCore/TcpEntity.cpp
+++ OrbitCore/TcpEntity.cpp
@@ -103,7 +103,13 @@ void TcpEntity::SendData() {
       --m_NumQueuedEntries;
       TcpSocket* socket = GetSocket();
       if (socket && socket->m_Socket && socket->m_Socket->is_open()) {
-        asio::write(*socket->m_Socket, SharedConstBuffer(buffer));
+        try {
+          asio::write(*socket->m_Socket, SharedConstBuffer(buffer));
+        } catch (std::exception& e) {
+          // We observed std::system_error being thrown by OrbitService when the
+          // client is stopped while being debugged. Refer to b/155464095.
+          ERROR("asio::write: %s", e.what());
+        }
       } else {
         ORBIT_ERROR;
       }
