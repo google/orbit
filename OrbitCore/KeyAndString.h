e6e60db8b3f20de8217289242075baf1c266e9a5
Ronald Wotzlaw <wotzlaw@google.com>
2020-05-29 14:45:21 +0200

    Use double slash style for copyright header in header files

--- OrbitCore/KeyAndString.h
+++ OrbitCore/KeyAndString.h
@@ -1,8 +1,6 @@
-/*
- * Copyright (c) 2020 The Orbit Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
- */
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef ORBIT_CORE_KEY_AND_STRING_H_
 #define ORBIT_CORE_KEY_AND_STRING_H_
