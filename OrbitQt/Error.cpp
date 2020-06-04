d53fb2485a3acd184f423782b9027aee95ad788f
antonrohr <antonrohr@google.com>
2020-06-04 16:54:06 +0200

    Added OrbitQt::Error class
    This uses std::error_code to work with outcome. It is currently only
    used in ServiceDeployManager

---
+++ OrbitQt/Error.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "Error.h"
+
+#include <absl/strings/str_format.h>
+
+namespace OrbitQt {
+
+std::string ErrorCategory::message(int condition) const {
+  switch (static_cast<Error>(condition)) {
+    case Error::kCouldNotConnectToServer:
+      return "Could not connect to remote server.";
+    case Error::kCouldNotUploadPackage:
+      return "Could not upload OrbitService package to remote. Please make "
+             "sure the .deb package is located in the `collector` folder";
+    case Error::kCouldNotUploadSignature:
+      return "Could not upload OrbitService signature to remote. Please make "
+             "sure the .deb.asc signature is located in the `collector` folder";
+    case Error::kCouldNotInstallPackage:
+      return "Could not install OrbitService on remote.";
+    case Error::kCouldNotStartTunnel:
+      return "Could not start tunnel to remote";
+  }
+
+  return absl::StrFormat("Unknown error condition: %i.", condition);
+}
+}  // namespace OrbitQt
