53bc617c319638bbd0e689a88768777843f86bc2
Florian Kübler <kuebler@google.com>
2020-06-04 15:02:12 +0000

    Tests that reading the buffer also clears it.

--- OrbitCore/LinuxTracingBuffer.h
+++ OrbitCore/LinuxTracingBuffer.h
@@ -5,6 +5,8 @@
 #ifndef ORBIT_CORE_LINUX_TRACING_BUFFER_H_
 #define ORBIT_CORE_LINUX_TRACING_BUFFER_H_
 
+#include <gtest/gtest_prod.h>
+
 #include "ContextSwitch.h"
 #include "EventBuffer.h"
 #include "KeyAndString.h"
@@ -71,6 +73,16 @@ class LinuxTracingBuffer {
 
   absl::Mutex thread_name_buffer_mutex_;
   std::vector<TidAndThreadName> thread_name_buffer_;
+
+  FRIEND_TEST(LinuxTracingBuffer, Empty);
+  FRIEND_TEST(LinuxTracingBuffer, ContextSwitches);
+  FRIEND_TEST(LinuxTracingBuffer, Timers);
+  FRIEND_TEST(LinuxTracingBuffer, Callstacks);
+  FRIEND_TEST(LinuxTracingBuffer, HashedCallstacks);
+  FRIEND_TEST(LinuxTracingBuffer, AddressInfos);
+  FRIEND_TEST(LinuxTracingBuffer, KeysAndStrings);
+  FRIEND_TEST(LinuxTracingBuffer, ThreadNames);
+  FRIEND_TEST(LinuxTracingBuffer, Reset);
 };
 
 #endif  // ORBIT_CORE_LINUX_TRACING_BUFFER_H_
