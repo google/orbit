2bf47b4ed7051a1fb2f351830864c40a73e0265c
Pierric Gimmig <pierric.gimmig@gmail.com>
2020-06-05 13:50:57 -0400

    Remove websocket related code from codebase.

--- OrbitCore/Tcp.cpp
+++ OrbitCore/Tcp.cpp
@@ -79,74 +79,13 @@ std::vector<std::string> TcpConnection::GetStats() {
   return std::vector<std::string>();
 }
 
-bool IsWebSocketHandshakeMessage(Message& message) {
-  char* str = reinterpret_cast<char*>(&message);
-  return str[0] == 'G' && str[1] == 'E' && str[2] == 'T';
-}
-
-void TcpConnection::handle_request_line(asio::error_code ec,
-                                        size_t bytes_transferred) {
-  if (!ec) {
-    num_bytes_received_ += bytes_transferred;
-
-    asio::streambuf::const_buffers_type bufs = stream_buf_.data();
-    std::string str(asio::buffers_begin(bufs),
-                    asio::buffers_begin(bufs) + bytes_transferred);
-
-    if (absl::StrContains(str, "Sec-WebSocket-Key:")) {
-      std::vector<std::string> tokens = Tokenize(str, " ");
-      web_socket_key_ = tokens[1];
-      ReplaceStringInPlace(web_socket_key_, "\r\n", "");
-    }
-
-    stream_buf_.consume(bytes_transferred);
-
-    if (bytes_transferred == 2) {
-      SendWebsocketResponse();
-    } else {
-      asio::async_read_until(
-          socket_, stream_buf_, "\r\n",
-          std::bind(&TcpConnection::handle_request_line, this,
-                    std::placeholders::_1, std::placeholders::_2));
-    }
-  } else {
-    PRINT_VAR(ec.message());
-  }
-}
-
 void handle_writes(const asio::error_code& error, size_t bytes_transferred) {
   std::string errorStr = error.message();
   PRINT_VAR(error.message().c_str());
   PRINT_VAR(bytes_transferred);
 }
 
-void TcpConnection::SendWebsocketResponse() {
-  const char* p =
-      "HTTP/1.1 101 Switching Protocols\r\nConnection: "
-      "upgrade\r\nSec-WebSocket-Accept: ";
-  const char* s = "\r\nUpgrade: websocket\r\n\r\n";
-
-  std::string response = p + web_socket_key_ + s;
-
-  asio::async_write(socket_, asio::buffer(response.data(), response.size()),
-                    handle_writes);
-}
-
-void TcpConnection::ReadWebsocketHandshake() {
-  asio::async_read_until(
-      socket_, stream_buf_, "\r\n",
-      std::bind(&TcpConnection::handle_request_line, this,
-                std::placeholders::_1, std::placeholders::_2));
-}
-
 void TcpConnection::ReadPayload() {
-  if (IsWebSocketHandshakeMessage(message_)) {
-    ReadWebsocketHandshake();
-    Message msg(Msg_WebSocketHandshake);
-    DecodeMessage(msg);
-    return;
-  }
-
   if (message_.m_Size == 0) {
     message_.m_Data = nullptr;
     ReadFooter();
