0db6aa9f46a6d834382f4e4be083f5d8d7b59994
ronaldfw <wotzlaw@gmail.com>
2020-06-08 20:49:14 +0200

    Use precise computation in time ticks for the start of the area to ignore

--- OrbitGl/ThreadTrack.cpp
+++ OrbitGl/ThreadTrack.cpp
@@ -175,7 +175,7 @@ void ThreadTrack::UpdatePrimitives(uint64_t min_tick, uint64_t max_tick) {
   uint64_t min_ignore = std::numeric_limits<uint64_t>::max();
   uint64_t max_ignore = std::numeric_limits<uint64_t>::min();
 
-  uint64_t pixel_delta = 
+  uint64_t pixel_delta_in_ticks =
     static_cast<uint64_t>(TicksFromMicroseconds(time_graph_->GetTimeWindowUs())) / canvas->getWidth();
 
   for (auto& text_boxes : chains_by_depth) {
@@ -225,9 +225,10 @@ void ThreadTrack::UpdatePrimitives(uint64_t min_tick, uint64_t max_tick) {
         auto type = PickingID::LINE;
         batcher->AddVerticalLine(pos, size[1], z, color, type, &text_box);
         // For lines, we can ignore the entire pixel into which this event falls.
-        min_ignore = time_graph_->GetTickFromWorld(std::floor(pos[0]));
-        max_ignore = min_ignore + pixel_delta;
-
+        uint64_t min_timegraph_tick = time_graph_->GetTickFromUs(time_graph_->GetMinTimeUs());
+        min_ignore = min_timegraph_tick +
+          ((timer.m_Start - min_timegraph_tick)/pixel_delta_in_ticks) * pixel_delta_in_ticks;
+        max_ignore = min_ignore + pixel_delta_in_ticks;
       }
     }
   }
