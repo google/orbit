3c6abf296bc0425955a8c003a6b4380a4e577412
IrinaShkviro <irinashkviro@google.com>
2020-06-09 11:31:35 +0200

    add mutex

--- OrbitBase/Logging.cpp
+++ OrbitBase/Logging.cpp
@@ -4,9 +4,19 @@
 
 #include <OrbitBase/Logging.h>
 
+static absl::Mutex log_file_mutex;
 std::ofstream log_file;
 
 void InitLogFile(const std::string& path) {
+  absl::MutexLock lock(&log_file_mutex);
   CHECK(!log_file.is_open());
   log_file.open(path, std::ofstream::out);
+}
+
+void LogToFile(const std::string& message) {
+  absl::MutexLock lock(&log_file_mutex);
+  if (log_file.is_open()) {
+    log_file << message;
+    log_file.flush();
+  }
 }
