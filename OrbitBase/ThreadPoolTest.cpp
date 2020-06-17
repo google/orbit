e72e894e5e9e9f518cba9bea7f6e46e6e55377c9
dimitry <dimitry@google.com>
2020-06-17 19:28:43 +0200

    Implement ThreadPool
    To be used to schedule events that need to be executed off main thread.
    
    Test: ./bin/OrbitBaseTests

---
+++ OrbitBase/ThreadPoolTest.cpp
@@ -0,0 +1,168 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gmock/gmock-matchers.h>
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <utility>
+
+#include "OrbitBase/ThreadPool.h"
+#include "absl/synchronization/mutex.h"
+#include "absl/time/clock.h"
+
+TEST(ThreadPool, Smoke) {
+  constexpr size_t kThreadPoolMinSize = 1;
+  constexpr size_t kThreadPoolMaxSize = 2;
+  std::unique_ptr<ThreadPool> thread_pool =
+      ThreadPool::Create(kThreadPoolMinSize, kThreadPoolMaxSize);
+
+  absl::Mutex mutex;
+  bool called = false;
+
+  {
+    absl::MutexLock lock(&mutex);
+    thread_pool->Schedule([&]() {
+      absl::MutexLock lock(&mutex);
+      called = true;
+    });
+
+    EXPECT_FALSE(called);
+
+    EXPECT_TRUE(mutex.AwaitWithTimeout(
+        absl::Condition(
+            +[](bool* called) { return *called; }, &called),
+        absl::Milliseconds(100)));
+
+    EXPECT_TRUE(called);
+    called = false;
+  }
+
+  thread_pool->ShutdownAndWait();
+
+  EXPECT_FALSE(called);
+}
+
+TEST(ThreadPool, QueuedActionsExecutedOnShutdown) {
+  constexpr size_t kThreadPoolMinSize = 1;
+  constexpr size_t kThreadPoolMaxSize = 2;
+  std::unique_ptr<ThreadPool> thread_pool =
+      ThreadPool::Create(kThreadPoolMinSize, kThreadPoolMaxSize);
+
+  absl::Mutex mutex;
+  size_t counter = 0;
+
+  constexpr size_t kNumberOfActions = 7;
+  {
+    absl::MutexLock lock(&mutex);
+    for (size_t i = 0; i < kNumberOfActions; ++i) {
+      thread_pool->Schedule([&]() {
+        absl::MutexLock lock(&mutex);
+        counter++;
+      });
+    }
+
+    // All actions are waiting on the mutex, they won't resume until we unlock
+    thread_pool->Shutdown();
+  }
+
+  thread_pool->Wait();
+
+  EXPECT_EQ(counter, kNumberOfActions);
+}
+
+TEST(ThreadPool, ExtendThreadPool) {
+  constexpr size_t kThreadPoolMinSize = 1;
+  constexpr size_t kThreadPoolMaxSize = 2;
+  std::unique_ptr<ThreadPool> thread_pool =
+      ThreadPool::Create(kThreadPoolMinSize, kThreadPoolMaxSize);
+
+  absl::Mutex actions_started_mutex;
+  size_t actions_started = 0;
+  absl::Mutex actions_executed_mutex;
+  size_t actions_executed = 0;
+
+  auto action = [&] {
+    actions_started_mutex.Lock();
+    actions_started++;
+    actions_started_mutex.Unlock();
+
+    absl::MutexLock lock(&actions_executed_mutex);
+    actions_executed++;
+  };
+
+  {
+    // Schedule 2 actions and check we have one worker thread
+    absl::MutexLock lock(&actions_executed_mutex);
+    thread_pool->Schedule(action);
+
+    actions_started_mutex.Lock();
+    EXPECT_TRUE(actions_started_mutex.AwaitWithTimeout(
+        absl::Condition(
+            +[](size_t* started) { return *started == 1; }, &actions_started),
+        absl::Milliseconds(100)))
+        << "actions_started=" << actions_started << ", expected 1";
+    actions_started_mutex.Unlock();
+
+    // Schedule another action and check that there are 2 workers threads now.
+    thread_pool->Schedule(action);
+
+    actions_started_mutex.Lock();
+    EXPECT_TRUE(actions_started_mutex.AwaitWithTimeout(
+        absl::Condition(
+            +[](size_t* started) { return *started == 2; }, &actions_started),
+        absl::Milliseconds(100)));
+    actions_started_mutex.Unlock();
+
+    // Add more and make sure worker thread number remains kThreadPoolMaxSize
+    for (size_t i = 0; i < 5; ++i) {
+      thread_pool->Schedule(action);
+    }
+
+    // I do not think there is a way around it - sleep for 100ms
+    absl::SleepFor(absl::Milliseconds(100));
+
+    actions_started_mutex.Lock();
+    EXPECT_EQ(actions_started, kThreadPoolMaxSize);
+    actions_started_mutex.Unlock();
+  }
+
+  thread_pool->ShutdownAndWait();
+
+  EXPECT_EQ(actions_started, 7);
+  EXPECT_EQ(actions_executed, 7);
+}
+
+TEST(ThreadPool, InvalidArguments) {
+  EXPECT_DEATH(ThreadPool::Create(0, 1), "");
+  EXPECT_DEATH(ThreadPool::Create(2, 1), "");
+  EXPECT_DEATH(ThreadPool::Create(0, 0), "");
+}
+
+TEST(ThreadPool, ScheduleAfterShutdown) {
+  EXPECT_DEATH(
+      {
+        constexpr size_t kThreadPoolMinSize = 1;
+        constexpr size_t kThreadPoolMaxSize = 2;
+        std::unique_ptr<ThreadPool> thread_pool =
+            ThreadPool::Create(kThreadPoolMinSize, kThreadPoolMaxSize);
+
+        thread_pool->Shutdown();
+        thread_pool->Schedule([] {});
+      },
+      "");
+}
+
+TEST(ThreadPool, WaitWithoutShutdown) {
+  EXPECT_DEATH(
+      {
+        constexpr size_t kThreadPoolMinSize = 1;
+        constexpr size_t kThreadPoolMaxSize = 2;
+        std::unique_ptr<ThreadPool> thread_pool =
+            ThreadPool::Create(kThreadPoolMinSize, kThreadPoolMaxSize);
+
+        thread_pool->Wait();
+      },
+      "");
+}
---
+++ OrbitBase/include/OrbitBase/ThreadPool.h
@@ -0,0 +1,69 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_BASE_THREAD_POOL_H_
+#define ORBIT_BASE_THREAD_POOL_H_
+
+#include <memory>
+
+#include "OrbitBase/Action.h"
+
+// This class implements a thread pool. ThreadPool allows to execute
+// actions in another thread without the need to manage creation and destruction
+// of that thread.
+//
+// Usage example:
+//
+// /* Schedule an action on the thread-pool */
+// thread_poool->Schedule(CreateAction([data]{
+//   DoSomethingWith(data);
+// }));
+//
+// /* Shutdown thread-pool, blocks until all currently executed actions are
+//    complete and stops all worker threads */
+//
+// thread_pool->Shutdown();
+// thread_pool->Wait();
+//
+class ThreadPool {
+ public:
+  ThreadPool() = default;
+  virtual ~ThreadPool() = default;
+
+  virtual void Schedule(std::unique_ptr<Action> action) = 0;
+
+  template <typename F>
+  void Schedule(F&& functor) {
+    Schedule(CreateAction(std::forward<F>(functor)));
+  }
+
+  // Initiates shutdown, any Schedule after this call will fail.
+  virtual void Shutdown() = 0;
+
+  // Wait until all tasks are complete. This should be called
+  // before the object is destroyed, usually after calling
+  // Shutdown().
+  virtual void Wait() = 0;
+
+  void ShutdownAndWait() {
+    Shutdown();
+    Wait();
+  }
+
+  // Create ThreadPool with specified minimum and maximum number of worker
+  // threads.
+  //
+  // Whenever an action is Scheduled the thread pool puts it in an internal
+  // queue. Worker threads pick actions from the queue and execute them.
+  // If at the time of scheduling new action there are no idle worker threads,
+  // the thread pool creates a new worker thread if current number of worker
+  // threads is less than maximum pool size.
+  //
+  // TODO: If queue is empty thread_pool reduces number of worker threads
+  // until they hit thread_pool_min_size
+  static std::unique_ptr<ThreadPool> Create(size_t thread_pool_min_size,
+                                            size_t thread_pool_max_size);
+};
+
+#endif  // ORBIT_BASE_THREAD_POOL_H_
