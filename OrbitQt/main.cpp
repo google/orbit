bd3020ffb534a597862ee3d366c6b233f00f126a
Davide Pallotti <dpallotti@google.com>
2020-06-19 10:39:47 +0200

    Revert "Add and use Qt style github.com/ColinDuquesnoy/QDarkStyleSheet"
    This reverts commit f968b298

--- OrbitQt/main.cpp
+++ OrbitQt/main.cpp
@@ -162,10 +162,26 @@ static outcome::result<void> RunUiInstance(
 }
 
 static void StyleOrbit(QApplication& app) {
-  QFile f(":qdarkstyle/style.qss");
-  f.open(QFile::ReadOnly | QFile::Text);
-  QTextStream ts(&f);
-  app.setStyleSheet(ts.readAll());
+  app.setStyle(QStyleFactory::create("Fusion"));
+
+  QPalette darkPalette;
+  darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::WindowText, Qt::white);
+  darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+  darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+  darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+  darkPalette.setColor(QPalette::Text, Qt::white);
+  darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::ButtonText, Qt::white);
+  darkPalette.setColor(QPalette::BrightText, Qt::red);
+  darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+  darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+  darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+  app.setPalette(darkPalette);
+  app.setStyleSheet(
+      "QToolTip { color: #ffffff; background-color: #2a82da; border: 1px "
+      "solid white; }");
 }
 
 static std::optional<OrbitQt::DeploymentConfiguration>
