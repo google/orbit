a827f4fe44221ba7ba6381514113e107d848d5b5
antonrohr <antonrohr@google.com>
2020-06-25 11:01:48 +0200

    Clean up, remove unused ModuleDebugInfo

--- OrbitCore/OrbitModule.cpp
+++ OrbitCore/OrbitModule.cpp
@@ -255,12 +255,3 @@ void Pdb::ApplyPresets(const Session& session) {
     }
   }
 }
-
-//-----------------------------------------------------------------------------
-ORBIT_SERIALIZE(ModuleDebugInfo, 3) {
-  ORBIT_NVP_VAL(0, m_Name);
-  ORBIT_NVP_VAL(0, m_Functions);
-  ORBIT_NVP_VAL(0, load_bias);
-  ORBIT_NVP_VAL(0, m_PdbName);
-  ORBIT_NVP_VAL(1, m_PID);
-}
