df24744e884e46032c2f3af489a53ee5d19ed565
Davide Pallotti <dpallotti@google.com>
2020-07-06 10:50:36 +0200

    Remove OrbitAsioServer, unused as all communication uses gRPC now

--- OrbitService/OrbitService.h
+++ OrbitService/OrbitService.h
@@ -15,11 +15,7 @@
 
 class OrbitService {
  public:
-  OrbitService(uint16_t grpc_port, uint16_t asio_port,
-               CaptureOptions capture_options)
-      : grpc_port_{grpc_port},
-        asio_port_{asio_port},
-        capture_options_{std::move(capture_options)} {}
+  explicit OrbitService(uint16_t grpc_port) : grpc_port_{grpc_port} {}
 
   void Run(std::atomic<bool>* exit_requested);
 
@@ -27,18 +23,16 @@ class OrbitService {
   bool IsSshWatchdogActive() { return last_stdin_message_ != std::nullopt; }
 
   uint16_t grpc_port_;
-  uint16_t asio_port_;
-  CaptureOptions capture_options_;
 
   std::optional<std::chrono::time_point<std::chrono::steady_clock>>
       last_stdin_message_ = std::nullopt;
-  const std::string_view start_passphrase_ = "start_watchdog";
+  const std::string_view kStartWatchdogPassphrase = "start_watchdog";
   // TODO(antonrohr) The main thread can currently be blocked by slow functions
   // like FunctionsDataView::DoSort and FunctionsDataView::DoFilter. The default
   // timeout of 10 seconds is not enough with the blocking behaviour. As soon as
   // the main thread does not block anymore, revert this from 25 seconds back to
   // 10 seconds.
-  const int timeout_in_seconds_ = 25;
+  const int kWatchdogTimeoutInSeconds = 25;
 };
 
 #endif  // ORBIT_SERVICE_ORBIT_SERVICE_H
