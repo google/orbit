c5fc9accc6ae59b7d0996689b18394c8fb5eef3d
dimitry <dimitry@google.com>
2020-07-08 11:17:08 +0200

    SafeStrerror returns const char*

--- OrbitBase/include/OrbitBase/SafeStrerror.h
+++ OrbitBase/include/OrbitBase/SafeStrerror.h
@@ -9,6 +9,6 @@
 // is not thread local and can be modified by a subsequent call to strerror or
 // strerror_l. This function instead uses a thread_local buffer with strerror_r
 // (or C11 strerror_s with MSVC).
-char* SafeStrerror(int errnum);
+const char* SafeStrerror(int errnum);
 
 #endif  // ORBIT_BASE_SAFE_STRERROR_H_
