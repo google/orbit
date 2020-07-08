1ea07c0747b3bdff743beb650aa96d81a975a6e9
Davide Pallotti <dpallotti@google.com>
2020-07-08 09:13:29 +0200

    Remove empty and unused Hashing.h

--- OrbitCore/Hashing.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2020 The Orbit Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#pragma once
-
-#include "xxhash.h"
