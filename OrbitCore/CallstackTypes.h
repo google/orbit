b2d25f225413c843a52d40ff3cfd345b310bcd35
Davide Pallotti <dpallotti@google.com>
2020-07-10 10:20:28 +0200

    Fix copyright of CallstackTypes.h

--- OrbitCore/CallstackTypes.h
+++ OrbitCore/CallstackTypes.h
@@ -1,10 +1,10 @@
-// Copyright Pierric Gimmig 2013-2017
-//-----------------------------------
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #pragma once
 
 #include <cstdint>
 
-//-----------------------------------------------------------------------------
-#define ORBIT_STACK_SIZE 192
 typedef uint64_t CallstackID;
 typedef int32_t ThreadID;
