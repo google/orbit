dbaa94f23bbc5f6259bb3f7b863586fede1fce63
Henning Becker <henning.becker@gmail.com>
2020-07-15 17:18:48 +0200

    Move to gtest 1.10.0
    This updated gtest to version 1.10.0 and make some smaller related
    changes:
    - gtest is now a build requirement in conan (pure semantic change)
    - We don't use the gtest-provided cmake config files, rather the
    integration provided by conan. This makes it more robust in cross
    compilation scenarios.
    - We provide our own gtest_main target which provides the same main
    function implementation as gtest. This change is needed since because the
    previous point does not allow us to choose whether we want to link
    against the gtest-provided main function on a per-test basis.

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -13,3 +13,30 @@ function(register_test)
       "$<TARGET_FILE:${TEST_TARGET}>"
       "--gtest_output=xml:${TESTRESULTS_DIRECTORY}/${TEST_NAME}_sponge_log.xml")
 endfunction()
+
+if(NOT TARGET GTest::GTest)
+        add_library(GTest::GTest INTERFACE IMPORTED)
+  target_link_libraries(GTest::GTest INTERFACE CONAN_PKG::gtest)
+endif()
+
+if(NOT TARGET GTest::Main)
+  set(FILE_PATH ${CMAKE_BINARY_DIR}/test_main.cpp)
+  if(NOT EXISTS ${FILE_PATH})
+    set(GTEST_MAIN_CPP "\
+#include <cstdio>\n\
+#include <gtest/gtest.h>\n\
+\n\
+int main(int argc, char* argv[]) {\n\
+  printf(\"Running main() from %s\\n\", __FILE__)\;\n\
+  ::testing::InitGoogleTest(&argc, argv)\;\n\
+  return RUN_ALL_TESTS()\;\n\
+}\n\
+")
+
+    file(WRITE ${FILE_PATH} ${GTEST_MAIN_CPP})
+  endif()
+
+  add_library(GTest_Main STATIC EXCLUDE_FROM_ALL ${FILE_PATH})
+  target_link_libraries(GTest_Main PUBLIC GTest::GTest)
+  add_library(GTest::Main ALIAS GTest_Main)
+endif()
