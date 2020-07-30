21bd0f7d3134d931ebbfbde50db9ea8a250ee5d0
Cristina Guerrero Romero <crisguerrero@google.com>
2020-07-30 18:46:22 +0100

    OrbitClientGgp basic structure
    Basic test in main that creates the channel (works) and tries to set
    CaptureClient (error because ClientListener hasnt been implemented yet).
    
    Main files and the structure of the project has been created and the
    logic related to the grpc client will be moved onto ClientGgp, which
    will implement CaptureListener as well.

---
+++ OrbitClientGgp/ClientGgp.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_CLIENT_GGP_CLIENT_GGP_H_
+#define ORBIT_CLIENT_GGP_CLIENT_GGP_H_
+
+#include "ClientGgpOptions.h"
+#include "OrbitCaptureClient/CaptureListener.h"
+
+class ClientGgp : public CaptureListener {};
+
+#endif // ORBIT_CLIENT_GGP_CLIENT_GGP_H_
