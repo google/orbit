ded8b6fb44b70210e462ff801f97af2302bbd5e4
Cristina Guerrero Romero <crisguerrero@google.com>
2020-07-31 12:56:26 +0100

    Move CaptureClient, CaptureListener and CaptureEventProcessor classes to OrbitCaptureClient project.
    Refactoring code related to CaptureClient so it is independent from
    OrbitGl. This is necessary for the creation of a new client able to handle
    capture requests independently from Gl

--- OrbitCaptureClient/CaptureClient.h
+++ OrbitCaptureClient/CaptureClient.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ORBIT_GL_CAPTURE_CLIENT_H_
-#define ORBIT_GL_CAPTURE_CLIENT_H_
+#ifndef ORBIT_CAPTURE_CLIENT_CAPTURE_CLIENT_H_
+#define ORBIT_CAPTURE_CLIENT_CAPTURE_CLIENT_H_
 
 #include <optional>
 
