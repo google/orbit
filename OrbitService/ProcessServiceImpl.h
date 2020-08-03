bc91312ddaf42036aab5f5a5eddadf4fc9c975f3
dimitry <dimitry@google.com>
2020-08-03 10:37:53 +0200

    Remove now unused GetSymbols grpc service call
    Since the symbol loading is now happening on the client side
    this call is no longer needed nor is it used.

--- OrbitService/ProcessServiceImpl.h
+++ OrbitService/ProcessServiceImpl.h
@@ -18,9 +18,6 @@ class ProcessServiceImpl final : public ProcessService::Service {
   grpc::Status GetProcessList(grpc::ServerContext* context,
                               const GetProcessListRequest* request,
                               GetProcessListResponse* response) override;
-  grpc::Status GetSymbols(grpc::ServerContext* context,
-                          const GetSymbolsRequest* request,
-                          GetSymbolsResponse* response) override;
 
   grpc::Status GetModuleList(grpc::ServerContext* context,
                              const GetModuleListRequest* request,
