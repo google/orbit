2b25b2cf4a6dc9a08b9576f19db18b713e09c8d3
Cristina Guerrero Romero <crisguerrero@google.com>
2020-08-05 15:58:38 +0100

    Instantiate CaptureClient in ClientGgp::InitClient
    Initialize capture client. CaptureListener is implemented because it is
    needed for CaptureClient but it does not do anything right now.

--- OrbitClientGgp/ClientGgp.cpp
+++ OrbitClientGgp/ClientGgp.cpp
@@ -32,7 +32,40 @@ bool ClientGgp::InitClient() {
   LOG("Created GRPC channel to %s", options_.grpc_server_address);
 
   // Instantiate CaptureClient
-  //capture_client_ = std::make_unique<CaptureClient>(grpc_channel_, nullptr);
+  capture_client_ = std::make_unique<CaptureClient>(grpc_channel_, this);
 
   return true;
+}
+
+// CaptureListener implementation
+void ClientGgp::OnTimer(const orbit_client_protos::TimerInfo& timer_info) {
+  (void)timer_info;
+  LOG("OnTimer called w/ ");
+}
+
+void ClientGgp::OnKeyAndString(uint64_t key, std::string str) {
+  (void)key;
+  (void)str;
+  LOG("OnKeyAndString called");
+}
+
+void ClientGgp::OnCallstack(CallStack callstack) {
+  (void)callstack;
+  LOG("OnCallstack called");
+}
+
+void ClientGgp::OnCallstackEvent(orbit_client_protos::CallstackEvent callstack_event) {
+  (void)callstack_event;
+  LOG("OnCallstackEvent called");
+}
+
+void ClientGgp::OnThreadName(int32_t thread_id, std::string thread_name) {
+  (void)thread_id;
+  (void)thread_name;
+  LOG("OnThreadName called");
+}
+
+void ClientGgp::OnAddressInfo(orbit_client_protos::LinuxAddressInfo address_info) {
+  (void)address_info;
+  LOG("OnAddressInfo called");
 }
