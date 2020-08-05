6baff7a4b2a39d9fcfe9f0fd77d92c6f9f73db4e
Cristina Guerrero Romero <crisguerrero@google.com>
2020-08-05 15:21:47 +0100

    Move client iniatilization to ClientGgp
    Grpc is handled in ClientGgp and main make calls to its methods to
    initialize the channel and clients

--- OrbitClientGgp/ClientGgp.cpp
+++ OrbitClientGgp/ClientGgp.cpp
@@ -3,3 +3,36 @@
 // found in the LICENSE file.
 
 #include "ClientGgp.h"
+
+ClientGgp::ClientGgp(ClientGgpOptions&& options) 
+    : options_(std::move(options)) {}
+
+bool ClientGgp::InitClient() {
+  if (options_.grpc_server_address.empty()){
+    ERROR("gRPC server address not provided");
+    return false;
+  }
+
+  // Create channel
+  grpc::ChannelArguments channel_arguments;
+  channel_arguments.SetMaxReceiveMessageSize(
+      std::numeric_limits<int32_t>::max());
+
+  grpc_channel_ = grpc::CreateCustomChannel(
+      options_.grpc_server_address, 
+      grpc::InsecureChannelCredentials(), 
+      channel_arguments);
+
+  if (!grpc_channel_) {
+    ERROR("Unable to create GRPC channel to %s",
+          options_.grpc_server_address);
+    return false;
+  }
+  
+  LOG("Created GRPC channel to %s", options_.grpc_server_address);
+
+  // Instantiate CaptureClient
+  //capture_client_ = std::make_unique<CaptureClient>(grpc_channel_, nullptr);
+
+  return true;
+}
