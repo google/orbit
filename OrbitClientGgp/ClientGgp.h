84994852713464b7b6848329d7cba459a02b5541
Cristina Guerrero Romero <crisguerrero@google.com>
2020-08-05 15:58:38 +0100

    Instantiate CaptureClient in ClientGgp::InitClient
    Initialize capture client. CaptureListener is implemented because it is
    needed for CaptureClient but it does not do anything right now.

--- OrbitClientGgp/ClientGgp.h
+++ OrbitClientGgp/ClientGgp.h
@@ -5,13 +5,21 @@
 #ifndef ORBIT_CLIENT_GGP_CLIENT_GGP_H_
 #define ORBIT_CLIENT_GGP_CLIENT_GGP_H_
 
+#include <string>
+
+#include "capture_data.pb.h"
 #include "grpcpp/grpcpp.h"
+
+#include "Callstack.h"
+#include "EventBuffer.h"
+#include "KeyAndString.h"
+#include "ScopeTimer.h"
 
 #include "ClientGgpOptions.h"
 #include "OrbitCaptureClient/CaptureClient.h"
 #include "OrbitCaptureClient/CaptureListener.h"
 
-class ClientGgp {
+class ClientGgp : public CaptureListener {
   public:
     ClientGgp(ClientGgpOptions&& options);
     bool InitClient();
@@ -19,12 +27,22 @@ class ClientGgp {
     void EndCapture();
     void SaveCapture();
 
+    // CaptureListener implementation
+    void OnTimer(const orbit_client_protos::TimerInfo& timer_info) override;
+    void OnKeyAndString(uint64_t key, std::string str) override;
+    void OnCallstack(CallStack callstack) override;
+    void OnCallstackEvent(
+      orbit_client_protos::CallstackEvent callstack_event) override;
+    void OnThreadName(int32_t thread_id, std::string thread_name) override;
+    void OnAddressInfo(
+      orbit_client_protos::LinuxAddressInfo address_info) override;
+
   private:
     ClientGgpOptions options_;
 
     std::shared_ptr<grpc::Channel> grpc_channel_;
-
-    //std::unique_ptr<CaptureClient> capture_client_;
+    
+    std::unique_ptr<CaptureClient> capture_client_;
 };
 
 #endif // ORBIT_CLIENT_GGP_CLIENT_GGP_H_
