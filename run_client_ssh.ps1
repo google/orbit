bf5e7ef80185a6ebc7f9ba1748d3b63920948780
Cristina Guerrero Romero <crisguerrero@google.com>
2020-08-06 14:15:06 +0100

    Created run_client_ssh.ps1
    Created script so it is easier and quicker to test the client. Deploys
    OrbitClientGgp if needed and runs it.

---
+++ run_client_ssh.ps1
@@ -0,0 +1,59 @@
+# Connects to the instance and starts OrbitClientGgp. To also deploy OrbitClientGgp,
+# invoke with a the argument -deploy and a path to the OrbitClientGgp executable
+# e.g. run_client_ssh.ps1 -deploy build/bin/OrbitClientGgp
+# All other arguments will be passed to OrbitClientGgp.
+
+param (
+    [Parameter(Mandatory=$false)][string]$deploy,
+    [Parameter(ValueFromRemainingArguments = $true)]$remainingArgs
+ )
+
+$ggp_sdk_path = $env:GGP_SDK_PATH
+
+IF(!$ggp_sdk_path) {
+    throw "GGP_SDK_PATH environment variable not found"
+}
+
+$ggp_path = "$($ggp_sdk_path)dev\bin\ggp.exe"
+
+IF($deploy) {
+    $ggp_put_command = "`"$($ggp_path)`" ssh put `"$($deploy)`""
+    Invoke-Expression "& $ggp_put_command"
+    $ggp_chmod_command = "`"$($ggp_path)`" ssh shell -- chmod u+x /mnt/developer/OrbitClientGgp"
+    Invoke-Expression "& $ggp_chmod_command"
+}
+
+$ggp_ssh_init_command = "`"$($ggp_path)`" ssh init"
+
+Invoke-Expression "& $ggp_ssh_init_command" | Tee-Object -Variable ggp_out
+
+$lines = $ggp_out -split "\n"
+
+ForEach ($line in $lines) {
+    if ($line.Contains("User:")) {
+        $ggp_user = $line.Replace("User:", "").Trim()
+    }
+    if ($line.Contains("Host:")) {
+        $ggp_host = $line.Replace("Host:", "").Trim()
+    }
+    if ($line.Contains("Port:")) {
+        $ggp_port = $line.Replace("Port:", "").Trim()
+    }
+    if ($line.Contains("Key Path:")) {
+        $ggp_key_path = $line.Replace("Key Path:", "").Trim()
+    }
+    if ($line.Contains("Known Hosts Path:")) {
+        $ggp_known_hosts_path = $line.Replace("Known Hosts Path:", "").Trim()
+    }
+}
+
+IF (!$ggp_user -Or !$ggp_host -Or !$ggp_port -Or !$ggp_key_path -Or !$ggp_known_hosts_path) {
+  throw "Unable to get all necessary information from ggp ssh init"
+}
+
+$ssh_path = "$($ggp_sdk_path)tools\OpenSSH-Win64\ssh.exe"
+
+$ssh_command = "`"$($ssh_path)`" -t -p`"$($ggp_port)`" -i`"$($ggp_key_path)`" -oStrictHostKeyChecking=yes -oUserKnownHostsFile=`"$($ggp_known_hosts_path)`" -L44766:localhost:44766 -L44765:localhost:44765 $($ggp_user)@$($ggp_host) -- /mnt/developer/OrbitClientGgp $($remainingArgs)"
+
+Invoke-Expression "& $ssh_command"
+
