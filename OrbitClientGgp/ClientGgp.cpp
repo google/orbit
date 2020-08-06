28af7f414ac8bb13ab9b99be0e2ac2686a50c47c
Cristina Guerrero Romero <crisguerrero@google.com>
2020-08-06 18:18:26 +0100

    Implement StartCapture and initialisations
    + Include -pid argument to OrbitClientGgp: A process is needed to start a capture and it must be provided when OrbitClientGgp is run by using the -pid argument
    + Include flags needed for CaptureClient to work, which can also be
      provided as an argument: -sampling_rate and -frame_pointer_unwinding
    + Implement ClientGgp::StartCapture()
    + Create InitCapture() which initialises all the elements needed for a
    capture to work
    + Create GetOrbitProcessByPid() to set a Process object with the
    information of the process to capture
    
    With these modifications, OrbitClientGgp starts a capture. However, it
    is not possible to stop it yet.

--- OrbitClientGgp/ClientGgp.cpp
+++ OrbitClientGgp/ClientGgp.cpp
@@ -4,6 +4,12 @@
 
 #include "ClientGgp.h"
 
+#include "capture_data.pb.h"
+
+#include "Capture.h"
+#include "OrbitBase/Logging.h"
+#include "OrbitBase/Result.h"
+
 ClientGgp::ClientGgp(ClientGgpOptions&& options) 
     : options_(std::move(options)) {}
 
@@ -31,41 +37,89 @@ bool ClientGgp::InitClient() {
   
   LOG("Created GRPC channel to %s", options_.grpc_server_address);
 
-  // Instantiate CaptureClient
+  // Initialisations needed for capture to work
+  InitCapture();
+  
   capture_client_ = std::make_unique<CaptureClient>(grpc_channel_, this);
 
   return true;
 }
 
+bool ClientGgp::StartCapture() {
+  CHECK(!Capture::IsCapturing());
+
+  ErrorMessageOr<void> result = Capture::StartCapture();
+  if (result.has_error()) {
+    ERROR("Error starting capture %s", result.error().message());
+    return false;
+  }
+
+  // Client capture request
+  int32_t pid = Capture::GProcessId;
+  LOG("Capture pid %d", pid);
+
+  // TODO: selected_functions available when UploadSymbols is included
+  std::vector<std::shared_ptr<orbit_client_protos::FunctionInfo>> selected_functions =
+      Capture::GSelectedFunctions;
+  capture_client_->Capture(pid, selected_functions);
+  LOG("Capture started");
+
+  return true;
+}
+
+void ClientGgp::StopCapture() {
+  LOG("Stop captured requested");
+  CHECK(Capture::IsCapturing());
+  Capture::StopCapture();
+
+  capture_client_->StopCapture();
+
+  LOG("Capture stopped");
+}
+
+void ClientGgp::InitCapture() {
+  Capture::Init();
+  std::shared_ptr<Process> process = GetOrbitProcessByPid(options_.capture_pid);
+  CHECK(process != nullptr);
+  Capture::SetTargetProcess(std::move(process));
+}
+
+std::shared_ptr<Process> ClientGgp::GetOrbitProcessByPid(int32_t pid) {
+  std::shared_ptr<Process> process = std::make_shared<Process>();
+  // TODO: study if we need more information. Requires extra steps
+  process->SetID(pid);
+  return process;
+}
+
 // CaptureListener implementation
 void ClientGgp::OnTimer(const orbit_client_protos::TimerInfo& timer_info) {
   (void)timer_info;
-  LOG("OnTimer called w/ ");
+  //LOG("OnTimer called");
 }
 
 void ClientGgp::OnKeyAndString(uint64_t key, std::string str) {
   (void)key;
   (void)str;
-  LOG("OnKeyAndString called");
+  //LOG("OnKeyAndString called");
 }
 
 void ClientGgp::OnCallstack(CallStack callstack) {
   (void)callstack;
-  LOG("OnCallstack called");
+  //LOG("OnCallstack called");
 }
 
 void ClientGgp::OnCallstackEvent(orbit_client_protos::CallstackEvent callstack_event) {
   (void)callstack_event;
-  LOG("OnCallstackEvent called");
+  //LOG("OnCallstackEvent called");
 }
 
 void ClientGgp::OnThreadName(int32_t thread_id, std::string thread_name) {
   (void)thread_id;
   (void)thread_name;
-  LOG("OnThreadName called");
+  //LOG("OnThreadName called");
 }
 
 void ClientGgp::OnAddressInfo(orbit_client_protos::LinuxAddressInfo address_info) {
   (void)address_info;
-  LOG("OnAddressInfo called");
+  //LOG("OnAddressInfo called");
 }
--- OrbitClientGgp/ClientGgpOptions.h
+++ OrbitClientGgp/ClientGgpOptions.h
@@ -12,6 +12,7 @@
 struct ClientGgpOptions {
   // gRPC connection string
   std::string grpc_server_address;
+  int32_t capture_pid;
 };
 
 #endif // ORBIT_CLIENT_GGP_CLIENT_GGP_OPTIONS_H_
