445f7403a122c73eb8896fe6af9455a58b1c4aef
antonrohr <antonrohr@google.com>
2020-08-12 10:10:30 +0200

    Fixed small bug in run_clang_format.sh
    run_clang_format.sh did format files in build_ folders, now it does not
    anymore. This is now the same in the .gitignore file.

--- run_clang_format.sh
+++ run_clang_format.sh
@@ -4,4 +4,4 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-find . \( -name '*.cpp' -o -name '*.h' \) ! -path './third_party/*' ! -path './build/*' ! -path './cmake-*' ! -name 'resource.h' | xargs clang-format -i
+find . \( -name '*.cpp' -o -name '*.h' \) ! -path './third_party/*' ! -path './build*/*' ! -path './cmake-*' ! -name 'resource.h' | xargs clang-format -i
