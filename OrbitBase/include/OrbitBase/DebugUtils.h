f7699f86e17e8ccf3e38d1a3fa95d13c457a8ad3
Pierric Gimmig <pierric.gimmig@gmail.com>
2020-08-14 15:46:34 -0700

    Add OrbitBase/DebugUtils.h

---
+++ OrbitBase/include/OrbitBase/DebugUtils.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_BASE_DEBUG_UTILS_H_
+#define ORBIT_BASE_DEBUG_UTILS_H_
+
+#include <absl/base/internal/sysinfo.h>
+
+#include <string>
+
+#include "Logging.h"
+
+#define DEBUG_UTILS_ENABLED 0  // Enable locally only.
+#if DEBUG_UTILS_ENABLED
+
+// Print variable's name and value in the form "name = value".
+#define PRINT_VAR(x) LOG("%s = %s", #x, std::to_string(x))
+// Print current function's name, file and line as url, and thread id.
+#define PRINT_FUNC                                      \
+  LOG("%s %s(%u) %s", __FUNCTION__, __FILE__, __LINE__, \
+      std::to_string(absl::base_internal::GetTID()))
+
+#endif  // DEBUG_UTILS_ENABLED
+
+#endif  // ORBIT_BASE_DEBUG_UTILS_H_
