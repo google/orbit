1833d7b813cb96d052372ba9f4028ad6a996142c
Florian Reichl <freichl@google.com>
2020-08-18 11:13:50 +0200

    Refactor caption window selection overlay
    The selection overlay is now no longer a TextBox, but is rendered
    directly in CaptureWindow.

--- OrbitGl/TextBox.cpp
+++ OrbitGl/TextBox.cpp
@@ -29,6 +29,7 @@ TextBox::TextBox(const Vec2& a_Pos, const Vec2& a_Size, const std::string& a_Tex
       size_(a_Size),
       text_(a_Text),
       color_(a_Color),
+      text_y_(FLT_MAX),
       main_frame_counter_(-1),
       elapsed_time_text_length_(0) {}
 
@@ -36,68 +37,13 @@ TextBox::TextBox(const Vec2& a_Pos, const Vec2& a_Size, const Color& a_Color)
     : pos_(a_Pos),
       size_(a_Size),
       color_(a_Color),
+      text_y_(FLT_MAX),
       main_frame_counter_(-1),
       elapsed_time_text_length_(0) {}
 
 TextBox::TextBox(const Vec2& a_Pos, const Vec2& a_Size)
-    : pos_(a_Pos), size_(a_Size), main_frame_counter_(-1), elapsed_time_text_length_(0) {}
-
-void TextBox::Draw(Batcher* batcher, TextRenderer& text_renderer, float min_x, bool visible,
-                   bool right_justify, bool is_inactive, unsigned int id, bool is_picking,
-                   bool is_highlighted) {
-  bool is_core_activity = timer_info_.type() == TimerInfo::kCoreActivity;
-  bool is_same_thread_id_as_selected =
-      is_core_activity && timer_info_.thread_id() == GOrbitApp->selected_thread_id();
-
-  if (GOrbitApp->selected_thread_id() != -1 && is_core_activity && !is_same_thread_id_as_selected) {
-    is_inactive = true;
-  }
-
-  static unsigned char g = 100;
-  Color grey(g, g, g, 255);
-  static Color selection_color(0, 128, 255, 255);
-
-  Color col = is_same_thread_id_as_selected ? m_Color : is_inactive ? grey : m_Color;
-
-  if (this == GOrbitApp->selected_text_box()) {
-    col = selection_color;
-  }
-
-  float z = is_highlighted
-                ? GlCanvas::Z_VALUE_CONTEXT_SWITCH
-                : is_inactive ? GlCanvas::Z_VALUE_BOX_INACTIVE : GlCanvas::Z_VALUE_BOX_ACTIVE;
-
-  Color color = col;
-  if (is_picking) {
-    memcpy(&color[0], &id, sizeof(unsigned int));
-    color[3] = 255;
-  }
-
-  if (visible) {
-    Box box(pos_, size_, z);
-    // TODO: This should be pickable??
-    batcher->AddBox(box, color);
-
-    static Color a_color(255, 255, 255, 255);
-
-    float pos_x = std::max(m_Pos[0], min_x);
-    if (right_justify) {
-      pos_x += m_Size[0];
-    }
-
-    float max_size = m_Pos[0] + m_Size[0] - pos_x;
-
-    const FunctionInfo* func =
-        Capture::capture_data_.GetSelectedFunction(timer_info_.function_address());
-    std::string function_name = func != nullptr ? FunctionUtils::GetDisplayName(*func) : "";
-    std::string text = absl::StrFormat("%s %s", function_name, text_.c_str());
-
-    if (!is_picking && !is_core_activity) {
-      text_renderer.AddText(text.c_str(), pos_x, m_TextY == FLT_MAX ? m_Pos[1] + 1.f : m_TextY,
-                            GlCanvas::Z_VALUE_TEXT, a_color, max_size, right_justify);
-    }
-  }
-
-  // TODO: This should be pickable??
-  batcher->AddVerticalLine(pos_, size_[1], z, grey);
-}
+    : pos_(a_Pos),
+      size_(a_Size),
+      main_frame_counter_(-1),
+      elapsed_time_text_length_(0),
+      text_y_(FLT_MAX) {}
