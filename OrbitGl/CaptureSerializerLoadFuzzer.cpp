50d48b5236bd303de9a9ec99c4160f43ffe84f5d
Henning Becker <henning.becker@gmail.com>
2020-08-19 10:48:35 +0200

    Revise CaptureSerializerLoadFuzzer
    This fuzzer is now based on libprotobuf-mutator which allows better
    structure-aware fuzzing which leads to better coverage.

--- OrbitGl/CaptureSerializerLoadFuzzer.cpp
+++ OrbitGl/CaptureSerializerLoadFuzzer.cpp
@@ -3,6 +3,8 @@
 // found in the LICENSE file.
 
 #include <absl/flags/flag.h>
+#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+#include <libfuzzer/libfuzzer_macro.h>
 
 #include <cstdio>
 #include <filesystem>
@@ -12,6 +14,7 @@
 #include "CaptureSerializer.h"
 #include "SamplingProfiler.h"
 #include "TimeGraph.h"
+#include "capture_data.pb.h"
 
 // Hack: This is declared in a header we include here
 // and the definition needs to take place somewhere.
@@ -22,29 +25,35 @@ ABSL_FLAG(bool, devmode, false, "Enable developer mode in the client's UI");
 ABSL_FLAG(uint16_t, sampling_rate, 1000, "Frequency of callstack sampling in samples per second");
 ABSL_FLAG(bool, frame_pointer_unwinding, false, "Use frame pointers for unwinding");
 
-std::string capture_file;
+DEFINE_PROTO_FUZZER(const orbit_client_protos::CaptureSerializerFuzzerInfo& info) {
+  std::string buffer{};
+  {
+    google::protobuf::io::StringOutputStream stream{&buffer};
+    google::protobuf::io::CodedOutputStream coded_stream{&stream};
+    orbit_client_protos::CaptureHeader capture_header{};
+    capture_header.set_version("1.52");
 
-extern "C" {
-
-int LLVMFuzzerTestOneInput(uint8_t* buf, size_t len) {
-  CaptureSerializer serializer{};
-  TimeGraph time_graph{};
-  auto string_manager = std::make_shared<StringManager>();
-  time_graph.SetStringManager(string_manager);
-  serializer.time_graph_ = &time_graph;
-
-  std::istringstream stream(std::string(reinterpret_cast<const char*>(buf), len));
-  try {
-    (void)serializer.Load(stream);
-  } catch (...) {
+    CaptureSerializer::WriteMessage(&capture_header, &coded_stream);
+    CaptureSerializer::WriteMessage(&info.capture_info(), &coded_stream);
+    for (const auto& timer : info.timers()) {
+      CaptureSerializer::WriteMessage(&timer, &coded_stream);
+    }
   }
 
-  Capture::GSamplingProfiler.reset();
-  return 0;
-}
-
-int LLVMFuzzerInitialize(int*, char***) {
   OrbitApp::Init({}, nullptr);
-  return 0;
+  TimeGraph time_graph{};
+  GCurrentTimeGraph = &time_graph;
+  auto string_manager = std::make_shared<StringManager>();
+  time_graph.SetStringManager(string_manager);
+  GOrbitApp->ClearCapture();
+
+  CaptureSerializer serializer{};
+  serializer.time_graph_ = GCurrentTimeGraph;
+
+  // NOLINTNEXTLINE
+  std::istringstream input_stream{std::move(buffer)};
+  (void)serializer.Load(input_stream);
+
+  GOrbitApp->GetThreadPool()->ShutdownAndWait();
+  GOrbitApp.reset();
 }
-}  // extern "C"
