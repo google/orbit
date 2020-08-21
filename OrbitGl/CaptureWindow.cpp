6c3c176821c466771869e64e6a8b3462ae59c483
Pablo Zimmermann <karupayun@google.com>
2020-08-21 11:49:18 +0000

    Make simpler the bar

--- OrbitGl/CaptureWindow.cpp
+++ OrbitGl/CaptureWindow.cpp
@@ -502,7 +502,6 @@ void CaptureWindow::DrawScreenSpace() {
   double timeSpan = time_graph_.GetCaptureTimeSpanUs();
 
   Color col = slider_->GetBarColor();
-  float height = slider_->GetPixelHeight();
   float canvasHeight = getHeight();
 
   const TimeGraphLayout& layout = time_graph_.GetLayout();
@@ -536,8 +535,8 @@ void CaptureWindow::DrawScreenSpace() {
   const Color kBackgroundColor(70, 70, 70, 255);
   float margin_x1 = getWidth();
   float margin_x0 = margin_x1 - vertical_margin;
-  Box box(Vec2(margin_x0, time_graph_.GetLayout().GetSliderWidth()),
-          Vec2(margin_x1 - margin_x0, canvasHeight - height), GlCanvas::Z_VALUE_MARGIN);
+  
+  Box box(Vec2(margin_x0, 0), Vec2(margin_x1 - margin_x0, canvasHeight), GlCanvas::Z_VALUE_MARGIN);
   ui_batcher_.AddBox(box, kBackgroundColor);
 
   // Time bar background
