949194f5b6279e109c6d221ea40ec47ecb777c79
Pierric Gimmig <pierric@google.com>
2020-08-25 13:52:54 -0700

    Add tests for Profiling.h

--- OrbitBase/include/OrbitBase/Profiling.h
+++ OrbitBase/include/OrbitBase/Profiling.h
@@ -10,37 +10,33 @@
 #ifdef _WIN32
 #include <Windows.h>
 using pid_t = uint32_t;
-#define CLOCK_MONOTONIC 1
 #else
 #include <sys/syscall.h>
 #include <unistd.h>
 #endif
 
+[[nodiscard]] inline uint64_t MonotonicTimestampNs() {
 #ifdef _WIN32
-inline void clock_gettime(uint32_t, struct timespec* spec) {
   __int64 time;
   GetSystemTimeAsFileTime((FILETIME*)&time);
-  spec->tv_sec = time / 10000000i64;
-  spec->tv_nsec = time % 10000000i64 * 100;
-}
-#endif
-
-inline uint64_t MonotonicTimestampNs() {
+  return static_cast<uint64_t>(time) * 100;
+#else
   timespec ts;
   clock_gettime(CLOCK_MONOTONIC, &ts);
   return 1000000000ll * ts.tv_sec + ts.tv_nsec;
+#endif
 }
 
-inline absl::Duration TicksToDuration(uint64_t start, uint64_t end) {
+[[nodiscard]] inline absl::Duration TicksToDuration(uint64_t start, uint64_t end) {
   return absl::Nanoseconds(end - start);
 }
 
-inline double TicksToMicroseconds(uint64_t start, uint64_t end) {
-  return double((end - start)) * 0.001;
+[[nodiscard]] inline double TicksToMicroseconds(uint64_t start, uint64_t end) {
+  return static_cast<double>((end - start)) * 0.001;
 }
 
 #ifdef __linux__
-inline pid_t GetThreadId() {
+[[nodiscard]] inline pid_t GetThreadId() {
   thread_local pid_t current_tid = syscall(__NR_gettid);
   return current_tid;
 }
