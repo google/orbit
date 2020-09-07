bb878597951231e3125a9a3d27aa002f677f48fb
Cristina Guerrero Romero <crisguerrero@google.com>
2020-09-07 09:48:42 +0100

    Split CaptureSerializer in two: step 1
    Move CaptureSerializer.h/cc to OrbitClientModel. This step on its own
    does not compile, it's meant to be able to track the changes in both
    files the CaptureSerializer is gonna be splitted into.

--- OrbitClientModel/CaptureSerializer.cpp
+++ OrbitClientModel/CaptureSerializer.cpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "CaptureSerializer.h"
+#include "OrbitClientModel/CaptureSerializer.h"
 
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
