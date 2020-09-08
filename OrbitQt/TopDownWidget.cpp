9853b957953388f83a68d4d48754abef32ac48a4
Davide Pallotti <dpallotti@google.com>
2020-09-08 10:29:39 +0200

    Capitalize TopDownWidget file names

--- OrbitQt/TopDownWidget.cpp
+++ OrbitQt/TopDownWidget.cpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "topdownwidget.h"
+#include "TopDownWidget.h"
 
 #include <QColor>
 #include <QMenu>
