146d2422f1f538860afaa32abc76d5b07294c86e
Florian Kübler <kuebler@google.com>
2020-09-11 13:05:15 +0000

    fix build

--- OrbitGl/CaptureDeserializerLoadFuzzer.cpp
+++ OrbitGl/CaptureDeserializerLoadFuzzer.cpp
@@ -50,7 +50,8 @@ DEFINE_PROTO_FUZZER(const orbit_client_protos::CaptureDeserializerFuzzerInfo& in
 
   // NOLINTNEXTLINE
   std::istringstream input_stream{std::move(buffer)};
-  (void)capture_deserializer::Load(input_stream, GOrbitApp.get());
+  bool cancelation_requested = false;
+  (void)capture_deserializer::Load(input_stream, GOrbitApp.get(), &cancelation_requested);
 
   GOrbitApp->GetThreadPool()->ShutdownAndWait();
   GOrbitApp.reset();
