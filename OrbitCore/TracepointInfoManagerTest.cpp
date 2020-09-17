1d35c5da30bcd942fd3f0dd23c982ea801e7c0e0
MariaSandru27 <mariasandru2@yahoo.com>
2020-09-17 15:24:25 +0000

    Minor details

--- OrbitCore/TracepointInfoManagerTest.cpp
+++ OrbitCore/TracepointInfoManagerTest.cpp
@@ -2,9 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <gtest/gtest.h>
-
 #include "TracepointInfoManager.h"
+#include "gtest/gtest.h"
 
 TEST(TracepointInfoManager, Contains) {
   TracepointInfoManager tracepoint_info_manager;
