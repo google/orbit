7b2ebf49476115716c5e805de2945c1814e5e9d4
Henning Becker <henning.becker@gmail.com>
2020-09-21 09:39:40 +0200

    Revamp NOTICE file generation
    The NOTICE and the NOTICE.Chromium file have been generated by
    concatenating license files. This is changed in this commit for
    NOTICE.Chromium which is now generated by conan in its import step (The
    import-stepi's purpose is to consume/copy packaged files from
    dependencies.) from metadata files included in the chromium source code.
    These metadata files list the real name, a URL and the license type
    among other things, but only the mentioned three fields will be
    integrated into the NOTICE.Chromium file as well as they are used to
    generate a new dependency table called NOTICE.Chromium.csv.

--- cmake/collect_licenses.cmake
+++ cmake/collect_licenses.cmake
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-function(DoGenerateThirdPartyLicenseFile OUTPUT_FILE INPUT_DIRECTORY)
+function(DoGenerateThirdPartyLicenseFile OUTPUT_FILE INPUT_DIRECTORY APPEND_FILES)
   get_filename_component(INPUT_DIRECTORY "${INPUT_DIRECTORY}" ABSOLUTE)
   get_filename_component(OUTPUT_FILE "${OUTPUT_FILE}" ABSOLUTE)
 
@@ -16,19 +16,28 @@ function(DoGenerateThirdPartyLicenseFile OUTPUT_FILE INPUT_DIRECTORY)
     file(APPEND "${OUTPUT_FILE}" "${content}")
     file(APPEND "${OUTPUT_FILE}" "\n\n\n")
   endforeach()
+
+  if(APPEND_FILES)
+    foreach(file ${APPEND_FILES})
+      file(READ "${file}" content)
+      file(APPEND "${OUTPUT_FILE}" "${content}")
+    endforeach()
+  endif()
 endfunction()
 
 set(GEN_THIRD_PARTY_SCRIPT "${CMAKE_CURRENT_LIST_FILE}")
 
 function(GenerateThirdPartyLicenseFile OUTPUT_FILE INPUT_DIRECTORY)
+  cmake_parse_arguments(GEN "" "" "APPEND_FILES" ${ARGN})
+
   get_filename_component(INPUT_DIRECTORY "${INPUT_DIRECTORY}" ABSOLUTE)
   get_filename_component(OUTPUT_FILE "${OUTPUT_FILE}" ABSOLUTE)
   string(MD5 OUTPUT_FILE_HASH "${OUTPUT_FILE}")
   string(SUBSTRING "${OUTPUT_FILE_HASH}" 0 12 OUTPUT_FILE_HASH)
-  add_custom_target("ThirdPartyLicenseFile_${OUTPUT_FILE_HASH}" ALL COMMAND ${CMAKE_COMMAND} -DIN_GENERATE_THIRD_PARTY_LICENSE_FILE=TRUE -DINPUT_DIRECTORY="${INPUT_DIRECTORY}" -DOUTPUT_FILE="${OUTPUT_FILE}" -P "${GEN_THIRD_PARTY_SCRIPT}" BYPRODUCTS "${OUTPUT_FILE}" COMMENT "Collecting licenses for the ${OUTPUT_FILE} file...")
+  add_custom_target("ThirdPartyLicenseFile_${OUTPUT_FILE_HASH}" ALL COMMAND ${CMAKE_COMMAND} -DIN_GENERATE_THIRD_PARTY_LICENSE_FILE=TRUE -DINPUT_DIRECTORY="${INPUT_DIRECTORY}" -DOUTPUT_FILE="${OUTPUT_FILE}" -DAPPEND_FILES="${GEN_APPEND_FILES}" -P "${GEN_THIRD_PARTY_SCRIPT}" BYPRODUCTS "${OUTPUT_FILE}" COMMENT "Collecting licenses for the ${OUTPUT_FILE} file...")
 endfunction()
 
 if(IN_GENERATE_THIRD_PARTY_LICENSE_FILE)
-  DoGenerateThirdPartyLicenseFile("${OUTPUT_FILE}" "${INPUT_DIRECTORY}")
+  DoGenerateThirdPartyLicenseFile("${OUTPUT_FILE}" "${INPUT_DIRECTORY}" "${APPEND_FILES}")
 endif()
 
