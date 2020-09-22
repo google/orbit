4d867b60b3664e9ec51d58d87ae968fa7f11d4b0
Henning Becker <henning.becker@gmail.com>
2020-09-22 16:32:47 +0200

    Add QWebChannelExtractor
    The JavaScript client library for Qt's WebChannel module is embedded
    into the QtWebChannel shared library. For the development workflow we
    need to extract that file. The simplest portable way to do that is to
    compile an exectuable link it to QtWebChannel and output the resource.
    
    On Windows where we use Qt from a conan package we could also copy the
    library from the source archive, but this won't work on Linux where
    we usually rely on system-installed Qt. That's why this extraction
    mechanism is the simplest portable way of getting the right version of
    that file.
    
    This mechanism has a limitation: It won't work with cross-compilation
    setups. But this can be easily added in the future by separating the
    extractor into its own conan package and require that package as a build
    requirement which means it will be compiled for the build platform and
    not for the target platform. Currently we don't need the cross
    compilation workflow for the UI.
    
    Bug: http://b/169113529

---
+++ WebUI/QWebChannelExtractor.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+#include <QWebChannel>
+#include <cstdio>
+
+int main(int argc, char* argv[]) {
+  // We need to consume a symbol from the QtWebChannel module. Otherwise
+  // the linker won't link against the corresponding library which contains
+  // the embedded resource that we're trying to extract.
+  QWebChannel _{};
+
+  QFile webchannel_js{":/qtwebchannel/qwebchannel.js"};
+
+  if (!webchannel_js.open(QIODevice::ReadOnly)) {
+    qCritical() << "QWebChannelExractor does not come with qwebchannel.js embedded!";
+    return 1;
+  }
+
+  if (argc != 2) {
+    QTextStream stream{stdout};
+    stream << webchannel_js.readAll();
+    return 0;
+  }
+
+  // Create the output directory if it does not exist
+  QFileInfo{argv[1]}.dir().mkpath(".");
+
+  QFile output_file{argv[1]};
+  if (!output_file.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
+    qCritical() << "Could not open the output file for writing.";
+    return 1;
+  }
+
+  output_file.write(webchannel_js.readAll());
+  return 0;
+}
