ea690a86c5cd087f17270bc6e3c03adb5ec1f306
Henning Becker <henning.becker@gmail.com>
2020-09-23 16:42:14 +0200

    Bump conan version to 1.29.2
    The bootstrap scripts will now install conan version 1.29.2 if conan is
    not installed or an older version than 1.29 is installed.
    
    A follow-up commit will make us of base-lockfiles - a new conan feature
    introduced in 1.28 and extended in 1.29.

--- bootstrap-orbit.ps1
+++ bootstrap-orbit.ps1
@@ -18,8 +18,8 @@ it available in the path.
 "@
   }
 
-  $result = Start-Process -FilePath $pip3.Path -ArgumentList "install","conan" -Wait -NoNewWindow -ErrorAction Stop -PassThru
-  if ($result.ExitCode -ne 0) {
+  & $pip3.Path install conan==1.29.2
+  if ($LastExitCode -ne 0) {
     Throw "Error while installing conan via pip3."
   }
     
@@ -35,19 +35,19 @@ You can call 'pip3 show -f conan' to figure out where conan.exe was placed.
 } else {
   Write-Host "Conan found. Checking version..."
 
-  $conan_version = (conan --version).split(" ")[2]
+  $conan_version = (& $conan.Path --version).split(" ")[2]
   $conan_version_major = $conan_version.split(".")[0] -as [int]
   $conan_version_minor = $conan_version.split(".")[1] -as [int]
 
   $conan_version_major_required = 1
-  $conan_version_minor_min = 27
+  $conan_version_minor_min = 29
 
   if ($conan_version_major -eq $conan_version_major_required -and $conan_version_minor -lt $conan_version_minor_min) {
     Write-Host "Your conan version $conan_version is too old. Let's try to update it."
 
     Try {
       $pip3 = Get-Command pip3
-      Start-Process -FilePath $pip3.Path -ArgumentList "install","--upgrade","conan" -Wait -NoNewWindow -PassThru
+      & $pip3.Path install --upgrade conan==1.29.2
     } Catch {
       Throw "Error while upgrading conan via pip3. Probably you have conan installed differently." + 
             " Please manually update conan to a at least version $conan_version_major_required.$conan_version_minor_min."
@@ -65,7 +65,7 @@ You can call 'pip3 show -f conan' to figure out where conan.exe was placed.
 # Start build
 if ($args) {
   & "$PSScriptRoot\build.ps1" $args
-} elseif (conan remote list | Select-String -NotMatch "Disabled:" | Select-String "artifactory:") {
+} elseif (& $conan.Path remote list | Select-String -NotMatch "Disabled:" | Select-String "artifactory:") {
   & "$PSScriptRoot\build.ps1" "default_relwithdebinfo" "ggp_relwithdebinfo"
 } else {
   & "$PSScriptRoot\build.ps1"
