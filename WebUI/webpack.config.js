f88d8a965658bb77d5fc58e6a0d2aac25fba809f
Henning Becker <henning.becker@gmail.com>
2020-09-28 08:37:44 +0200

    Add first webpack bundling integration
    This is a first step for integrating the Webpack bundler into our cmake-
    and conan-based build system.
    
    conan will provide a distribution of NodeJS as a build dependency. CMake
    will then take care to install required NPM-packages during its
    configuration phase.
    
    The actual invocation of webpack takes place during the build phase and
    it supports incremental builds. The incremental part is currently
    mainly handled in cmake which requires that all input files are also
    enlisted in the CMakeLists.txt file. This can become a source for bugs
    pretty easily and maybe also an annoyance. So this can be changed to a
    different approach which would make the cmake-code more complex. That's
    why I went with the current (explicit) solution for now.

---
+++ WebUI/webpack.config.js
@@ -0,0 +1,83 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const path = require('path');
+const process = require('process');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const QrcGeneratorPlugin = require('./plugins/qrc-generator');
+const MinimizeWritesPlugin = require('./plugins/minimize-writes');
+
+if (!process.env.ORBIT_BUILD_FOLDER &&
+  !fs.existsSync(path.resolve(__dirname, "qtwebchannel", "qwebchannel.js"))) {
+  console.error(`Could not find qtwebchannel/qwebchannel.js!
+  
+Webpack needs a version of "qwebchannel.js" which fits the currently used
+version of Qt. Our build system automatically extracts the correct version into
+WebUI/qtwebchannel/qwebchannel.js inside of the Orbit build directory.
+  
+You have two options:
+  - Either set the ORBIT_BUILD_FOLDER environment variable and have it 
+    point to your corresponding Orbit-build directory,
+    (i.e. ${path.resolve(__dirname, '..', 'build_default_relwithdebinfo')})
+  - Or copy qtwebchannel/qwebchannel.js from your Orbit build directory into 
+    the WebUI source directory (${__dirname}).
+`)
+  process.exit();
+}
+
+const qwebchannel_search_path = (() => {
+  if (process.env.ORBIT_BUILD_FOLDER) {
+    return path.resolve(process.env.ORBIT_BUILD_FOLDER, "WebUI");
+  } else {
+    return __dirname;
+  }
+})();
+
+const config = {
+  mode: 'development',
+  entry: {
+    dummy: './src/dummy.js',
+  },
+  output: {
+    filename: '[name]/bundle.js',
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      qwebchannel_search_path
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['dummy'],
+      filename: 'dummy/index.html',
+      title: "Dummy"
+    }),
+    new QrcGeneratorPlugin(),
+    new MinimizeWritesPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
+
+
+module.exports = (env, args) => {
+
+  const is_development = () => {
+    return !args.mode || args.mode === 'development';
+  };
+
+  if (!is_development()) {
+    config.mode = 'production';
+    config.output.path = path.resolve(process.env.ORBIT_BUILD_FOLDER, "WebUI", "dist");
+    config.output.publicPath = 'qrc:/webUI/';
+  }
+
+  return config;
+};
