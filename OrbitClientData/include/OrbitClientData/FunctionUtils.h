e65c9b5b2cc187a2e81289143b4f7ba70e371538
Florian Kübler <kuebler@google.com>
2020-09-30 08:49:53 +0000

    Add [[nodiscard]] to FunctionUtils

--- OrbitClientData/include/OrbitClientData/FunctionUtils.h
+++ OrbitClientData/include/OrbitClientData/FunctionUtils.h
@@ -13,22 +13,23 @@
 
 namespace FunctionUtils {
 
-inline const std::string& GetDisplayName(const orbit_client_protos::FunctionInfo& func) {
+[[nodiscard]] inline const std::string& GetDisplayName(
+    const orbit_client_protos::FunctionInfo& func) {
   return func.pretty_name().empty() ? func.name() : func.pretty_name();
 }
 
-std::string GetLoadedModuleName(const orbit_client_protos::FunctionInfo& func);
-uint64_t GetHash(const orbit_client_protos::FunctionInfo& func);
+[[nodiscard]] std::string GetLoadedModuleName(const orbit_client_protos::FunctionInfo& func);
+[[nodiscard]] uint64_t GetHash(const orbit_client_protos::FunctionInfo& func);
 
 // Calculates and returns the absolute address of the function.
-uint64_t Offset(const orbit_client_protos::FunctionInfo& func);
-inline uint64_t GetAbsoluteAddress(const orbit_client_protos::FunctionInfo& func) {
+[[nodiscard]] uint64_t Offset(const orbit_client_protos::FunctionInfo& func);
+[[nodiscard]] inline uint64_t GetAbsoluteAddress(const orbit_client_protos::FunctionInfo& func) {
   return func.address() + func.module_base_address() - func.load_bias();
 }
 
-bool IsOrbitFunc(const orbit_client_protos::FunctionInfo& func);
+[[nodiscard]] bool IsOrbitFunc(const orbit_client_protos::FunctionInfo& func);
 
-std::unique_ptr<orbit_client_protos::FunctionInfo> CreateFunctionInfo(
+[[nodiscard]] std::unique_ptr<orbit_client_protos::FunctionInfo> CreateFunctionInfo(
     const orbit_grpc_protos::SymbolInfo& symbol_info, uint64_t load_bias,
     const std::string& module_path, uint64_t module_base_address);
 
