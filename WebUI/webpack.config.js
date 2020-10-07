eb04a3e4fe7c58f4b5395b028b5ea324adf3057e
Henning Becker <henning.becker@gmail.com>
2020-10-07 15:20:27 +0200

    Apply prettyfier to webpack.config.js
    That's basically clang-format for JavaScript files.

--- WebUI/webpack.config.js
+++ WebUI/webpack.config.js
@@ -2,15 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const path = require('path');
-const process = require('process');
-const fs = require('fs');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const QrcGeneratorPlugin = require('./plugins/qrc-generator');
-const MinimizeWritesPlugin = require('./plugins/minimize-writes');
+const path = require("path");
+const process = require("process");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const QrcGeneratorPlugin = require("./plugins/qrc-generator");
+const MinimizeWritesPlugin = require("./plugins/minimize-writes");
 
-if (!process.env.ORBIT_BUILD_FOLDER &&
-  !fs.existsSync(path.resolve(__dirname, "qtwebchannel", "qwebchannel.js"))) {
+if (
+  !process.env.ORBIT_BUILD_FOLDER &&
+  !fs.existsSync(path.resolve(__dirname, "qtwebchannel", "qwebchannel.js"))
+) {
   console.error(`Could not find qtwebchannel/qwebchannel.js!
   
 Webpack needs a version of "qwebchannel.js" which fits the currently used
@@ -20,10 +22,10 @@ WebUI/qtwebchannel/qwebchannel.js inside of the Orbit build directory.
 You have two options:
   - Either set the ORBIT_BUILD_FOLDER environment variable and have it 
     point to your corresponding Orbit-build directory,
-    (i.e. ${path.resolve(__dirname, '..', 'build_default_relwithdebinfo')})
+    (i.e. ${path.resolve(__dirname, "..", "build_default_relwithdebinfo")})
   - Or copy qtwebchannel/qwebchannel.js from your Orbit build directory into 
     the WebUI source directory (${__dirname}).
-`)
+`);
   process.exit();
 }
 
@@ -36,19 +38,16 @@ const qwebchannel_search_path = (() => {
 })();
 
 const config = {
-  mode: 'development',
+  mode: "development",
   entry: {
     dummy: './src/dummy.js',
   },
   output: {
-    filename: '[name]/bundle.js',
-    path: path.resolve(__dirname, "dist")
+    filename: "[name]/bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   resolve: {
-    modules: [
-      'node_modules',
-      qwebchannel_search_path
-    ]
+    modules: ["node_modules", qwebchannel_search_path],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -57,25 +56,27 @@ const config = {
       title: "Dummy"
     }),
     new QrcGeneratorPlugin(),
-    new MinimizeWritesPlugin()
+    new MinimizeWritesPlugin(),
   ],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
-    port: 9000
-  }
+    port: 9000,
+  },
 };
 
-
 module.exports = (env, args) => {
-
   const is_development = () => {
-    return !args.mode || args.mode === 'development';
+    return !args.mode || args.mode === "development";
   };
 
   if (!is_development()) {
-    config.mode = 'production';
-    config.output.path = path.resolve(process.env.ORBIT_BUILD_FOLDER, "WebUI", "dist");
+    config.mode = "production";
+    config.output.path = path.resolve(
+      process.env.ORBIT_BUILD_FOLDER,
+      "WebUI",
+      "dist"
+    );
     config.output.publicPath = 'qrc:/webUI/';
   }
 
