ec26f0ca00f5f56b280f01c8e8a54ec3a0798ac0
Cristina Guerrero Romero <crisguerrero@google.com>
2020-10-16 16:05:56 +0100

    Rename GetNBusyThreads to GetNumberOfBusyThreads
    Rename new method in ThreadPool so it is more clear

--- OrbitBase/include/OrbitBase/ThreadPool.h
+++ OrbitBase/include/OrbitBase/ThreadPool.h
@@ -53,7 +53,7 @@ class ThreadPool {
   }
 
   virtual size_t GetPoolSize() = 0;
-  virtual size_t GetNBusyThreads() = 0;
+  virtual size_t GetNumberOfBusyThreads() = 0;
 
   // Create ThreadPool with specified minimum and maximum number of worker
   // threads.
