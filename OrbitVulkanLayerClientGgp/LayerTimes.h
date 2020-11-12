622089b36915db04ce1c586f8e59461402e4d45d
Cristina Guerrero Romero <crisguerrero@google.com>
2020-11-12 13:23:39 +0000

    Use absl::Time instead of chronos to measure times
    Implement time duration checks for time frame and capture length with
    absl instead of chronos. It is more precise and it compares double instead of
    long.

--- OrbitVulkanLayerClientGgp/LayerTimes.h
+++ OrbitVulkanLayerClientGgp/LayerTimes.h
@@ -5,11 +5,11 @@
 #ifndef ORBIT_VULKAN_LAYER_CLIENT_GGP_LAYER_TIMES_H_
 #define ORBIT_VULKAN_LAYER_CLIENT_GGP_LAYER_TIMES_H_
 
-#include <chrono>
+#include "absl/time/time.h"
 
 struct LayerTimes {
-  std::chrono::steady_clock::time_point last_frame;
-  std::chrono::steady_clock::time_point capture_started;
+  absl::Time last_frame;
+  absl::Time capture_started;
 };
 
 #endif  // ORBIT_VULKAN_LAYER_CLIENT_GGP_LAYER_TIMES_H_
