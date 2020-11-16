50648152314f593565c029354a44dbbeb8716806
Pablo Zimmermann <karupayun@google.com>
2020-11-16 13:26:53 +0000

    Temporarily don't pin the scheduler track

--- OrbitGl/SchedulerTrack.cpp
+++ OrbitGl/SchedulerTrack.cpp
@@ -18,7 +18,7 @@ using orbit_client_protos::TimerInfo;
 const Color kInactiveColor(100, 100, 100, 255);
 const Color kSelectionColor(0, 128, 255, 255);
 
-SchedulerTrack::SchedulerTrack(TimeGraph* time_graph) : TimerTrack(time_graph) { SetPinned(true); }
+SchedulerTrack::SchedulerTrack(TimeGraph* time_graph) : TimerTrack(time_graph) { SetPinned(false); }
 
 float SchedulerTrack::GetHeight() const {
   TimeGraphLayout& layout = time_graph_->GetLayout();
