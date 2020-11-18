1a2e57ed9a877ba23afb1fff585d051196c86a8d
Florian Kübler <kuebler@google.com>
2020-11-18 16:03:55 +0100

    Add Vulkan Queue Manager for the Vulkan layer. (#1468)
    Add Vulkan Queue Manager for the Vulkan layer.
    
    This will create a new project for Orbit's new vulkan layer.
    Its first class is a queue manager. That maintains mappings from
    vulkan queues to its corresponding logical device (VkDevice).
    
    Test: Compile & Test
    Bug: http://b/168200502

---
+++ OrbitVulkanLayer/QueueManager.h
@@ -0,0 +1,45 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_VULKAN_LAYER_QUEUE_MANAGER_H_
+#define ORBIT_VULKAN_LAYER_QUEUE_MANAGER_H_
+
+#include "absl/container/flat_hash_map.h"
+#include "absl/synchronization/mutex.h"
+#include "vulkan/vulkan.h"
+
+namespace orbit_vulkan_layer {
+
+/*
+ * This class tracks the mapping from queues to the device (via vkGetDeviceQueue).
+ *
+ * Note: There is no "untrack" function as there is no such mechanism in Vulkan. We could
+ *  use vkDestroyDevice, and iterate over all queues of that device, but as there is usually only
+ *  one device anyways and destroy will be called at the end of a game, that would not really help.
+ *
+ * Thread-Safety: This class is internally synchronized (using read/write locks) and can be safely
+ * accessed from different threads.
+ */
+class QueueManager {
+ public:
+  QueueManager() = default;
+  /*
+   * Establishes a mapping from the given queue to the given logical device. There must be no
+   * existing mapping for that queue to a different device.
+   */
+  void TrackQueue(VkQueue queue, VkDevice device);
+
+  /*
+   * Returns the logical device associated with that queue. It expects that the mapping for that
+   * queue is known.
+   */
+  [[nodiscard]] VkDevice GetDeviceOfQueue(VkQueue queue);
+
+ private:
+  absl::Mutex mutex_;
+  absl::flat_hash_map<VkQueue, VkDevice> queue_to_device_;
+};
+}  // namespace orbit_vulkan_layer
+
+#endif  // ORBIT_VULKAN_LAYER_QUEUE_MANAGER_H_
---
+++ OrbitVulkanLayer/QueueManagerTest.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "QueueManager.h"
+#include "gtest/gtest.h"
+
+namespace orbit_vulkan_layer {
+
+TEST(QueueManager, AnNonTrackedQueueCanNotBeRetrieved) {
+  QueueManager manager;
+  VkQueue queue = {};
+  EXPECT_DEATH({ (void)manager.GetDeviceOfQueue(queue); }, "");
+}
+
+TEST(QueueManager, AQueueCanBeTrackedAndRetrieved) {
+  QueueManager manager;
+  VkDevice device = nullptr;
+  VkQueue queue = nullptr;
+
+  manager.TrackQueue(queue, device);
+  VkDevice result = manager.GetDeviceOfQueue(queue);
+  EXPECT_EQ(device, result);
+}
+
+}  // namespace orbit_vulkan_layer
