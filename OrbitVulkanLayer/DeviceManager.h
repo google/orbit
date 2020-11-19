4db52ffdafc447dae48544a3f8239cab0d501318
Florian Kübler <kuebler@google.com>
2020-11-19 13:09:41 +0100

    Add Physical Device Tracking to Vulkan Layer. (#1470)
    Add Physical Device Tracking to Vulkan Layer.
    
    This adds a manager for physical devices. It maintains a mapping of
    logical to corresponding physical devices and holds properties (such
    as the timestamp period) for this device.
    
    Test: Compile & run tests
    Bug: http://b/168200502

---
+++ OrbitVulkanLayer/DeviceManager.h
@@ -0,0 +1,100 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_VULKAN_LAYER_DEVICE_MANAGER_H_
+#define ORBIT_VULKAN_LAYER_DEVICE_MANAGER_H_
+
+#include "OrbitBase/Logging.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "absl/synchronization/mutex.h"
+#include "vulkan/vulkan.h"
+
+namespace orbit_vulkan_layer {
+
+/*
+ * This class maintains a mapping from logical to physical devices (via `vkCreateDevice` and
+ * `vkDestroyDevice`).
+ * `TrackLogicalDevice` establishes that mapping, while `UntrackLogicalDevice` can be used to
+ * release that mapping. To retrieve a logical device's physical device, use `GetPhysicalDevice`.
+ *
+ * For each physical device, it also provides the `VkPhysicalDeviceProperties`, which can be queried
+ * using `GetPhysicalDeviceProperties`. The `DeviceManager` is responsible for retrieving this
+ * information (using `vkGetPhysicalDeviceProperties`). These properties can be used e.g. for
+ * converting clock cycles to nanosecond timestamps.
+ *
+ * Thread-Safety: This class is internally synchronized (using read/write locks) and can be safely
+ * accessed from different threads.
+ */
+template <class DispatchTable>
+class DeviceManager {
+ public:
+  explicit DeviceManager(DispatchTable* dispatch_table) : dispatch_table_(dispatch_table) {}
+
+  void TrackLogicalDevice(VkPhysicalDevice physical_device, VkDevice logical_device) {
+    absl::WriterMutexLock lock(&mutex_);
+    CHECK(!logical_device_to_physical_device_.contains(logical_device));
+    logical_device_to_physical_device_[logical_device] = physical_device;
+
+    CHECK(!physical_device_to_logical_devices_.contains(physical_device) ||
+          !physical_device_to_logical_devices_.at(physical_device).contains(logical_device));
+    if (!physical_device_to_logical_devices_.contains(physical_device)) {
+      physical_device_to_logical_devices_[physical_device] = {logical_device};
+    } else {
+      physical_device_to_logical_devices_.at(physical_device).insert(logical_device);
+    }
+
+    if (physical_device_to_properties_.contains(physical_device)) {
+      return;
+    }
+
+    VkPhysicalDeviceProperties properties;
+    dispatch_table_->GetPhysicalDeviceProperties(physical_device)(physical_device, &properties);
+    physical_device_to_properties_[physical_device] = properties;
+  }
+
+  [[nodiscard]] VkPhysicalDevice GetPhysicalDeviceOfLogicalDevice(VkDevice logical_device) {
+    absl::ReaderMutexLock lock(&mutex_);
+    CHECK(logical_device_to_physical_device_.contains(logical_device));
+    return logical_device_to_physical_device_.at(logical_device);
+  }
+
+  void UntrackLogicalDevice(VkDevice logical_device) {
+    absl::WriterMutexLock lock(&mutex_);
+    CHECK(logical_device_to_physical_device_.contains(logical_device));
+    VkPhysicalDevice physical_device = logical_device_to_physical_device_.at(logical_device);
+    logical_device_to_physical_device_.erase(logical_device);
+
+    CHECK(physical_device_to_logical_devices_.contains(physical_device));
+    absl::flat_hash_set<VkDevice>& logical_devices =
+        physical_device_to_logical_devices_.at(physical_device);
+    CHECK(logical_devices.contains(logical_device));
+    logical_devices.erase(logical_device);
+
+    if (!logical_devices.empty()) {
+      return;
+    }
+    physical_device_to_logical_devices_.erase(physical_device);
+    physical_device_to_properties_.erase(physical_device);
+  }
+
+  [[nodiscard]] VkPhysicalDeviceProperties GetPhysicalDeviceProperties(
+      VkPhysicalDevice physical_device) {
+    absl::ReaderMutexLock lock(&mutex_);
+    CHECK(physical_device_to_properties_.contains(physical_device));
+    return physical_device_to_properties_.at(physical_device);
+  }
+
+ private:
+  absl::Mutex mutex_;
+  DispatchTable* dispatch_table_;
+  absl::flat_hash_map<VkPhysicalDevice, VkPhysicalDeviceProperties> physical_device_to_properties_;
+  absl::flat_hash_map<VkDevice, VkPhysicalDevice> logical_device_to_physical_device_;
+  absl::flat_hash_map<VkPhysicalDevice, absl::flat_hash_set<VkDevice>>
+      physical_device_to_logical_devices_;
+};
+
+}  // namespace orbit_vulkan_layer
+
+#endif  // ORBIT_VULKAN_LAYER_DEVICE_MANAGER_H_
---
+++ OrbitVulkanLayer/DeviceManagerTest.cpp
@@ -0,0 +1,90 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "DeviceManager.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+using ::testing::Return;
+
+namespace orbit_vulkan_layer {
+
+class MockDispatchTable {
+ public:
+  MOCK_METHOD(PFN_vkGetPhysicalDeviceProperties, GetPhysicalDeviceProperties,
+              (VkPhysicalDevice dispatchable_object));
+};
+
+TEST(DeviceManager, AnUntrackedDeviceCannotBeQueried) {
+  MockDispatchTable dispatch_table;
+  DeviceManager manager(&dispatch_table);
+  VkDevice device = {};
+  EXPECT_DEATH({ (void)manager.GetPhysicalDeviceOfLogicalDevice(device); }, "");
+}
+
+TEST(DeviceManager, DevicePropertiesCannotBeQueriedForUntrackedDevices) {
+  MockDispatchTable dispatch_table;
+  DeviceManager manager(&dispatch_table);
+  VkPhysicalDevice device = {};
+  EXPECT_DEATH({ (void)manager.GetPhysicalDeviceProperties(device); }, "");
+}
+
+VkPhysicalDeviceProperties physical_device_properties = {
+    .apiVersion = 1, .driverVersion = 2, .limits = {.timestampPeriod = 3.14f}};
+static void MockGetPhysicalDeviceProperties(VkPhysicalDevice,
+                                            VkPhysicalDeviceProperties* out_properties) {
+  *out_properties = physical_device_properties;
+}
+
+TEST(DeviceManager, ATrackedDeviceCanBeQueried) {
+  MockDispatchTable dispatch_table;
+  DeviceManager manager(&dispatch_table);
+  VkDevice logical_device = {};
+  VkPhysicalDevice physical_device = {};
+
+  EXPECT_CALL(dispatch_table, GetPhysicalDeviceProperties)
+      .WillOnce(Return(&MockGetPhysicalDeviceProperties));
+
+  manager.TrackLogicalDevice(physical_device, logical_device);
+
+  ASSERT_EQ(physical_device, manager.GetPhysicalDeviceOfLogicalDevice(logical_device));
+  VkPhysicalDeviceProperties actual_properties =
+      manager.GetPhysicalDeviceProperties(physical_device);
+  EXPECT_EQ(actual_properties.apiVersion, physical_device_properties.apiVersion);
+  EXPECT_EQ(actual_properties.driverVersion, physical_device_properties.driverVersion);
+  EXPECT_EQ(actual_properties.limits.timestampPeriod,
+            physical_device_properties.limits.timestampPeriod);
+}
+
+TEST(DeviceManager, UntrackingRemovesTrackedDevice) {
+  MockDispatchTable dispatch_table;
+  DeviceManager manager(&dispatch_table);
+  VkDevice logical_device = {};
+  VkPhysicalDevice physical_device = {};
+
+  EXPECT_CALL(dispatch_table, GetPhysicalDeviceProperties)
+      .WillOnce(Return(&MockGetPhysicalDeviceProperties));
+
+  manager.TrackLogicalDevice(physical_device, logical_device);
+  manager.UntrackLogicalDevice(logical_device);
+
+  EXPECT_DEATH({ (void)manager.GetPhysicalDeviceOfLogicalDevice(logical_device); }, "");
+}
+
+TEST(DeviceManager, UntrackingRemovesDeviceProperties) {
+  MockDispatchTable dispatch_table;
+  DeviceManager manager(&dispatch_table);
+  VkDevice logical_device = {};
+  VkPhysicalDevice physical_device = {};
+
+  EXPECT_CALL(dispatch_table, GetPhysicalDeviceProperties)
+      .WillOnce(Return(&MockGetPhysicalDeviceProperties));
+
+  manager.TrackLogicalDevice(physical_device, logical_device);
+
+  manager.UntrackLogicalDevice(logical_device);
+  EXPECT_DEATH({ (void)manager.GetPhysicalDeviceProperties(physical_device); }, "");
+}
+
+}  // namespace orbit_vulkan_layer
