3a1f7243ad2c10f73cefe83b323fec4ab54f0f41
Henning Becker <henning.becker@gmail.com>
2020-11-23 12:59:03 +0100

    Add tests for orbit_qt::EventLoop

---
+++ OrbitQt/EventLoopTest.cpp
@@ -0,0 +1,74 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <QApplication>
+#include <QMetaObject>
+#include <system_error>
+
+#include "EventLoop.h"
+#include "gtest/gtest.h"
+
+TEST(EventLoop, exec) {
+  int argc = 0;
+
+  QApplication app{argc, nullptr};
+
+  // Case 1: The event loop finishes successfully
+  {
+    orbit_qt::EventLoop loop{};
+    ASSERT_FALSE(loop.isRunning());
+
+    QMetaObject::invokeMethod(
+        &loop,
+        [&]() {
+          ASSERT_TRUE(loop.isRunning());
+          loop.quit();
+        },
+        Qt::QueuedConnection);
+    {
+      const auto result = loop.exec();
+      ASSERT_FALSE(result.has_error());
+    }
+  }
+
+  // Case 2: The event loop returns an error that occured
+  // while processing events/tasks.
+  {
+    orbit_qt::EventLoop loop{};
+    ASSERT_FALSE(loop.isRunning());
+
+    QMetaObject::invokeMethod(
+        &loop,
+        [&]() {
+          ASSERT_TRUE(loop.isRunning());
+          loop.error(std::make_error_code(std::errc::bad_message));
+        },
+        Qt::QueuedConnection);
+    {
+      const auto result = loop.exec();
+      ASSERT_TRUE(result.has_error());
+    }
+  }
+
+  // Case 3: The event loop immediately returns due to a queued error.
+  {
+    orbit_qt::EventLoop loop{};
+    ASSERT_FALSE(loop.isRunning());
+    loop.error(std::make_error_code(std::errc::bad_message));
+
+    QMetaObject::invokeMethod(
+        &loop,
+        []() {
+          FAIL();  // This task will be queued but never executes since the event loop is supposed
+                   // to return early.
+        },
+        Qt::QueuedConnection);
+
+    {
+      const auto result = loop.exec();
+      ASSERT_TRUE(result.has_error());
+      ASSERT_EQ(result.error(), std::errc::bad_message);
+    }
+  }
+}
