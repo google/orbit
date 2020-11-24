7996a7c81102119ca89bdf45a7a80ad5886184e1
antonrohr <antonrohr@google.com>
2020-11-24 13:45:17 +0100

    Add OverlayWidget class
    This class is an overlay above any other large enough widget
    (e.g. a table). The overlay provides an animation to signal to the user
    that something is happening an the program is not frozen. It also provides
    a status message field and a button to cancel. Whoever uses the
    OverlayWidget has to make sure to react to the "Cancelled" signal, and
    shutdown the overlay afterwards (either setVisible(false) or remove all
    together)

---
+++ OrbitQt/OverlayWidget.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "OverlayWidget.h"
+
+namespace {
+
+// This color is used as a "background" for the overlay. The alpha value of 128 makes it
+// transparent
+const QColor kOverlayShadeColor{100, 100, 100, 128};
+
+}  // namespace
+
+namespace orbit_qt {
+
+OverlayWidget::OverlayWidget(QWidget* parent)
+    : QWidget(parent), ui_(std::make_unique<Ui::OverlayWidget>()) {
+  CHECK(parent != nullptr);
+  parent->installEventFilter(this);
+  ui_->setupUi(this);
+  ui_->cancelButton->setEnabled(true);
+
+  QObject::connect(ui_->cancelButton, &QPushButton::clicked, this, [this] { emit Cancelled(); });
+}
+
+void OverlayWidget::paintEvent(QPaintEvent* /*event*/) {
+  QPainter(this).fillRect(rect(), kOverlayShadeColor);
+}
+
+bool OverlayWidget::eventFilter(QObject* obj, QEvent* event) {
+  if (obj == parent() && event->type() == QEvent::Resize) {
+    resize(parentWidget()->size());
+  }
+  return false;
+}
+
+}  // namespace orbit_qt
