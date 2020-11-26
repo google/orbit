a7b8506dafd2258ae7ac3e413e927ed3caa28713
antonrohr <antonrohr@google.com>
2020-11-26 11:55:49 +0100

    Change namespace for ProcessItemModel
    It changes from OrbitQt to orbit_qt to follow the styleguide.
    Adds nodiscard

--- OrbitQt/ProcessItemModel.h
+++ OrbitQt/ProcessItemModel.h
@@ -6,34 +6,40 @@
 #define ORBIT_QT_PROCESS_ITEM_MODEL_H_
 
 #include <QAbstractItemModel>
+#include <QModelIndex>
+#include <QObject>
+#include <QString>
 #include <QVariant>
+#include <QtCore>
+#include <vector>
 
 #include "process.pb.h"
 
-namespace OrbitQt {
+namespace orbit_qt {
 
 class ProcessItemModel : public QAbstractItemModel {
   Q_OBJECT
  public:
   enum class Column { kName, kPid, kCpu, kEnd };
 
-  int columnCount(const QModelIndex& parent = {}) const override;
-  QVariant data(const QModelIndex& idx, int role = Qt::DisplayRole) const override;
-  QVariant headerData(int section, Qt::Orientation orientation,
-                      int role = Qt::DisplayRole) const override;
-  QModelIndex index(int row, int column, const QModelIndex& parent = {}) const override;
-  QModelIndex parent(const QModelIndex& parent) const override;
-  int rowCount(const QModelIndex& parent = {}) const override;
+  [[nodiscard]] int columnCount(const QModelIndex& parent = {}) const override;
+  [[nodiscard]] QVariant data(const QModelIndex& idx, int role = Qt::DisplayRole) const override;
+  [[nodiscard]] QVariant headerData(int section, Qt::Orientation orientation,
+                                    int role = Qt::DisplayRole) const override;
+  [[nodiscard]] QModelIndex index(int row, int column,
+                                  const QModelIndex& parent = {}) const override;
+  [[nodiscard]] QModelIndex parent(const QModelIndex& parent) const override;
+  [[nodiscard]] int rowCount(const QModelIndex& parent = {}) const override;
 
   void SetProcesses(std::vector<orbit_grpc_protos::ProcessInfo> processes);
-  bool HasProcesses() const { return !processes_.empty(); }
+  [[nodiscard]] bool HasProcesses() const { return !processes_.empty(); }
   void Clear() { SetProcesses({}); }
 
  private:
   std::vector<orbit_grpc_protos::ProcessInfo> processes_;
 };
 
-}  // namespace OrbitQt
+}  // namespace orbit_qt
 
 Q_DECLARE_METATYPE(const orbit_grpc_protos::ProcessInfo*);
 
