ad5b8fa2f4334aa57e538057d0973df20584e614
Davide Pallotti <dpallotti@google.com>
2020-11-26 15:55:15 +0100

    Add ProducerSideServer
    gRPC Server that registers `ProducerSideServiceImpl` for the communication
    between producers and OrbitService.
    
    Bug: http://b/174028631

---
+++ OrbitService/ProducerSideServer.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_SERVICE_TARGET_SIDE_SERVER_H_
+#define ORBIT_SERVICE_TARGET_SIDE_SERVER_H_
+
+#include "CaptureStartStopListener.h"
+#include "OrbitBase/Logging.h"
+#include "ProducerSideServiceImpl.h"
+#include "grpcpp/grpcpp.h"
+
+namespace orbit_service {
+
+// Wrapper around a grpc::Server that registers the service ProducerSideServiceImpl
+// and listens on a Unix domain socket.
+class ProducerSideServer final : public CaptureStartStopListener {
+ public:
+  bool BuildAndStart(std::string_view unix_domain_socket_path);
+  void ShutdownAndWait();
+
+  void OnCaptureStartRequested(CaptureEventBuffer* capture_event_buffer) override;
+  void OnCaptureStopRequested() override;
+
+ private:
+  ProducerSideServiceImpl producer_side_service_;
+  std::unique_ptr<grpc::Server> server_;
+};
+
+}  // namespace orbit_service
+
+#endif  // ORBIT_SERVICE_TARGET_SIDE_SERVER_H_
