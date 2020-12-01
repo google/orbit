676b0d4a42e6e371549621aa7adeb51d866fd6ff
Henning Becker <henning.becker@gmail.com>
2020-12-01 16:09:01 +0100

    Remove unused files from repo root
    This removes the Windows batch script which have been deprecated and
    converted to powershell a long time ago.
    
    Furthermore this remove the windows cmake readme which is outdated.
    
    I believe `pack.bat` is also not in use anymore.

--- bootstrap-orbit-ggp.bat
+++
@@ -1,6 +0,0 @@
-REM Copyright (c) 2020 The Orbit Authors. All rights reserved.
-REM Use of this source code is governed by a BSD-style license that can be
-REM found in the LICENSE file.
-
-@echo off
-call powershell "& %~dp0\bootstrap-orbit-ggp.ps1 %*"
--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-call powershell "& %~dp0\build.ps1 %*"
-
-echo[
-echo -------------------------------------------------------------------
-echo - By the way: "build.bat" is deprecated and will be removed soon. -
-echo - Please use "build.ps1" - the PowerShell equivalent - instead.   -
-echo -------------------------------------------------------------------
