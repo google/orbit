fc08f11eefb4074bff2b1fe7821ee2bc81a77243
Davide Pallotti <dpallotti@google.com>
2020-12-03 11:44:59 +0100

    Move PerfEventQueue to its own files
    This is just for better clarity. For example, the test file now has the name of
    the class that is actually tested (`PerfEventQueueTest.cpp` vs
    `PerfEventProcessorTest.cpp`).

--- OrbitLinuxTracing/PerfEventProcessor.cpp
+++ OrbitLinuxTracing/PerfEventProcessor.cpp
@@ -7,53 +7,11 @@
 #include <OrbitBase/Logging.h>
 
 #include <memory>
-#include <queue>
 
 #include "LinuxTracingUtils.h"
 #include "PerfEvent.h"
 
 namespace LinuxTracing {
-
-void PerfEventQueue::PushEvent(int origin_fd, std::unique_ptr<PerfEvent> event) {
-  if (fd_event_queues_.count(origin_fd) > 0) {
-    std::shared_ptr<std::queue<std::unique_ptr<PerfEvent>>> event_queue =
-        fd_event_queues_.at(origin_fd);
-    CHECK(!event_queue->empty());
-    // Fundamental assumption: events from the same file descriptor come already
-    // in order.
-    CHECK(event->GetTimestamp() >= event_queue->front()->GetTimestamp());
-    event_queue->push(std::move(event));
-  } else {
-    auto event_queue = std::make_shared<std::queue<std::unique_ptr<PerfEvent>>>();
-    fd_event_queues_.insert(std::make_pair(origin_fd, event_queue));
-    event_queue->push(std::move(event));
-    event_queues_queue_.push(std::make_pair(origin_fd, event_queue));
-  }
-}
-
-bool PerfEventQueue::HasEvent() { return !event_queues_queue_.empty(); }
-
-PerfEvent* PerfEventQueue::TopEvent() { return event_queues_queue_.top().second->front().get(); }
-
-std::unique_ptr<PerfEvent> PerfEventQueue::PopEvent() {
-  std::pair<int, std::shared_ptr<std::queue<std::unique_ptr<PerfEvent>>>> top_fd_queue =
-      event_queues_queue_.top();
-  event_queues_queue_.pop();
-  const int& top_fd = top_fd_queue.first;
-  std::shared_ptr<std::queue<std::unique_ptr<PerfEvent>>>& top_queue = top_fd_queue.second;
-
-  std::unique_ptr<PerfEvent> top_event = std::move(top_queue->front());
-  top_queue->pop();
-  if (top_queue->empty()) {
-    fd_event_queues_.erase(top_fd);
-  } else {
-    // Remove and re-insert so that the queue is in the right position in the
-    // heap after the front of the queue has been removed.
-    event_queues_queue_.push(top_fd_queue);
-  }
-
-  return top_event;
-}
 
 void PerfEventProcessor::AddEvent(int origin_fd, std::unique_ptr<PerfEvent> event) {
   if (last_processed_timestamp_ns_ > 0 && event->GetTimestamp() < last_processed_timestamp_ns_) {
--- OrbitLinuxTracing/PerfEventProcessor.h
+++ OrbitLinuxTracing/PerfEventProcessor.h
@@ -7,58 +7,12 @@
 
 #include <ctime>
 #include <memory>
-#include <queue>
 
 #include "PerfEvent.h"
+#include "PerfEventQueue.h"
 #include "PerfEventVisitor.h"
-#include "absl/container/flat_hash_map.h"
 
 namespace LinuxTracing {
-
-// This class implements a data structure that holds a large number of different
-// perf_event_open records coming from multiple ring buffers, and allows reading
-// them in order (oldest first).
-// Instead of keeping a single priority queue with all the events to process,
-// on which push/pop operations would be logarithmic in the number of events,
-// we leverage the fact that events coming from the same perf_event_open ring
-// buffer are already sorted. We then keep a priority queue of queues, where
-// the events in each queue come from the same ring buffer. Whenever an event
-// is removed from a queue, we need to move such queue down the priority
-// queue. As std::priority_queue does not support decreasing the priority of
-// an element, we achieve this by removing and re-inserting.
-// In order to be able to add an event to a queue, we also need to maintain
-// the association between a queue and its ring buffer. We use the file
-// descriptor used to read from the ring buffer as identifier for a ring
-// buffer. Keeping this association is what the pairs and the map are for.
-// TODO: Implement a custom priority queue that supports decreasing the
-//  priority.
-class PerfEventQueue {
- public:
-  void PushEvent(int origin_fd, std::unique_ptr<PerfEvent> event);
-  bool HasEvent();
-  PerfEvent* TopEvent();
-  std::unique_ptr<PerfEvent> PopEvent();
-
- private:
-  // Comparator for the priority queue: pop will return the queue associated
-  // with the file descriptor from which the oldest event still to process
-  // originated.
-  struct QueueFrontTimestampReverseCompare {
-    bool operator()(
-        const std::pair<int, std::shared_ptr<std::queue<std::unique_ptr<PerfEvent>>>>& lhs,
-        const std::pair<int, std::shared_ptr<std::queue<std::unique_ptr<PerfEvent>>>>& rhs) {
-      return lhs.second->front()->GetTimestamp() > rhs.second->front()->GetTimestamp();
-    }
-  };
-
-  std::priority_queue<
-      std::pair<int, std::shared_ptr<std::queue<std::unique_ptr<PerfEvent>>>>,
-      std::vector<std::pair<int, std::shared_ptr<std::queue<std::unique_ptr<PerfEvent>>>>>,
-      QueueFrontTimestampReverseCompare>
-      event_queues_queue_{};
-  absl::flat_hash_map<int, std::shared_ptr<std::queue<std::unique_ptr<PerfEvent>>>>
-      fd_event_queues_{};
-};
 
 // This class receives perf_event_open events coming from several ring buffers
 // and processes them in order according to their timestamps.
