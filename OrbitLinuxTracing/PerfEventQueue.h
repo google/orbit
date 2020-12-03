b266982d4aff67145bc34c0ecbafdb47a824dddf
Davide Pallotti <dpallotti@google.com>
2020-12-03 17:13:56 +0100

    Optimize PerfEventQueue with a custom implementation of a heap
    This implementation supports changing the top element without having to remove
    it and re-insert it.
    
    From various measurements on Trata, this is another ~15% improvement on the
    previous implementation.
    
    Test: Extensive testing profiling Trata (while measuring performance of the
    implementation).

--- OrbitLinuxTracing/PerfEventQueue.h
+++ OrbitLinuxTracing/PerfEventQueue.h
@@ -5,6 +5,7 @@
 #ifndef ORBIT_LINUX_TRACING_PERF_EVENT_QUEUE_H_
 #define ORBIT_LINUX_TRACING_PERF_EVENT_QUEUE_H_
 
+#include <memory>
 #include <queue>
 
 #include "PerfEvent.h"
@@ -12,23 +13,18 @@
 
 namespace LinuxTracing {
 
-// This class implements a data structure that holds a large number of different
-// perf_event_open records coming from multiple ring buffers, and allows reading
-// them in order (oldest first).
-// Instead of keeping a single priority queue with all the events to process,
-// on which push/pop operations would be logarithmic in the number of events,
-// we leverage the fact that events coming from the same perf_event_open ring
-// buffer are already sorted. We then keep a priority queue of queues, where
-// the events in each queue come from the same ring buffer. Whenever an event
-// is removed from a queue, we need to move such queue down the priority
-// queue. As std::priority_queue does not support decreasing the priority of
-// an element, we achieve this by removing and re-inserting.
-// In order to be able to add an event to a queue, we also need to maintain
-// the association between a queue and its ring buffer. We use the file
-// descriptor used to read from the ring buffer as identifier for a ring
-// buffer. Keeping this association is what the pairs and the map are for.
-// TODO: Implement a custom priority queue that supports decreasing the
-//  priority.
+// This class implements a data structure that holds a large number of different perf_event_open
+// records coming from multiple ring buffers, and allows reading them in order (oldest first).
+//
+// Instead of keeping a single priority queue with all the events to process, on which push/pop
+// operations would be logarithmic in the number of events, we leverage the fact that events coming
+// from the same perf_event_open ring buffer are already sorted. We then keep a priority queue of
+// queues, where the events in each queue come from the same ring buffer. Whenever an event is
+// removed from a queue, we need to move such queue down the priority queue.
+//
+// In order to be able to add an event to a queue, we also need to maintain the association between
+// a queue and its ring buffer, which is what the map is for. We use the file descriptor used to
+// read from the ring buffer as identifier for a ring buffer.
 class PerfEventQueue {
  public:
   void PushEvent(std::unique_ptr<PerfEvent> event);
@@ -37,20 +33,15 @@ class PerfEventQueue {
   std::unique_ptr<PerfEvent> PopEvent();
 
  private:
-  // Comparator for the priority queue: pop will return the queue associated with
-  // the file descriptor from which the oldest event still to process originated.
-  struct QueueFrontTimestampReverseCompare {
-    bool operator()(std::queue<std::unique_ptr<PerfEvent>>* lhs,
-                    std::queue<std::unique_ptr<PerfEvent>>* rhs) {
-      return lhs->front()->GetTimestamp() > rhs->front()->GetTimestamp();
-    }
-  };
+  // Floats down the element at the top of the heap to its correct place. Used when the key
+  // of the top element changes, or as part of the process of removing the top element.
+  void MoveDownHeapFront();
+  // Floats up an element that it is know should be further up in the heap. Used on insertion.
+  void MoveUpHeapBack();
 
-  std::priority_queue<std::queue<std::unique_ptr<PerfEvent>>*,
-                      std::vector<std::queue<std::unique_ptr<PerfEvent>>*>,
-                      QueueFrontTimestampReverseCompare>
-      queues_heap_{};
-  absl::flat_hash_map<int, std::unique_ptr<std::queue<std::unique_ptr<PerfEvent>>>> queues_{};
+ private:
+  absl::flat_hash_map<int, std::unique_ptr<std::queue<std::unique_ptr<PerfEvent>>>> queues_;
+  std::vector<std::queue<std::unique_ptr<PerfEvent>>*> queues_heap_;
 };
 
 }  // namespace LinuxTracing
