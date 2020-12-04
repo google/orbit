99a3976e1dc2bec51b3406d9096b45921e2d3e91
Florian <kuebler@google.com>
2020-12-04 16:03:02 +0000

    Fix include

--- OrbitBase/include/OrbitBase/ThreadConstants.h
+++ OrbitBase/include/OrbitBase/ThreadConstants.h
@@ -4,6 +4,8 @@
 #ifndef ORBIT_BASE_THREAD_CONSTANTS_H
 #define ORBIT_BASE_THREAD_CONSTANTS_H
 
+#include <cstdint>
+
 namespace orbit_base {
 
 // Represents a fake thread id to specify the set of all thread ids of the current process.
