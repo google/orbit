a088e543b4f44365682ff9daac81e21ab5419653
Davide Pallotti <dpallotti@google.com>
2020-12-14 16:01:15 +0100

    Minor cleanup of GpuTracepointEventProcessor

--- OrbitLinuxTracing/GpuTracepointEventProcessor.cpp
+++ OrbitLinuxTracing/GpuTracepointEventProcessor.cpp
@@ -22,28 +22,27 @@ using orbit_grpc_protos::GpuJob;
 int GpuTracepointEventProcessor::ComputeDepthForEvent(const std::string& timeline,
                                                       uint64_t start_timestamp,
                                                       uint64_t end_timestamp) {
-  if (timeline_to_latest_timestamp_per_depth_.count(timeline) == 0) {
-    std::vector<uint64_t> vec;
-    timeline_to_latest_timestamp_per_depth_.emplace(timeline, vec);
+  if (!timeline_to_latest_timestamp_per_depth_.contains(timeline)) {
+    timeline_to_latest_timestamp_per_depth_.emplace(timeline, std::vector<uint64_t>{});
   }
   auto it = timeline_to_latest_timestamp_per_depth_.find(timeline);
-  std::vector<uint64_t>& vec = it->second;
+  std::vector<uint64_t>& latest_timestamps_per_depth = it->second;
 
-  for (size_t d = 0; d < vec.size(); ++d) {
+  for (size_t depth = 0; depth < latest_timestamps_per_depth.size(); ++depth) {
     // We add a small amount of slack on each row of the GPU track timeline to
     // make sure events don't get too crowded.
-    constexpr uint64_t slack_ns = 1 * 1000000;
-    if (start_timestamp >= (vec[d] + slack_ns)) {
-      vec[d] = end_timestamp;
-      return d;
+    static constexpr uint64_t kSlackNs = 1'000'000;
+    if (start_timestamp >= (latest_timestamps_per_depth[depth] + kSlackNs)) {
+      latest_timestamps_per_depth[depth] = end_timestamp;
+      return depth;
     }
   }
 
   // Note that this vector only grows in size until a certain maximum depth is
   // reached. Since there are only O(10) events per frame created, the depth
   // is not likely to grow to a very large size.
-  vec.push_back(end_timestamp);
-  return static_cast<int>(vec.size() - 1);
+  latest_timestamps_per_depth.push_back(end_timestamp);
+  return static_cast<int>(latest_timestamps_per_depth.size() - 1);
 }
 
 void GpuTracepointEventProcessor::CreateGpuExecutionEventIfComplete(const Key& key) {
@@ -69,10 +68,10 @@ void GpuTracepointEventProcessor::CreateGpuExecutionEventIfComplete(const Key& k
   // timeline_to_latest_dma_signal_. If a previous job is still running
   // at the timestamp of scheduling the current job, we push the start
   // time for starting on the hardware back.
-  if (timeline_to_latest_dma_signal_.count(timeline) == 0) {
+  if (!timeline_to_latest_dma_signal_.contains(timeline)) {
     timeline_to_latest_dma_signal_.emplace(timeline, dma_it->second.timestamp_ns);
   }
-  auto it = timeline_to_latest_dma_signal_.find(timeline);
+  auto latest_dma_it = timeline_to_latest_dma_signal_.find(timeline);
   // We do not have an explicit event for the following timestamp. We
   // assume that, when the GPU queue corresponding to timeline is
   // not executing a job, that this job starts exactly when it is
@@ -80,8 +79,8 @@ void GpuTracepointEventProcessor::CreateGpuExecutionEventIfComplete(const Key& k
   // when the previous job has signaled that it is done. Since we do
   // not have an explicit signal here, this is the best we can do.
   uint64_t hw_start_time = sched_it->second.timestamp_ns;
-  if (hw_start_time < it->second) {
-    hw_start_time = it->second;
+  if (hw_start_time < latest_dma_it->second) {
+    hw_start_time = latest_dma_it->second;
   }
 
   int depth =
@@ -97,11 +96,12 @@ void GpuTracepointEventProcessor::CreateGpuExecutionEventIfComplete(const Key& k
   gpu_job.set_gpu_hardware_start_time_ns(hw_start_time);
   gpu_job.set_dma_fence_signaled_time_ns(dma_it->second.timestamp_ns);
 
+  CHECK(listener_ != nullptr);
   listener_->OnGpuJob(std::move(gpu_job));
 
   // We need to update the timestamp when the last GPU job so far seen
   // finishes on this timeline.
-  it->second = std::max(it->second, dma_it->second.timestamp_ns);
+  latest_dma_it->second = std::max(latest_dma_it->second, dma_it->second.timestamp_ns);
 
   amdgpu_cs_ioctl_events_.erase(key);
   amdgpu_sched_run_job_events_.erase(key);
@@ -111,12 +111,12 @@ void GpuTracepointEventProcessor::CreateGpuExecutionEventIfComplete(const Key& k
 // The following three overloaded PushEvent methods handle the three different
 // types of events that we can get from the GPU driver tracepoints we are
 // tracing.
-// We allow for the possibility that these events arrive out-of-order
-// (which is something we have actually observed) with the following approach:
-// We record all three types of events in different maps. Whenever a new event
-// arrives, we add it to the corresponding map and then try to create a complete
-// GPU execution event. This event is only created when all three types of GPU
-// events have been received.
+// We allow for the possibility that these events arrive out-of-order (depending
+// on the order in which events are read from perf_event_open ring buffers) with
+// the following approach: We record all three types of events in different
+// maps. Whenever a new event arrives, we add it to the corresponding map and
+// then try to create a complete GPU execution event. This event is only created
+// when all three types of GPU events have been received.
 
 void GpuTracepointEventProcessor::PushEvent(const AmdgpuCsIoctlPerfEvent& sample) {
   AmdgpuCsIoctlEvent event{sample.GetTid(), sample.GetTimestamp(), sample.GetContext(),
