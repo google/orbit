51943e71fc3e44b014ea92a6d0fa5a4135c12549
Florian Reichl <freichl@google.com>
2020-12-17 09:12:47 +0100

    Move Gl Accessibility into its own module
    OrbitAccessibility now contains all functionality required for
    accessibility in OrbitGl.

---
+++ OrbitAccessibility/include/OrbitAccessibility/AccessibleInterfaceRegistry.h
@@ -0,0 +1,45 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_ACCESSIBILITY_ACCESSIBLE_INTERFACE_REGISTRY_H_
+#define ORBIT_ACCESSIBILITY_ACCESSIBLE_INTERFACE_REGISTRY_H_
+
+#include <functional>
+
+#include "OrbitAccessibility/AccessibleInterface.h"
+
+namespace orbit_accessibility {
+
+/* Singleton class, keeps track of created and destroyed AccessibleInterface instances.
+ * This is required to respond to the destruction of interfaces outside of OrbitGl.
+ *
+ * orbit_qt::AccessibilityAdapter will register itself for the "SetOnUnregisterCallback" callback.
+ * See OrbitQt/AccessibilityAdapter.h for more documentation.
+ */
+class AccessibleInterfaceRegistry {
+ public:
+  using Callback = std::function<void(AccessibleInterface*)>;
+
+  void Register(AccessibleInterface* iface);
+  void Unregister(AccessibleInterface* iface);
+
+  void SetOnRegisterCallback(Callback callback);
+  void SetOnUnregisterCallback(Callback callback);
+
+  [[nodiscard]] bool Exists(AccessibleInterface* iface) { return interfaces_.contains(iface); }
+
+  static AccessibleInterfaceRegistry& Get();
+
+ private:
+  AccessibleInterfaceRegistry(){};
+  ~AccessibleInterfaceRegistry();
+
+  absl::flat_hash_set<AccessibleInterface*> interfaces_;
+  Callback on_registered_ = nullptr;
+  Callback on_unregistered_ = nullptr;
+};
+
+}  // namespace orbit_accessibility
+
+#endif
