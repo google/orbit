d19dfa74caf20a2891a2323915e6a0e20b36a7d3
Florian Reichl <freichl@google.com>
2020-12-22 17:03:54 +0100

    Track tests, renamed capture_window.py to tracks.py

--- contrib/automation_tests/orbit_tracks.py
+++ contrib/automation_tests/orbit_tracks.py
@@ -7,13 +7,24 @@ found in the LICENSE file.
 from absl import app
 
 from core.orbit_e2e import E2ETest
-from fragments.capture_window import SelectTrack, MoveTrack
+from fragments.tracks import SelectTrack, DeselectTrack, MoveTrack, FilterTracks, MatchTracks
 
 
 def main(argv):
-    fragments = [SelectTrack(-1),
-                 SelectTrack(0, expect_failure=True),   # Scheduler track cannot be selected
-                 MoveTrack(-1, 0, 0)]
+    fragments = [MatchTracks(expected_count=9,
+                             expected_names=["Scheduler", "gfx", "sdma0",
+                                             "hello_ggp_stand", "hello_ggp_stand", "GgpSwapchain",
+                                             "GgpVideoIpcRead", "GgpVideoIpcRead", "GgpVideoIpc"]),
+                 SelectTrack(track_index=-1),
+                 DeselectTrack(),
+                 SelectTrack(track_index=0, expect_failure=True),   # Scheduler track cannot be selected
+                 MoveTrack(track_index=-1, new_index=0),
+                 MoveTrack(track_index=0, new_index=3),
+                 MoveTrack(track_index=3, new_index=-1),
+                 FilterTracks(filter_string="hello", expected_count=2),
+                 FilterTracks(filter_string="Hello", expected_count=2),
+                 FilterTracks(filter_string="ggp", expected_count=6),
+                 FilterTracks(filter_string="", expected_count=9)]
     test = E2ETest(test_name="Track Interaction", fragments=fragments)
     test.execute()
 
