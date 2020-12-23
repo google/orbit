8412299a88ffbbf495593c2be4c2f22ad4cd704a
Florian Reichl <freichl@google.com>
2020-12-23 11:32:03 +0100

    Fixup: Remove additional include in GlCanvas.cpp
    (cherry picked from commit 8bdace692f54cbff8fac9d0c2fc2606b3718f74c)

--- OrbitGl/GlCanvas.cpp
+++ OrbitGl/GlCanvas.cpp
@@ -1,12 +1,12 @@
-#include "GlCanvas.h"
 // Copyright (c) 2020 The Orbit Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "GlCanvas.h"
+
 #include <string>
 
 #include "App.h"
-#include "GlCanvas.h"
 #include "GlUtils.h"
 #include "ImGuiOrbit.h"
 #include "IntrospectionWindow.h"
