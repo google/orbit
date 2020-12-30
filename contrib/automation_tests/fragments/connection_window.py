f4742f6764d55e928a2288dd4f9ac154d814ebe7
Florian Reichl <freichl@google.com>
2020-12-30 17:43:49 +0100

    Update move_tabs test to work with the new UI
    Start to group the functionality in Fragments according to larger UI
    elements. MoveTabs has been moved to main_window.py, which will
    contain all "global" functionality for Orbits main window.
    
    Added connection_window.py with all functionality for the new
    connection window. The functionality to select processes from
    connection_window.py will also work with the old UI.

---
+++ contrib/automation_tests/fragments/connection_window.py
@@ -0,0 +1,86 @@
+"""
+Copyright (c) 2020 The Orbit Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+"""
+
+import logging
+import time
+
+from absl import flags
+from pywinauto.application import Application
+
+from core.orbit_e2e import E2ETestCase, wait_for_condition, find_control
+
+
+flags.DEFINE_bool('enable_ui_beta', False, 'Expect Orbit to be started with the new UI')
+
+
+def wait_for_main_window(application: Application):
+    wait_for_condition(lambda: application.top_window().class_name() == "OrbitMainWindow", 30)
+
+
+class ConnectToStadiaInstance(E2ETestCase):
+    """
+    Connect to the first available stadia instance
+    """
+    def _execute(self):
+        window = self.suite.top_window()
+
+        logging.info('Start connecting to gamelet.')
+        if flags.FLAGS.enable_ui_beta:
+            connect_radio = self.find_control('RadioButton', 'ConnectToStadia')
+            connect_radio.click_input()
+
+        # Wait for the first data item in the instance list to exist
+        # We're not using find_control here because magic lookup enables us to easily wait for the existence of a row
+        window.InstanceList.click_input()
+        window.InstanceList.DataItem0.wait('exists', timeout=100)
+        instance_list = self.find_control('Table', 'InstanceList')
+        logging.info('Found %s rows in the instance list', instance_list.item_count())
+        self.expect_true(instance_list.item_count() >= 1, 'Found at least one instance')
+
+        window.InstanceList.DataItem0.double_click_input()
+        logging.info('Connecting to Instance, waiting for the process list...')
+
+        if flags.FLAGS.enable_ui_beta:
+            # In the new UI, use small waits until the process list is active, and then some more for the
+            # semi-transparent "loading" Overlay of the tables to disappear...
+            wait_for_condition(lambda: self.find_control('Custom', 'ProcessesFrame').is_enabled() is True, 15)
+            wait_for_condition(lambda: self.find_control('Table', 'ProcessList').is_active(), 10)
+            # TODO(b/177037834): Get rid of this sleep and correctly wait for the overlay to be hidden
+            time.sleep(2)
+        else:
+            wait_for_main_window(self.suite.application)
+            window = self.suite.top_window(True)
+            self.expect_eq(window.class_name(), "OrbitMainWindow", 'Main window is visible')
+        logging.info('Process list ready')
+
+
+class FilterAndSelectFirstProcess(E2ETestCase):
+    """
+    Select the first process in the process list and verify there is at least one entry in the list
+    """
+    def _execute(self, process_filter):
+        window = self.suite.top_window()
+        logging.info('Setting filter text for process list')
+
+        if flags.FLAGS.enable_ui_beta:
+            filter_edit = self.find_control('Edit', 'FilterProcesses')
+            process_list = window.ProcessList
+        else:
+            filter_edit = self.find_control('Edit', 'Filter', parent=window.DataViewProcesses)
+            process_list = window.DataViewProcesses.DataView
+
+        if process_filter:
+            filter_edit.set_edit_text(process_filter)
+        self.expect_true(process_list.item_count() > 0, 'Process list has at least one entry')
+
+        if flags.FLAGS.enable_ui_beta:
+            logging.info('Process selected, continuing to main window...')
+            process_list.DataItem0.double_click_input()
+            wait_for_main_window(self.suite.application)
+            window = self.suite.top_window(True)
+            self.expect_eq(window.class_name(), "OrbitMainWindow", 'Main window is visible')
+        else:
+            process_list.DataItem0.click_input()
--- contrib/automation_tests/orbit_move_tabs.py
+++ contrib/automation_tests/orbit_move_tabs.py
@@ -4,14 +4,20 @@ Use of this source code is governed by a BSD-style license that can be
 found in the LICENSE file.
 """
 
-from absl import app
+from absl import app, flags
 
 from core.orbit_e2e import E2ETestSuite
-from fragments.move_tabs import MoveFunctionsTab
+from fragments.connection_window import ConnectToStadiaInstance, FilterAndSelectFirstProcess
+from fragments.main_window import MoveTab
 
 
 def main(argv):
-    tests = [MoveFunctionsTab()]
+    tab_title = "Symbols" if flags.FLAGS.enable_ui_beta else "Functions"
+    tests = [
+        ConnectToStadiaInstance(),
+        FilterAndSelectFirstProcess(process_filter="hello_ggp"),
+        MoveTab(tab_title=tab_title, tab_name="FunctionsTab")
+    ]
     suite = E2ETestSuite(test_name="Move tabs", tests=tests)
     suite.execute()
 
