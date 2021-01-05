2c940395a6e7cbf1ac07911f049407307ba6af6d
Davide Pallotti <dpallotti@google.com>
2021-01-05 17:06:59 +0100

    Add CaptureOptionsDialog
    For now it only allows to enable or disable the collection of thread states.
    The choice is persisted using `QSettings`.
    The dialog is opened with the new "Capture Options" button in the capture view's
    toolbar.
    
    Bug: http://b/176791430

---
+++ OrbitQt/CaptureOptionsDialog.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "CaptureOptionsDialog.h"
+
+#include <QDialog>
+#include <QSettings>
+#include <QWidget>
+
+#include "ui_CaptureOptionsDialog.h"
+
+namespace orbit_qt {
+
+CaptureOptionsDialog::CaptureOptionsDialog(QWidget* parent)
+    : QDialog{parent}, ui_(std::make_unique<Ui::CaptureOptionsDialog>()) {
+  ui_->setupUi(this);
+
+  QObject::connect(ui_->buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+  QObject::connect(ui_->buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
+}
+
+bool CaptureOptionsDialog::GetCollectThreadStates() const {
+  return ui_->threadStateCheckBox->isChecked();
+}
+
+void CaptureOptionsDialog::SetCollectThreadStates(bool collect_thread_state) {
+  ui_->threadStateCheckBox->setChecked(collect_thread_state);
+}
+
+}  // namespace orbit_qt
