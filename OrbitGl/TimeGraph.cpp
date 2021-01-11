4dd2cdd81c10f08355fee9c8b0f39718363b6e66
Florian <kuebler@google.com>
2021-01-11 10:08:24 +0000

    review comments

--- OrbitGl/TimeGraph.cpp
+++ OrbitGl/TimeGraph.cpp
@@ -252,18 +252,9 @@ void TimeGraph::ProcessTimer(const TimerInfo& timer_info, const FunctionInfo* fu
   // TODO (b/175869409): Change the way to create and get the tracks. Move this part to
   // TrackManager.
   switch (timer_info.type()) {
-    case TimerInfo::kGpuActivity: {
-      uint64_t timeline_hash = timer_info.timeline_hash();
-      GpuTrack* track = track_manager_->GetOrCreateGpuTrack(timeline_hash);
-      track->OnTimer(timer_info);
-      break;
-    }
-    case TimerInfo::kGpuCommandBuffer: {
-      uint64_t timeline_hash = timer_info.timeline_hash();
-      GpuTrack* track = track_manager_->GetOrCreateGpuTrack(timeline_hash);
-      track->OnTimer(timer_info);
-      break;
-    }
+    // All GPU timers are handled equally here.
+    case TimerInfo::kGpuActivity:
+    case TimerInfo::kGpuCommandBuffer:
     case TimerInfo::kGpuDebugMarker: {
       uint64_t timeline_hash = timer_info.timeline_hash();
       GpuTrack* track = track_manager_->GetOrCreateGpuTrack(timeline_hash);
