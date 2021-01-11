18c82a37363daa41e43bd1ce6389cdfcb32f8b81
Davide Pallotti <dpallotti@google.com>
2021-01-11 16:07:02 +0100

    Add smoke test for thread state collection
    Only a smoke test until more advanced facilities to inspect the capture view
    are introduced.
    
    Bug: http://b/176960292
    
    Test: Run the test locally.

---
+++ contrib/automation_tests/orbit_thread_state.py
@@ -0,0 +1,31 @@
+"""
+Copyright (c) 2020 The Orbit Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+"""
+
+from absl import app
+
+from core.orbit_e2e import E2ETestSuite
+from test_cases.connection_window import FilterAndSelectFirstProcess, ConnectToStadiaInstance
+from test_cases.capture_window import Capture
+
+"""Smoke test for thread state collection.
+
+This automated test takes a capture on "hello_ggp_standalone" with thread state
+collection enabled and verifies the presence of at least one track in the
+capture window.
+"""
+
+def main(argv):
+    test_cases = [
+        ConnectToStadiaInstance(),
+        FilterAndSelectFirstProcess(process_filter='hello_'),
+        Capture(collect_thread_states=True)
+    ]
+    suite = E2ETestSuite(test_name="Collect Thread States", test_cases=test_cases)
+    suite.execute()
+
+
+if __name__ == '__main__':
+    app.run(main)
