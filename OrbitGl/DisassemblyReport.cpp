e49625db627ad9df923c28edffcac73a7d65a7ac
Davide Pallotti <dpallotti@google.com>
2021-01-12 10:08:21 +0100

    Add copyright to DisassemblyReport.cc and rename to .cpp

--- OrbitGl/DisassemblyReport.cpp
+++ OrbitGl/DisassemblyReport.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "DisassemblyReport.h"
 
 uint32_t DisassemblyReport::GetNumSamplesAtLine(size_t line) const {
