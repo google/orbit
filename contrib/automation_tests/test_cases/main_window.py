2cc29f12516f3ae79934c1e3226acd21f6be04eb
Florian Reichl <freichl@google.com>
2021-01-12 17:45:39 +0100

    Fix move tabs test
    control_count() contains potentially visible arrows if the tabs cannot
    fit into one row. The new functionality explicitly only counts tab
    items.

--- contrib/automation_tests/test_cases/main_window.py
+++ contrib/automation_tests/test_cases/main_window.py
@@ -6,6 +6,8 @@ found in the LICENSE file.
 
 import logging
 
+from pywinauto.base_wrapper import BaseWrapper
+
 from core.orbit_e2e import E2ETestCase
 
 
@@ -18,6 +20,10 @@ class MoveTab(E2ETestCase):
         context_menu = self.suite.application.window(best_match="TabBarContextMenu")
         self.find_control("MenuItem", name_contains="Move", parent=context_menu).click_input(button='left')
 
+    @staticmethod
+    def _count_tabs(tab_control: BaseWrapper) -> int:
+        return len(tab_control.children(control_type='TabItem'))
+
     def _execute(self, tab_title, tab_name):
         # Find tab and left and right tab bar
         tab_item = self.find_control("TabItem", tab_title)
@@ -25,8 +31,8 @@ class MoveTab(E2ETestCase):
         left_tab_bar = self.find_control("Tab", parent=self.find_control("Group", "MainTabWidget"), recurse=False)
 
         # Init tests
-        left_tab_count = left_tab_bar.control_count()
-        right_tab_count = right_tab_bar.control_count()
+        left_tab_count = self._count_tabs(left_tab_bar)
+        right_tab_count = self._count_tabs(right_tab_bar)
 
         tab_parent = tab_item.parent()
         self.expect_eq(tab_parent, right_tab_bar, "%s tab is initialized in the right pane" % tab_title)
@@ -34,8 +40,8 @@ class MoveTab(E2ETestCase):
         # Move "Functions" tab to the left pane, check no. of tabs and if the tab is enabled
         logging.info('Moving tab to the left pane (current tab count: %d)', right_tab_count)
         self.right_click_move_context(tab_item)
-        self.expect_eq(right_tab_bar.control_count(), right_tab_count - 1, "1 tab removed from right pane")
-        self.expect_eq(left_tab_bar.control_count(), left_tab_count + 1, "1 tab added to the left pane")
+        self.expect_eq(self._count_tabs(right_tab_bar), right_tab_count - 1, "1 tab removed from right pane")
+        self.expect_eq(self._count_tabs(left_tab_bar), left_tab_count + 1, "1 tab added to the left pane")
 
         tab_item = self.find_control("TabItem", name=tab_title)
         self.expect_eq(tab_item.parent(), left_tab_bar, "Tab is parented under the left pane")
@@ -44,8 +50,8 @@ class MoveTab(E2ETestCase):
         # Move back, check no. of tabs
         logging.info('Moving "%s" tab back to the right pane', tab_title)
         self.right_click_move_context(tab_item)
-        self.expect_eq(right_tab_bar.control_count(), right_tab_count, "1 tab removed from left pane")
-        self.expect_eq(left_tab_bar.control_count(), left_tab_count, "1 tab added to the right pane")
+        self.expect_eq(self._count_tabs(right_tab_bar), right_tab_count, "1 tab removed from left pane")
+        self.expect_eq(self._count_tabs(left_tab_bar), left_tab_count, "1 tab added to the right pane")
 
         tab_item = self.find_control("TabItem", name=tab_title)
         self.expect_eq(tab_item.parent(), right_tab_bar, "Tab is parented under the right pane")
