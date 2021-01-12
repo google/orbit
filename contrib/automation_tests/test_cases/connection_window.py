b6191b8a9d46c6ac95a47c1a9b281cf4a7fc26b4
Florian Reichl <freichl@google.com>
2021-01-12 17:44:37 +0100

    Wait for the desired process to show up in the list

--- contrib/automation_tests/test_cases/connection_window.py
+++ contrib/automation_tests/test_cases/connection_window.py
@@ -64,8 +64,6 @@ class FilterAndSelectFirstProcess(E2ETestCase):
     Select the first process in the process list and verify there is at least one entry in the list
     """
     def _execute(self, process_filter):
-        window = self.suite.top_window()
-
         if flags.FLAGS.enable_ui_beta:
             filter_edit = self.find_control('Edit', 'FilterProcesses')
             process_list = self.find_control('Table', 'ProcessList')
@@ -81,7 +79,8 @@ class FilterAndSelectFirstProcess(E2ETestCase):
             filter_edit.set_focus()
             filter_edit.set_edit_text('')
             send_keys(process_filter)
-        self.expect_true(process_list.item_count() > 0, 'Process list has at least one entry')
+        # Wait for the process to show up - it may still be starting
+        wait_for_condition(lambda: process_list.item_count() > 0, 30)
 
         if flags.FLAGS.enable_ui_beta:
             logging.info('Process selected, continuing to main window...')
