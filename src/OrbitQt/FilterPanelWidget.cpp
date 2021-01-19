5ed53dae2fa4031e2157d4adbf07cf3d5cd8d6d4
vickyliu-go4it <vickyliu@google.com>
2021-01-19 10:54:25 +0100

    Add FilterPanelWidget
    To populate the extension pop up with the filter panel, we need a
    customized widget action inherited from `QWidgetAction` and implement
    the filter panel inside the `createWidget()` method.
    For this purpose, we put the filter panel to a separated ui file and
    create the corresponding `FilterPanelWidget` class.

---
+++ src/OrbitQt/FilterPanelWidget.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "FilterPanelWidget.h"
+
+#include <QFontMetrics>
+#include <QFrame>
+#include <QWidget>
+
+#include "orbitmainwindow.h"
+#include "ui_FilterPanelWidget.h"
+
+FilterPanelWidget::FilterPanelWidget(QWidget* parent)
+    : QFrame(parent), ui(new Ui::FilterPanelWidget) {
+  ui->setupUi(this);
+
+  connect(ui->filterTracks, &QLineEdit::textChanged, this,
+          &FilterPanelWidget::FilterTracksTextChanged);
+  connect(ui->filterFunctions, &QLineEdit::textChanged, this,
+          &FilterPanelWidget::FilterFunctionsTextChanged);
+
+  QFontMetrics fm(ui->timerLabel->font());
+  int pixel_width = fm.horizontalAdvance("w");
+  ui->timerLabel->setMinimumWidth(5 * pixel_width);
+}
+
+void FilterPanelWidget::SetFilterFunctionsText(const QString& text) {
+  ui->filterFunctions->blockSignals(true);
+  ui->filterFunctions->setText(text);
+  ui->filterFunctions->blockSignals(false);
+}
+
+void FilterPanelWidget::SetTimerLabelText(const QString& text) { ui->timerLabel->setText(text); }
---
+++ src/OrbitQt/FilterPanelWidget.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_QT_FILTER_PANEL_WIDGET_H_
+#define ORBIT_QT_FILTER_PANEL_WIDGET_H_
+
+#include <QFrame>
+#include <QWidget>
+
+#include "ui_FilterPanelWidget.h"
+
+namespace Ui {
+class FilterPanelWidget;
+}
+
+/*
+ * A widget containing a track filter, a function filter, and a timer label. Will be added as a
+ * widget action to the capture toolbar. (see FilterPanelWidgetAction.h for more details)
+ */
+class FilterPanelWidget : public QFrame {
+  Q_OBJECT
+
+ public:
+  explicit FilterPanelWidget(QWidget* parent = nullptr);
+
+  void SetFilterFunctionsText(const QString& text);
+  void SetTimerLabelText(const QString& text);
+
+ signals:
+  void FilterTracksTextChanged(const QString& text);
+  void FilterFunctionsTextChanged(const QString& text);
+
+ private:
+  Ui::FilterPanelWidget* ui;
+};
+#endif  // ORBIT_QT_FILTER_PANEL_WIDGET_H_
