8fcdae99c3e261738b29a8b66f5bd60ecbc986ae
Henning Becker <henning.becker@gmail.com>
2021-01-19 10:19:13 +0100

    Add orbit_qt_utils::FutureWatcher
    Adds a simple qt-based class which can "actively"
    wait on the completion of a future. That means the
    FutureWatcher::WaitFor-method will continue processing
    UI events as long as the future is not completed.

---
+++ src/QtUtils/FutureWatcher.cpp
@@ -0,0 +1,138 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "QtUtils/FutureWatcher.h"
+
+#include <absl/synchronization/mutex.h>
+
+#include <QDebug>
+#include <QEventLoop>
+#include <QObject>
+#include <QPointer>
+#include <QTimer>
+#include <chrono>
+
+namespace orbit_qt_utils {
+
+FutureWatcher::FutureWatcher(QObject* parent) : QObject{parent} {}
+
+void FutureWatcher::Abort() { emit AbortRequested(); }
+
+FutureWatcher::Reason FutureWatcher::WaitFor(
+    const orbit_base::Future<void>& future,
+    std::optional<std::chrono::milliseconds> timeout) const {
+  if (!future.IsValid() || future.IsFinished()) return Reason::kFutureCompleted;
+
+  QTimer timer{};
+  timer.setSingleShot(true);
+
+  QEventLoop loop{};
+  QObject::connect(this, &FutureWatcher::AbortRequested, &loop, &QEventLoop::quit);
+  QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+
+  if (timeout.has_value()) {
+    timer.start(timeout.value());
+  }
+
+  future.RegisterContinuation(
+      [loop = QPointer{&loop}] { QMetaObject::invokeMethod(loop, &QEventLoop::quit); });
+
+  // Ensure that no race condition occured. The futured could have completed while we were setting
+  // the continuation.
+  if (future.IsFinished()) return Reason::kFutureCompleted;
+
+  loop.exec();
+
+  if (future.IsFinished()) return Reason::kFutureCompleted;
+
+  constexpr int kInvalidRemainingTime = -1;
+  if (timeout.has_value() && timer.remainingTime() == kInvalidRemainingTime) {
+    return Reason::kTimeout;
+  }
+
+  return Reason::kAbortRequested;
+}
+
+FutureWatcher::Reason FutureWatcher::WaitForAll(
+    absl::Span<orbit_base::Future<void>> futures,
+    std::optional<std::chrono::milliseconds> timeout) const {
+  if (futures.empty()) return Reason::kFutureCompleted;
+
+  QTimer timer{};
+  timer.setSingleShot(true);
+
+  QEventLoop loop{};
+  QObject::connect(this, &FutureWatcher::AbortRequested, &loop, &QEventLoop::quit);
+  QObject::connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
+
+  if (timeout.has_value()) {
+    timer.start(timeout.value());
+  }
+
+  struct SharedData {
+    absl::Mutex mutex;
+    size_t completion_counter{};
+    std::vector<bool> finished_indicator;
+  };
+
+  auto shared_data = std::make_shared<SharedData>();
+  shared_data->finished_indicator.resize(futures.size(), false);
+  shared_data->completion_counter = futures.size();  // Counts downwards
+
+  auto indicator_iterator = shared_data->finished_indicator.begin();
+  for (auto& future : futures) {
+    if (!future.IsValid() || future.IsFinished()) {
+      absl::MutexLock lock{&shared_data->mutex};
+      --shared_data->completion_counter;
+      *indicator_iterator = true;
+    } else {
+      future.RegisterContinuation([shared_data, indicator_iterator, loop = QPointer{&loop}]() {
+        absl::MutexLock lock{&shared_data->mutex};
+
+        if (shared_data->completion_counter == 0) return;
+
+        if (!*indicator_iterator) {
+          *indicator_iterator = true;
+          --shared_data->completion_counter;
+        }
+
+        if (shared_data->completion_counter == 0) {
+          QMetaObject::invokeMethod(loop, &QEventLoop::quit);
+        }
+      });
+
+      if (future.IsFinished()) {
+        absl::MutexLock lock{&shared_data->mutex};
+        if (!*indicator_iterator) {
+          --shared_data->completion_counter;
+          *indicator_iterator = true;
+        }
+      }
+    }
+
+    ++indicator_iterator;
+  }
+
+  {
+    absl::MutexLock lock{&shared_data->mutex};
+    if (shared_data->completion_counter == 0) return Reason::kFutureCompleted;
+  }
+
+  loop.exec();
+
+  {
+    absl::MutexLock lock{&shared_data->mutex};
+    if (shared_data->completion_counter == 0) return Reason::kFutureCompleted;
+
+    // If we reach this point, it means the waiting was cancelled (either by Abort or by timeout).
+    // We set completion_counter to zero to inform later executing continuations that we are done.
+    shared_data->completion_counter = 0;
+  }
+
+  constexpr int kInvalidRemainingTime = -1;
+  if (timeout && timer.remainingTime() == kInvalidRemainingTime) return Reason::kTimeout;
+
+  return Reason::kAbortRequested;
+}
+}  // namespace orbit_qt_utils
---
+++ src/QtUtils/include/QtUtils/FutureWatcher.h
@@ -0,0 +1,58 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <absl/types/span.h>
+
+#include <QObject>
+#include <chrono>
+
+#include "OrbitBase/Future.h"
+
+namespace orbit_qt_utils {
+
+// FutureWatcher is a helper class to work in conjunction with ThreadPool and orbit_base::Future.
+// It can wait for a future to complete while still running a QEventLoop that can process other
+// events happening, for example drawing requests, when the user moves the window.
+//
+// You probably don't want to use this class directly and rather use MainThreadExecutor::WaitFor and
+// WaitForAll.
+//
+// The methods WaitFor and WaitForAll are stateless, nevertheless this needs to be class,
+// because in most cases we want to connect a Qt-signal to the Abort-slot. For example when
+// the user closes the window, we want to abort waiting for a completion.
+//
+// Currently only orbit_base::Future<void> is supported, since MainThreadExecutor can't be
+// templated. Support for arbitrary return types can be added when we allow Qt-linking in OrbitGl or
+// move the code up.
+//
+// Usage example:
+// orbit_base::Future<void> future = thread_pool_->Schedule([]() { foo(); });
+// FutureWatcher watcher{};
+// QObject::connect(&orbit_main_window, &OrbitMainWindow::CloseRequested,
+//                  &watcher, &FutureWatcher::Abort);
+//
+// const FutureWatcher::Reason result = watcher.WaitFor(future);
+// if (result != FutureWatcher::Reason::kFutureCompleted) return;
+//
+// /* Continue do my main thread stuff */
+//
+class FutureWatcher : public QObject {
+  Q_OBJECT
+ public:
+  explicit FutureWatcher(QObject* parent = nullptr);
+
+  enum class Reason { kFutureCompleted, kTimeout, kAbortRequested };
+
+  [[nodiscard]] Reason WaitFor(const orbit_base::Future<void>& future,
+                               std::optional<std::chrono::milliseconds> timeout) const;
+  [[nodiscard]] Reason WaitForAll(absl::Span<orbit_base::Future<void>> futures,
+                                  std::optional<std::chrono::milliseconds> timeout) const;
+
+  void Abort();
+
+ signals:
+  void AbortRequested();
+};
+
+}  // namespace orbit_qt_utils
