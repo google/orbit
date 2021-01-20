a070486a68534dea6cf53d7a8dab5236e887daf6
Davide Pallotti <dpallotti@google.com>
2021-01-20 18:57:19 +0100

    Address some lint warnings in SliderTest

--- src/OrbitGl/SliderTest.cpp
+++ src/OrbitGl/SliderTest.cpp
@@ -23,7 +23,7 @@ class MockCanvas : public GlCanvas {
 };
 
 template <int dim>
-void pick(GlSlider& slider, int start, int other_dim = 0) {
+void Pick(GlSlider& slider, int start, int other_dim = 0) {
   static_assert(dim >= 0 && dim <= 1);
   if constexpr (dim == 0) {
     slider.OnPick(start, other_dim);
@@ -33,7 +33,7 @@ void pick(GlSlider& slider, int start, int other_dim = 0) {
 }
 
 template <int dim>
-void drag(GlSlider& slider, int end, int other_dim = 0) {
+void Drag(GlSlider& slider, int end, int other_dim = 0) {
   static_assert(dim >= 0 && dim <= 1);
   if constexpr (dim == 0) {
     slider.OnDrag(end, other_dim);
@@ -43,24 +43,24 @@ void drag(GlSlider& slider, int end, int other_dim = 0) {
 }
 
 template <int dim>
-void pick_drag(GlSlider& slider, int start, int end = -1, int other_dim = 0) {
+void PickDrag(GlSlider& slider, int start, int end = -1, int other_dim = 0) {
   static_assert(dim >= 0 && dim <= 1);
   if (end < 0) {
     end = start;
   }
-  pick<dim>(slider, start, other_dim);
-  drag<dim>(slider, end, other_dim);
+  Pick<dim>(slider, start, other_dim);
+  Drag<dim>(slider, end, other_dim);
 }
 
 template <int dim>
-void pick_drag_release(GlSlider& slider, int start, int end = -1, int other_dim = 0) {
+void PickDragRelease(GlSlider& slider, int start, int end = -1, int other_dim = 0) {
   static_assert(dim >= 0 && dim <= 1);
-  pick_drag<dim>(slider, start, end, other_dim);
+  PickDrag<dim>(slider, start, end, other_dim);
   slider.OnRelease();
 }
 
 template <typename SliderClass>
-std::pair<std::unique_ptr<SliderClass>, std::unique_ptr<MockCanvas>> setup() {
+std::pair<std::unique_ptr<SliderClass>, std::unique_ptr<MockCanvas>> Setup() {
   std::unique_ptr<MockCanvas> canvas = std::make_unique<MockCanvas>();
   // Simulate a 1000x100 canvas
   EXPECT_CALL(*canvas, GetWidth()).WillRepeatedly(::testing::Return(150));
@@ -82,8 +82,8 @@ std::pair<std::unique_ptr<SliderClass>, std::unique_ptr<MockCanvas>> setup() {
 const float kEpsilon = 0.01f;
 
 template <typename SliderClass, int dim>
-void test_drag_type() {
-  auto [slider, canvas] = setup<SliderClass>();
+static void TestDragType() {
+  auto [slider, canvas] = Setup<SliderClass>();
 
   const float kPos = 0.5f;
   const float kSize = 0.5f;
@@ -106,13 +106,13 @@ void test_drag_type() {
   // Use different scales for x and y to make sure dims are chosen correctly
   int scale = static_cast<int>(pow(10, dim));
 
-  pick_drag_release<dim>(*slider, 50 * scale);
+  PickDragRelease<dim>(*slider, 50 * scale);
   EXPECT_EQ(drag_count, 1);
   EXPECT_EQ(size_count, 0);
   EXPECT_EQ(pos, kPos);
   EXPECT_EQ(size, kSize);
 
-  pick_drag_release<dim>(*slider, 25 * scale + kOffset);
+  PickDragRelease<dim>(*slider, 25 * scale + kOffset);
   if (slider->CanResize()) {
     EXPECT_EQ(drag_count, 2);
     EXPECT_EQ(size_count, 1);
@@ -123,7 +123,7 @@ void test_drag_type() {
   EXPECT_EQ(pos, kPos);
   EXPECT_EQ(size, kSize);
 
-  pick_drag_release<dim>(*slider, 75 * scale - kOffset);
+  PickDragRelease<dim>(*slider, 75 * scale - kOffset);
   if (slider->CanResize()) {
     EXPECT_EQ(drag_count, 3);
     EXPECT_EQ(size_count, 2);
@@ -137,13 +137,13 @@ void test_drag_type() {
   drag_count = 0;
   size_count = 0;
 
-  pick_drag_release<dim>(*slider, kOffset);
+  PickDragRelease<dim>(*slider, kOffset);
   EXPECT_EQ(drag_count, 1);
   EXPECT_EQ(size_count, 0);
   EXPECT_NE(pos, kPos);
   EXPECT_EQ(size, kSize);
 
-  pick_drag_release<dim>(*slider, 100 * scale - kOffset);
+  PickDragRelease<dim>(*slider, 100 * scale - kOffset);
   EXPECT_EQ(drag_count, 2);
   EXPECT_EQ(size_count, 0);
   EXPECT_NE(pos, kPos);
@@ -151,13 +151,13 @@ void test_drag_type() {
 }
 
 TEST(Slider, DragType) {
-  test_drag_type<GlHorizontalSlider, 0>();
-  test_drag_type<GlVerticalSlider, 1>();
+  TestDragType<GlHorizontalSlider, 0>();
+  TestDragType<GlVerticalSlider, 1>();
 }
 
 template <typename SliderClass, int dim>
-void test_scroll(float slider_length = 0.25) {
-  auto [slider, canvas] = setup<SliderClass>();
+static void TestScroll(float slider_length = 0.25) {
+  auto [slider, canvas] = Setup<SliderClass>();
 
   // Use different scales for x and y to make sure dims are chosen correctly
   int scale = static_cast<int>(pow(10, dim));
@@ -167,22 +167,22 @@ void test_scroll(float slider_length = 0.25) {
   slider->SetDragCallback([&](float ratio) { pos = ratio; });
   slider->SetNormalizedLength(slider_length);
 
-  pick_drag_release<dim>(*slider, kOffset);
+  PickDragRelease<dim>(*slider, kOffset);
   EXPECT_LT(pos, 0.5f);
   const float kCurPos = pos;
 
-  pick_drag_release<dim>(*slider, 100 * scale - kOffset);
+  PickDragRelease<dim>(*slider, 100 * scale - kOffset);
   EXPECT_GT(pos, kCurPos);
 }
 
 TEST(Slider, Scroll) {
-  test_scroll<GlHorizontalSlider, 0>();
-  test_scroll<GlVerticalSlider, 1>();
+  TestScroll<GlHorizontalSlider, 0>();
+  TestScroll<GlVerticalSlider, 1>();
 }
 
 template <typename SliderClass, int dim>
-void test_drag(float slider_length = 0.25) {
-  auto [slider, canvas] = setup<SliderClass>();
+static void TestDrag(float slider_length = 0.25) {
+  auto [slider, canvas] = Setup<SliderClass>();
 
   // Use different scales for x and y to make sure dims are chosen correctly
   int scale = static_cast<int>(pow(10, dim));
@@ -191,46 +191,46 @@ void test_drag(float slider_length = 0.25) {
   slider->SetDragCallback([&](float ratio) { pos = ratio; });
   slider->SetNormalizedLength(slider_length);
 
-  pick<dim>(*slider, 50 * scale);
+  Pick<dim>(*slider, 50 * scale);
 
   // Expect the slider to be dragged all the way to the right
   // (overshoot, first, then go back to exact drag pos)
-  drag<dim>(*slider, 100 * scale);
+  Drag<dim>(*slider, 100 * scale);
   EXPECT_NEAR(pos, 1.f, kEpsilon);
   EXPECT_EQ(slider->GetPosRatio(), pos);
-  drag<dim>(*slider, 100 * scale - static_cast<int>(slider->GetPixelLength() / 2));
+  Drag<dim>(*slider, 100 * scale - static_cast<int>(slider->GetPixelLength() / 2));
   EXPECT_NEAR(pos, 1.f, kEpsilon);
 
   // Drag to middle
-  drag<dim>(*slider, 50 * scale);
+  Drag<dim>(*slider, 50 * scale);
   EXPECT_NEAR(pos, 0.5f, kEpsilon);
 
   // Drag to left
-  drag<dim>(*slider, 0);
+  Drag<dim>(*slider, 0);
   EXPECT_NEAR(pos, 0.f, kEpsilon);
 
   // Back to middle
-  drag<dim>(*slider, 50 * scale);
+  Drag<dim>(*slider, 50 * scale);
   EXPECT_NEAR(pos, 0.5f, kEpsilon);
 
   // "Invalid" drags sanity check
-  drag<dim>(*slider, 50 * scale, 5000);
+  Drag<dim>(*slider, 50 * scale, 5000);
   EXPECT_NEAR(pos, 0.5f, kEpsilon);
 }
 
 TEST(Slider, Drag) {
-  test_drag<GlHorizontalSlider, 0>();
-  test_drag<GlVerticalSlider, 1>();
+  TestDrag<GlHorizontalSlider, 0>();
+  TestDrag<GlVerticalSlider, 1>();
 }
 
 TEST(Slider, DragBreakTests) {
-  test_drag<GlHorizontalSlider, 0>(0.0001f);
-  test_drag<GlVerticalSlider, 1>(0.0001f);
+  TestDrag<GlHorizontalSlider, 0>(0.0001f);
+  TestDrag<GlVerticalSlider, 1>(0.0001f);
 }
 
 template <typename SliderClass, int dim>
-void test_scaling() {
-  auto [slider, canvas] = setup<SliderClass>();
+static void TestScaling() {
+  auto [slider, canvas] = Setup<SliderClass>();
 
   if (!slider->CanResize()) {
     return;
@@ -247,52 +247,52 @@ void test_scaling() {
   slider->SetDragCallback([&](float ratio) { pos = ratio; });
 
   // Pick on the left
-  pick<dim>(*slider, 25 * scale + kOffset);
+  Pick<dim>(*slider, 25 * scale + kOffset);
 
   // Resize 10% to the left, then all the way
-  drag<dim>(*slider, 15 * scale + kOffset);
+  Drag<dim>(*slider, 15 * scale + kOffset);
   EXPECT_NEAR(size, 0.6f, kEpsilon);
   EXPECT_NEAR(pos, 0.15f / 0.4f, kEpsilon);
   EXPECT_EQ(slider->GetLengthRatio(), size);
   EXPECT_EQ(slider->GetPosRatio(), pos);
 
-  drag<dim>(*slider, 0);
+  Drag<dim>(*slider, 0);
   EXPECT_NEAR(size, 0.75f, kEpsilon);
   EXPECT_NEAR(pos, 0.f, kEpsilon);
 
   // Drag back
-  drag<dim>(*slider, 25 * scale + kOffset);
+  Drag<dim>(*slider, 25 * scale + kOffset);
   EXPECT_NEAR(size, 0.5f, kEpsilon);
   EXPECT_NEAR(pos, 0.5f, kEpsilon);
   slider->OnRelease();
 
   // Pick on the right
-  pick<dim>(*slider, 75 * scale - kOffset);
+  Pick<dim>(*slider, 75 * scale - kOffset);
 
   // Resize 10% to the right, then all the way
-  drag<dim>(*slider, 85 * scale - kOffset);
+  Drag<dim>(*slider, 85 * scale - kOffset);
   EXPECT_NEAR(size, 0.6f, kEpsilon);
   EXPECT_NEAR(pos, 0.25f / 0.4f, kEpsilon);
 
-  drag<dim>(*slider, 100 * scale);
+  Drag<dim>(*slider, 100 * scale);
   EXPECT_NEAR(size, 0.75f, kEpsilon);
   EXPECT_NEAR(pos, 1.f, kEpsilon);
 
   // Drag back
-  drag<dim>(*slider, 75 * scale - kOffset);
+  Drag<dim>(*slider, 75 * scale - kOffset);
   EXPECT_NEAR(size, 0.5f, kEpsilon);
   EXPECT_NEAR(pos, 0.25f / 0.5f, kEpsilon);
   slider->OnRelease();
 }
 
 TEST(Slider, Scale) {
-  test_scaling<GlHorizontalSlider, 0>();
-  test_scaling<GlVerticalSlider, 1>();
+  TestScaling<GlHorizontalSlider, 0>();
+  TestScaling<GlVerticalSlider, 1>();
 }
 
 template <typename SliderClass, int dim>
-void test_break_scaling() {
-  auto [slider, canvas] = setup<SliderClass>();
+static void TestBreakScaling() {
+  auto [slider, canvas] = Setup<SliderClass>();
 
   if (!slider->CanResize()) {
     return;
@@ -308,7 +308,7 @@ void test_break_scaling() {
   // Pick on the right, then drag across the end of the slider
   pos = slider->GetPixelPos();
   len = slider->GetPixelLength();
-  pick_drag_release<dim>(*slider, 75 * scale - kOffset, 0);
+  PickDragRelease<dim>(*slider, 75 * scale - kOffset, 0);
   EXPECT_NEAR(slider->GetPixelPos(), pos, kEpsilon);
   EXPECT_NEAR(slider->GetPixelLength(), slider->GetMinSliderPixelLength(), kEpsilon);
 
@@ -316,12 +316,12 @@ void test_break_scaling() {
   slider->SetNormalizedLength(0.5f);
 
   // Pick on the left, then drag across the end of the slider
-  pick_drag_release<dim>(*slider, 25 * scale + kOffset, 100 * scale);
+  PickDragRelease<dim>(*slider, 25 * scale + kOffset, 100 * scale);
   EXPECT_NEAR(slider->GetPixelPos(), pos + len - slider->GetMinSliderPixelLength(), kEpsilon);
   EXPECT_NEAR(slider->GetPixelLength(), slider->GetMinSliderPixelLength(), kEpsilon);
 }
 
 TEST(Slider, BreakScale) {
-  test_break_scaling<GlHorizontalSlider, 0>();
-  test_break_scaling<GlVerticalSlider, 1>();
+  TestBreakScaling<GlHorizontalSlider, 0>();
+  TestBreakScaling<GlVerticalSlider, 1>();
 }
