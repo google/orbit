8f6d7731e77df1f89f7187ef65679cf72b9ef457
Daniel Fenner <dfenner@google.com>
2021-01-21 16:56:58 +0100

    address comments

--- src/OrbitBase/include/OrbitBase/GetProcessIds.h
+++ src/OrbitBase/include/OrbitBase/GetProcessIds.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ORBIT_BASE_GET_PROCESS_IDS_H
-#define ORBIT_BASE_GET_PROCESS_IDS_H
+#ifndef ORBIT_BASE_GET_PROCESS_IDS_H_
+#define ORBIT_BASE_GET_PROCESS_IDS_H_
 
 #include <sys/types.h>
 
@@ -24,4 +24,4 @@ std::vector<pid_t> GetAllTids();
 
 }  // namespace orbit_base
 
-#endif  // ORBIT_BASE_GET_PROCESS_IDS_H
+#endif  // ORBIT_BASE_GET_PROCESS_IDS_H_
