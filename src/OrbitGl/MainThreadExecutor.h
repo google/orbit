255deae8a0328527c82ba376ab37f9de3cdbd4b0
Henning Becker <henning.becker@gmail.com>
2021-01-25 18:54:09 +0100

    Add ScheduleAfter to MainThreadExecutor
    `ScheduleAfter(future,continuation)` allows to schedule `continuation`
    on the MainThreadExecutor after `future` has been completed.
    
    This will be the basis for chaining futures with Future::Then.

--- src/OrbitGl/MainThreadExecutor.h
+++ src/OrbitGl/MainThreadExecutor.h
@@ -10,6 +10,8 @@
 #include <chrono>
 #include <memory>
 #include <thread>
+#include <type_traits>
+#include <utility>
 
 #include "OrbitBase/Action.h"
 #include "OrbitBase/Future.h"
@@ -31,7 +33,7 @@
 // manager->Schedule(CreateAction([data]{
 //   UpdateSomethingWith(data);
 // }));
-class MainThreadExecutor {
+class MainThreadExecutor : public std::enable_shared_from_this<MainThreadExecutor> {
  public:
   MainThreadExecutor() = default;
   virtual ~MainThreadExecutor() = default;
@@ -63,6 +65,142 @@ class MainThreadExecutor {
     }
 
     return future;
+  }
+
+  // ScheduleAfter schedules the continuation `functor` to be executed
+  // on `*this` after `future` has completed.
+  //
+  // Note: The continuation is only executed if `*this` is still alive when `future` completes.
+  template <typename F>
+  [[nodiscard]] auto ScheduleAfter(const orbit_base::Future<void>& future, F&& functor)
+      -> orbit_base::Future<std::decay_t<decltype(functor())>> {
+    CHECK(future.IsValid());
+
+    using ReturnType = std::decay_t<decltype(functor())>;
+
+    // Since std::function needs to be copyable, promise also needs to be copyable.
+    auto promise = std::make_shared<orbit_base::Promise<ReturnType>>();
+    orbit_base::Future<ReturnType> resulting_future = promise->GetFuture();
+
+    // Since MSVC2017 crashes with a compiler error when using `if constexpr` inside of lambda, we
+    // have to put the check on the outside. That's unfortunate since it leads to four almost
+    // identical version of the same code.
+    if constexpr (std::is_same_v<ReturnType, void>) {
+      auto continuation = [functor = std::forward<F>(functor), executor_weak_ptr = weak_from_this(),
+                           promise = std::move(promise)]() mutable {
+        auto executor = executor_weak_ptr.lock();
+        if (executor == nullptr) return;
+
+        auto function_wrapper = [functor = std::move(functor),
+                                 promise = std::move(promise)]() mutable {
+          functor();
+          promise->MarkFinished();
+        };
+        executor->Schedule(CreateAction(std::move(function_wrapper)));
+      };
+
+      const orbit_base::FutureRegisterContinuationResult result =
+          future.RegisterContinuation(std::move(continuation));
+
+      if (result != orbit_base::FutureRegisterContinuationResult::kSuccessfullyRegistered) {
+        // If the future has already been finished, we call the continuation here.
+        // Keep in mind, this will not run the task synchronously. This will only
+        // SCHEDULE the task synchronously.
+        continuation();
+      }
+    } else {
+      auto continuation = [functor = std::forward<F>(functor), executor_weak_ptr = weak_from_this(),
+                           promise = std::move(promise)]() mutable {
+        auto executor = executor_weak_ptr.lock();
+        if (executor == nullptr) return;
+
+        auto function_wrapper = [functor = std::move(functor),
+                                 promise = std::move(promise)]() mutable {
+          promise->SetResult(functor());
+        };
+        executor->Schedule(CreateAction(std::move(function_wrapper)));
+      };
+
+      const orbit_base::FutureRegisterContinuationResult result =
+          future.RegisterContinuation(std::move(continuation));
+
+      if (result != orbit_base::FutureRegisterContinuationResult::kSuccessfullyRegistered) {
+        // If the future has already been finished, we call the continuation here.
+        // Keep in mind, this will not run the task synchronously. This will only
+        // SCHEDULE the task synchronously.
+        continuation();
+      }
+    }
+
+    return resulting_future;
+  }
+
+  // ScheduleAfter schedules the continuation `functor` to be executed
+  // on `*this` after `future` has completed.
+  //
+  // Note: The continuation is only executed if `*this` is still alive when `future` completes.
+  template <typename T, typename F>
+  [[nodiscard]] auto ScheduleAfter(const orbit_base::Future<T>& future, F&& functor)
+      -> orbit_base::Future<std::decay_t<decltype(functor(std::declval<T>()))>> {
+    CHECK(future.IsValid());
+
+    using ReturnType = std::decay_t<decltype(functor(std::declval<T>()))>;
+
+    // Since std::function needs to be copyable, promise also needs to be copyable.
+    auto promise = std::make_shared<orbit_base::Promise<ReturnType>>();
+    orbit_base::Future<ReturnType> resulting_future = promise->GetFuture();
+
+    // Since MSVC2017 crashes with a compiler error when using `if constexpr` inside of lambda, we
+    // have to put the check on the outside. That's unfortunate since it leads to four almost
+    // identical version of the same code.
+    if constexpr (std::is_same_v<ReturnType, void>) {
+      auto continuation = [functor = std::forward<F>(functor), executor_weak_ptr = weak_from_this(),
+                           promise = std::move(promise)](auto&& argument) mutable {
+        auto executor = executor_weak_ptr.lock();
+        if (executor == nullptr) return;
+
+        auto function_wrapper = [functor = std::move(functor), promise = std::move(promise),
+                                 argument = std::forward<decltype(argument)>(argument)]() mutable {
+          functor(std::move(argument));
+          promise->MarkFinished();
+        };
+        executor->Schedule(CreateAction(std::move(function_wrapper)));
+      };
+
+      const orbit_base::FutureRegisterContinuationResult result =
+          future.RegisterContinuation(std::move(continuation));
+
+      if (result != orbit_base::FutureRegisterContinuationResult::kSuccessfullyRegistered) {
+        // If the future has already been finished, we call the continuation here.
+        // Keep in mind, this will not run the task synchronously. This will only
+        // SCHEDULE the task synchronously.
+        continuation(future.Get());
+      }
+    } else {
+      auto continuation = [functor = std::forward<F>(functor), executor_weak_ptr = weak_from_this(),
+                           promise = std::move(promise)](auto&& argument) mutable {
+        auto executor = executor_weak_ptr.lock();
+        if (executor == nullptr) return;
+
+        auto function_wrapper = [functor = std::move(functor), promise = std::move(promise),
+                                 argument = std::forward<decltype(argument)>(argument)]() mutable {
+          promise->SetResult(functor(std::move(argument)));
+        };
+        executor->Schedule(CreateAction(std::move(function_wrapper)));
+      };
+
+      const orbit_base::FutureRegisterContinuationResult result =
+          future.RegisterContinuation(std::move(continuation));
+
+      if (result != orbit_base::FutureRegisterContinuationResult::kSuccessfullyRegistered) {
+        // If the future has already been finished, we call the continuation here.
+        // Keep in mind, this will not run the task synchronously. This will only
+        // SCHEDULE the task synchronously.
+        continuation(future.Get());
+      }
+    }
+
+    return resulting_future;
   }
 
   enum class WaitResult { kCompleted, kTimedOut, kAborted };
