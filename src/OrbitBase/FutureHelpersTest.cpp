d8060b38a907dc67e678c3d7971a9eb401599ced
Henning Becker <henning.becker@gmail.com>
2021-01-27 14:39:26 +0100

    Add FutureHelpers
    This adds two helper functions.
    
    `RegisterContinuationOrCallDirectly` tries to register a continuation.
    When that fails due to the future being already completed (often due to
    a race condition), the continuation is called directly.
    
    `UnwrapFuture` takes a `Future<Future<T>>` and turns it into a
    `Future<T>` by chaining the continuations. The situation often occurs
    when chaining tasks with `Future::Then` and the continuation being
    asynchronous itself.

---
+++ src/OrbitBase/FutureHelpersTest.cpp
@@ -0,0 +1,129 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+#include "OrbitBase/Future.h"
+#include "OrbitBase/FutureHelpers.h"
+#include "OrbitBase/Promise.h"
+
+namespace orbit_base {
+TEST(RegisterContinuationOrCallDirectly, RegisteringSucceeds) {
+  Promise<void> promise;
+  auto future = promise.GetFuture();
+
+  bool called = false;
+  RegisterContinuationOrCallDirectly(future, [&called]() { called = true; });
+
+  promise.MarkFinished();
+  EXPECT_TRUE(called);
+}
+
+TEST(RegisterContinuationOrCallDirectly, DirectCallSucceeds) {
+  Promise<void> promise;
+  promise.MarkFinished();
+  auto future = promise.GetFuture();
+
+  bool called = false;
+  RegisterContinuationOrCallDirectly(future, [&called]() { called = true; });
+  EXPECT_TRUE(called);
+}
+
+TEST(UnwrapFuture, PassthroughWithVoid) {
+  Promise<void> promise;
+  auto future = promise.GetFuture();
+
+  auto unwrapped_future = UnwrapFuture(future);
+  EXPECT_EQ(future.IsFinished(), unwrapped_future.IsFinished());
+
+  promise.MarkFinished();
+  EXPECT_EQ(future.IsFinished(), unwrapped_future.IsFinished());
+}
+
+TEST(UnwrapFuture, PassthroughWithInt) {
+  Promise<int> promise;
+  auto future = promise.GetFuture();
+
+  auto unwrapped_future = UnwrapFuture(future);
+  EXPECT_EQ(future.IsFinished(), unwrapped_future.IsFinished());
+
+  promise.SetResult(42);
+  EXPECT_EQ(future.IsFinished(), unwrapped_future.IsFinished());
+  EXPECT_EQ(future.Get(), unwrapped_future.Get());
+}
+
+TEST(UnwrapFuture, InnerFutureCompletesFirstWithVoid) {
+  Promise<Future<void>> outer_promise;
+  auto outer_future = outer_promise.GetFuture();
+
+  auto unwrapped_future = UnwrapFuture(outer_future);
+  EXPECT_TRUE(unwrapped_future.IsValid());
+  EXPECT_FALSE(unwrapped_future.IsFinished());
+
+  Promise<void> inner_promise;
+  auto inner_future = inner_promise.GetFuture();
+
+  outer_promise.SetResult(inner_future);
+  EXPECT_FALSE(unwrapped_future.IsFinished());
+
+  inner_promise.MarkFinished();
+  EXPECT_TRUE(unwrapped_future.IsFinished());
+}
+
+TEST(UnwrapFuture, OuterFutureCompletesFirstWithVoid) {
+  Promise<Future<void>> outer_promise;
+  auto outer_future = outer_promise.GetFuture();
+
+  auto unwrapped_future = UnwrapFuture(outer_future);
+  EXPECT_TRUE(unwrapped_future.IsValid());
+  EXPECT_FALSE(unwrapped_future.IsFinished());
+
+  Promise<void> inner_promise;
+  auto inner_future = inner_promise.GetFuture();
+
+  inner_promise.MarkFinished();
+  EXPECT_FALSE(unwrapped_future.IsFinished());
+
+  outer_promise.SetResult(inner_future);
+  EXPECT_TRUE(unwrapped_future.IsFinished());
+}
+
+TEST(UnwrapFuture, InnerFutureCompletesFirstWithInt) {
+  Promise<Future<int>> outer_promise;
+  auto outer_future = outer_promise.GetFuture();
+
+  auto unwrapped_future = UnwrapFuture(outer_future);
+  EXPECT_TRUE(unwrapped_future.IsValid());
+  EXPECT_FALSE(unwrapped_future.IsFinished());
+
+  Promise<int> inner_promise;
+  auto inner_future = inner_promise.GetFuture();
+
+  outer_promise.SetResult(inner_future);
+  EXPECT_FALSE(unwrapped_future.IsFinished());
+
+  inner_promise.SetResult(42);
+  EXPECT_TRUE(unwrapped_future.IsFinished());
+  EXPECT_EQ(unwrapped_future.Get(), 42);
+}
+
+TEST(UnwrapFuture, OuterFutureCompletesFirstWithInt) {
+  Promise<Future<int>> outer_promise;
+  auto outer_future = outer_promise.GetFuture();
+
+  auto unwrapped_future = UnwrapFuture(outer_future);
+  EXPECT_TRUE(unwrapped_future.IsValid());
+  EXPECT_FALSE(unwrapped_future.IsFinished());
+
+  Promise<int> inner_promise;
+  auto inner_future = inner_promise.GetFuture();
+
+  inner_promise.SetResult(42);
+  EXPECT_FALSE(unwrapped_future.IsFinished());
+
+  outer_promise.SetResult(inner_future);
+  EXPECT_TRUE(unwrapped_future.IsFinished());
+  EXPECT_EQ(unwrapped_future.Get(), 42);
+}
+}  // namespace orbit_base
