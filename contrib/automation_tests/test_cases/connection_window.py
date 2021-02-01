2a11b1492a6e764dde23313e724ce7d7bc35d1de
Florian Reichl <freichl@google.com>
2021-02-01 16:28:32 +0100

    Remove the enable_ui_beta flag from the E2E tests

--- contrib/automation_tests/test_cases/connection_window.py
+++ contrib/automation_tests/test_cases/connection_window.py
@@ -7,15 +7,10 @@ found in the LICENSE file.
 import logging
 import time
 
-from absl import flags
 from pywinauto.application import Application
 from pywinauto.keyboard import send_keys
 
 from core.orbit_e2e import E2ETestCase, wait_for_condition
-from core.common_controls import DataViewPanel
-
-# TODO(b/177988872): Remove this flag. What used to be the ui beta, is now the only ui.
-flags.DEFINE_bool('enable_ui_beta', True, 'Expect Orbit to be started with the new UI')
 
 
 def wait_for_main_window(application: Application):
@@ -30,9 +25,8 @@ class ConnectToStadiaInstance(E2ETestCase):
         window = self.suite.top_window()
 
         logging.info('Start connecting to gamelet.')
-        if flags.FLAGS.enable_ui_beta:
-            connect_radio = self.find_control('RadioButton', 'ConnectToStadia')
-            connect_radio.click_input()
+        connect_radio = self.find_control('RadioButton', 'ConnectToStadia')
+        connect_radio.click_input()
 
         # Wait for the first data item in the instance list to exist
         # We're not using find_control here because magic lookup enables us to easily wait for the existence of a row
@@ -45,17 +39,12 @@ class ConnectToStadiaInstance(E2ETestCase):
         window.InstanceList.DataItem0.double_click_input()
         logging.info('Connecting to Instance, waiting for the process list...')
 
-        if flags.FLAGS.enable_ui_beta:
-            # In the new UI, use small waits until the process list is active, and then some more for the
-            # semi-transparent "loading" Overlay of the tables to disappear...
-            wait_for_condition(lambda: self.find_control('Custom', 'ProcessesFrame').is_enabled() is True, 15)
-            wait_for_condition(lambda: self.find_control('Table', 'ProcessList').is_active(), 10)
-            # TODO(b/177037834): Get rid of this sleep and correctly wait for the overlay to be hidden
-            time.sleep(2)
-        else:
-            wait_for_main_window(self.suite.application)
-            window = self.suite.top_window(True)
-            self.expect_eq(window.class_name(), "OrbitMainWindow", 'Main window is visible')
+        # In the new UI, use small waits until the process list is active, and then some more for the
+        # semi-transparent "loading" Overlay of the tables to disappear...
+        wait_for_condition(lambda: self.find_control('Custom', 'ProcessesFrame').is_enabled() is True, 15)
+        wait_for_condition(lambda: self.find_control('Table', 'ProcessList').is_active(), 10)
+        # TODO(b/177037834): Get rid of this sleep and correctly wait for the overlay to be hidden
+        time.sleep(2)
         logging.info('Process list ready')
 
 
@@ -64,13 +53,8 @@ class FilterAndSelectFirstProcess(E2ETestCase):
     Select the first process in the process list and verify there is at least one entry in the list
     """
     def _execute(self, process_filter):
-        if flags.FLAGS.enable_ui_beta:
-            filter_edit = self.find_control('Edit', 'FilterProcesses')
-            process_list = self.find_control('Table', 'ProcessList')
-        else:
-            process_data_view = DataViewPanel(self.find_control('Group', 'ProcessesDataView'))
-            filter_edit = process_data_view.filter
-            process_list = process_data_view.table
+        filter_edit = self.find_control('Edit', 'FilterProcesses')
+        process_list = self.find_control('Table', 'ProcessList')
 
         logging.info('Waiting for process list to be populated')
         wait_for_condition(lambda: process_list.item_count() > 0, 30)
@@ -82,11 +66,8 @@ class FilterAndSelectFirstProcess(E2ETestCase):
         # Wait for the process to show up - it may still be starting
         wait_for_condition(lambda: process_list.item_count() > 0, 30)
 
-        if flags.FLAGS.enable_ui_beta:
-            logging.info('Process selected, continuing to main window...')
-            process_list.children(control_type='DataItem')[0].double_click_input()
-            wait_for_main_window(self.suite.application)
-            window = self.suite.top_window(True)
-            self.expect_eq(window.class_name(), "OrbitMainWindow", 'Main window is visible')
-        else:
-            process_list.children(control_type='TreeItem')[0].click_input()
+        logging.info('Process selected, continuing to main window...')
+        process_list.children(control_type='DataItem')[0].double_click_input()
+        wait_for_main_window(self.suite.application)
+        window = self.suite.top_window(True)
+        self.expect_eq(window.class_name(), "OrbitMainWindow", 'Main window is visible')
