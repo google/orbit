b0a941ec6be02bacf4ef564ee5c10e14152dab62
Henning Becker <henning.becker@gmail.com>
2021-02-08 08:21:42 +0100

    Introduce ImmediateExecutor
    ImmediateExecutor is a helper class to be used in conjunction with
    `Future::Then`. It allows to chain a task to be executed whenever the
    future completes. The task will be executed in the execution context
    that sets the result of the promise.
    
    This type of executor should only be used for tasks that don't depend on
    any outside resources since it can't be guaranteed when and where this
    task is executed.

---
+++ src/OrbitBase/include/OrbitBase/ImmediateExecutor.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_BASE_IMMEDIATE_EXECUTOR_H_
+#define ORBIT_BASE_IMMEDIATE_EXECUTOR_H_
+
+#include <type_traits>
+
+#include "OrbitBase/Future.h"
+#include "OrbitBase/FutureHelpers.h"
+#include "OrbitBase/Promise.h"
+#include "OrbitBase/PromiseHelpers.h"
+
+namespace orbit_base {
+
+// ImmediateExecutor is a special type of executor that executes its action during the Schedule or
+// ScheduleAfter call. When called directly it's just a verbose abstraction around a standard
+// function call.
+//
+// Its usage only makes sense in combination with Future::Then.
+//
+// Example:
+// Future<std::string> result = thread_pool_->Schedule(/* whatever */);
+// ImmediateExecutor immediate_executor{};
+// Future<void> void_future = result.Then(&immediate_executor, [](const std::string& str) {
+//   (void) str;
+// });
+// main_thread_executor_->WaitFor(void_future); // WaitFor only works with Future<void>
+//
+class ImmediateExecutor {
+ public:
+  template <typename F>
+  auto Schedule(F&& invocable) {
+    using ReturnType = decltype(invocable());
+    if constexpr (std::is_same_v<ReturnType, void>) {
+      invocable();
+      return Future<void>{};
+    } else {
+      return Future<ReturnType>{invocable()};
+    }
+  }
+
+  template <typename T, typename F>
+  auto ScheduleAfter(const Future<T>& future, F&& invocable) {
+    CHECK(future.IsValid());
+
+    using ReturnType = typename ContinuationReturnType<T, F>::Type;
+    orbit_base::Promise<ReturnType> promise{};
+    orbit_base::Future<ReturnType> resulting_future = promise.GetFuture();
+
+    auto continuation = [invocable = std::forward<F>(invocable),
+                         promise = std::move(promise)](auto&&... argument) mutable {
+      orbit_base::CallTaskAndSetResultInPromise<ReturnType> helper{&promise};
+      helper.Call(invocable, argument...);
+    };
+
+    orbit_base::RegisterContinuationOrCallDirectly(future, std::move(continuation));
+    return resulting_future;
+  }
+};
+}  // namespace orbit_base
+
+#endif  // ORBIT_BASE_IMMEDIATE_EXECUTOR_H_
