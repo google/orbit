bb67969bf64adc55a0966369e3e6aa1157e11c37
Henning Becker <henning.becker@gmail.com>
2021-02-08 08:21:33 +0100

    Introduce AnyInvocable
    AnyInvocable a type-erased function wrapper - similar to `std::function`
    - that wraps anything that fulfills the `std::invocable` concept. Just
    like `std::invocable` - and unlink `std::function` - this wrapper does
    not require the function object to be copyable and is itself not
    copyable.
    
    In other codebases this type is also often called `unique_function`. In
    anticipation to replace this code by the upcoming Abseil implementation,
    I named it `AnyInvocable` as this will probably be its name in Abseil.

---
+++ src/OrbitBase/AnyInvocableTest.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+#include "OrbitBase/AnyInvocable.h"
+
+namespace orbit_base {
+
+static int GetRandomNumber() {
+  return 4;  // chosen by a fair dice roll. guaranteed to be random.
+}
+
+TEST(AnyInvocable, ShouldStoreAndCallLambda) {
+  AnyInvocable<int()> invocable{[]() { return 42; }};
+  EXPECT_NE(invocable, nullptr);
+  EXPECT_TRUE(invocable);
+
+  EXPECT_EQ(invocable(), 42);
+}
+
+TEST(AnyInvocable, ShouldStoreAndCallFunctionPointer) {
+  AnyInvocable<int()> invocable{&GetRandomNumber};
+  EXPECT_NE(invocable, nullptr);
+  EXPECT_TRUE(invocable);
+
+  EXPECT_EQ(invocable(), GetRandomNumber());
+}
+
+TEST(AnyInvocable, ShouldStoreAndCallMoveOnlyLambda) {
+  AnyInvocable<int()> invocable{[val = std::make_unique<int>(42)]() { return *val; }};
+  EXPECT_NE(invocable, nullptr);
+  EXPECT_TRUE(invocable);
+
+  EXPECT_EQ(invocable(), 42);
+}
+
+TEST(AnyInvocable, ShouldBeMovableAndStillCallable) {
+  AnyInvocable<int()> first{[val = std::make_unique<int>(42)]() { return *val; }};
+
+  auto second = std::move(first);
+  EXPECT_EQ(first, nullptr);  // NOLINT
+  EXPECT_NE(second, nullptr);
+
+  EXPECT_EQ(second(), 42);
+}
+
+}  // namespace orbit_base
