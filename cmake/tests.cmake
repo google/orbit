9bdbb580d7c0dc2906b696d716432ecabd2c771a
Henning Becker <henning.becker@gmail.com>
2021-02-08 08:12:04 +0100

    Add convenience targets for Qt-based tests
    Two new targets GTest::QtCoreMain and GTest::QtGuiMain are introduced.
    They act as a replacement for GTest::Main whenever a QApplication or
    QCoreApplication instance is needed. That's often the case for Qt-based
    code.

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -48,3 +48,51 @@ int main(int argc, char* argv[]) {\n\
   target_link_libraries(GTest_Main PUBLIC GTest::GTest)
   add_library(GTest::Main ALIAS GTest_Main)
 endif()
+
+if(NOT TARGET GTest::QtCoreMain AND TARGET Qt5::Core)
+  set(FILE_PATH ${CMAKE_BINARY_DIR}/test_qtcore_main.cpp)
+  if(NOT EXISTS ${FILE_PATH})
+    set(GTEST_MAIN_CPP "\
+#include <cstdio>\n\
+#include <gtest/gtest.h>\n\
+#include <QCoreApplication>\n\
+\n\
+int main(int argc, char* argv[]) {\n\
+  QCoreApplication app{argc, argv}\;\n\
+  printf(\"Running main() from %s\\n\", __FILE__)\;\n\
+  ::testing::InitGoogleTest(&argc, argv)\;\n\
+  return RUN_ALL_TESTS()\;\n\
+}\n\
+")
+
+    file(WRITE ${FILE_PATH} ${GTEST_MAIN_CPP})
+  endif()
+
+  add_library(GTest_QtCoreMain STATIC EXCLUDE_FROM_ALL ${FILE_PATH})
+  target_link_libraries(GTest_QtCoreMain PUBLIC GTest::GTest Qt5::Core)
+  add_library(GTest::QtCoreMain ALIAS GTest_QtCoreMain)
+endif()
+
+if(NOT TARGET GTest::QtGuiMain AND TARGET Qt5::Widgets)
+  set(FILE_PATH ${CMAKE_BINARY_DIR}/test_qtgui_main.cpp)
+  if(NOT EXISTS ${FILE_PATH})
+    set(GTEST_MAIN_CPP "\
+#include <cstdio>\n\
+#include <gtest/gtest.h>\n\
+#include <QApplication>\n\
+\n\
+int main(int argc, char* argv[]) {\n\
+  QApplication app{argc, argv}\;\n\
+  printf(\"Running main() from %s\\n\", __FILE__)\;\n\
+  ::testing::InitGoogleTest(&argc, argv)\;\n\
+  return RUN_ALL_TESTS()\;\n\
+}\n\
+")
+
+    file(WRITE ${FILE_PATH} ${GTEST_MAIN_CPP})
+  endif()
+
+  add_library(GTest_QtGuiMain STATIC EXCLUDE_FROM_ALL ${FILE_PATH})
+  target_link_libraries(GTest_QtGuiMain PUBLIC GTest::GTest Qt5::Widgets)
+  add_library(GTest::QtGuiMain ALIAS GTest_QtGuiMain)
+endif()
