8e5b3cbdc60bdf4c4c265c57a93cf909b679bc70
dimitry <dimitry@google.com>
2021-02-10 14:46:13 +0100

    Dedup interned strings and callstacks, add unit-tests
    ProducerEventProcessor is now depuping callstacks and strings
    from different producers.
    
    This change also removes couple of protos that are not currently
    used from ProducerCaptureEvent.
    
    Test: run unit-tests

--- src/Service/ProducerEventProcessor.cpp
+++ src/Service/ProducerEventProcessor.cpp
@@ -39,8 +39,7 @@ using orbit_grpc_protos::ThreadStateSlice;
 template <typename T>
 class InternedCache final {
  public:
-  InternedCache() = delete;
-  explicit InternedCache(std::atomic<uint64_t>* id_counter) : id_counter_{id_counter} {}
+  InternedCache() = default;
 
   // Return pair of <id, assigned>, where assigned is true if the entry was assigned a new id
   // and false if returning id for already existing entry.
@@ -51,14 +50,14 @@ class InternedCache final {
       return std::make_pair(it->second, false);
     }
 
-    uint64_t new_id = (*id_counter_)++;
+    uint64_t new_id = id_counter_++;
     auto [unused_it, inserted] = entry_to_id_.insert_or_assign(entry, new_id);
     CHECK(inserted);
     return std::make_pair(new_id, true);
   }
 
  private:
-  std::atomic<uint64_t>* id_counter_ = nullptr;
+  uint64_t id_counter_ = 1;  // 0 is reserved for invalid_id
   absl::flat_hash_map<T, uint64_t> entry_to_id_;
   absl::Mutex entry_to_id_mutex_;
 };
@@ -67,16 +66,7 @@ class ProducerEventProcessorImpl : public ProducerEventProcessor {
  public:
   ProducerEventProcessorImpl() = delete;
   explicit ProducerEventProcessorImpl(CaptureEventBuffer* capture_event_buffer)
-      : capture_event_buffer_{capture_event_buffer},
-        callstack_id_counter_{1},
-        string_id_counter_{1},
-        tracepoint_id_counter_{1},
-        callstack_cache_{&callstack_id_counter_},
-        string_cache_{&string_id_counter_},
-        tracepoint_cache_{&tracepoint_id_counter_},
-        interned_callstack_id_cache_{&callstack_id_counter_},
-        interned_string_id_cache_{&string_id_counter_},
-        interned_tracepoint_id_cache_{&tracepoint_id_counter_} {}
+      : capture_event_buffer_{capture_event_buffer} {}
 
   void ProcessEvent(uint64_t producer_id, ProducerCaptureEvent event) override;
 
@@ -90,36 +80,29 @@ class ProducerEventProcessorImpl : public ProducerEventProcessor {
   void ProcessInternedCallstackSample(uint64_t producer_id,
                                       InternedCallstackSample* callstack_sample);
   void ProcessInternedString(uint64_t producer_id, InternedString* interned_string);
-  void ProcessInternedTracepointInfo(uint64_t producer_id,
-                                     InternedTracepointInfo* interned_tracepoint_info);
   void ProcessIntrospectionScope(IntrospectionScope* introspection_scope);
   void ProcessModuleUpdateEvent(ModuleUpdateEvent* module_update_event);
   void ProcessSchedulingSlice(SchedulingSlice* scheduling_slice);
   void ProcessThreadName(ThreadName* thread_name);
   void ProcessThreadStateSlice(ThreadStateSlice* thread_state_slice);
   void ProcessFullTracepointEvent(FullTracepointEvent* tracepoint_event);
-  void ProcessInternedTracepointEvent(uint64_t producer_id,
-                                      InternedTracepointEvent* tracepoint_event);
 
   void SendStringAndKeyEvent(uint64_t key, std::string value);
 
   CaptureEventBuffer* capture_event_buffer_;
 
-  std::atomic<uint64_t> callstack_id_counter_;
-  std::atomic<uint64_t> string_id_counter_;
-  std::atomic<uint64_t> tracepoint_id_counter_;
-
   InternedCache<std::vector<uint64_t>> callstack_cache_;
   InternedCache<std::string> string_cache_;
   InternedCache<std::pair<std::string, std::string>> tracepoint_cache_;
 
-  // These are mapping InternStrings and Callstacks from producer ids
+  // These are mapping InternStrings and InternedCallstacks from producer ids
   // to client ids:
-  // <producer_id, producer_string_id> -> client_string_id and
   // <producer_id, producer_callstack_id> -> client_callstack_id
-  InternedCache<std::pair<uint64_t, uint64_t>> interned_callstack_id_cache_;
-  InternedCache<std::pair<uint64_t, uint64_t>> interned_string_id_cache_;
-  InternedCache<std::pair<uint64_t, uint64_t>> interned_tracepoint_id_cache_;
+  absl::flat_hash_map<std::pair<uint64_t, uint64_t>, uint64_t>
+      producer_interned_callstack_id_to_client_callstack_id_;
+  // <producer_id, producer_string_id> -> client_string_id
+  absl::flat_hash_map<std::pair<uint64_t, uint64_t>, uint64_t>
+      producer_interned_string_id_to_client_string_id_;
 };
 
 void ProducerEventProcessorImpl::ProcessFullAddressInfo(FullAddressInfo* full_address_info) {
@@ -180,10 +163,10 @@ void ProducerEventProcessorImpl::ProcessGpuQueueSubmission(
     uint64_t producer_id, GpuQueueSubmission* gpu_queue_submission) {
   // Translate debug marker keys
   for (GpuDebugMarker& mutable_marker : *gpu_queue_submission->mutable_completed_markers()) {
-    auto [client_text_key, assigned] =
-        interned_string_id_cache_.GetOrAssignId({producer_id, mutable_marker.text_key()});
-    CHECK(!assigned);
-    mutable_marker.set_text_key(client_text_key);
+    auto it = producer_interned_string_id_to_client_string_id_.find(
+        {producer_id, mutable_marker.text_key()});
+    CHECK(it != producer_interned_string_id_to_client_string_id_.end());
+    mutable_marker.set_text_key(it->second);
   }
 
   ClientCaptureEvent event;
@@ -216,11 +199,23 @@ void ProducerEventProcessorImpl::ProcessFullCallstackSample(FullCallstackSample*
 
 void ProducerEventProcessorImpl::ProcessInternedCallstack(uint64_t producer_id,
                                                           InternedCallstack* interned_callstack) {
-  auto [interned_callstack_id, assigned] =
-      interned_callstack_id_cache_.GetOrAssignId({producer_id, interned_callstack->key()});
-  CHECK(assigned);
-  interned_callstack->set_key(interned_callstack_id);
+  // TODO(http://b/180235290): replace with error message
+  CHECK(!producer_interned_callstack_id_to_client_callstack_id_.contains(
+      {producer_id, interned_callstack->key()}));
 
+  std::vector<uint64_t> callstack_data{interned_callstack->intern().pcs().begin(),
+                                       interned_callstack->intern().pcs().end()};
+  auto [interned_callstack_id, assigned] = callstack_cache_.GetOrAssignId(callstack_data);
+
+  producer_interned_callstack_id_to_client_callstack_id_.insert_or_assign(
+      {producer_id, interned_callstack->key()}, interned_callstack_id);
+
+  if (!assigned) {
+    return;
+  }
+
+  // If this is first time we see it -> send it over with client_id
+  interned_callstack->set_key(interned_callstack_id);
   ClientCaptureEvent event;
   *event.mutable_interned_callstack() = std::move(*interned_callstack);
   capture_event_buffer_->AddEvent(std::move(event));
@@ -228,11 +223,12 @@ void ProducerEventProcessorImpl::ProcessInternedCallstack(uint64_t producer_id,
 
 void ProducerEventProcessorImpl::ProcessInternedCallstackSample(
     uint64_t producer_id, InternedCallstackSample* callstack_sample) {
-  // traslate ids
-  auto [callstack_id, assigned] =
-      interned_callstack_id_cache_.GetOrAssignId({producer_id, callstack_sample->callstack_id()});
-  CHECK(!assigned);
-  callstack_sample->set_callstack_id(callstack_id);
+  // translate producer id to client id
+  auto it = producer_interned_callstack_id_to_client_callstack_id_.find(
+      {producer_id, callstack_sample->callstack_id()});
+  // TODO(http://b/180235290): replace with error message
+  CHECK(it != producer_interned_callstack_id_to_client_callstack_id_.end());
+  callstack_sample->set_callstack_id(it->second);
 
   ClientCaptureEvent event;
   *event.mutable_interned_callstack_sample() = std::move(*callstack_sample);
@@ -241,25 +237,22 @@ void ProducerEventProcessorImpl::ProcessInternedCallstackSample(
 
 void ProducerEventProcessorImpl::ProcessInternedString(uint64_t producer_id,
                                                        InternedString* interned_string) {
-  auto [interned_string_id, assigned] =
-      interned_string_id_cache_.GetOrAssignId({producer_id, interned_string->key()});
-  CHECK(assigned);
-  interned_string->set_key(interned_string_id);
+  // TODO(http://b/180235290): replace with error message
+  CHECK(!producer_interned_string_id_to_client_string_id_.contains(
+      {producer_id, interned_string->key()}));
+
+  auto [client_string_id, assigned] = string_cache_.GetOrAssignId(interned_string->intern());
+  producer_interned_string_id_to_client_string_id_.insert_or_assign(
+      {producer_id, interned_string->key()}, client_string_id);
+
+  if (!assigned) {
+    return;
+  }
+
+  interned_string->set_key(client_string_id);
 
   ClientCaptureEvent event;
   *event.mutable_interned_string() = std::move(*interned_string);
-  capture_event_buffer_->AddEvent(std::move(event));
-}
-
-void ProducerEventProcessorImpl::ProcessInternedTracepointInfo(
-    uint64_t producer_id, InternedTracepointInfo* interned_tracepoint_info) {
-  auto [interned_tracepoint_id, assigned] =
-      interned_tracepoint_id_cache_.GetOrAssignId({producer_id, interned_tracepoint_info->key()});
-  CHECK(assigned);
-  interned_tracepoint_info->set_key(interned_tracepoint_id);
-
-  ClientCaptureEvent event;
-  *event.mutable_interned_tracepoint_info() = std::move(*interned_tracepoint_info);
   capture_event_buffer_->AddEvent(std::move(event));
 }
 
@@ -316,17 +309,6 @@ void ProducerEventProcessorImpl::ProcessFullTracepointEvent(FullTracepointEvent*
   capture_event_buffer_->AddEvent(std::move(event));
 }
 
-void ProducerEventProcessorImpl::ProcessInternedTracepointEvent(
-    uint64_t producer_id, InternedTracepointEvent* tracepoint_event) {
-  auto [tracepoint_key, assigned] = interned_tracepoint_id_cache_.GetOrAssignId(
-      {producer_id, tracepoint_event->tracepoint_info_key()});
-  tracepoint_event->set_tracepoint_info_key(tracepoint_key);
-
-  ClientCaptureEvent event;
-  *event.mutable_interned_tracepoint_event() = std::move(*tracepoint_event);
-  capture_event_buffer_->AddEvent(std::move(event));
-}
-
 void ProducerEventProcessorImpl::ProcessEvent(uint64_t producer_id, ProducerCaptureEvent event) {
   switch (event.event_case()) {
     case ProducerCaptureEvent::kInternedCallstack:
@@ -364,12 +346,6 @@ void ProducerEventProcessorImpl::ProcessEvent(uint64_t producer_id, ProducerCapt
       break;
     case ProducerCaptureEvent::kFullAddressInfo:
       ProcessFullAddressInfo(event.mutable_full_address_info());
-      break;
-    case ProducerCaptureEvent::kInternedTracepointEvent:
-      ProcessInternedTracepointEvent(producer_id, event.mutable_interned_tracepoint_event());
-      break;
-    case ProducerCaptureEvent::kInternedTracepointInfo:
-      ProcessInternedTracepointInfo(producer_id, event.mutable_interned_tracepoint_info());
       break;
     case ProducerCaptureEvent::kIntrospectionScope:
       ProcessIntrospectionScope(event.mutable_introspection_scope());
