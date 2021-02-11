917206e7ff50dd31c0a62200299e1e41b1669b39
Florian Reichl <freichl@google.com>
2021-02-11 10:43:37 +0100

    Rename TrackAccessibility to AccessibleTrack

--- src/OrbitGl/AccessibleTrack.h
+++ src/OrbitGl/AccessibleTrack.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ORBIT_GL_TRACK_ACCESSIBILITY_H_
-#define ORBIT_GL_TRACK_ACCESSIBILITY_H_
+#ifndef ORBIT_GL_ACCESSIBLE_TRACK_H_
+#define ORBIT_GL_ACCESSIBLE_TRACK_H_
 
 #include <string>
 
