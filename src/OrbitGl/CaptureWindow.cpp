ccc7f33a5649fcdaf86b4638e794fd42a1d9288f
Daniel Fenner <dfenner@google.com>
2021-02-16 15:40:07 +0100

    format

--- src/OrbitGl/CaptureWindow.cpp
+++ src/OrbitGl/CaptureWindow.cpp
@@ -62,7 +62,7 @@ class AccessibleCaptureWindow : public AccessibleWidgetBridge {
     return window_->GetTimeGraph()->GetOrCreateAccessibleInterface();
   }
 
- private: 
+ private:
   CaptureWindow* window_;
 };
 
