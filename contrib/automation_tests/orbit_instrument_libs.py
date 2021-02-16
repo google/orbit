af4cf7e27d7afa99eb9fd6dd937d9b68aca28598
Daniel Fenner <dfenner@google.com>
2021-02-16 10:04:02 +0100

    Address comment.

--- contrib/automation_tests/orbit_instrument_libs.py
+++ contrib/automation_tests/orbit_instrument_libs.py
@@ -48,7 +48,7 @@ def main(argv):
         LoadSymbols(module_search_string="libggp"),
         FilterAndHookMultipleFunctions(function_search_string='GgpIssueFrameToken_v'),
         Capture(),
-        VerifyOneFunctionWasCalled(function_name='GgpIssueFrameToken_v')
+        VerifyOneFunctionWasCalled(function_name='GgpIssueFrameToken_v', min_calls=30, max_calls=3000)
     ]
     suite = E2ETestSuite(test_name="Instrument libggp", test_cases=test_cases)
     suite.execute()
