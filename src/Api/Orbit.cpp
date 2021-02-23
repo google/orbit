023b45a604a9ae2b00ad42a8fcf1881a1e900c0e
Pierric Gimmig <pierric@google.com>
2021-02-23 15:19:14 -0800

    Add "API" module which generates liborbit.so
    Add module that defines the Orbit programming interface and generates
    the shared library that will be dynamically loaded by user code in order
    to interact programmatically with Orbit.

---
+++ src/Api/Orbit.cpp
@@ -0,0 +1,78 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#define ORBIT_API_INTERNAL_IMPL
+#include "Api/Orbit.h"
+
+#include "Api/EncodedEvent.h"
+#include "OrbitBase/Profiling.h"
+#include "OrbitBase/ThreadUtils.h"
+
+// Temporary dummy function, proper implementation is in next PR.
+// See https://github.com/google/orbit/pull/1702 for fully working prototype.
+static void EnqueueIntermediateEvent(const orbit_api::ApiEvent& event) { (void)event; }
+
+static void EnqueueApiEvent(orbit_api::EventType type, const char* name = nullptr,
+                            uint64_t data = 0, orbit_api_color color = kOrbitColorAuto) {
+  uint64_t timestamp_ns = orbit_base::CaptureTimestampNs();
+  static pid_t pid = orbit_base::GetCurrentProcessId();
+  thread_local pid_t tid = orbit_base::GetCurrentThreadId();
+
+  orbit_api::ApiEvent api_event(pid, tid, timestamp_ns, type, name, data, color);
+  EnqueueIntermediateEvent(api_event);
+}
+
+extern "C" {
+
+void orbit_api_start(const char* name, orbit_api_color color) {
+  EnqueueApiEvent(orbit_api::EventType::kScopeStart, name, /*data=*/0, color);
+}
+
+void orbit_api_stop() { EnqueueApiEvent(orbit_api::EventType::kScopeStop); }
+
+void orbit_api_start_async(const char* name, uint64_t id, orbit_api_color color) {
+  EnqueueApiEvent(orbit_api::EventType::kScopeStartAsync, name, id, color);
+}
+
+void orbit_api_stop_async(uint64_t id) {
+  EnqueueApiEvent(orbit_api::EventType::kScopeStopAsync, /*name=*/nullptr, id);
+}
+
+void orbit_api_track_int(const char* name, int value, orbit_api_color color) {
+  EnqueueApiEvent(orbit_api::kTrackInt, name, orbit_api::Encode<uint64_t>(value), color);
+}
+
+void orbit_api_track_int64(const char* name, int64_t value, orbit_api_color color) {
+  EnqueueApiEvent(orbit_api::kTrackInt64, name, orbit_api::Encode<uint64_t>(value), color);
+}
+
+void orbit_api_track_uint(const char* name, uint32_t value, orbit_api_color color) {
+  EnqueueApiEvent(orbit_api::kTrackUint, name, orbit_api::Encode<uint64_t>(value), color);
+}
+
+void orbit_api_track_uint64(const char* name, uint64_t value, orbit_api_color color) {
+  EnqueueApiEvent(orbit_api::kTrackUint64, name, orbit_api::Encode<uint64_t>(value), color);
+}
+
+void orbit_api_track_float(const char* name, float value, orbit_api_color color) {
+  EnqueueApiEvent(orbit_api::kTrackFloat, name, orbit_api::Encode<uint64_t>(value), color);
+}
+
+void orbit_api_track_double(const char* name, double value, orbit_api_color color) {
+  EnqueueApiEvent(orbit_api::kTrackDouble, name, orbit_api::Encode<uint64_t>(value), color);
+}
+
+void orbit_api_async_string(const char* str, uint64_t id, orbit_api_color color) {
+  if (str == nullptr) return;
+  char buffer[orbit_api::kMaxEventStringSize] = {};
+  constexpr size_t chunk_size = orbit_api::kMaxEventStringSize - 1;
+  const char* end = str + strlen(str);
+  while (str < end) {
+    std::strncpy(buffer, str, chunk_size);
+    buffer[chunk_size] = 0;
+    EnqueueApiEvent(orbit_api::EventType::kString, buffer, id, color);
+    str += chunk_size;
+  }
+}
+}
