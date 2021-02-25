befb0bed52b64e40ebf7eed797cd5f25b14dfbeb
Henning Becker <henning.becker@gmail.com>
2021-02-25 18:15:35 +0100

    Add demo for SourcePathsMappingDialog

---
+++ src/SourcePathsMappingDialogDemo/main.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <QApplication>
+
+#include "ConfigWidgets/SourcePathsMappingDialog.h"
+#include "SourcePathsMapping/MappingManager.h"
+
+int main(int argc, char* argv[]) {
+  QApplication app{argc, argv};
+  QApplication::setApplicationName("SourcePathsMappingDialogDemo");
+  QApplication::setOrganizationName("The Orbit Authors");
+
+  orbit_source_paths_mapping::MappingManager manager{};
+
+  orbit_config_widgets::SourcePathsMappingDialog dialog{};
+  dialog.SetMappings(manager.GetMappings());
+  const int result_code = dialog.exec();
+
+  if (result_code == QDialog::Accepted) {
+    manager.SetMappings(dialog.GetMappings());
+  }
+
+  return result_code;
+}
