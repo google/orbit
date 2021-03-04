fe65f75506163ae3489cf0fbb3ada01bd2f7c07a
danielfenner <56686285+danielfenner@users.noreply.github.com>
2021-03-04 14:43:46 +0100

    Test lib for the injection mechanism.
    In order to load shared object into attached process we need a simple
    .so first to test / develop that. Thats why the produced binary is
    checked in to the testdata folder.
    
    bug: http://b/181304023
    test: NA

---
+++ src/UserSpaceInstrumentation/UserSpaceInstrumentationTestLib.cpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "UserSpaceInstrumentationTestLib.h"
+
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+namespace {
+
+std::filesystem::path GetTmpFilePath() {
+  static std::filesystem::path p = std::tmpnam(nullptr);
+  return p;
+}
+
+}  // namespace
+
+void InitTestLib() {
+  auto p = GetTmpFilePath();
+  std::cout << "Init Lib. Tmp file is: " << p << std::endl;
+}
+
+void UseTestLib(std::string_view s) {
+  auto p = GetTmpFilePath();
+  std::ofstream ofs;
+  ofs.open(p, std::ofstream::out | std::ofstream::app);
+  ofs << s << std::endl;
+  ofs.close();
+}
+
+void CloseTestLib() {
+  auto p = GetTmpFilePath();
+
+  std::cout << "Close Lib. Content of " << p << std::endl;
+
+  std::ifstream tmp_file(p);
+  std::string line;
+  while (std::getline(tmp_file, line)) {
+    std::cout << line << std::endl;
+  }
+  tmp_file.close();
+  std::cout << std::endl << std::endl;
+
+  std::remove(p.c_str());
+}
---
+++ src/UserSpaceInstrumentation/UserSpaceInstrumentationTestLib.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef USER_SPACE_INSTRUMENTATION_TEST_LIB_H_
+#define USER_SPACE_INSTRUMENTATION_TEST_LIB_H_
+
+#include <string_view>
+
+// Library functions for some trivial logging. This library is merely used in tests: The test
+// injects a binary produced by this code into its child.
+
+// Call first to initialize the library.
+void InitTestLib();
+
+// Log a string into a temporary file.
+void UseTestLib(std::string_view s);
+
+// Call to end using the library. Prints the entire log to standard output and removes the
+// temporary log file.
+void CloseTestLib();
+
+#endif  // USER_SPACE_INSTRUMENTATION_TEST_LIB_H_
