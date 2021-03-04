ebc7d85592b2155a3ffd1981bfd31940d2b96603
Henning Becker <henning.becker@gmail.com>
2021-03-04 08:44:14 +0100

    Fix source paths mapping
    The source paths mapping had a problem when source and target path used
    different file path separators.
    
    This commit fixes that by using iteration over file path
    segments for comparison. The previous approach compared strings instead
    which obviously fails in that case.
    
    There is also a test for mixed separators now. The additional test for
    `InferMappingFromExample` was written to figure out if the problem lies
    with that function. But that wasn't the case. I left the additional test
    anyway.

--- src/SourcePathsMapping/MappingTest.cpp
+++ src/SourcePathsMapping/MappingTest.cpp
@@ -36,6 +36,30 @@ TEST(Mapping, MapToFirstMatchingTargetFalsePredicate) {
   ASSERT_FALSE(no_file.has_value());
 }
 
+#ifdef _WIN32
+TEST(Mapping, MapToFirstMatchingTargetWindowsSeparators) {
+  Mapping mapping{"C:\\UE4", "C:/Users/username/Downloads/UE4_424/UE4"};
+
+  const auto file_txt = MapToFirstMatchingTarget(
+      {mapping}, "C:\\UE4\\Engine\\Source\\Runtime\\Core\\Private\\HAL\\PThreadRunnableThread.cpp",
+      &AlwaysTruePredicate);
+  ASSERT_TRUE(file_txt.has_value());
+
+  const std::filesystem::path target_path{
+      "C:\\Users\\username\\Downloads\\UE4_"
+      "424\\UE4\\Engine\\Source\\Runtime\\Core\\Private\\HAL\\PThreadRunnableThread.cpp"};
+  EXPECT_EQ(file_txt.value(), target_path);
+
+  // Even though we are on Windows, file paths in debug information are strings and should be
+  // considered strings. If the case doesn't match, it means the mapping refers to a different
+  // build.
+  const auto other_txt = MapToFirstMatchingTarget(
+      {mapping}, "C:\\ue4\\Engine\\Source\\Runtime\\Core\\Private\\HAL\\PThreadRunnableThread.cpp",
+      &AlwaysTruePredicate);
+  ASSERT_FALSE(other_txt.has_value());
+}
+#endif
+
 TEST(Mapping, MapToFirstMatchingTargetMultiple) {
   Mapping mapping0{"/build/project", "/home/user/project0"};
   Mapping mapping1{"/src/project2", "/home/user/project1"};
@@ -111,4 +135,20 @@ TEST(Mapping, InferMappingFromExampleIdentity) {
   auto maybe_mapping = InferMappingFromExample(source_path, target_path);
   ASSERT_FALSE(maybe_mapping);
 }
+
+#ifdef _WIN32
+TEST(Mapping, InferMappingFromExampleWindows) {
+  const std::filesystem::path source_path =
+      "C:\\UE4\\Engine\\Source\\Runtime\\Core\\Private\\HAL\\PThreadRunnableThread.cpp";
+  const std::filesystem::path target_path =
+      "C:/Users/user/Downloads/UE4_424/UE4/Engine/Source/Runtime/Core/Private/HAL/"
+      "PThreadRunnableThread.cpp";
+
+  auto maybe_mapping = InferMappingFromExample(source_path, target_path);
+  ASSERT_TRUE(maybe_mapping);
+  EXPECT_EQ(maybe_mapping.value().source_path, "C:\\");
+  EXPECT_EQ(maybe_mapping.value().target_path, "C:/Users/user/Downloads/UE4_424");
+}
+#endif
+
 }  // namespace orbit_source_paths_mapping
