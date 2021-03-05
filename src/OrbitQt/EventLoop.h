514a3c4a623e1c0c836146f031cb9c7721b05a89
sidhys1 <sidhys1@github.com>
2021-03-04 21:06:37 -0500

    Fixed accidental delete of a #ifndef line

--- src/OrbitQt/EventLoop.h
+++ src/OrbitQt/EventLoop.h
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifndef ORBIT_QT_EVENT_LOOP_H_
 #define ORBIT_QT_EVENT_LOOP_H_
 
 #include <QEventLoop>
