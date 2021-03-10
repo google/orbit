6e37a19d487a8a9857169cbf53e529560792547b
antonrohr <antonrohr@google.com>
2021-03-10 18:58:33 +0100

    Add TargetLabel class

---
+++ src/OrbitQt/TargetLabel.h
@@ -0,0 +1,67 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_QT_TARGET_LABEL_H_
+#define ORBIT_QT_TARGET_LABEL_H_
+
+#include <QObject>
+#include <QWidget>
+#include <memory>
+#include <optional>
+
+#include "OrbitClientData/ProcessData.h"
+#include "OrbitGgp/Instance.h"
+#include "TargetConfiguration.h"
+
+namespace Ui {
+class TargetLabel;  // IWYU pragma: keep
+}
+
+namespace orbit_qt {
+
+class TargetLabel : public QWidget {
+  Q_OBJECT
+ public:
+  enum class IconType { kGreenConnectedIcon, kOrangeDisconnectedIcon, kRedDisconnectedIcon };
+  explicit TargetLabel(QWidget* parent = nullptr);
+  ~TargetLabel() override;
+
+  void ChangeToFileTarget(const FileTarget& file_target);
+  void ChangeToFileTarget(const std::filesystem::path& path);
+  void ChangeToStadiaTarget(const StadiaTarget& stadia_target);
+  void ChangeToStadiaTarget(const ProcessData& process, const orbit_ggp::Instance& instance);
+  void ChangeToStadiaTarget(const QString& process_name, double cpu_usage,
+                            const QString& instance_name);
+  void ChangeToLocalTarget(const LocalTarget& local_target);
+  void ChangeToLocalTarget(const ProcessData& process);
+  void ChangeToLocalTarget(const QString& process_name, double cpu_usage);
+
+  bool SetProcessCpuUsageInPercent(double cpu_usage);
+  bool SetProcessEnded();
+  bool SetConnectionDead(const QString& error_message);
+
+  void Clear();
+
+  [[nodiscard]] QColor GetColor() const;
+  [[nodiscard]] QString GetText() const;
+  [[nodiscard]] QString GetToolTip() const { return toolTip(); }
+  [[nodiscard]] const std::optional<IconType>& GetIconType() const { return icon_type_; }
+
+ signals:
+  void SizeChanged();
+
+ private:
+  std::unique_ptr<Ui::TargetLabel> ui_;
+  QString process_;
+  QString machine_;
+  std::optional<IconType> icon_type_;
+
+  void SetColor(const QColor& color);
+  void SetIcon(IconType icon_type);
+  void ClearIcon();
+};
+
+}  // namespace orbit_qt
+
+#endif  // ORBIT_QT_TARGET_LABEL_H_
---
+++ src/OrbitQt/TargetLabelTest.cpp
@@ -0,0 +1,219 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+#include "TargetLabel.h"
+#include "process.pb.h"
+
+namespace orbit_qt {
+
+TEST(TargetLabel, Constructor) {
+  TargetLabel label{};
+  EXPECT_TRUE(label.GetText().isEmpty());
+  EXPECT_TRUE(label.GetToolTip().isEmpty());
+  EXPECT_FALSE(label.GetIconType().has_value());
+}
+
+TEST(TargetLabel, ChangeToFileTarget) {
+  TargetLabel label{};
+
+  const std::filesystem::path filename = "file.orbit";
+  const std::filesystem::path path = "dummy/path/" / filename;
+
+  label.ChangeToFileTarget(path);
+
+  EXPECT_EQ(label.GetText().toStdString(), filename);
+  EXPECT_TRUE(label.GetToolTip().isEmpty());
+  EXPECT_FALSE(label.GetIconType().has_value());
+}
+
+TEST(TargetLabel, ChangeToStadiaTarget) {
+  TargetLabel label{};
+  const QColor initial_color = label.GetColor();
+
+  const QString process_name = "test process";
+  const double cpu_usage = 50.1;
+  const QString instance_name = "test instance";
+
+  label.ChangeToStadiaTarget(process_name, cpu_usage, instance_name);
+
+  EXPECT_EQ(label.GetText(), "test process (50%) @ test instance");
+  EXPECT_TRUE(label.GetToolTip().isEmpty());
+  EXPECT_NE(label.GetColor(), initial_color);
+  ASSERT_TRUE(label.GetIconType().has_value());
+  EXPECT_EQ(label.GetIconType().value(), TargetLabel::IconType::kGreenConnectedIcon);
+}
+
+TEST(TargetLabel, ChangeToLocalTarget) {
+  TargetLabel label{};
+  const QColor initial_color = label.GetColor();
+
+  const QString process_name = "test process";
+  const double cpu_usage = 50.1;
+
+  label.ChangeToLocalTarget(process_name, cpu_usage);
+
+  EXPECT_EQ(label.GetText(), "test process (50%) @ localhost");
+  EXPECT_TRUE(label.GetToolTip().isEmpty());
+  EXPECT_NE(label.GetColor(), initial_color);
+  ASSERT_TRUE(label.GetIconType().has_value());
+  EXPECT_EQ(label.GetIconType().value(), TargetLabel::IconType::kGreenConnectedIcon);
+}
+
+TEST(TargetLabel, SetProcessCpuUsageInPercent) {
+  TargetLabel label({});
+  const QString initial_text = label.GetText();
+  const QString initial_tool_tip = label.GetToolTip();
+  const QColor initial_color = label.GetColor();
+
+  {
+    const bool result = label.SetProcessCpuUsageInPercent(20);
+    EXPECT_FALSE(result);
+    EXPECT_EQ(label.GetText(), initial_text);
+    EXPECT_EQ(label.GetToolTip(), initial_tool_tip);
+    EXPECT_EQ(label.GetColor(), initial_color);
+    EXPECT_FALSE(label.GetIconType().has_value());
+  }
+
+  label.ChangeToLocalTarget("test", 10.2);
+  const QString updated_text = label.GetText();
+  EXPECT_NE(updated_text, initial_text);
+  const QColor updated_color = label.GetColor();
+  EXPECT_NE(updated_color, initial_color);
+  {
+    const bool result = label.SetProcessCpuUsageInPercent(20.7);
+    EXPECT_TRUE(result);
+    EXPECT_NE(label.GetText(), updated_text);
+    EXPECT_TRUE(label.GetText().contains("21%"));
+    EXPECT_TRUE(label.GetToolTip().isEmpty());
+    EXPECT_EQ(label.GetColor(), updated_color);
+    ASSERT_TRUE(label.GetIconType().has_value());
+    EXPECT_EQ(label.GetIconType().value(), TargetLabel::IconType::kGreenConnectedIcon);
+  }
+}
+
+TEST(TargetLabel, SetProcessEnded) {
+  TargetLabel label({});
+  const QString initial_text = label.GetText();
+  const QString initial_tool_tip = label.GetToolTip();
+  const QColor initial_color = label.GetColor();
+
+  {
+    const bool result = label.SetProcessEnded();
+    EXPECT_FALSE(result);
+    EXPECT_EQ(label.GetText(), initial_text);
+    EXPECT_EQ(label.GetToolTip(), initial_tool_tip);
+    EXPECT_EQ(label.GetColor(), initial_color);
+    EXPECT_FALSE(label.GetIconType().has_value());
+  }
+
+  label.ChangeToLocalTarget("test", 10.2);
+  const QString updated_text = label.GetText();
+  EXPECT_NE(updated_text, initial_text);
+  const QColor updated_color = label.GetColor();
+  EXPECT_NE(updated_color, initial_color);
+  {
+    const bool result = label.SetProcessEnded();
+    EXPECT_TRUE(result);
+    EXPECT_NE(label.GetText(), updated_text);
+    EXPECT_FALSE(label.GetText().contains("10%"));
+    EXPECT_EQ(label.GetToolTip(), "The process ended.");
+    EXPECT_NE(label.GetColor(), initial_color);
+    EXPECT_NE(label.GetColor(), updated_color);
+    ASSERT_TRUE(label.GetIconType().has_value());
+    EXPECT_EQ(label.GetIconType().value(), TargetLabel::IconType::kOrangeDisconnectedIcon);
+  }
+}
+
+TEST(TargetLabel, SetConnectionDead) {
+  TargetLabel label({});
+  const QString initial_text = label.GetText();
+  const QString initial_tool_tip = label.GetToolTip();
+  const QColor initial_color = label.GetColor();
+
+  {
+    const bool result = label.SetConnectionDead("");
+    EXPECT_FALSE(result);
+    EXPECT_EQ(label.GetText(), initial_text);
+    EXPECT_EQ(label.GetToolTip(), initial_tool_tip);
+    EXPECT_EQ(label.GetColor(), initial_color);
+    EXPECT_FALSE(label.GetIconType().has_value());
+  }
+
+  label.ChangeToLocalTarget("test", 10.2);
+  const QString updated_text = label.GetText();
+  EXPECT_NE(updated_text, initial_text);
+  const QColor updated_color = label.GetColor();
+  EXPECT_NE(updated_color, initial_color);
+
+  const QString error_message = "test error message";
+  {
+    const bool result = label.SetConnectionDead(error_message);
+    EXPECT_TRUE(result);
+    EXPECT_NE(label.GetText(), updated_text);
+    EXPECT_FALSE(label.GetText().contains("10%"));
+    EXPECT_EQ(label.GetToolTip(), error_message);
+    EXPECT_NE(label.GetColor(), initial_color);
+    EXPECT_NE(label.GetColor(), updated_color);
+    ASSERT_TRUE(label.GetIconType().has_value());
+    EXPECT_EQ(label.GetIconType().value(), TargetLabel::IconType::kRedDisconnectedIcon);
+  }
+}
+
+TEST(TargetLabel, Clear) {
+  TargetLabel label;
+
+  label.ChangeToLocalTarget("test", 10.2);
+  label.SetProcessEnded();
+
+  EXPECT_FALSE(label.GetText().isEmpty());
+  EXPECT_FALSE(label.GetToolTip().isEmpty());
+
+  const QColor ended_color = label.GetColor();
+
+  label.Clear();
+  EXPECT_TRUE(label.GetText().isEmpty());
+  EXPECT_TRUE(label.GetToolTip().isEmpty());
+  EXPECT_NE(label.GetColor(), ended_color);
+  ASSERT_FALSE(label.GetIconType().has_value());
+}
+
+TEST(TargetLabel, DifferentColors) {
+  TargetLabel label{};
+
+  label.ChangeToFileTarget("test/path");
+  const QColor file_color = label.GetColor();
+
+  label.ChangeToStadiaTarget("test process", 0, "test instance");
+  const QColor stadia_color = label.GetColor();
+
+  label.ChangeToLocalTarget("test process", 0);
+  const QColor local_color = label.GetColor();
+
+  label.SetProcessCpuUsageInPercent(10);
+  const QColor cpu_usage_updated_color = label.GetColor();
+
+  label.SetProcessEnded();
+  const QColor process_ended_color = label.GetColor();
+
+  label.SetConnectionDead("test error");
+  const QColor connection_dead_color = label.GetColor();
+
+  EXPECT_EQ(stadia_color, local_color);
+  EXPECT_EQ(stadia_color, cpu_usage_updated_color);
+
+  EXPECT_NE(file_color, stadia_color);
+  EXPECT_NE(file_color, process_ended_color);
+  EXPECT_NE(file_color, connection_dead_color);
+
+  EXPECT_NE(stadia_color, process_ended_color);
+  EXPECT_NE(stadia_color, connection_dead_color);
+
+  EXPECT_NE(process_ended_color, connection_dead_color);
+}
+
+}  // namespace orbit_qt
