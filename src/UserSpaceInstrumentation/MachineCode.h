7ff4f283224a8be8abce0c682019ad1b76b40d90
danielfenner <56686285+danielfenner@users.noreply.github.com>
2021-03-10 15:32:49 +0100

    Close to trival tooling to write machine code. (#2022)
    This is really just a slightly more structured way to assemble a vector of
    bytes.
    
    bug: http://b/181304023

---
+++ src/UserSpaceInstrumentation/MachineCode.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef USER_SPACE_INSTRUMENTATION_MACHINE_CODE_H_
+#define USER_SPACE_INSTRUMENTATION_MACHINE_CODE_H_
+
+#include <cstdint>
+#include <vector>
+
+namespace orbit_user_space_instrumentation {
+
+// Tooling to write machine code and build the corresponding vector of bytes in a structured way.
+//
+// Usage example:
+// code.AppendBytes({0x48, 0xb8})
+//     .AppendImmediate64(address)
+//     .AppendBytes({0xff, 0xd0})
+//     .AppendBytes({0xcc});
+//
+// WriteTraceesMemory(pid, address_code, code.GetResultAsVector());
+//
+class MachineCode {
+ public:
+  MachineCode& AppendBytes(const std::vector<uint8_t>& data);
+  MachineCode& AppendImmediate64(uint64_t data);
+  MachineCode& AppendImmediate32(uint32_t data);
+  const std::vector<uint8_t>& GetResultAsVector() const { return data_; }
+
+ private:
+  std::vector<uint8_t> data_;
+};
+
+}  // namespace orbit_user_space_instrumentation
+
+#endif  // USER_SPACE_INSTRUMENTATION_MACHINE_CODE_H_
