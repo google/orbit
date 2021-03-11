86cb7af04f5723f4e0ae47e3d728b0521982adab
Florian Reichl <freichl@google.com>
2021-03-11 14:36:27 +0100

    Move coordinate conversions to new Viewport class

---
+++ src/OrbitGl/ViewportTest.cpp
@@ -0,0 +1,176 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+#include "Viewport.h"
+
+namespace orbit_gl {
+
+TEST(Viewport, ResizingAndDirty) {
+  Viewport viewport(100, 200);
+  EXPECT_TRUE(viewport.IsDirty());
+  viewport.ClearDirtyFlag();
+
+  // Test initial values
+  EXPECT_EQ(viewport.GetWidth(), 100);
+  EXPECT_EQ(viewport.GetHeight(), 200);
+  EXPECT_EQ(viewport.GetWorldExtents(), Vec2(100, 200));
+  EXPECT_EQ(viewport.GetWorldWidth(), 100.f);
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(0.f, 0.f));
+
+  // Test: Resizing should not affect the world extents
+  viewport.Resize(1000, 2000);
+  EXPECT_TRUE(viewport.IsDirty());
+  viewport.ClearDirtyFlag();
+
+  EXPECT_EQ(viewport.GetWidth(), 1000);
+  EXPECT_EQ(viewport.GetHeight(), 2000);
+  EXPECT_EQ(viewport.GetWorldWidth(), 100.f);
+  EXPECT_EQ(viewport.GetWorldHeight(), 200.f);
+  EXPECT_EQ(viewport.GetWorldExtents(), Vec2(100, 200));
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(0.f, 0.f));
+
+  // Test: Changing the world width / height should not affect screen extents
+  viewport.SetWorldWidth(500.f);
+  EXPECT_TRUE(viewport.IsDirty());
+  viewport.ClearDirtyFlag();
+
+  viewport.SetWorldHeight(600.f);
+  EXPECT_TRUE(viewport.IsDirty());
+  viewport.ClearDirtyFlag();
+
+  EXPECT_EQ(viewport.GetWidth(), 1000);
+  EXPECT_EQ(viewport.GetHeight(), 2000);
+  EXPECT_EQ(viewport.GetWorldWidth(), 500.f);
+  EXPECT_EQ(viewport.GetWorldHeight(), 600.f);
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(0.f, 0.f));
+  EXPECT_EQ(viewport.GetWorldExtents(), Vec2(100, 200));
+
+  // Test: Changing the world extent should not affect screen size and visible world size
+  viewport.SetWorldExtents(1000, 2000);
+  EXPECT_TRUE(viewport.IsDirty());
+  viewport.ClearDirtyFlag();
+
+  EXPECT_EQ(viewport.GetWidth(), 1000);
+  EXPECT_EQ(viewport.GetHeight(), 2000);
+  EXPECT_EQ(viewport.GetWorldWidth(), 500.f);
+  EXPECT_EQ(viewport.GetWorldHeight(), 600.f);
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(0.f, 0.f));
+  EXPECT_EQ(viewport.GetWorldExtents(), Vec2(1000, 2000));
+
+  // Test: Scrolling (within correct range) should mark as dirty
+  viewport.Resize(100, 200);
+  viewport.ClearDirtyFlag();
+
+  viewport.SetWorldTopLeftX(100.f);
+  EXPECT_TRUE(viewport.IsDirty());
+  viewport.ClearDirtyFlag();
+
+  viewport.SetWorldTopLeftY(-50.f);
+  EXPECT_TRUE(viewport.IsDirty());
+  viewport.ClearDirtyFlag();
+
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(100.f, -50.f));
+
+  // Setting everything to the same values again should not mark the viewport as dirty
+  viewport.Resize(100, 200);
+  viewport.SetWorldWidth(500.f);
+  viewport.SetWorldHeight(600.f);
+  viewport.SetWorldTopLeftX(100.f);
+  viewport.SetWorldTopLeftY(-50.f);
+  viewport.SetWorldExtents(1000, 2000);
+  EXPECT_FALSE(viewport.IsDirty());
+}
+
+TEST(Viewport, ScrollingWithoutZoom) {
+  Viewport viewport(100, 200);
+  viewport.SetWorldExtents(200, 300);
+
+  viewport.SetWorldTopLeftX(100.f);
+  viewport.SetWorldTopLeftY(-50.f);
+
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(100.f, -50.f));
+
+  viewport.SetWorldTopLeftX(150.f);
+  viewport.SetWorldTopLeftY(-150.f);
+
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(100.f, -100.f));
+  EXPECT_EQ(viewport.IsDirty(), true);
+  viewport.ClearDirtyFlag();
+
+  // Scrolling further after clamping should not result in a dirty flag
+  viewport.SetWorldTopLeftX(200.f);
+  viewport.SetWorldTopLeftY(-200.f);
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(100.f, -100.f));
+  EXPECT_EQ(viewport.IsDirty(), false);
+
+  // Resizing should clamp
+  viewport.SetWorldExtents(150, 250);
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(50.f, -50.f));
+}
+
+TEST(Viewport, ScrollingWithZoom) {
+  Viewport viewport(100, 200);
+  viewport.SetWorldWidth(200);
+  viewport.SetWorldHeight(400);
+  viewport.SetWorldExtents(400, 800);
+
+  viewport.SetWorldTopLeftX(250.f);
+  viewport.SetWorldTopLeftY(-350.f);
+
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(200.f, -350.f));
+
+  viewport.SetWorldTopLeftX(300.f);
+  viewport.SetWorldTopLeftY(-450.f);
+
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(200.f, -400.f));
+  EXPECT_EQ(viewport.IsDirty(), true);
+  viewport.ClearDirtyFlag();
+
+  // Scrolling further after clamping should not result in a dirty flag
+  viewport.SetWorldTopLeftX(350.f);
+  viewport.SetWorldTopLeftY(-600.f);
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(200.f, -400.f));
+  EXPECT_EQ(viewport.IsDirty(), false);
+
+  // Resizing should clamp
+  viewport.SetWorldExtents(250, 450);
+  EXPECT_EQ(viewport.GetWorldTopLeft(), Vec2(50.f, -50.f));
+}
+
+void VerifyConversion(Viewport& viewport, const Vec2i& screen_pos, const Vec2& world_pos,
+                      const Vec2& world_size) {
+  EXPECT_EQ(viewport.ScreenToWorldPos(screen_pos), world_pos);
+  EXPECT_EQ(viewport.ScreenToWorldHeight(screen_pos[1]), world_size[1]);
+  EXPECT_EQ(viewport.ScreenToWorldWidth(screen_pos[0]), world_size[0]);
+
+  EXPECT_EQ(viewport.WorldToScreenPos(world_pos), screen_pos);
+  EXPECT_EQ(viewport.WorldToScreenHeight(world_size[1]), screen_pos[1]);
+  EXPECT_EQ(viewport.WorldToScreenWidth(world_size[0]), screen_pos[0]);
+
+  EXPECT_EQ(viewport.QtToGlScreenPos(screen_pos),
+            Vec2i(screen_pos[0], viewport.GetHeight() - screen_pos[1]));
+}
+
+TEST(Viewport, CoordinateConversion) {
+  Viewport viewport(10, 100);
+  // Extents are large enough to scroll
+  viewport.SetWorldExtents(500, 500);
+  viewport.SetWorldWidth(20.f);
+  viewport.SetWorldHeight(50.f);
+
+  // NOTE: World space is y-down, but (0,0) is top left
+  Vec2i screen_pos = Vec2i(8, 20);
+  Vec2 world_pos = Vec2(16.f, -10.f);
+  Vec2 world_size = Vec2(16.f, 10.f);
+  VerifyConversion(viewport, screen_pos, world_pos, world_size);
+
+  viewport.SetWorldTopLeftX(10.f);
+  viewport.SetWorldTopLeftY(-100.f);
+  world_pos = Vec2(26.f, -110.f);
+  VerifyConversion(viewport, screen_pos, world_pos, world_size);
+}
+
+}  // namespace orbit_gl
