d725e0f5897dd89d00a1f23b55c0cd7c3f49d403
Davide Pallotti <dpallotti@google.com>
2021-03-18 11:38:31 +0100

    Add VulkanTutorial module
    This contains `OffscreenRenderingVulkanTutorialLib`, a simplified
    implementation of the Vulkan Tutorial triangle that renders to memory instead of
    to a surface. This will be used, possibly expanded, for GPU-related tests,
    starting with `LinuxTracingIntegrationTests`.
    It also contains `OffscreenRenderingVulkanTutorial` with a `main` that allows to
    run it.
    
    Bug: http://b/182793342
    
    Test: Run `OffscreenRenderingVulkanTutorial`, take a capture, see the expected
    GPU jobs in the capture.

---
+++ src/VulkanTutorial/OffscreenRenderingVulkanTutorialMain.cpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <atomic>
+#include <csignal>
+#include <thread>
+
+#include "VulkanTutorial/OffscreenRenderingVulkanTutorial.h"
+
+namespace {
+std::atomic<bool> exit_requested = false;
+
+void SigintHandler(int signum) {
+  if (signum == SIGINT) {
+    exit_requested = true;
+  }
+}
+
+// Use SIGINT to stop the main rendering loop of the VulkanTutorial, transition to its orderly
+// shutdown of Vulkan, and exit the program.
+void InstallSigintHandler() {
+  struct sigaction act {};
+  act.sa_handler = SigintHandler;
+  sigemptyset(&act.sa_mask);
+  act.sa_flags = 0;
+  act.sa_restorer = nullptr;
+  sigaction(SIGINT, &act, nullptr);
+}
+}  // namespace
+
+int main() {
+  InstallSigintHandler();
+
+  orbit_vulkan_tutorial::OffscreenRenderingVulkanTutorial tutorial;
+
+  std::thread thread{[&tutorial] {
+    while (!exit_requested) {
+      std::this_thread::sleep_for(std::chrono::milliseconds{100});
+    }
+    tutorial.StopAsync();
+  }};
+
+  tutorial.Run();
+  thread.join();
+
+  return 0;
+}
---
+++ src/VulkanTutorial/include/VulkanTutorial/OffscreenRenderingVulkanTutorial.h
@@ -0,0 +1,84 @@
+#ifndef VULKAN_TUTORIAL_OFFSCREEN_RENDERING_VULKAN_TUTORIAL_H_
+#define VULKAN_TUTORIAL_OFFSCREEN_RENDERING_VULKAN_TUTORIAL_H_
+
+#include <absl/base/thread_annotations.h>
+#include <absl/synchronization/mutex.h>
+#include <vulkan/vulkan.h>
+
+#include <cstdint>
+#include <optional>
+#include <thread>
+#include <vector>
+
+namespace orbit_vulkan_tutorial {
+
+// This class is a simplified version of the "triangle example" from the Vulkan Tutorial
+// (https://vulkan-tutorial.com/Drawing_a_triangle).
+// By rendering to a `VkImage` in memory instead of rendering to a surface, this saves us from
+// having to deal with the swap chain and from having to create a window.
+// Note, though, that this means that `vkQueuePresentKHR` is never called.
+class OffscreenRenderingVulkanTutorial {
+ public:
+  // This method runs the entire example from setup to cleanup, rendering `frame_count` frames
+  // offscreen in the main loop. If no parameter is passed, the main loop continues indefinitely
+  // until `StopAsync` is called (from a different thread).
+  void Run(uint64_t frame_count = std::numeric_limits<uint64_t>::max());
+
+  // Call this method from a different thread than `Run` to stop the main rendering loop when this
+  // is running indefinitely or before `frame_count` frames have been rendered.
+  void StopAsync();
+
+ private:
+  void InitVulkan();
+  void MainLoop(uint64_t frame_count);
+  void CleanUp();
+
+  struct QueueFamilyIndices {
+    std::optional<uint32_t> graphics_family;
+  };
+
+  void CreateInstance();
+  static bool AreValidationLayersSupported();
+  void PickPhysicalDevice();
+  static bool IsPhysicalDeviceSuitable(const VkPhysicalDevice& physical_device);
+  static QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice physical_device);
+  void CreateLogicalDevice();
+  void CreateOffscreenImage();
+  void CreateImageView();
+  void CreateRenderPass();
+  VkShaderModule CreateShaderModule(const uint8_t* shader_code, size_t shader_code_size);
+  void CreateGraphicsPipeline();
+  void CreateFramebuffer();
+  void CreateCommandPool();
+  void CreateCommandBuffer();
+  void CreateFence();
+  void DrawFrame();
+
+  static const std::vector<const char*> kValidationLayerNames;
+  static constexpr uint32_t kImageWidth = 800;
+  static constexpr uint32_t kImageHeight = 600;
+
+  VkInstance instance_ = VK_NULL_HANDLE;
+  VkPhysicalDevice physical_device_ = VK_NULL_HANDLE;
+  VkDevice device_ = VK_NULL_HANDLE;
+  VkQueue graphics_queue_ = VK_NULL_HANDLE;
+  VkFormat image_format_ = VkFormat::VK_FORMAT_UNDEFINED;
+  VkExtent3D image_extent_{};
+  VkImage image_ = VK_NULL_HANDLE;
+  VkDeviceMemory memory_ = VK_NULL_HANDLE;
+  VkImageView image_view_ = VK_NULL_HANDLE;
+  VkRenderPass render_pass_ = VK_NULL_HANDLE;
+  VkPipelineLayout pipeline_layout_ = VK_NULL_HANDLE;
+  VkPipeline graphics_pipeline_ = VK_NULL_HANDLE;
+  VkFramebuffer framebuffer_ = VK_NULL_HANDLE;
+  VkCommandPool command_pool_ = VK_NULL_HANDLE;
+  VkCommandBuffer command_buffer_ = VK_NULL_HANDLE;
+  VkFence fence_ = VK_NULL_HANDLE;
+
+  absl::Mutex stop_requested_mutex_;
+  bool stop_requested_ GUARDED_BY(stop_requested_mutex_) = false;
+};
+
+}  // namespace orbit_vulkan_tutorial
+
+#endif  // VULKAN_TUTORIAL_OFFSCREEN_RENDERING_VULKAN_TUTORIAL_H_
