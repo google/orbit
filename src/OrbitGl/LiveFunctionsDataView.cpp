a866217450f8ddd0102379f7afc0c2cba2cb6b64
IrinaShkviro <irinashkviro@google.com>
2021-04-08 14:03:20 +0200

    Remove nullptr checks for MetricsUploader

--- src/OrbitGl/LiveFunctionsDataView.cpp
+++ src/OrbitGl/LiveFunctionsDataView.cpp
@@ -331,10 +331,8 @@ void LiveFunctionsDataView::OnContextMenu(const std::string& action, int menu_in
           app_->GetCaptureData().GetFunctionStatsOrDefault(instrumented_function_id);
       if (stats.count() > 0) {
         live_functions_->AddIterator(instrumented_function_id, &instrumented_function);
-        if (metrics_uploader_ != nullptr) {
-          metrics_uploader_->SendLogEvent(
-              orbit_metrics_uploader::OrbitLogEvent_LogEventType_ORBIT_ITERATOR_ADD);
-        }
+        metrics_uploader_->SendLogEvent(
+            orbit_metrics_uploader::OrbitLogEvent_LogEventType_ORBIT_ITERATOR_ADD);
       }
     }
   } else if (action == kMenuActionEnableFrameTrack) {
