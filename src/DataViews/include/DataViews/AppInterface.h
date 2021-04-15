f4e793a6e67738c6ed8cad1db13ff0abd0580fd9
Henning Becker <henning.becker@gmail.com>
2021-04-15 17:10:16 +0200

    Add functions to AppInterface
    The functions added to AppInterface are the ones needed by DataView (the
    base class).

--- src/DataViews/include/DataViews/AppInterface.h
+++ src/DataViews/include/DataViews/AppInterface.h
@@ -5,11 +5,27 @@
 #ifndef DATA_VIEWS_APP_INTERFACE_H_
 #define DATA_VIEWS_APP_INTERFACE_H_
 
+#include <absl/types/span.h>
+#include <stdint.h>
+
+#include <string>
+
+#include "ClientData/ModuleData.h"
+#include "ClientData/ProcessData.h"
+#include "ClientModel/CaptureData.h"
+#include "OrbitBase/Future.h"
+#include "capture_data.pb.h"
+
 namespace orbit_data_views {
 
 class AppInterface {
  public:
   virtual ~AppInterface() noexcept = default;
+
+  virtual void SetClipboard(const std::string& contents) = 0;
+  [[nodiscard]] virtual std::string GetSaveFile(const std::string& extension) const = 0;
+
+  virtual void SendErrorToUi(const std::string& title, const std::string& text) = 0;
 };
 
 }  // namespace orbit_data_views
