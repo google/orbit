dcd67273f3a7329ccd3c2ff07fc14c561b2e75e9
Henning Becker <henning.becker@gmail.com>
2021-04-15 14:39:25 +0200

    Add DataViews module and AppInterface
    orbit_data_views::AppInterface will be implemented by OrbitApp and will
    provide all the methods needed by the data views.
    
    Having an interface class allows for easy mocking as long as we don't
    have proper decoupling yet.

---
+++ src/DataViews/include/DataViews/AppInterface.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DATA_VIEWS_APP_INTERFACE_H_
+#define DATA_VIEWS_APP_INTERFACE_H_
+
+namespace orbit_data_views {
+
+class AppInterface {
+ public:
+  virtual ~AppInterface() noexcept = default;
+};
+
+}  // namespace orbit_data_views
+
+#endif  // DATA_VIEWS_APP_INTERFACE_H_
