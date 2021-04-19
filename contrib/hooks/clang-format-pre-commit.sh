58a0c61249e0b2febf39b40719f8cbf3a023145d
Florian Kübler <kuebler@google.com>
2021-04-19 14:35:11 +0000

    Add a clang-format script to be invoked as pre-commit hook.
    This is a first step into having an automated formatting upon commits.
    It adds a script that can be executed from the pre-commit hook directly.
    
    All the files touched (staged and unstaged) will be formatted completely
    and the script will ask you which of the changes should be included
    to the commit. Thus it also works with when working with hunks.
    
    Test: Add "source path/to/file/" to the pre-commit hook script.

---
+++ contrib/hooks/clang-format-pre-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright (c) 2021 The Orbit Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# A pre-commit hook that applies clang-format-9 to all the changes
+# (staged and unstaged).
+# After the formatting has been applied, it will ask you which changes should
+
+echo "Formatting all changed files now..."
+git clang-format-9 -f
+# Only show this to the user of there are unstaged changes.
+if ! (git diff -- --quiet)
+then
+  exec < /dev/tty
+  echo "\n\n"
+  echo "Please select the changes to be included in this commit ('a' in include all):"
+  git add -p .
+fi
+
