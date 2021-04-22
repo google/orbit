c6f80bd2b7e33f9336e49ae0e40a28d7a4e667b4
danielfenner <56686285+danielfenner@users.noreply.github.com>
2021-04-22 20:43:03 +0200

    Tooling for trampolines.
    AllInstructionPointersFromProcess returns the set of the instruction
    pointers from all the threads of a halted tracee.
    
    LengthOfOverriddenInstructions returns the number of bytes occupied by
    the instructions that got hit when overriding a given number of bytes in
    the beginning of a piece of code. (Proper documentation in the header)
    
    Bug: http://b/185933738, http://b/185935100
    Test: Unit tests in PR.

--- src/UserSpaceInstrumentation/Trampoline.cpp
+++ src/UserSpaceInstrumentation/Trampoline.cpp
@@ -14,8 +14,11 @@
 
 #include "AllocateInTracee.h"
 #include "OrbitBase/ExecuteCommand.h"
+#include "OrbitBase/GetProcessIds.h"
 #include "OrbitBase/Logging.h"
 #include "OrbitBase/ReadFileToString.h"
+#include "OrbitBase/UniqueResource.h"
+#include "RegisterState.h"
 
 namespace orbit_user_space_instrumentation {
 
@@ -156,6 +159,40 @@ ErrorMessageOr<uint64_t> AllocateMemoryForTrampolines(pid_t pid, const AddressRa
   OUTCOME_TRY(unavailable_ranges, GetUnavailableAddressRanges(pid));
   OUTCOME_TRY(address_range, FindAddressRangeForTrampoline(unavailable_ranges, code_range, size));
   return AllocateInTracee(pid, address_range.start, size);
+}
+
+ErrorMessageOr<absl::flat_hash_set<uint64_t>> GetInstructionPointersFromProcess(pid_t pid) {
+  absl::flat_hash_set<uint64_t> result;
+  std::vector<pid_t> tids = orbit_base::GetTidsOfProcess(pid);
+  for (pid_t tid : tids) {
+    RegisterState registers;
+    OUTCOME_TRY(registers.BackupRegisters(tid));
+    result.insert(registers.GetGeneralPurposeRegisters()->x86_64.rip);
+  }
+  return result;
+}
+
+std::optional<int> LengthOfOverwrittenInstructions(csh handle, const std::vector<uint8_t>& code,
+                                                   int bytes_to_overwrite) {
+  cs_insn* instruction = cs_malloc(handle);
+  CHECK(instruction != nullptr);
+  orbit_base::unique_resource scope_exit{instruction,
+                                         [](cs_insn* instruction) { cs_free(instruction, 1); }};
+
+  const uint8_t* code_pointer = code.data();
+  size_t code_size = code.size();
+  uint64_t address = 0;
+  int length = 0;
+  while (cs_disasm_iter(handle, &code_pointer, &code_size, &address, instruction)) {
+    length += instruction->size;
+    if (length >= bytes_to_overwrite) break;
+  }
+
+  // Function too short?
+  if (length < bytes_to_overwrite) {
+    return std::nullopt;
+  }
+  return length;
 }
 
 }  // namespace orbit_user_space_instrumentation
