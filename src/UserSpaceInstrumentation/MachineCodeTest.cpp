a1491e24349e20ece57b85826f241725b9bab6a1
Daniel Fenner <dfenner@google.com>
2021-05-03 13:54:03 +0200

    Add overloads to AppendImmediate*

--- src/UserSpaceInstrumentation/MachineCodeTest.cpp
+++ src/UserSpaceInstrumentation/MachineCodeTest.cpp
@@ -12,11 +12,19 @@ namespace orbit_user_space_instrumentation {
 
 TEST(MachineCodeTest, BuildCode) {
   MachineCode code;
-  code.AppendBytes({0x48, 0xc7, 0xc3}).AppendImmediate32(0x42).AppendImmediate64(0x54);
+  uint32_t kSigned8 = 0x08;
+  uint32_t kUnsigned32 = 0x42;
+  int32_t kSigned32 = -1;
+  uint64_t kUnsigned64 = 0x54;
+  code.AppendBytes({0x48, 0xc7, 0xc3})
+      .AppendImmediate8(kSigned8)
+      .AppendImmediate32(kUnsigned32)
+      .AppendImmediate32(kSigned32)
+      .AppendImmediate64(kUnsigned64);
 
   EXPECT_EQ(code.GetResultAsVector(),
-            std::vector<uint8_t>({0x48, 0xc7, 0xc3, 0x42, 0x00, 0x00, 0x00, 0x54, 0x00, 0x00, 0x00,
-                                  0x00, 0x00, 0x00, 0x00}));
+            std::vector<uint8_t>({0x48, 0xc7, 0xc3, 0x08, 0x42, 0x00, 0x00, 0x00, 0xff, 0xff,
+                                  0xff, 0xff, 0x54, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}));
 }
 
 }  // namespace orbit_user_space_instrumentation
