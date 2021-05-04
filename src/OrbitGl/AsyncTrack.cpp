c0f16468510dd3327a1c582a130a9b91841143cd
Pierric Gimmig <pierric@google.com>
2021-05-04 11:08:38 -0700

    Fix crash when hovering on manually instrumented async scope
    TimerInfo::kApiEvent don't have an "InstrumentedFunction" associated
    with them so don't expect one.
    
    Test: profile OrbitTest with manual instrumentation enabled, hover on
    a profiling scope in one of the "async" tracks.

--- src/OrbitGl/AsyncTrack.cpp
+++ src/OrbitGl/AsyncTrack.cpp
@@ -54,7 +54,8 @@ AsyncTrack::AsyncTrack(CaptureViewElement* parent, TimeGraph* time_graph,
       capture_data_
           ? capture_data_->GetInstrumentedFunctionById(text_box->GetTimerInfo().function_id())
           : nullptr;
-  CHECK(func || timer_info.type() == TimerInfo::kIntrospection);
+  CHECK(func || timer_info.type() == TimerInfo::kIntrospection ||
+        timer_info.type() == TimerInfo::kApiEvent);
   std::string module_name =
       func != nullptr ? function_utils::GetLoadedModuleNameByPath(func->file_path()) : "unknown";
   const uint64_t event_id = event.data;
