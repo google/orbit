ca036e42e32788f086c9ec307de5fe35f571ac8d
dimitry <dimitry@google.com>
2021-05-11 19:40:44 +0000

    Save function names in presets
    Get rid of hashes in preset files.
    
    Bug: http://b/168799822
    Test: load old preset file
    Test: save preset to a file
    Test: load new preset file

--- src/OrbitGl/PresetFile.h
+++ src/OrbitGl/PresetFile.h
@@ -5,13 +5,38 @@
 #ifndef ORBIT_GL_PRESET_FILE_H_
 #define ORBIT_GL_PRESET_FILE_H_
 
+#include <filesystem>
+#include <vector>
+
 #include "preset.pb.h"
 
 namespace orbit_gl {
 
-struct PresetFile {
-  std::filesystem::path file_path;
-  orbit_client_protos::PresetInfo preset_info;
+class PresetFile final {
+ public:
+  explicit PresetFile(std::filesystem::path file_path, orbit_client_protos::PresetInfo preset_info)
+      : file_path_{std::move(file_path)}, preset_info_{std::move(preset_info)} {}
+
+  [[nodiscard]] const std::filesystem::path& file_path() const { return file_path_; }
+
+  [[nodiscard]] std::vector<std::filesystem::path> GetModulePaths() const;
+  [[nodiscard]] size_t GetNumberOfFunctionsForModule(
+      const std::filesystem::path& module_path) const;
+  [[nodiscard]] bool IsLegacyFileFormat() const;
+
+  [[nodiscard]] std::vector<uint64_t> GetSelectedFunctionHashesForModuleLegacy(
+      const std::filesystem::path& module_path) const;
+  [[nodiscard]] std::vector<uint64_t> GetFrameTrackFunctionHashesForModuleLegacy(
+      const std::filesystem::path& module_path) const;
+
+  [[nodiscard]] std::vector<std::string> GetSelectedFunctionNamesForModule(
+      const std::filesystem::path& module_path) const;
+  [[nodiscard]] std::vector<std::string> GetFrameTrackFunctionNamesForModule(
+      const std::filesystem::path& module_path) const;
+
+ private:
+  std::filesystem::path file_path_;
+  orbit_client_protos::PresetInfo preset_info_;
 };
 
 }  // namespace orbit_gl
