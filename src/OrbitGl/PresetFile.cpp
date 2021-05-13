7594b11f6ceab91c0204508304177e7c59a95453
dimitry <dimitry@google.com>
2021-05-13 22:07:46 +0000

    Make new preset format human-readable
    And enable auto-conversion from old to new format on successful
    preset load.
    
    Bug: http://b/168799822
    Test: Load preset in old format, check that it is in new format
          and human readable.

--- src/OrbitGl/PresetFile.cpp
+++ src/OrbitGl/PresetFile.cpp
@@ -4,12 +4,46 @@
 
 #include "PresetFile.h"
 
+#include <absl/strings/match.h>
+#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+#include <google/protobuf/text_format.h>
+
+#include "OrbitBase/File.h"
 #include "OrbitBase/Logging.h"
+#include "OrbitBase/ReadFileToString.h"
 
 namespace orbit_gl {
 
+using orbit_client_protos::PresetInfo;
+using orbit_client_protos::PresetInfoLegacy;
+
+namespace {
+
+constexpr const char* kPresetFileSignature = "# ORBIT preset file\n";
+
+ErrorMessageOr<PresetInfo> ReadPresetFromString(std::string_view content) {
+  google::protobuf::io::ArrayInputStream is{content.data(), static_cast<int>(content.size())};
+  PresetInfo preset_info;
+  if (!google::protobuf::TextFormat::Parse(&is, &preset_info)) {
+    return ErrorMessage{"Unable to parse message"};
+  }
+
+  return preset_info;
+}
+
+ErrorMessageOr<PresetInfoLegacy> ReadLegacyPresetFromString(const std::string& content) {
+  PresetInfoLegacy preset_info;
+  if (!preset_info.ParseFromString(content)) {
+    return ErrorMessage{"Unable to parse message"};
+  }
+
+  return preset_info;
+}
+
+}  // namespace
+
 [[nodiscard]] static std::vector<std::filesystem::path> GetPresetModulePathsLegacy(
-    const orbit_client_protos::PresetInfo& preset_info) {
+    const orbit_client_protos::PresetInfoLegacy& preset_info) {
   std::vector<std::filesystem::path> module_paths;
   for (const auto& entry : preset_info.path_to_module()) {
     module_paths.emplace_back(entry.first);
@@ -19,8 +53,8 @@ namespace orbit_gl {
 }
 
 std::vector<std::filesystem::path> PresetFile::GetModulePaths() const {
-  if (preset_info_.modules().empty()) {
-    return GetPresetModulePathsLegacy(preset_info_);
+  if (IsLegacyFileFormat()) {
+    return GetPresetModulePathsLegacy(preset_info_legacy_);
   }
 
   std::vector<std::filesystem::path> module_paths;
@@ -32,20 +66,20 @@ std::vector<std::filesystem::path> PresetFile::GetModulePaths() const {
 }
 
 size_t PresetFile::GetNumberOfFunctionsForModule(const std::filesystem::path& module_path) const {
-  if (preset_info_.modules().empty()) {
-    return preset_info_.path_to_module().at(module_path.string()).function_hashes_size();
+  if (IsLegacyFileFormat()) {
+    return preset_info_legacy_.path_to_module().at(module_path.string()).function_hashes_size();
   }
 
   return preset_info_.modules().at(module_path.string()).function_names_size();
 }
 
-bool PresetFile::IsLegacyFileFormat() const { return preset_info_.modules().empty(); }
+bool PresetFile::IsLegacyFileFormat() const { return is_legacy_format_; }
 
 std::vector<uint64_t> PresetFile::GetSelectedFunctionHashesForModuleLegacy(
     const std::filesystem::path& module_path) const {
   CHECK(IsLegacyFileFormat());
   const auto& function_hashes =
-      preset_info_.path_to_module().at(module_path.string()).function_hashes();
+      preset_info_legacy_.path_to_module().at(module_path.string()).function_hashes();
   return {function_hashes.begin(), function_hashes.end()};
 }
 
@@ -53,7 +87,7 @@ std::vector<uint64_t> PresetFile::GetFrameTrackFunctionHashesForModuleLegacy(
     const std::filesystem::path& module_path) const {
   CHECK(IsLegacyFileFormat());
   const auto& frame_track_function_hashes =
-      preset_info_.path_to_module().at(module_path.string()).frame_track_function_hashes();
+      preset_info_legacy_.path_to_module().at(module_path.string()).frame_track_function_hashes();
   return {frame_track_function_hashes.begin(), frame_track_function_hashes.end()};
 }
 
@@ -70,6 +104,50 @@ std::vector<std::string> PresetFile::GetFrameTrackFunctionNamesForModule(
   const auto& function_names =
       preset_info_.modules().at(module_path.string()).frame_track_function_names();
   return {function_names.begin(), function_names.end()};
+}
+
+ErrorMessageOr<PresetFile> ReadPresetFromFile(const std::filesystem::path& file_path) {
+  OUTCOME_TRY(file_content, orbit_base::ReadFileToString(file_path));
+
+  // If signature is not detected assume the file is in old format.
+  if (!absl::StartsWith(file_content, kPresetFileSignature)) {
+    OUTCOME_TRY(preset_info_legacy, ReadLegacyPresetFromString(file_content));
+    return PresetFile{file_path, preset_info_legacy};
+  }
+
+  OUTCOME_TRY(preset_info,
+              ReadPresetFromString(file_content.c_str() + strlen(kPresetFileSignature)));
+  return PresetFile{file_path, preset_info};
+}
+
+ErrorMessageOr<void> PresetFile::SaveToFile() const {
+  CHECK(!is_legacy_format_);
+
+  OUTCOME_TRY(fd, orbit_base::OpenFileForWriting(file_path_));
+  LOG("Saving preset to \"%s\"", file_path_.string());
+
+  auto write_result = orbit_base::WriteFully(fd, kPresetFileSignature);
+  if (write_result.has_error()) {
+    std::string error_message = absl::StrFormat(
+        "Failed to save preset to \"%s\": %s", file_path_.string(), write_result.error().message());
+    ERROR("%s", error_message);
+    return ErrorMessage(error_message);
+  }
+
+  std::string content;
+  if (!google::protobuf::TextFormat::PrintToString(preset_info_, &content)) {
+    return ErrorMessage{"Unable to convert message to string"};
+  }
+
+  write_result = orbit_base::WriteFully(fd, content);
+  if (write_result.has_error()) {
+    std::string error_message = absl::StrFormat(
+        "Failed to save preset to \"%s\": %s", file_path_.string(), write_result.error().message());
+    ERROR("%s", error_message);
+    return ErrorMessage(error_message);
+  }
+
+  return outcome::success();
 }
 
 }  // namespace orbit_gl
