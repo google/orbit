f8e0aeb928453ea9b019e17dc51f916f2a9aeddd
Henning Becker <henning.becker@gmail.com>
2021-05-19 14:17:46 +0200

    Move orbit style into its separate module

---
+++ src/Style/include/Style/Style.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef STYLE_STYLE_H_
+#define STYLE_STYLE_H_
+
+#include <QApplication>
+
+namespace orbit_style {
+void ApplyStyle(QApplication* app);
+}
+
+#endif  // STYLE_STYLE_H_
