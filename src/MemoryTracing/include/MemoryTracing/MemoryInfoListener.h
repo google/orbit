3694f63fef1c6022d9bb1d033e8f564f53d8314e
Tingwei LIU <vickyliu@google.com>
2021-05-19 17:10:59 +0200

    Update MemoryInfoListener
    With this change, we update the MemoryInfoListener to support handling
    CGroupMemoryUsage and ProcessMemoryUsage.

--- src/MemoryTracing/include/MemoryTracing/MemoryInfoListener.h
+++ src/MemoryTracing/include/MemoryTracing/MemoryInfoListener.h
@@ -9,11 +9,13 @@
 
 namespace orbit_memory_tracing {
 
-// This class serves as an event listener for the SystemMemoryUsage event.
+// This class serves as an event listener for the memory events.
 class MemoryInfoListener {
  public:
   virtual ~MemoryInfoListener() = default;
   virtual void OnSystemMemoryUsage(orbit_grpc_protos::SystemMemoryUsage memory_info) = 0;
+  virtual void OnProcessMemoryUsage(orbit_grpc_protos::ProcessMemoryUsage memory_info) = 0;
+  virtual void OnCGroupMemoryUsage(orbit_grpc_protos::CGroupMemoryUsage memory_info) = 0;
 };
 
 }  // namespace orbit_memory_tracing
