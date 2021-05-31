fcd83ca194bade67fc51de3b215a3986c4452f01
antonrohr <antonrohr@google.com>
2021-05-31 14:51:38 +0000

    Add CaptureFileInfo Manager class and tests
    http://b/178379643

---
+++ src/CaptureFileInfo/Manager.cpp
@@ -0,0 +1,80 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "CaptureFileInfo/Manager.h"
+
+#include <QDateTime>
+#include <QSettings>
+#include <algorithm>
+
+#include "CaptureFileInfo/CaptureFileInfo.h"
+
+constexpr const char* kCaptureFileInfoArrayKey = "capture_file_infos";
+constexpr const char* kCaptureFileInfoPathKey = "capture_file_info_path";
+constexpr const char* kCaptureFileInfoLastUsedKey = "capture_file_info_last_used";
+
+namespace orbit_capture_file_info {
+
+Manager::Manager() {
+  LoadCaptureFileInfos();
+  PurgeNonExistingFiles();
+}
+
+void Manager::LoadCaptureFileInfos() {
+  QSettings settings{};
+  const int size = settings.beginReadArray(kCaptureFileInfoArrayKey);
+  capture_file_infos_.clear();
+  capture_file_infos_.reserve(size);
+
+  for (int i = 0; i < size; ++i) {
+    settings.setArrayIndex(i);
+    QString path = settings.value(kCaptureFileInfoPathKey).toString();
+    QDateTime last_used(settings.value(kCaptureFileInfoLastUsedKey).toDateTime());
+    capture_file_infos_.emplace_back(path, std::move(last_used));
+  }
+  settings.endArray();
+}
+
+void Manager::SaveCaptureFileInfos() {
+  QSettings settings{};
+  settings.beginWriteArray(kCaptureFileInfoArrayKey, static_cast<int>(capture_file_infos_.size()));
+  for (size_t i = 0; i < capture_file_infos_.size(); ++i) {
+    settings.setArrayIndex(i);
+    const CaptureFileInfo& capture_file_info = capture_file_infos_[i];
+    settings.setValue(kCaptureFileInfoPathKey, capture_file_info.FilePath());
+    settings.setValue(kCaptureFileInfoLastUsedKey, capture_file_info.LastUsed());
+  }
+  settings.endArray();
+}
+
+void Manager::AddOrTouchCaptureFile(const QString& path) {
+  auto it = std::find_if(capture_file_infos_.begin(), capture_file_infos_.end(),
+                         [&](const CaptureFileInfo& capture_file_info) {
+                           return capture_file_info.FilePath() == path;
+                         });
+
+  if (it == capture_file_infos_.end()) {
+    capture_file_infos_.emplace_back(path);
+  } else {
+    it->Touch();
+  }
+
+  SaveCaptureFileInfos();
+}
+
+void Manager::Clear() {
+  capture_file_infos_.clear();
+  SaveCaptureFileInfos();
+}
+
+void Manager::PurgeNonExistingFiles() {
+  capture_file_infos_.erase(std::remove_if(capture_file_infos_.begin(), capture_file_infos_.end(),
+                                           [](const CaptureFileInfo& capture_file_info) {
+                                             return !capture_file_info.FileExists();
+                                           }),
+                            capture_file_infos_.end());
+  SaveCaptureFileInfos();
+}
+
+}  // namespace orbit_capture_file_info
