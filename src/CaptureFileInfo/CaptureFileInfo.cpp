d5de83afd1670c04d0fc71dc5f384a9202792c5f
antonrohr <antonrohr@google.com>
2021-05-31 13:25:04 +0000

    Add CaptureFileInfo class and test
    http://b/178379643

---
+++ src/CaptureFileInfo/CaptureFileInfo.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "CaptureFileInfo/CaptureFileInfo.h"
+
+namespace orbit_capture_file_info {
+
+CaptureFileInfo::CaptureFileInfo(const QString& path)
+    : file_info_(path), last_used_(QDateTime::currentDateTime()) {}
+
+CaptureFileInfo::CaptureFileInfo(const QString& path, QDateTime last_used)
+    : file_info_(path), last_used_(std::move(last_used)) {}
+
+bool CaptureFileInfo::FileExists() const { return file_info_.exists() && file_info_.isFile(); }
+
+}  // namespace orbit_capture_file_info
---
+++ src/CaptureFileInfo/include/CaptureFileInfo/CaptureFileInfo.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CAPTURE_FILE_INFO_CAPTURE_FILE_INFO_H_
+#define CAPTURE_FILE_INFO_CAPTURE_FILE_INFO_H_
+
+#include <QDateTime>
+#include <QFileInfo>
+#include <filesystem>
+#include <utility>
+
+namespace orbit_capture_file_info {
+
+class CaptureFileInfo {
+ public:
+  explicit CaptureFileInfo(const QString& path);
+  explicit CaptureFileInfo(const QString& path, QDateTime last_used);
+
+  [[nodiscard]] QString FilePath() const { return file_info_.filePath(); }
+  [[nodiscard]] QString FileName() const { return file_info_.fileName(); }
+
+  [[nodiscard]] QDateTime LastUsed() const { return last_used_; }
+  [[nodiscard]] QDateTime Created() const { return file_info_.birthTime(); }
+
+  [[nodiscard]] bool FileExists() const;
+
+  void Touch() { last_used_ = QDateTime::currentDateTime(); }
+
+ private:
+  QFileInfo file_info_;
+  QDateTime last_used_;
+};
+
+}  // namespace orbit_capture_file_info
+
+#endif  // CAPTURE_FILE_INFO_CAPTURE_FILE_INFO_H_
