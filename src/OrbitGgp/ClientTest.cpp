71c62fa921594bb4871321f82b4968a270fbc668
antonrohr <antonrohr@google.com>
2021-06-16 13:01:01 +0000

    Add tests for OrbitGgp::Client
    http://b/185089025

---
+++ src/OrbitGgp/ClientTest.cpp
@@ -0,0 +1,127 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gmock/gmock-matchers.h>
+#include <gtest/gtest.h>
+
+#include <QCoreApplication>
+#include <QString>
+#include <chrono>
+
+#include "OrbitBase/ExecutablePath.h"
+#include "OrbitBase/Result.h"
+#include "OrbitBase/TestUtils.h"
+#include "OrbitGgp/Client.h"
+#include "OrbitGgp/Instance.h"
+#include "OrbitGgp/SshInfo.h"
+
+namespace orbit_ggp {
+
+using orbit_base::HasError;
+using orbit_base::HasValue;
+
+TEST(OrbitGgpClient, CreateFailing) {
+  {
+    constexpr const char* kNonExistentProgram = "path/to/not/existing/program";
+    auto client = Client::Create(nullptr, kNonExistentProgram);
+    EXPECT_THAT(client, HasError("No such file or directory"));
+  }
+  {
+    const std::filesystem::path mock_ggp_failing =
+        orbit_base::GetExecutableDir() / "OrbitMockGgpFailing";
+    auto client = Client::Create(nullptr, QString::fromStdString(mock_ggp_failing.string()));
+    EXPECT_THAT(client, HasError("exit code: 5"));
+  }
+}
+
+TEST(OrbitGgpClient, GetInstancesAsyncWorking) {
+  const std::filesystem::path mock_ggp_working =
+      orbit_base::GetExecutableDir() / "OrbitMockGgpWorking";
+
+  auto client = Client::Create(nullptr, QString::fromStdString(mock_ggp_working.string()));
+  ASSERT_THAT(client, HasValue());
+
+  bool callback_was_called = false;
+  client.value()->GetInstancesAsync(
+      [&callback_was_called](outcome::result<QVector<Instance>> instances) {
+        QCoreApplication::exit();
+        callback_was_called = true;
+        ASSERT_TRUE(instances.has_value());
+        EXPECT_EQ(instances.value().size(), 2);
+      });
+
+  QCoreApplication::exec();
+
+  EXPECT_TRUE(callback_was_called);
+}
+
+TEST(OrbitGgpClient, GetInstancesAsyncTimeout) {
+  const std::filesystem::path mock_ggp_working_slow =
+      orbit_base::GetExecutableDir() / "OrbitMockGgpWorking";
+
+  auto client = Client::Create(nullptr, QString::fromStdString(mock_ggp_working_slow.string()),
+                               std::chrono::milliseconds{5});
+  ASSERT_THAT(client, HasValue());
+
+  bool callback_was_called = false;
+  client.value()->GetInstancesAsync(
+      [&callback_was_called](const outcome::result<QVector<Instance>>& instances) {
+        QCoreApplication::exit();
+        callback_was_called = true;
+        EXPECT_TRUE(instances.has_error());
+      },
+      0);
+
+  QCoreApplication::exec();
+
+  EXPECT_TRUE(callback_was_called);
+}
+
+TEST(OrbitGgpClient, GetSshInfoAsyncWorking) {
+  const std::filesystem::path mock_ggp_working =
+      orbit_base::GetExecutableDir() / "OrbitMockGgpWorking";
+
+  auto client = Client::Create(nullptr, QString::fromStdString(mock_ggp_working.string()));
+  ASSERT_THAT(client, HasValue());
+
+  Instance test_instance;
+  test_instance.id = "instance/test/id";
+
+  bool callback_was_called = false;
+  client.value()->GetSshInfoAsync(test_instance,
+                                  [&callback_was_called](const outcome::result<SshInfo>& ssh_info) {
+                                    QCoreApplication::exit();
+                                    callback_was_called = true;
+                                    EXPECT_TRUE(ssh_info.has_value());
+                                  });
+  QCoreApplication::exec();
+
+  EXPECT_TRUE(callback_was_called);
+}
+
+TEST(OrbitGgpClient, GetSshInfoAsyncTimeout) {
+  const std::filesystem::path mock_ggp_working_slow =
+      orbit_base::GetExecutableDir() / "OrbitMockGgpWorking";
+
+  Instance test_instance;
+  test_instance.id = "instance/test/id";
+
+  auto client = Client::Create(nullptr, QString::fromStdString(mock_ggp_working_slow.string()),
+                               std::chrono::milliseconds{5});
+  ASSERT_THAT(client, HasValue());
+
+  bool callback_was_called = false;
+  client.value()->GetSshInfoAsync(test_instance,
+                                  [&callback_was_called](const outcome::result<SshInfo>& ssh_info) {
+                                    QCoreApplication::exit();
+                                    callback_was_called = true;
+                                    EXPECT_TRUE(ssh_info.has_error());
+                                  });
+
+  QCoreApplication::exec();
+
+  EXPECT_TRUE(callback_was_called);
+}
+
+}  // namespace orbit_ggp
---
+++ src/OrbitGgp/MockGgp/Working.cpp
@@ -0,0 +1,81 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+int GgpVersion(char* argv[]) {
+  if (std::string_view{argv[1]} != "version") {
+    std::cout << "arguments are formatted wrong" << std::endl;
+    return 1;
+  }
+
+  std::cout << "12345.1.67.0 Mon 12 Dec 2012 12:12:12 PM UTC" << std::endl;
+  return 0;
+}
+
+int GgpInstanceList(char* argv[]) {
+  if (std::string_view{argv[1]} != "instance" || std::string_view{argv[2]} != "list" ||
+      std::string_view{argv[3]} != "-s") {
+    std::cout << "arguments are formatted wrong" << std::endl;
+    return 1;
+  }
+  std::cout << R"([
+ {
+  "displayName": "displayName-1",
+  "id": "id/of/instance1",
+  "ipAddress": "123.456.789.012",
+  "lastUpdated": "2012-12-12T12:12:12Z",
+  "owner": "owner@",
+  "pool": "pool-of-test_instance_1"
+ },
+ {
+  "displayName": "displayName-2",
+  "id": "id/of/instance2",
+  "ipAddress": "123.456.789.012",
+  "lastUpdated": "2012-12-12T12:12:12Z",
+  "owner": "owner@",
+  "pool": "pool-of-test_instance_2"
+ }
+])" << std::endl;
+  return 0;
+}
+
+int GgpSshInit(char* argv[]) {
+  if (std::string_view{argv[1]} != "ssh" || std::string_view{argv[2]} != "init" ||
+      std::string_view{argv[3]} != "-s" || std::string_view{argv[4]} != "--instance" ||
+      std::string_view{argv[5]} != "instance/test/id") {
+    std::cout << "arguments are wrong" << std::endl;
+    return 1;
+  }
+  std::cout << R"({
+ "host": "123.456.789.012",
+ "keyPath": "example/path/to/a/key",
+ "knownHostsPath": "example/path/to/known_hosts",
+ "port": "12345",
+ "user": "example_user"
+})" << std::endl;
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+  // This sleep is here for 2 reasons:
+  // 1. The ggp cli which this program is mocking, does have quite a bit of delay, hence having a
+  // delay in this mock program, mimics the behaviour of the real ggp cli more closely
+  // 2. To test the timeout functionaliy in OrbitGgp::Client
+  std::this_thread::sleep_for(std::chrono::milliseconds{30});
+
+  switch (argc) {
+    case 2:
+      return GgpVersion(argv);
+    case 4:
+      return GgpInstanceList(argv);
+    case 6:
+      return GgpSshInit(argv);
+    default:
+      std::cout << "Wrong amount of arguments" << std::endl;
+      return 1;
+  }
+}
