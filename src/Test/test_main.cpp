33e6e15550a394307ca0592c22765ec7b1bdb597
Henning Becker <henning.becker@gmail.com>
2021-06-17 13:24:38 +0200

    Move gtest's main functions into separate module
    gtest's main functions used to be created by CMake on the fly. This
    commit is moving them into its own cmake module and subdirectory and
    treats their targets as if they were standard library targets.
    
    It is also changing when QApplication and QCoreApplication instances are
    created. By using a test listener we postpone the creation to right
    before the test start.
    
    That's important because this way gtest command line options like
    `--help` or `--gtest_list_tests` won't need a QApplication instance and
    hance they work in headless setups.

---
+++ src/Test/test_main.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+#include <cstdio>
+
+int main(int argc, char* argv[]) {
+  printf("Running main() from %s\n", __FILE__);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
