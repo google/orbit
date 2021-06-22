2d3f683b0b1bb9e3bde9efa0e60688a2f3038a8b
Henning Becker <henning.becker@gmail.com>
2021-06-22 13:41:58 +0200

    Update clang9 Docker image

--- third_party/conan/docker/digests.sh
+++ third_party/conan/docker/digests.sh
@@ -7,6 +7,9 @@
 # an exact version of a container.
 declare -rA docker_image_digest_mapping=(
   [clang_format]="gcr.io/orbitprofiler/clang_format@sha256:6729c09de588cf10c7385dcd7efb3dfbeae9b1d866eda1f5ac23a4379835d216" \
+  [clang9_release]="gcr.io/orbitprofiler/clang9_release@sha256:1ed8154b371a5503a90cdce9fd437c31ca68f91c7a738fe233ef9f8c5d5132de" \
+  [clang9_relwithdebinfo]="gcr.io/orbitprofiler/clang9_release@sha256:1ed8154b371a5503a90cdce9fd437c31ca68f91c7a738fe233ef9f8c5d5132de" \
+  [clang9_debug]="gcr.io/orbitprofiler/clang9_debug@sha256:1ed8154b371a5503a90cdce9fd437c31ca68f91c7a738fe233ef9f8c5d5132de" \
   [gcc9_release]="gcr.io/orbitprofiler/gcc9_release@sha256:0a2e07d422122cd9fb4413149ca1e16a69cd6d7dd5989ed7938b221b4be32f45" \
   [gcc9_relwithdebinfo]="gcr.io/orbitprofiler/gcc9_release@sha256:0a2e07d422122cd9fb4413149ca1e16a69cd6d7dd5989ed7938b221b4be32f45" \
   [gcc9_debug]="gcr.io/orbitprofiler/gcc9_debug@sha256:0a2e07d422122cd9fb4413149ca1e16a69cd6d7dd5989ed7938b221b4be32f45" \
