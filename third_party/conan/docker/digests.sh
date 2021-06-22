5dde946448d40854722000c90d93b19aa1b38934
Henning Becker <henning.becker@gmail.com>
2021-06-22 11:11:09 +0200

    Update GGP Docker image
    The new image is based on Ubuntu 20.04 LTS.

--- third_party/conan/docker/digests.sh
+++ third_party/conan/docker/digests.sh
@@ -7,5 +7,8 @@
 # an exact version of a container.
 declare -rA docker_image_digest_mapping=(
   [clang_format]="gcr.io/orbitprofiler/clang_format@sha256:6729c09de588cf10c7385dcd7efb3dfbeae9b1d866eda1f5ac23a4379835d216" \
+  [ggp_release]="gcr.io/orbitprofiler/ggp_release@sha256:e52179e3b1c31a06d0ae92a8f750963c2696cf741506290baa340dfa63c437ca" \
+  [ggp_relwithdebinfo]="gcr.io/orbitprofiler/ggp_release@sha256:e52179e3b1c31a06d0ae92a8f750963c2696cf741506290baa340dfa63c437ca" \
+  [ggp_debug]="gcr.io/orbitprofiler/ggp_debug@sha256:e52179e3b1c31a06d0ae92a8f750963c2696cf741506290baa340dfa63c437ca" \
   [license_headers]="gcr.io/orbitprofiler/license_headers@sha256:f131914386ba4cd91610c89722bfcdf02eba1bf5d47ed49050a629df28173e93" \
 )
