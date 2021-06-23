ab06de9adf123c034ed166ec3c1ab263923376ef
Davide Pallotti <dpallotti@google.com>
2021-06-23 15:36:36 +0200

    Remove `UNUSED` from `OrbitCore/CoreUtils.h`

--- src/OrbitCore/CoreUtils.h
+++ src/OrbitCore/CoreUtils.h
@@ -38,7 +38,6 @@
 #define CONCAT_(x, y) x##y
 #define CONCAT(x, y) CONCAT_(x, y)
 #define UNIQUE_ID CONCAT(Id_, __LINE__)
-#define UNUSED(x) (void)(x)
 
 inline uint64_t StringHash(std::string_view str) {
   return XXH64(str.data(), str.size(), 0xBADDCAFEDEAD10CC);
