fdbcde3783a6b65deabaebfb5c164e58a1513831
antonrohr <antonrohr@google.com>
2021-06-23 11:46:53 +0000

    Extend SessionSetupDialogTest

--- src/SessionSetup/SessionSetupDialogTest.cpp
+++ src/SessionSetup/SessionSetupDialogTest.cpp
@@ -6,38 +6,89 @@
 
 #include <QApplication>
 #include <QPushButton>
+#include <QTest>
+#include <memory>
 #include <optional>
+#include <variant>
 
 #include "MetricsUploader/MetricsUploaderStub.h"
 #include "OrbitSsh/Context.h"
 #include "SessionSetup/Connections.h"
+#include "SessionSetup/DeploymentConfigurations.h"
+#include "SessionSetup/ServiceDeployManager.h"
 #include "SessionSetup/SessionSetupDialog.h"
+#include "SessionSetup/TargetConfiguration.h"
+#include "SessionSetup/TargetLabel.h"
 
 namespace orbit_session_setup {
 
-// TODO(b/189838600) Add tests that check correctness.
+namespace {
 
-TEST(SessionSetupDialog, SmokeTest) {
-  auto ssh_context = orbit_ssh::Context::Create();
-  ASSERT_TRUE(ssh_context.has_value());
+class SessionSetupDialogTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    auto ssh_context = orbit_ssh::Context::Create();
+    ASSERT_TRUE(ssh_context.has_value());
+    ssh_context_ = std::make_unique<orbit_ssh::Context>(std::move(ssh_context.value()));
+    ssh_artifacts_ = std::make_unique<SshConnectionArtifacts>(
+        ssh_context_.get(), ServiceDeployManager::GrpcPort{0}, &deploy_config_);
+  }
+  std::unique_ptr<orbit_ssh::Context> ssh_context_;
+  DeploymentConfiguration deploy_config_{NoDeployment{}};
+  std::unique_ptr<SshConnectionArtifacts> ssh_artifacts_;
+  orbit_metrics_uploader::MetricsUploaderStub uploader_stub_;
+};
 
-  DeploymentConfiguration config{NoDeployment{}};
+}  // namespace
 
-  SshConnectionArtifacts ssh_artifacts{&ssh_context.value(), ServiceDeployManager::GrpcPort{0},
-                                       &config};
+TEST_F(SessionSetupDialogTest, CreateExecAndRejectEmptyDialogueReturnsNoConfiguration) {
+  SessionSetupDialog dialog{ssh_artifacts_.get(), std::nullopt, &uploader_stub_};
 
-  orbit_metrics_uploader::MetricsUploaderStub uploader{};
-
-  SessionSetupDialog dialog{&ssh_artifacts, std::nullopt, &uploader};
-
-  QApplication::processEvents();
   EXPECT_TRUE(dialog.isEnabled());
 
   QMetaObject::invokeMethod(
-      &dialog, [&dialog]() { dialog.reject(); }, Qt::ConnectionType::QueuedConnection);
+      &dialog,
+      [&dialog]() {
+        QApplication::processEvents();
+        auto* confirm_button = dialog.findChild<QPushButton*>("confirmButton");
+        ASSERT_NE(confirm_button, nullptr);
+        EXPECT_FALSE(confirm_button->isEnabled());
+
+        dialog.reject();
+      },
+      Qt::ConnectionType::QueuedConnection);
 
   std::optional<TargetConfiguration> result = dialog.Exec();
   EXPECT_EQ(result, std::nullopt);
+}
+
+TEST_F(SessionSetupDialogTest, CreateExecAndStartDialogueWithFileTargetReturnsValidConfiguration) {
+  const std::filesystem::path file_path = "test/path/to/file";
+  FileTarget file_target{file_path};
+
+  SessionSetupDialog dialog(ssh_artifacts_.get(), file_target, &uploader_stub_);
+
+  QMetaObject::invokeMethod(
+      &dialog,
+      [&]() {
+        QApplication::processEvents();
+        auto* target_label = dialog.findChild<TargetLabel*>("targetLabel");
+        ASSERT_NE(target_label, nullptr);
+        EXPECT_EQ(target_label->GetFileText().toStdString(), file_path.filename().string());
+
+        auto* confirm_button = dialog.findChild<QPushButton*>("confirmButton");
+        ASSERT_NE(confirm_button, nullptr);
+        EXPECT_TRUE(confirm_button->isEnabled());
+
+        QTest::mouseClick(confirm_button, Qt::MouseButton::LeftButton);
+      },
+      Qt::ConnectionType::QueuedConnection);
+
+  std::optional<TargetConfiguration> result = dialog.Exec();
+  ASSERT_TRUE(result.has_value());
+  ASSERT_TRUE(std::holds_alternative<FileTarget>(result.value()));
+  const FileTarget& result_file_target{std::get<FileTarget>(result.value())};
+  EXPECT_EQ(result_file_target.GetCaptureFilePath(), file_path);
 }
 
 }  // namespace orbit_session_setup
