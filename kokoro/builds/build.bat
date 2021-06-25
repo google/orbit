8c201515e414f8df4686cfdb393df8a9c8180ab9
Henning Becker <henning.becker@gmail.com>
2021-06-25 15:55:22 +0200

    Move Kokoro config to an internal repository

--- kokoro/builds/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set script=%0
-call C:\cygwin64\bin\bash.exe -o igncr %script:.bat=.sh% %*
--- kokoro/builds/collect.sh
+++
@@ -1,9 +0,0 @@
-# Fail on any error.
-set -e
-
-# Trivial repackaging of the build artefacts: The collector gets integrated into Orbit.zip.
-cd "$KOKORO_GFILE_DIR"
-unzip Collector.zip
-unzip Orbit.zip
-rm Orbit.zip
-zip -r Orbit.zip Orbit/
