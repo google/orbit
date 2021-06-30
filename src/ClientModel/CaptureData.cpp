google/orbit

commit 73c1002bb910edea695b47c8405ce551b1b6c6e0

Report only executable segmetents in Module events
Since modules may end up being mapped between other modules,
and sometimes do (see http://b/191732187), we only report
now executable segment mappings for modules. This also brings
snapshot event data in line with module update data.

Since exectuable segment is located at the offset different
from 0 we need to take it into account while translating
absolute addresses to function address (and vice versa).
For this reason we introduce exectuable_segment_offset
to ModuleInfo

Bug: http://b/191732187
Test: Run orbit, intstrument a function, take a capture, go to
      Disassebly.

src/ClientModel/CaptureData.cpp
@@ -209,7 +209,8 @@ CaptureData::FindFunctionAbsoluteAddressByInstructionAbsoluteAddressUsingModules
   const auto* function_info = module->FindFunctionByOffset(offset, false);
   if (function_info == nullptr) return std::nullopt;
 
-  return module_base_address - module->load_bias() + function_info->address();
+  return module_base_address - module->load_bias() - module->executable_segment_offset() +
+         function_info->address();
 }
 
 const FunctionInfo* CaptureData::FindFunctionByModulePathBuildIdAndOffset(
@@ -254,15 +255,17 @@ const FunctionInfo* CaptureData::FindFunctionByAddress(uint64_t absolute_address
                                                        bool is_exact) const {
   const auto module_or_error = process_.FindModuleByAddress(absolute_address);
   if (module_or_error.has_error()) return nullptr;
-  const std::string& module_path = module_or_error.value().file_path();
-  const std::string& module_build_id = module_or_error.value().build_id();
-  const uint64_t module_base_address = module_or_error.value().start();
+  const auto& module_in_memory = module_or_error.value();
+  const std::string& module_path = module_in_memory.file_path();
+  const std::string& module_build_id = module_in_memory.build_id();
+  const uint64_t module_base_address = module_in_memory.start();
 
   const ModuleData* module =
       module_manager_->GetModuleByPathAndBuildId(module_path, module_build_id);
   if (module == nullptr) return nullptr;
 
-  const uint64_t offset = absolute_address - module_base_address;
+  const uint64_t offset =
+      absolute_address - module_base_address + module->executable_segment_offset();
   return module->FindFunctionByOffset(offset, is_exact);
 }
 

src/ClientData/FunctionUtils.cpp
@@ -63,7 +63,8 @@ std::optional<uint64_t> GetAbsoluteAddress(const orbit_client_protos::FunctionIn
 
   CHECK(!base_addresses.empty());
 
-  return func.address() + base_addresses.at(0) - module.load_bias();
+  return func.address() + base_addresses.at(0) - module.load_bias() -
+         module.executable_segment_offset();
 }
 
 bool IsOrbitFunctionFromType(const FunctionInfo::OrbitType& type) {

src/ClientData/ProcessData.cpp
@@ -59,8 +59,7 @@ const std::string& ProcessData::build_id() const {
   return process_info_.build_id();
 }
 
-[[maybe_unused]] static bool IsModuleMapValid(
-    const std::map<uint64_t, ModuleInMemory>& module_map) {
+static bool IsModuleMapValid(const std::map<uint64_t, ModuleInMemory>& module_map) {
   // Check that modules do not intersect in the address space.
   uint64_t last_end_address = 0;
   for (const auto& [unused_address, module_in_memory] : module_map) {
@@ -82,8 +81,7 @@ void ProcessData::UpdateModuleInfos(absl::Span<const ModuleInfo> module_infos) {
     CHECK(success);
   }
 
-  // TODO(b/191732187): Re-enable after the bug is properly fixed.
-  // CHECK(IsModuleMapValid(start_address_to_module_in_memory_));
+  CHECK(IsModuleMapValid(start_address_to_module_in_memory_));
 }
 
 std::vector<std::string> ProcessData::FindModuleBuildIdsByPath(
@@ -123,8 +121,7 @@ void ProcessData::AddOrUpdateModuleInfo(const ModuleInfo& module_info) {
   start_address_to_module_in_memory_.insert_or_assign(module_info.address_start(),
                                                       module_in_memory);
 
-  // TODO(b/191732187): Re-enable after the bug is properly fixed.
-  // CHECK(IsModuleMapValid(start_address_to_module_in_memory_));
+  CHECK(IsModuleMapValid(start_address_to_module_in_memory_));
 }
 
 ErrorMessageOr<ModuleInMemory> ProcessData::FindModuleByAddress(uint64_t absolute_address) const {

src/ClientData/include/ClientData/ModuleData.h
@@ -33,6 +33,9 @@ class ModuleData final {
   [[nodiscard]] uint64_t file_size() const { return module_info_.file_size(); }
   [[nodiscard]] const std::string& build_id() const { return module_info_.build_id(); }
   [[nodiscard]] uint64_t load_bias() const { return module_info_.load_bias(); }
+  [[nodiscard]] uint64_t executable_segment_offset() const {
+    return module_info_.executable_segment_offset();
+  }
   [[nodiscard]] bool is_loaded() const;
   // Returns true of module was unloaded and false otherwise
   [[nodiscard]] bool UpdateIfChangedAndUnload(orbit_grpc_protos::ModuleInfo info);

src/FakeClient/FakeClientMain.cpp
@@ -77,14 +77,14 @@ void ManipulateModuleManagerAndSelectedFunctionsToAddUprobeFromOffset(
   FAIL_IF(error_or_elf_file.has_error(), "%s", error_or_elf_file.error().message());
   std::unique_ptr<orbit_object_utils::ElfFile>& elf_file = error_or_elf_file.value();
   std::string build_id = elf_file->GetBuildId();
-  ErrorMessageOr<uint64_t> error_or_load_bias = elf_file->GetLoadBias();
-  FAIL_IF(error_or_load_bias.has_error(), "%s", error_or_load_bias.error().message());
-  uint64_t load_bias = error_or_load_bias.value();
+  uint64_t load_bias = elf_file->GetLoadBias();
+  uint64_t executable_segment_offset = elf_file->GetExecutableSegmentOffset();
 
   orbit_grpc_protos::ModuleInfo module_info;
   module_info.set_file_path(file_path);
   module_info.set_build_id(build_id);
   module_info.set_load_bias(load_bias);
+  module_info.set_executable_segment_offset(executable_segment_offset);
   CHECK(module_manager->AddOrUpdateModules({module_info}).empty());
 
   orbit_client_protos::FunctionInfo function_info;
@@ -104,14 +104,14 @@ void ManipulateModuleManagerAndSelectedFunctionsToAddUprobeFromFunctionName(
   FAIL_IF(error_or_elf_file.has_error(), "%s", error_or_elf_file.error().message());
   std::unique_ptr<orbit_object_utils::ElfFile>& elf_file = error_or_elf_file.value();
   std::string build_id = elf_file->GetBuildId();
-  ErrorMessageOr<uint64_t> error_or_load_bias = elf_file->GetLoadBias();
-  FAIL_IF(error_or_load_bias.has_error(), "%s", error_or_load_bias.error().message());
-  uint64_t load_bias = error_or_load_bias.value();
+  uint64_t load_bias = elf_file->GetLoadBias();
+  uint64_t executable_segment_offset = elf_file->GetExecutableSegmentOffset();
 
   orbit_grpc_protos::ModuleInfo module_info;
   module_info.set_file_path(file_path);
   module_info.set_build_id(build_id);
   module_info.set_load_bias(load_bias);
+  module_info.set_executable_segment_offset(executable_segment_offset);
   CHECK(module_manager->AddOrUpdateModules({module_info}).empty());
 
   ErrorMessageOr<orbit_grpc_protos::ModuleSymbols> symbols_or_error =

src/GrpcProtos/module.proto
@@ -9,13 +9,16 @@ package orbit_grpc_protos;
 message ModuleInfo {
   // General purpose name of the module, can be used as a pretty name in the UI.
   // If you need the ELF specific soname, use the field "soname" below.
+  // NextId: 10
   string name = 1;
   string file_path = 2;
   uint64 file_size = 3;
   uint64 address_start = 4;
   uint64 address_end = 5;
   string build_id = 6;
+
+  // ELF specific fields: load_bias, soname and executable_segment_offset.
   uint64 load_bias = 7;
-  // ELF specific soname.
+  uint64 executable_segment_offset = 8;
   string soname = 9;
 }

src/ObjectUtils/LinuxMap.cpp
@@ -67,18 +67,12 @@ ErrorMessageOr<ModuleInfo> CreateModule(const std::filesystem::path& module_path
     CHECK(elf_file != nullptr);
     module_info.set_build_id(elf_file->GetBuildId());
     module_info.set_soname(elf_file->GetSoname());
-
-    ErrorMessageOr<uint64_t> load_bias_or_error = elf_file->GetLoadBias();
-    // Every loadable ELF module contains a load bias.
-    if (load_bias_or_error.has_error()) {
-      return load_bias_or_error.error();
-    }
-    module_info.set_load_bias(load_bias_or_error.value());
+    module_info.set_load_bias(elf_file->GetLoadBias());
+    module_info.set_executable_segment_offset(elf_file->GetExecutableSegmentOffset());
   }
 
   // All fields we need to set for COFF files are already set, no need to handle COFF
   // specifically here.
-
   return module_info;
 }
 
@@ -89,15 +83,10 @@ ErrorMessageOr<std::vector<ModuleInfo>> ReadModules(int32_t pid) {
 }
 
 ErrorMessageOr<std::vector<ModuleInfo>> ParseMaps(std::string_view proc_maps_data) {
-  struct AddressRange {
-    uint64_t start_address;
-    uint64_t end_address;
-    bool is_executable;
-  };
-
   const std::vector<std::string> proc_maps = absl::StrSplit(proc_maps_data, '\n');
 
-  std::map<std::string, AddressRange> address_map;
+  std::vector<ModuleInfo> result;
+
   for (const std::string& line : proc_maps) {
     std::vector<std::string> tokens = absl::StrSplit(line, ' ', absl::SkipEmpty());
     // tokens[4] is the inode column. If inode equals 0, then the memory is not
@@ -113,30 +102,16 @@ ErrorMessageOr<std::vector<ModuleInfo>> ParseMaps(std::string_view proc_maps_dat
     uint64_t end = std::stoull(addresses[1], nullptr, 16);
     bool is_executable = tokens[1].size() == 4 && tokens[1][2] == 'x';
 
-    auto iter = address_map.find(module_path);
-    if (iter == address_map.end()) {
-      address_map[module_path] = {start, end, is_executable};
-    } else {
-      AddressRange& address_range = iter->second;
-      address_range.start_address = std::min(address_range.start_address, start);
-      address_range.end_address = std::max(address_range.end_address, end);
-      address_range.is_executable |= is_executable;
-    }
-  }
+    // Skip non-executable mappings
+    if (!is_executable) continue;
+    ErrorMessageOr<ModuleInfo> module_info_or_error = CreateModule(module_path, start, end);
 
-  std::vector<ModuleInfo> result;
-  for (const auto& [module_path, address_range] : address_map) {
-    // Filter out entries which are not executable
-    if (!address_range.is_executable) continue;
-
-    ErrorMessageOr<ModuleInfo> module_info_or_error =
-        CreateModule(module_path, address_range.start_address, address_range.end_address);
     if (module_info_or_error.has_error()) {
       ERROR("Unable to create module: %s", module_info_or_error.error().message());
       continue;
     }
 
-    result.push_back(std::move(module_info_or_error.value()));
+    result.emplace_back(std::move(module_info_or_error.value()));
   }
 
   return result;

src/ObjectUtils/LinuxMapTest.cpp
@@ -197,8 +197,8 @@ TEST(LinuxMap, ParseMaps) {
     EXPECT_EQ(hello_module_info->name(), "hello_world_elf");
     EXPECT_EQ(hello_module_info->file_path(), hello_world_path);
     EXPECT_EQ(hello_module_info->file_size(), 16616);
-    EXPECT_EQ(hello_module_info->address_start(), 0x7f6874285000);
-    EXPECT_EQ(hello_module_info->address_end(), 0x7f6874290000);
+    EXPECT_EQ(hello_module_info->address_start(), 0x7f6874288000);
+    EXPECT_EQ(hello_module_info->address_end(), 0x7f687428c000);
     EXPECT_EQ(hello_module_info->build_id(), "d12d54bc5b72ccce54a408bdeda65e2530740ac8");
     EXPECT_EQ(hello_module_info->load_bias(), 0x0);
 

commit b6c14a18e0676609296efb92522efb9f96f699a7

Fix function offset calculation
This fixes regression introduced in 73c1002bb910edea695b47c8405ce551b1b6c6e0

Bug: http://b/191732187
Bug: http://b/192439368
Test: take capture -> load a module -> check that existing function stats
      did not change.

src/ClientModel/CaptureData.cpp
@@ -205,7 +205,8 @@ CaptureData::FindFunctionAbsoluteAddressByInstructionAbsoluteAddressUsingModules
       module_manager_->GetModuleByPathAndBuildId(module_path, module_build_id);
   if (module == nullptr) return std::nullopt;
 
-  const uint64_t offset = absolute_address - module_base_address;
+  const uint64_t offset =
+      absolute_address - module_base_address + module->executable_segment_offset();
   const auto* function_info = module->FindFunctionByOffset(offset, false);
   if (function_info == nullptr) return std::nullopt;
 