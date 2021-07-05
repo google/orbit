6d79d9d76db4ae8f8922b5feb534ef3ee79d30ef
Florian Reichl <freichl@google.com>
2021-07-05 17:40:39 +0200

    Rework TrackManagerTest to allow sharing resources

---
+++ src/OrbitGl/TrackManagerTest.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_GL_TRACK_MANAGER_TEST_H_
+#define ORBIT_GL_TRACK_MANAGER_TEST_H_
+
+#include <cstdint>
+#include <memory>
+
+#include "ClientModel/CaptureData.h"
+
+namespace orbit_gl {
+
+struct TrackTestData {
+  static constexpr uint64_t kCallstackId = 1;
+  static constexpr uint64_t kFunctionAbsoluteAddress = 0x30;
+  static constexpr uint64_t kInstructionAbsoluteAddress = 0x31;
+  static constexpr int32_t kThreadId = 42;
+  static constexpr const char* kFunctionName = "example function";
+  static constexpr const char* kModuleName = "example module";
+  static constexpr const char* kThreadName = "example thread";
+
+  static constexpr size_t kTimerOnlyThreadId = 128;
+  static constexpr const char* kTimerOnlyThreadName = "timer only thread";
+
+  static std::unique_ptr<orbit_client_model::CaptureData> GenerateTestCaptureData();
+};
+
+}  // namespace orbit_gl
+
+#endif
