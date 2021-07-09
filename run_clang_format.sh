ee4bde557fa23ecfb5820cd5d421b7287454a67b
Daniel Fenner <dfenner@google.com>
2021-07-09 17:51:34 +0200

    Make run_clang_format.sh run in git bash under windows.

--- run_clang_format.sh
+++ run_clang_format.sh
@@ -3,5 +3,11 @@
 # Copyright (c) 2020 The Orbit Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+clang_format_path="/usr/bin/clang-format-9";
 
-find src/ \( -name '*.cpp' -o -name '*.h' \) ! -name 'resource.h' | xargs /usr/bin/clang-format-9 -i
+# If we are running in git bash this is the default location:
+if  [ -f "/c/Program Files/LLVM/bin/clang-format.exe" ]; then
+  clang_format_path="/c/Program Files/LLVM/bin/clang-format.exe";
+fi
+
+find src/ \( -name '*.cpp' -o -name '*.h' \) ! -name 'resource.h' | xargs "$clang_format_path" -i
