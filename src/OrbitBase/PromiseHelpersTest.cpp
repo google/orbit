1737edece7ac947fe02998be5f9a5f98defab88f
Henning Becker <henning.becker@gmail.com>
2021-07-28 10:29:28 +0200

    Remove internal header include
    I missed this when cleaning those up recently.

--- src/OrbitBase/PromiseHelpersTest.cpp
+++ src/OrbitBase/PromiseHelpersTest.cpp
@@ -3,7 +3,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <gtest/gtest-typed-test.h>
 #include <gtest/gtest.h>
 
 #include "OrbitBase/Promise.h"
