12bd36c87100c2cd8acfcd03d4da2ef973903894
Henning Becker <henning.becker@gmail.com>
2021-07-28 12:02:59 +0200

    Let register_test handle testdata
    This is giving control of the testdata location to the build system.
    This allows to have the testdata in different locations depending on
    whether CMake is used or something else.
    
    The new function `orbit_test::GetTestdataDir()` allows to access the
    testdata path.

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -4,6 +4,10 @@
 
 include(${CMAKE_SOURCE_DIR}/third_party/cmake/Modules/GoogleTest.cmake)
 
+# `register_test` registers a test target with ctest. Individual test
+# cases will be detected automatically and executed in separate processes.
+# In addition this function takes care of collecting test results in xUNIT
+# XML files and also handles the `testdata` subdirectories.
 function(register_test TEST_TARGET)
   if(CMAKE_CROSSCOMPILING)
     return()
@@ -20,6 +24,16 @@ function(register_test TEST_TARGET)
     XML_OUTPUT_DIR "${TESTRESULTS_DIRECTORY}"
     PROPERTIES ${ARGS_PROPERTIES}
     DISCOVERY_MODE PRE_TEST)
+
+  if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/testdata")
+    add_custom_command(TARGET ${TEST_TARGET} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E remove_directory
+        $<TARGET_FILE_DIR:${TEST_TARGET}>/testdata/${TEST_TARGET}
+      COMMAND ${CMAKE_COMMAND} -E copy_directory
+        ${CMAKE_CURRENT_LIST_DIR}/testdata
+        $<TARGET_FILE_DIR:${TEST_TARGET}>/testdata/${TEST_TARGET})
+    target_link_libraries(${TEST_TARGET} PRIVATE OrbitBase)
+  endif()
 endfunction()
 
 if(NOT TARGET GTest::GTest)
---
+++ src/Test/Path.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "Test/Path.h"
+
+#include <cstdlib>
+#include <filesystem>
+
+#include "OrbitBase/ExecutablePath.h"
+
+namespace orbit_test {
+
+// This is the current working directory that was set during the start of the application.
+// Any later changes don't affect the value of `initial_working_directory`.
+const std::filesystem::path initial_working_directory = std::filesystem::current_path();
+
+std::filesystem::path GetTestdataDir() {
+  const char* const override_path = std::getenv("ORBIT_OVERRIDE_TESTDATA_PATH");
+
+  if (override_path != nullptr) {
+    std::filesystem::path testdata_dir{std::string{override_path}};
+    if (testdata_dir.is_relative()) testdata_dir = initial_working_directory / testdata_dir;
+    return testdata_dir;
+  }
+
+  const auto test_name = orbit_base::GetExecutablePath().stem();
+  return orbit_base::GetExecutableDir() / "testdata" / test_name;
+}
+
+}  // namespace orbit_test
