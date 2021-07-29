aecbefe95d753b4c14ecfd6348799a40136ec4f1
antonrohr <antonrohr@google.com>
2021-07-29 13:41:30 +0000

    Start SymbolPaths component
    This adds the SymbolPaths component and QSettingsWrapper.h/cpp, which
    contains the free functions LoadPaths and SavePaths

---
+++ src/SymbolPaths/include/SymbolPaths/QSettingsWrapper.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SYMBOL_PATHS_QSETTINGS_WRAPPER_H_
+#define SYMBOL_PATHS_QSETTINGS_WRAPPER_H_
+
+#include <filesystem>
+#include <vector>
+
+namespace orbit_symbol_paths {
+
+// This is a simple wrapper around QSettings, which makes this thread safe, see
+// https://doc.qt.io/qt-5/qsettings.html
+void SavePaths(const std::vector<std::filesystem::path>& paths);
+
+// This is a simple wrapper around QSettings, which makes this thread safe, see
+// https://doc.qt.io/qt-5/qsettings.html
+[[nodiscard]] std::vector<std::filesystem::path> LoadPaths();
+
+}  // namespace orbit_symbol_paths
+
+#endif  // SYMBOL_PATHS_QSETTINGS_WRAPPER_H_
