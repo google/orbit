e9f51455665bec88df2cce8630ab15c7714da4e6
antonrohr <antonrohr@google.com>
2021-07-29 13:41:30 +0000

    Start SymbolPaths component
    This adds the SymbolPaths component and QSettingsWrapper.h/cpp, which
    contains the free functions LoadPaths and SavePaths

---
+++ src/SymbolPaths/QSettingsWrapper.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "SymbolPaths/QSettingsWrapper.h"
+
+#include <QSettings>
+#include <filesystem>
+
+constexpr const char* kSymbolPathsSettingsKey = "symbol_directories";
+constexpr const char* kDirectoryPathKey = "directory_path";
+
+namespace orbit_symbol_paths {
+
+std::vector<std::filesystem::path> LoadPaths() {
+  QSettings settings{};
+  const int size = settings.beginReadArray(kSymbolPathsSettingsKey);
+  std::vector<std::filesystem::path> paths{};
+  paths.reserve(size);
+  for (int i = 0; i < size; ++i) {
+    settings.setArrayIndex(i);
+    paths.emplace_back(settings.value(kDirectoryPathKey).toString().toStdString());
+  }
+  settings.endArray();
+  return paths;
+}
+
+void SavePaths(const std::vector<std::filesystem::path>& paths) {
+  QSettings settings{};
+  settings.beginWriteArray(kSymbolPathsSettingsKey, static_cast<int>(paths.size()));
+  for (size_t i = 0; i < paths.size(); ++i) {
+    settings.setArrayIndex(static_cast<int>(i));
+    settings.setValue(kDirectoryPathKey, QString::fromStdString(paths[i].string()));
+  }
+  settings.endArray();
+}
+
+}  // namespace orbit_symbol_paths
---
+++ src/SymbolPaths/QSettingsWrapperTest.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gtest/gtest.h>
+
+#include <QCoreApplication>
+#include <QSettings>
+#include <filesystem>
+
+#include "SymbolPaths/QSettingsWrapper.h"
+
+const std::filesystem::path path0{"/path/to/symbols/path"};
+const std::filesystem::path path1{"/home/src/project/build/"};
+const std::filesystem::path path2{R"(c:\project\build\)"};
+
+constexpr const char* kOrgName = "The Orbit Authors";
+
+namespace orbit_symbol_paths {
+
+TEST(SymbolPathsManager, LoadAndSave) {
+  QCoreApplication::setOrganizationDomain(kOrgName);
+  QCoreApplication::setApplicationName("SymbolPathsManager.SetAndGet");
+
+  {  // clear before test;
+    QSettings settings;
+    settings.clear();
+  }
+
+  EXPECT_EQ(LoadPaths(), std::vector<std::filesystem::path>{});
+
+  std::vector<std::filesystem::path> paths{
+      path0,
+      path1,
+      path2,
+  };
+
+  SavePaths(paths);
+  EXPECT_EQ(LoadPaths(), paths);
+}
+
+}  // namespace orbit_symbol_paths
