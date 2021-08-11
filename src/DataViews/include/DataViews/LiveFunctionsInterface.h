422c107002dee737e153406a1073ecb8711c930e
Tingwei LIU <vickyliu@google.com>
2021-08-11 16:30:01 +0200

    Add LiveFunctionsInterface
    With this change, we add `LiveFunctionsInterface`. We made
    `LiveFunctionsController` to inherit from `LiveFunctionsInterface` and
    change `LiveFunctionsDataView` to use the interface class.
    
    The purpose is to make `LiveFunctionsDataView` independent with the
    OrbitGl module such that it can be moved to the DataViews module.
    `LiveFunctionsDataView` only need the `AddIterator` method from
    `LiveFunctionsController`, hence we can make a interface to provide that
    functionality.

---
+++ src/DataViews/include/DataViews/LiveFunctionsInterface.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DATA_VIEWS_LIVE_FUNCTIONS_INTERFACE_H_
+#define DATA_VIEWS_LIVE_FUNCTIONS_INTERFACE_H_
+
+#include <stdint.h>
+
+#include "capture_data.pb.h"
+
+namespace orbit_data_views {
+
+class LiveFunctionsInterface {
+ public:
+  virtual ~LiveFunctionsInterface() noexcept = default;
+
+  virtual void AddIterator(uint64_t instrumented_function_id,
+                           const orbit_client_protos::FunctionInfo* function) = 0;
+};
+
+}  // namespace orbit_data_views
+
+#endif  // DATA_VIEWS_LIVE_FUNCTIONS_INTERFACE_H_
