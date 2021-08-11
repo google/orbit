f4212dc76d9a95c6b92e3eda874a30aa0e5d8183
Henning Becker <henning.becker@gmail.com>
2021-08-11 15:43:14 +0200

    Add test for GTest string_view support
    That's an unusual test for a third party dependency. But it's very easy
    to compile GTest in C++11 mode instead of C++17 and you won't notice it
    before link time (missing std::string_view symbols). So this test
    protects us from this problem in future updates of GTest.

---
+++ src/Test/StringViewTest.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <string_view>
+
+namespace orbit_test {
+
+// Ensures that GTest was compiled with std::string_view support.
+TEST(Test, MatchersSupportStringView) {
+  std::string_view some_string = "Hello World!";
+  EXPECT_THAT(some_string, testing::HasSubstr("World!"));
+}
+}  // namespace orbit_test
