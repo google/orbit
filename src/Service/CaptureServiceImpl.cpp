19931e294581983a4b0e2029409446b8bee42369
Henning Becker <henning.becker@gmail.com>
2021-08-18 13:39:11 +0000

    Add mutex annotation to SenderThreadCaptureEventBuffer
    We need to tell the compiler that the condition-lambda is called with
    the mutex acquired.

--- src/Service/CaptureServiceImpl.cpp
+++ src/Service/CaptureServiceImpl.cpp
@@ -88,7 +88,8 @@ class SenderThreadCaptureEventBuffer final : public CaptureEventBuffer {
 
       events_being_buffered_mutex_.LockWhenWithTimeout(
           absl::Condition(
-              +[](SenderThreadCaptureEventBuffer* self) {
+              +[](SenderThreadCaptureEventBuffer* self) EXCLUSIVE_LOCKS_REQUIRED(
+                   self->events_being_buffered_mutex_) {
                 return self->events_being_buffered_.size() >= kSendEventCountInterval ||
                        self->stop_requested_;
               },
