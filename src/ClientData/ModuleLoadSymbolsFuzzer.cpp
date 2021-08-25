e7ad4d3b0889a98c5348963e9274c5d9bc9dd0bd
Henning Becker <henning.becker@gmail.com>
2021-08-25 14:09:42 +0000

    Use new macro in ModuleLoadSymbolsFuzzer

--- src/ClientData/ModuleLoadSymbolsFuzzer.cpp
+++ src/ClientData/ModuleLoadSymbolsFuzzer.cpp
@@ -2,16 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <libfuzzer/libfuzzer_macro.h>
-
 #include "ClientData/ModuleData.h"
+#include "FuzzingUtils/ProtoFuzzer.h"
 #include "module.pb.h"
 #include "symbol.pb.h"
 
 using orbit_client_data::ModuleData;
 using orbit_grpc_protos::ModuleSymbols;
 
-DEFINE_PROTO_FUZZER(const ModuleSymbols& symbols) {
+ORBIT_DEFINE_PROTO_FUZZER(const ModuleSymbols& symbols) {
   ModuleData module{orbit_grpc_protos::ModuleInfo{}};
   module.AddSymbols(symbols);
 }
