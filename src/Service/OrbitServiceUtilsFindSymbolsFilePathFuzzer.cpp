03ecfa13d40e8243ca9e8986b6fc73f576615205
Henning Becker <henning.becker@gmail.com>
2021-08-25 14:11:06 +0000

    Use new macro in OrbitServiceUtilsFindSymbolsFilePathFuzzer

--- src/Service/OrbitServiceUtilsFindSymbolsFilePathFuzzer.cpp
+++ src/Service/OrbitServiceUtilsFindSymbolsFilePathFuzzer.cpp
@@ -2,10 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <libfuzzer/libfuzzer_macro.h>
-
+#include "FuzzingUtils/ProtoFuzzer.h"
 #include "ServiceUtils.h"
 
-DEFINE_PROTO_FUZZER(const orbit_grpc_protos::GetDebugInfoFileRequest& request) {
+ORBIT_DEFINE_PROTO_FUZZER(const orbit_grpc_protos::GetDebugInfoFileRequest& request) {
   (void)orbit_service::utils::FindSymbolsFilePath(request);
 }
