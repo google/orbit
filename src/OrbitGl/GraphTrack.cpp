google/orbit

commit fa6bee45b8ed35f8685441fa2f5f3f9f3baa52a1

Updating all tracks to the new coordinate system

src/OrbitGl/GraphTrack.cpp
@@ -21,6 +21,10 @@
 #include "TimeGraphLayout.h"
 #include "Viewport.h"
 
+namespace {
+constexpr const float kBoxHeightMultiplier = 1.5f;
+}  // namespace
+
 template <size_t Dimension>
 GraphTrack<Dimension>::GraphTrack(CaptureViewElement* parent, TimeGraph* time_graph,
                                   orbit_gl::Viewport* viewport, TimeGraphLayout* layout,
@@ -31,19 +35,22 @@ GraphTrack<Dimension>::GraphTrack(CaptureViewElement* parent, TimeGraph* time_gr
 
 template <size_t Dimension>
 float GraphTrack<Dimension>::GetHeight() const {
-  float scale_factor = IsCollapsed() ? 1 : Dimension;
-  float height = layout_->GetTrackTabHeight() + GetLegendHeight() +
-                 layout_->GetTextBoxHeight() * scale_factor +
-                 layout_->GetSpaceBetweenTracksAndThread() + layout_->GetEventTrackHeightFromTid() +
-                 layout_->GetTrackBottomMargin();
+  // Top content margin is counted twice because it it inserted before and after the legend
+  float height = layout_->GetTrackTabHeight() + layout_->GetTrackContentTopMargin() * 2 +
+                 GetLegendHeight() + GetGraphContentHeight() +
+                 layout_->GetTrackContentBottomMargin();
   return height;
 }
 
 template <size_t Dimension>
 float GraphTrack<Dimension>::GetLegendHeight() const {
-  bool has_legend = !IsCollapsed() && Dimension > 1;
-  float legend_height = has_legend ? layout_->GetTextBoxHeight() : 0;
-  return legend_height;
+  if (IsCollapsed() || Dimension <= 1) {
+    return 0;
+  }
+  // This is a bit of an arbitrary choice - I don't want to introduce an additional layout
+  // parameter just for the legend size, but it should be smaller than all regular textboxes
+  // across Orbit.
+  return layout_->GetTextBoxHeight() / 2.f;
 }
 
 template <size_t Dimension>
@@ -82,8 +89,8 @@ void GraphTrack<Dimension>::UpdatePrimitives(Batcher* batcher, uint64_t min_tick
 
   float content_height = GetGraphContentHeight();
   Vec2 content_pos = pos_;
-  content_pos[1] -= layout_->GetTrackTabHeight();
-  Box box(content_pos, Vec2(GetWidth(), -content_height - GetLegendHeight()), track_z);
+  content_pos[1] += layout_->GetTrackTabHeight();
+  Box box(content_pos, Vec2(GetWidth(), content_height + GetLegendHeight()), track_z);
   batcher->AddBox(box, GetTrackBackgroundColor(), shared_from_this());
 
   const bool picking = picking_mode != PickingMode::kNone;
@@ -100,9 +107,18 @@ Color GraphTrack<Dimension>::GetColor(size_t index) const {
 }
 
 template <size_t Dimension>
+float GraphTrack<Dimension>::GetGraphContentHeight() const {
+  float result = layout_->GetTextBoxHeight() * kBoxHeightMultiplier;
+  if (!IsCollapsed()) {
+    result *= Dimension;
+  }
+  return result;
+}
+
+template <size_t Dimension>
 float GraphTrack<Dimension>::GetLabelYFromValues(
     const std::array<double, Dimension>& /*values*/) const {
-  return GetGraphContentBaseY() + GetGraphContentHeight() / 2.f;
+  return GetGraphContentBottomY() - GetGraphContentHeight() / 2.f;
 }
 
 template <size_t Dimension>
@@ -167,8 +183,7 @@ void GraphTrack<Dimension>::DrawLabel(Batcher& batcher, TextRenderer& text_rende
       target_pos[1] - text_box_size[1] / 2.f);
 
   float label_z = GlCanvas::kZValueTrackLabel + draw_context.z_offset;
-  float bottom_y_of_top_line = text_box_position[1] + text_box_size[1] - single_line_height;
-  text_renderer.AddText(text.c_str(), text_box_position[0], bottom_y_of_top_line, label_z,
+  text_renderer.AddText(text.c_str(), text_box_position[0], text_box_position[1], label_z,
                         {font_size, text_color, text_box_size[0]});
 
   Vec2 arrow_box_position(text_box_position[0] - kTextLeftMargin,
@@ -195,30 +210,33 @@ void GraphTrack<Dimension>::DrawLegend(Batcher& batcher, TextRenderer& text_rend
                                        const Color& legend_text_color) {
   const float kSpaceBetweenLegendSymbolAndText = layout_->GetGenericFixedSpacerWidth();
   const float kSpaceBetweenLegendEntries = layout_->GetGenericFixedSpacerWidth() * 2;
-  float legend_symbol_height = GetLegendHeight() / 2.f;
-  float legend_symbol_width = legend_symbol_height;
+  const float legend_symbol_height = GetLegendHeight();
+  const float legend_symbol_width = legend_symbol_height;
   float x0 = pos_[0] + layout_->GetRightMargin();
-  float y0 = pos_[1] - layout_->GetTrackTabHeight() - layout_->GetTextBoxHeight() / 2.f;
+  const float y0 = pos_[1] + layout_->GetTrackTabHeight() + layout_->GetTrackContentTopMargin();
   uint32_t font_size = GetLegendFontSize(draw_context.indentation_level);
   const Color kFullyTransparent(255, 255, 255, 0);
 
   float text_z = GlCanvas::kZValueTrackText + draw_context.z_offset;
   for (size_t i = 0; i < Dimension; ++i) {
-    batcher.AddShadedBox(Vec2(x0, y0 - legend_symbol_height / 2.f),
-                         Vec2(legend_symbol_width, legend_symbol_height), text_z, GetColor(i));
+    batcher.AddShadedBox(Vec2(x0, y0), Vec2(legend_symbol_width, legend_symbol_height), text_z,
+                         GetColor(i));
     x0 += legend_symbol_width + kSpaceBetweenLegendSymbolAndText;
 
-    float legend_text_width = text_renderer.GetStringWidth(series_names[i].c_str(), font_size);
-    Vec2 legend_text_box_size(legend_text_width, layout_->GetTextBoxHeight());
-    Vec2 legend_text_box_position(x0, y0 - layout_->GetTextBoxHeight() / 2.f);
-    text_renderer.AddText(series_names[i].c_str(), legend_text_box_position[0],
-                          legend_text_box_position[1] + layout_->GetTextOffset(), text_z,
-                          {font_size, legend_text_color, legend_text_box_size[0]});
+    const float legend_text_width =
+        text_renderer.GetStringWidth(series_names[i].c_str(), font_size);
+    const Vec2 legend_text_box_size(legend_text_width, layout_->GetTextBoxHeight());
+
+    TextRenderer::TextFormatting formatting{font_size, legend_text_color, legend_text_box_size[0]};
+    formatting.valign = TextRenderer::VAlign::Middle;
+
+    text_renderer.AddText(series_names[i].c_str(), x0, y0 + legend_symbol_height / 2.f, text_z,
+                          formatting);
     x0 += legend_text_width + kSpaceBetweenLegendEntries;
 
     auto user_data = std::make_unique<PickingUserData>(
         nullptr, [this, i](PickingId /*id*/) { return GetLegendTooltips(i); });
-    batcher.AddShadedBox(legend_text_box_position, legend_text_box_size, text_z, kFullyTransparent,
+    batcher.AddShadedBox(Vec2(x0, y0), legend_text_box_size, text_z, kFullyTransparent,
                          std::move(user_data));
   }
 }
@@ -259,15 +277,16 @@ template <size_t Dimension>
 void GraphTrack<Dimension>::DrawSingleSeriesEntry(
     Batcher* batcher, uint64_t start_tick, uint64_t end_tick,
     const std::array<float, Dimension>& normalized_cumulative_values, float z) {
-  float x0 = time_graph_->GetWorldFromTick(start_tick);
-  float width = time_graph_->GetWorldFromTick(end_tick) - x0;
-  float content_height = GetGraphContentHeight();
-  float base_y = GetGraphContentBaseY();
+  const float x0 = time_graph_->GetWorldFromTick(start_tick);
+  const float width = time_graph_->GetWorldFromTick(end_tick) - x0;
+  const float content_height = GetGraphContentHeight();
+  const float base_y = GetGraphContentBottomY();
   float y0 = base_y;
+  // Draw the stacked values from bottom to top
   for (size_t i = 0; i < Dimension; ++i) {
-    float height = base_y + normalized_cumulative_values[i] * content_height - y0;
+    float height = normalized_cumulative_values[i] * content_height - (base_y - y0);
+    y0 -= height;
     batcher->AddShadedBox(Vec2(x0, y0), Vec2(width, height), z, GetColor(i));
-    y0 += height;
   }
 }
 

src/OrbitGl/AnnotationTrack.cpp
@@ -19,7 +19,7 @@ void AnnotationTrack::DrawAnnotation(Batcher& batcher, TextRenderer& text_render
   Vec2 track_pos = GetAnnotatedTrackPosition();
 
   float content_right_x = track_pos[0] + track_size[0];
-  float content_bottom_y = track_pos[1] - track_size[1] + layout->GetTrackBottomMargin();
+  float content_bottom_y = track_pos[1] + track_size[1] - layout->GetTrackContentBottomMargin();
   float content_height = GetAnnotatedTrackContentHeight();
 
   // Add value upper bound text box (e.g., the "System Memory Total" text box for memory tracks).
@@ -27,10 +27,8 @@ void AnnotationTrack::DrawAnnotation(Batcher& batcher, TextRenderer& text_render
     std::string text = value_upper_bound_.value().first;
     float string_width = text_renderer.GetStringWidth(text.c_str(), font_size);
     Vec2 text_box_size(string_width, layout->GetTextBoxHeight());
-    Vec2 text_box_position(content_right_x - text_box_size[0],
-                           content_bottom_y + content_height - text_box_size[1]);
-    text_renderer.AddText(text.c_str(), text_box_position[0],
-                          text_box_position[1] + layout->GetTextOffset(), z,
+    Vec2 text_box_position(content_right_x - text_box_size[0], content_bottom_y - content_height);
+    text_renderer.AddText(text.c_str(), text_box_position[0], text_box_position[1], z,
                           {font_size, kWhite, text_box_size[0]});
 
     if (!GetValueUpperBoundTooltip().empty()) {
@@ -47,9 +45,10 @@ void AnnotationTrack::DrawAnnotation(Batcher& batcher, TextRenderer& text_render
     float string_width = text_renderer.GetStringWidth(text.c_str(), font_size);
     Vec2 text_box_size(string_width, layout->GetTextBoxHeight());
     Vec2 text_box_position(content_right_x - text_box_size[0], content_bottom_y);
-    text_renderer.AddText(text.c_str(), text_box_position[0],
-                          text_box_position[1] + layout->GetTextOffset(), z,
-                          {font_size, kWhite, text_box_size[0]});
+
+    TextRenderer::TextFormatting formatting{font_size, kWhite, text_box_size[0]};
+    formatting.valign = TextRenderer::VAlign::Bottom;
+    text_renderer.AddText(text.c_str(), text_box_position[0], text_box_position[1], z, formatting);
   }
 
   // Add warning threshold text box and line.
@@ -67,13 +66,12 @@ void AnnotationTrack::DrawAnnotation(Batcher& batcher, TextRenderer& text_render
     float string_width = text_renderer.GetStringWidth(text.c_str(), font_size);
     Vec2 text_box_size(string_width, layout->GetTextBoxHeight());
     Vec2 text_box_position(track_pos[0] + layout->GetRightMargin(), y - text_box_size[1] / 2.f);
-    text_renderer.AddText(text.c_str(), text_box_position[0],
-                          text_box_position[1] + layout->GetTextOffset(), z,
+    text_renderer.AddText(text.c_str(), text_box_position[0], text_box_position[1], z,
                           {font_size, kThresholdColor, text_box_size[0]});
 
     Vec2 from(track_pos[0], y);
     Vec2 to(track_pos[0] + track_size[0], y);
     batcher.AddLine(from, from + Vec2(layout->GetRightMargin() / 2.f, 0), z, kThresholdColor);
-    batcher.AddLine(Vec2(text_box_position[0] + text_box_size[0], y), to, z, kThresholdColor);
+    batcher.AddLine(Vec2(text_box_position[0] - text_box_size[0], y), to, z, kThresholdColor);
   }
 }

src/OrbitGl/BasicPageFaultsTrack.cpp
@@ -93,7 +93,7 @@ void BasicPageFaultsTrack::DrawSingleSeriesEntry(
   float x0 = time_graph_->GetWorldFromTick(start_tick);
   float width = time_graph_->GetWorldFromTick(end_tick) - x0;
   float content_height = GetGraphContentHeight();
-  float y0 = pos_[1] - GetHeight() + layout_->GetTrackBottomMargin();
+  float y0 = GetGraphContentBottomY() - GetGraphContentHeight();
   batcher->AddShadedBox(Vec2(x0, y0), Vec2(width, content_height), z, kHightlightingColor);
 }
 

src/OrbitGl/CallstackThreadBar.cpp
@@ -64,7 +64,7 @@ void CallstackThreadBar::Draw(Batcher& batcher, TextRenderer& text_renderer,
                           : GlCanvas::kZValueEventBar;
   event_bar_z += draw_context.z_offset;
   Color color = GetColor();
-  Box box(pos_, Vec2(GetWidth(), -GetHeight()), event_bar_z);
+  Box box(pos_, Vec2(GetWidth(), GetHeight()), event_bar_z);
   batcher.AddBox(box, color, shared_from_this());
 
   if (batcher.GetPickingManager()->IsThisElementPicked(this)) {
@@ -74,7 +74,7 @@ void CallstackThreadBar::Draw(Batcher& batcher, TextRenderer& text_renderer,
   float x0 = pos_[0];
   float y0 = pos_[1];
   float x1 = x0 + GetWidth();
-  float y1 = y0 - GetHeight();
+  float y1 = y0 + GetHeight();
 
   batcher.AddLine(pos_, Vec2(x1, y0), event_bar_z, color, shared_from_this());
   batcher.AddLine(Vec2(x1, y1), Vec2(x0, y1), event_bar_z, color, shared_from_this());
@@ -86,10 +86,10 @@ void CallstackThreadBar::Draw(Batcher& batcher, TextRenderer& text_renderer,
     x0 = from[0];
     y0 = pos_[1];
     x1 = to[0];
-    y1 = y0 - GetHeight();
+    y1 = y0 + GetHeight();
 
     Color picked_color(0, 128, 255, 128);
-    Box picked_box(Vec2(x0, y0), Vec2(x1 - x0, -GetHeight()),
+    Box picked_box(Vec2(x0, y0), Vec2(x1 - x0, GetHeight()),
                    GlCanvas::kZValueUi + draw_context.z_offset);
     batcher.AddBox(picked_box, picked_color, shared_from_this());
   }
@@ -119,7 +119,7 @@ void CallstackThreadBar::UpdatePrimitives(Batcher* batcher, uint64_t min_tick, u
           CallstackInfo::kComplete) {
         color = kGreyError;
       }
-      batcher->AddVerticalLine(pos, -track_height, z, color);
+      batcher->AddVerticalLine(pos, track_height, z, color);
     };
 
     if (GetThreadId() == orbit_base::kAllProcessThreadsTid) {
@@ -135,7 +135,7 @@ void CallstackThreadBar::UpdatePrimitives(Batcher* batcher, uint64_t min_tick, u
     selected_color.fill(kGreenSelection);
     for (const CallstackEvent& event : time_graph_->GetSelectedCallstackEvents(GetThreadId())) {
       Vec2 pos(time_graph_->GetWorldFromTick(event.time()), pos_[1]);
-      batcher->AddVerticalLine(pos, -track_height, z, kGreenSelection);
+      batcher->AddVerticalLine(pos, track_height, z, kGreenSelection);
     }
   } else {
     // Draw boxes instead of lines to make picking easier, even if this may
@@ -146,7 +146,7 @@ void CallstackThreadBar::UpdatePrimitives(Batcher* batcher, uint64_t min_tick, u
     auto action_on_callstack_events = [=](const CallstackEvent& event) {
       const uint64_t time = event.time();
       CHECK(time >= min_tick && time <= max_tick);
-      Vec2 pos(time_graph_->GetWorldFromTick(time) - kPickingBoxOffset, pos_[1] - track_height + 1);
+      Vec2 pos(time_graph_->GetWorldFromTick(time) - kPickingBoxOffset, pos_[1] + track_height - 1);
       Vec2 size(kPickingBoxWidth, track_height);
       auto user_data = std::make_unique<PickingUserData>(
           nullptr,

src/OrbitGl/FrameTrack.cpp
@@ -69,14 +69,12 @@ FrameTrack::FrameTrack(CaptureViewElement* parent, TimeGraph* time_graph,
   collapse_toggle_->SetCollapsed(true);
 }
 
-float FrameTrack::GetHeaderHeight() const { return layout_->GetTrackTabHeight(); }
-
 float FrameTrack::GetHeight() const {
-  return GetHeaderHeight() + GetMaximumBoxHeight() + layout_->GetTrackBottomMargin();
+  return GetHeaderHeight() + GetMaximumBoxHeight() + layout_->GetTrackContentBottomMargin();
 }
 
-float FrameTrack::GetYFromTimer(const TimerInfo& /*timer_info*/) const {
-  return pos_[1] - GetHeaderHeight() - GetMaximumBoxHeight();
+float FrameTrack::GetYFromTimer(const TimerInfo& timer_info) const {
+  return pos_[1] + GetHeaderHeight() + (GetMaximumBoxHeight() - GetDynamicBoxHeight(timer_info));
 }
 
 float FrameTrack::GetDefaultBoxHeight() const {
@@ -213,7 +211,7 @@ void FrameTrack::Draw(Batcher& batcher, TextRenderer& text_renderer,
   const Color kWhiteColor(255, 255, 255, 255);
   const Color kBlackColor(0, 0, 0, 255);
 
-  float y = pos_[1] - GetHeaderHeight() - GetMaximumBoxHeight() + GetAverageBoxHeight();
+  float y = pos_[1] + GetHeaderHeight() + GetMaximumBoxHeight() - GetAverageBoxHeight();
   float x = pos_[0];
   Vec2 from(x, y);
   Vec2 to(x + GetWidth(), y);
@@ -224,15 +222,14 @@ void FrameTrack::Draw(Batcher& batcher, TextRenderer& text_renderer,
   std::string label = absl::StrFormat("Avg: %s", avg_time);
   uint32_t font_size = layout_->CalculateZoomedFontSize();
   float string_width = text_renderer.GetStringWidth(label.c_str(), font_size);
-  Vec2 white_text_box_size(string_width, layout_->GetTextBoxHeight());
-  Vec2 white_text_box_position(pos_[0] + layout_->GetRightMargin(),
-                               y - layout_->GetTextBoxHeight() / 2.f);
+  Vec2 white_text_box_position(pos_[0] + layout_->GetRightMargin(), y);
 
   batcher.AddLine(from, from + Vec2(layout_->GetRightMargin() / 2.f, 0), text_z, kWhiteColor);
-  batcher.AddLine(Vec2(white_text_box_position[0] + white_text_box_size[0], y), to, text_z,
-                  kWhiteColor);
+  batcher.AddLine(Vec2(white_text_box_position[0] + string_width, y), to, text_z, kWhiteColor);
 
-  text_renderer.AddText(label.c_str(), white_text_box_position[0],
-                        white_text_box_position[1] + layout_->GetTextOffset(), text_z,
-                        {font_size, kWhiteColor, white_text_box_size[0]});
+  TextRenderer::TextFormatting formatting{font_size, kWhiteColor, string_width};
+  formatting.valign = TextRenderer::VAlign::Middle;
+
+  text_renderer.AddText(label.c_str(), white_text_box_position[0], white_text_box_position[1],
+                        text_z, formatting);
 }

src/OrbitGl/FrameTrack.h
@@ -46,7 +46,6 @@ class FrameTrack : public TimerTrack {
   [[nodiscard]] float GetDefaultBoxHeight() const override;
   [[nodiscard]] float GetDynamicBoxHeight(
       const orbit_client_protos::TimerInfo& timer_info) const override;
-  [[nodiscard]] float GetHeaderHeight() const override;
 
   [[nodiscard]] std::string GetTimesliceText(
       const orbit_client_protos::TimerInfo& timer) const override;

src/OrbitGl/GpuDebugMarkerTrack.cpp
@@ -111,15 +111,15 @@ float GpuDebugMarkerTrack::GetYFromTimer(const TimerInfo& timer_info) const {
   if (collapse_toggle_->IsCollapsed()) {
     depth = 0;
   }
-  return pos_[1] - layout_->GetTrackTabHeight() - layout_->GetTextBoxHeight() * (depth + 1.f);
+  return pos_[1] + layout_->GetTrackTabHeight() + layout_->GetTextBoxHeight() * depth;
 }
 
 float GpuDebugMarkerTrack::GetHeight() const {
   bool collapsed = collapse_toggle_->IsCollapsed();
   uint32_t depth =
       collapsed ? std::min<uint32_t>(1, track_data_->GetMaxDepth()) : track_data_->GetMaxDepth();
-  return layout_->GetTrackTabHeight() + layout_->GetTextBoxHeight() * depth +
-         layout_->GetTrackBottomMargin();
+  return layout_->GetTrackTabHeight() + layout_->GetTrackContentTopMargin() +
+         layout_->GetTextBoxHeight() * depth + layout_->GetTrackContentBottomMargin();
 }
 
 bool GpuDebugMarkerTrack::TimerFilter(const TimerInfo& timer_info) const {

src/OrbitGl/GpuSubmissionTrack.cpp
@@ -151,8 +151,7 @@ float GpuSubmissionTrack::GetYFromTimer(const TimerInfo& timer_info) const {
   if (timer_info.type() == TimerInfo::kGpuCommandBuffer) {
     adjusted_depth += 1.f;
   }
-  return pos_[1] - layout_->GetTrackTabHeight() -
-         layout_->GetTextBoxHeight() * (adjusted_depth + 1.f) - gap_space;
+  return pos_[1] + GetHeaderHeight() + layout_->GetTextBoxHeight() * adjusted_depth - gap_space;
 }
 
 // When track or its parent is collapsed, only draw "hardware execution" timers.
@@ -180,8 +179,9 @@ float GpuSubmissionTrack::GetHeight() const {
   if (has_vulkan_layer_command_buffer_timers_ && !collapsed) {
     depth *= 2;
   }
-  return layout_->GetTrackTabHeight() + layout_->GetTextBoxHeight() * depth +
-         (num_gaps * layout_->GetSpaceBetweenGpuDepths()) + layout_->GetTrackBottomMargin();
+  return layout_->GetTrackTabHeight() + layout_->GetTrackContentTopMargin() +
+         layout_->GetTextBoxHeight() * depth + (num_gaps * layout_->GetSpaceBetweenGpuDepths()) +
+         layout_->GetTrackContentBottomMargin();
 }
 
 const TimerInfo* GpuSubmissionTrack::GetLeft(const TimerInfo& timer_info) const {

commit 9e11523ce8771a0a9134df80254402994995adb3

Fix position in legend's tooltip
Regression from https://github.com/google/orbit/pull/2670.

We changed the position of the transparent box where we were drawing
the legend, and as it wasn't aligned with the text drawn, we were
showing an incorrect tooltip there.

Fixed it by simply moving together the text and box before modifying the
position.

https://b/197844515

src/OrbitGl/GraphTrack.cpp
@@ -230,12 +230,12 @@ void GraphTrack<Dimension>::DrawLegend(Batcher& batcher, TextRenderer& text_rend
 
     text_renderer.AddText(series_names[i].c_str(), x0, y0 + legend_symbol_height / 2.f, text_z,
                           formatting);
-    x0 += legend_text_width + kSpaceBetweenLegendEntries;
-
     auto user_data = std::make_unique<PickingUserData>(
         nullptr, [this, i](PickingId /*id*/) { return GetLegendTooltips(i); });
     batcher.AddShadedBox(Vec2(x0, y0), legend_text_box_size, text_z, kFullyTransparent,
                          std::move(user_data));
+
+    x0 += legend_text_width + kSpaceBetweenLegendEntries;
   }
 }
 