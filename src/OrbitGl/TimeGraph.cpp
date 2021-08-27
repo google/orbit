google/orbit

commit cbc7a811576578e8f4c20cfb4bd0c3ef4725e7e7

Time window should not include the right margin
Previously the time window corresponds the range with the length
`Viewport::GetWorldVisibleWidth()`, which means the right margin is also
considered.

Now we change the time window to correspond the trange with the length
of track width.

src/OrbitGl/TimeGraph.cpp
@@ -499,7 +499,7 @@ float TimeGraph::GetWorldFromTick(uint64_t time) const {
   if (time_window_us_ > 0) {
     double start = TicksToMicroseconds(capture_min_timestamp_, time) - min_time_us_;
     double normalized_start = start / time_window_us_;
-    auto pos = float(world_start_x_ + normalized_start * world_width_);
+    auto pos = float(world_start_x_ + normalized_start * (world_width_ - right_margin_));
     return pos;
   }
 
@@ -515,8 +515,9 @@ double TimeGraph::GetUsFromTick(uint64_t time) const {
 }
 
 uint64_t TimeGraph::GetTickFromWorld(float world_x) const {
+  float visible_width = world_width_ - right_margin_;
   double ratio =
-      world_width_ != 0 ? static_cast<double>((world_x - world_start_x_) / world_width_) : 0;
+      visible_width > 0 ? static_cast<double>((world_x - world_start_x_) / visible_width) : 0;
   auto time_span_ns = static_cast<uint64_t>(1000 * GetTime(ratio));
   return capture_min_timestamp_ + time_span_ns;
 }

src/OrbitGl/ThreadTrack.cpp
@@ -276,20 +276,20 @@ void ThreadTrack::Draw(Batcher& batcher, TextRenderer& text_renderer,
   const float thread_state_track_height = layout_->GetThreadStateTrackHeight();
   const float event_track_height = layout_->GetEventTrackHeightFromTid(thread_id_);
   const float tracepoint_track_height = layout_->GetEventTrackHeightFromTid(thread_id_);
-  const float world_width = viewport_->GetVisibleWorldWidth();
+  const float track_width = size_[0];
 
   if (!thread_state_bar_->IsEmpty()) {
-    thread_state_bar_->SetSize(world_width, thread_state_track_height);
+    thread_state_bar_->SetSize(track_width, thread_state_track_height);
     thread_state_bar_->Draw(batcher, text_renderer, current_mouse_time_ns, picking_mode, z_offset);
   }
 
   if (!event_bar_->IsEmpty()) {
-    event_bar_->SetSize(world_width, event_track_height);
+    event_bar_->SetSize(track_width, event_track_height);
     event_bar_->Draw(batcher, text_renderer, current_mouse_time_ns, picking_mode, z_offset);
   }
 
   if (!tracepoint_bar_->IsEmpty()) {
-    tracepoint_bar_->SetSize(world_width, tracepoint_track_height);
+    tracepoint_bar_->SetSize(track_width, tracepoint_track_height);
     tracepoint_bar_->Draw(batcher, text_renderer, current_mouse_time_ns, picking_mode, z_offset);
   }
 }
@@ -455,15 +455,15 @@ void ThreadTrack::OnCaptureComplete() {
   double elapsed_us = end_us - start_us;
   double normalized_start = start_us * draw_data.inv_time_window;
   double normalized_length = elapsed_us * draw_data.inv_time_window;
+  float world_timer_width = static_cast<float>(normalized_length * draw_data.track_width);
   float world_timer_x =
-      static_cast<float>(draw_data.world_start_x + normalized_start * draw_data.world_width);
-  float world_timer_width = static_cast<float>(normalized_length * draw_data.world_width);
+      static_cast<float>(draw_data.track_start_x + normalized_start * draw_data.track_width);
   return {world_timer_x, world_timer_width};
 }
 
 [[nodiscard]] static inline uint64_t GetNextPixelBoundaryTimeNs(
     float world_x, const internal::DrawData& draw_data) {
-  float normalized_x = (world_x - draw_data.world_start_x) / draw_data.world_width;
+  float normalized_x = (world_x - draw_data.track_start_x) / draw_data.track_width;
   int pixel_x = static_cast<int>(ceil(normalized_x * draw_data.viewport->GetScreenWidth()));
   return draw_data.min_tick + pixel_x * draw_data.ns_per_pixel;
 }
@@ -479,7 +479,7 @@ void ThreadTrack::UpdatePrimitives(Batcher* batcher, uint64_t min_tick, uint64_t
   UpdateBoxHeight();
 
   const internal::DrawData draw_data = GetDrawData(
-      min_tick, max_tick, z_offset, batcher, time_graph_, viewport_,
+      min_tick, max_tick, size_[0], z_offset, batcher, time_graph_, viewport_,
       collapse_toggle_->IsCollapsed(), app_->selected_timer(), app_->GetFunctionIdToHighlight());
 
   absl::MutexLock lock(&scope_tree_mutex_);
@@ -507,7 +507,7 @@ void ThreadTrack::UpdatePrimitives(Batcher* batcher, uint64_t min_tick, uint64_t
       auto timer_duration = timer_info.end() - timer_info.start();
       if (timer_duration > draw_data.ns_per_pixel) {
         if (!collapse_toggle_->IsCollapsed()) {
-          DrawTimesliceText(timer_info, draw_data.world_start_x, z_offset, pos, size);
+          DrawTimesliceText(timer_info, draw_data.track_start_x, z_offset, pos, size);
         }
         batcher->AddShadedBox(pos, size, draw_data.z, color, std::move(user_data));
       } else {

src/OrbitGl/TimerTrack.cpp
@@ -67,16 +67,16 @@ struct WorldXInfo {
   float world_x_width;
 };
 
-WorldXInfo ToWorldX(double start_us, double end_us, double inv_time_window, float world_start_x,
-                    float world_width) {
+WorldXInfo ToWorldX(double start_us, double end_us, double inv_time_window, float track_start_x,
+                    float track_width) {
   double width_us = end_us - start_us;
 
   double normalized_start = start_us * inv_time_window;
   double normalized_width = width_us * inv_time_window;
 
   WorldXInfo result{};
-  result.world_x_start = static_cast<float>(world_start_x + normalized_start * world_width);
-  result.world_x_width = static_cast<float>(normalized_width * world_width);
+  result.world_x_start = static_cast<float>(track_start_x + normalized_start * track_width);
+  result.world_x_width = static_cast<float>(normalized_width * track_width);
   return result;
 }
 
@@ -172,13 +172,13 @@ bool TimerTrack::DrawTimer(const TimerInfo* prev_timer_info, const TimerInfo* ne
     bool is_visible_width = ((text_x_end_us - text_x_start_us) * draw_data.inv_time_window *
                              draw_data.viewport->GetScreenWidth()) > 1;
     WorldXInfo world_x_info = ToWorldX(text_x_start_us, text_x_end_us, draw_data.inv_time_window,
-                                       draw_data.world_start_x, draw_data.world_width);
+                                       draw_data.track_start_x, draw_data.track_width);
 
     if (is_visible_width) {
       Vec2 pos{world_x_info.world_x_start, world_timer_y};
       Vec2 size{world_x_info.world_x_width, GetBoxHeight(*current_timer_info)};
 
-      DrawTimesliceText(*current_timer_info, draw_data.world_start_x, draw_data.z_offset, pos,
+      DrawTimesliceText(*current_timer_info, draw_data.track_start_x, draw_data.z_offset, pos,
                         size);
     }
   }
@@ -196,12 +196,12 @@ bool TimerTrack::DrawTimer(const TimerInfo* prev_timer_info, const TimerInfo* ne
 
   if (is_visible_width) {
     WorldXInfo world_x_info_left_overlap =
-        ToWorldX(start_us, start_or_prev_end_us, draw_data.inv_time_window, draw_data.world_start_x,
-                 draw_data.world_width);
+        ToWorldX(start_us, start_or_prev_end_us, draw_data.inv_time_window, draw_data.track_start_x,
+                 draw_data.track_width);
 
     WorldXInfo world_x_info_right_overlap =
-        ToWorldX(end_or_next_start_us, end_us, draw_data.inv_time_window, draw_data.world_start_x,
-                 draw_data.world_width);
+        ToWorldX(end_or_next_start_us, end_us, draw_data.inv_time_window, draw_data.track_start_x,
+                 draw_data.track_width);
 
     Vec3 top_left(world_x_info_left_overlap.world_x_start, world_timer_y + box_height, draw_data.z);
     Vec3 bottom_left(
@@ -222,7 +222,7 @@ bool TimerTrack::DrawTimer(const TimerInfo* prev_timer_info, const TimerInfo* ne
         [&, batcher](PickingId id) { return this->GetBoxTooltip(*batcher, id); });
 
     WorldXInfo world_x_info = ToWorldX(start_us, end_us, draw_data.inv_time_window,
-                                       draw_data.world_start_x, draw_data.world_width);
+                                       draw_data.track_start_x, draw_data.track_width);
 
     Vec2 pos(world_x_info.world_x_start, world_timer_y);
     draw_data.batcher->AddVerticalLine(pos, GetBoxHeight(*current_timer_info), draw_data.z, color,
@@ -258,8 +258,8 @@ void TimerTrack::UpdatePrimitives(Batcher* batcher, uint64_t min_tick, uint64_t 
   draw_data.batcher = batcher;
   draw_data.viewport = viewport_;
 
-  draw_data.world_start_x = viewport_->GetWorldTopLeft()[0];
-  draw_data.world_width = viewport_->GetVisibleWorldWidth();
+  draw_data.track_start_x = viewport_->GetWorldTopLeft()[0];
+  draw_data.track_width = size_[0];
   draw_data.inv_time_window = 1.0 / time_graph_->GetTimeWindowUs();
   draw_data.is_collapsed = collapse_toggle_->IsCollapsed();
 
@@ -415,8 +415,8 @@ std::string TimerTrack::GetBoxTooltip(const Batcher& /*batcher*/, PickingId /*id
 
 float TimerTrack::GetHeaderHeight() const { return layout_->GetTrackTabHeight(); }
 
-internal::DrawData TimerTrack::GetDrawData(uint64_t min_tick, uint64_t max_tick, float z_offset,
-                                           Batcher* batcher, TimeGraph* time_graph,
+internal::DrawData TimerTrack::GetDrawData(uint64_t min_tick, uint64_t max_tick, float track_width,
+                                           float z_offset, Batcher* batcher, TimeGraph* time_graph,
                                            orbit_gl::Viewport* viewport, bool is_collapsed,
                                            const orbit_client_protos::TimerInfo* selected_timer,
                                            uint64_t highlighted_function_id) {
@@ -426,8 +426,8 @@ internal::DrawData TimerTrack::GetDrawData(uint64_t min_tick, uint64_t max_tick,
   draw_data.z_offset = z_offset;
   draw_data.batcher = batcher;
   draw_data.viewport = viewport;
-  draw_data.world_start_x = viewport->GetWorldTopLeft()[0];
-  draw_data.world_width = viewport->GetVisibleWorldWidth();
+  draw_data.track_start_x = viewport->GetWorldTopLeft()[0];
+  draw_data.track_width = track_width;
   draw_data.inv_time_window = 1.0 / time_graph->GetTimeWindowUs();
   draw_data.is_collapsed = is_collapsed;
   draw_data.z = GlCanvas::kZValueBox + z_offset;

src/OrbitGl/TimerTrack.h
@@ -41,8 +41,8 @@ struct DrawData {
   orbit_gl::Viewport* viewport;
   const orbit_client_protos::TimerInfo* selected_timer;
   double inv_time_window;
-  float world_start_x;
-  float world_width;
+  float track_start_x;
+  float track_width;
   float z_offset;
   float z;
   bool is_collapsed;
@@ -139,8 +139,8 @@ class TimerTrack : public Track {
                                  float /*z_offset*/, Vec2 /*box_pos*/, Vec2 /*box_size*/);
 
   [[nodiscard]] static internal::DrawData GetDrawData(
-      uint64_t min_tick, uint64_t max_tick, float z_offset, Batcher* batcher, TimeGraph* time_graph,
-      orbit_gl::Viewport* viewport, bool is_collapsed,
+      uint64_t min_tick, uint64_t max_tick, float track_width, float z_offset, Batcher* batcher,
+      TimeGraph* time_graph, orbit_gl::Viewport* viewport, bool is_collapsed,
       const orbit_client_protos::TimerInfo* selected_timer, uint64_t highlighted_function_id);
 
   TextRenderer* text_renderer_ = nullptr;

commit 67f3645ab669986d87baa6d179c18c9d711f6f21

Fix inconsistency with coordinates in TimeGraph
A regression from https://github.com/google/orbit/pull/2573.

After not including the right margin in TimeGraph, we have to not use
Viewport->GetWidth methods anymore. In particular in world_width_.

http://b/197204311.

src/OrbitGl/TimeGraph.cpp
@@ -518,7 +518,7 @@ void TimeGraph::UpdatePrimitives(Batcher* /*batcher*/, uint64_t /*min_tick*/, ui
 
   time_window_us_ = max_time_us_ - min_time_us_;
   world_start_x_ = viewport_->GetWorldTopLeft()[0];
-  world_width_ = viewport_->GetVisibleWorldWidth();
+  world_width_ = GetWidth();
   uint64_t min_tick = GetTickFromUs(min_time_us_);
   uint64_t max_tick = GetTickFromUs(max_time_us_);
 
@@ -667,7 +667,7 @@ void TimeGraph::DrawOverlay(Batcher& batcher, TextRenderer& text_renderer,
   x_coords.reserve(timers.size());
 
   float world_start_x = viewport_->GetWorldTopLeft()[0];
-  float world_width = viewport_->GetVisibleWorldWidth();
+  float world_width = GetWidth();
 
   float world_start_y = viewport_->GetWorldTopLeft()[1];
   float world_height = viewport_->GetVisibleWorldHeight();