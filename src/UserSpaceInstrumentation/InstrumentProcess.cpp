be2a4ef9d66d7b51e8323512d3753fbb27981e0b
Henning Becker <henning.becker@gmail.com>
2021-09-02 15:13:57 +0000

    Add interal path for libOrbitUserSpaceInstrumentation
    The variable "@@LIB_ORBIT_USER_SPACE_INSTRUMENTATION_PATH@@" will be
    replaced by copybara.

--- src/UserSpaceInstrumentation/InstrumentProcess.cpp
+++ src/UserSpaceInstrumentation/InstrumentProcess.cpp
@@ -40,6 +40,9 @@ ErrorMessageOr<std::filesystem::path> GetLibraryPath() {
   const std::filesystem::path exe_dir = orbit_base::GetExecutableDir();
   std::vector<std::filesystem::path> potential_paths = {exe_dir / kLibName,
                                                         exe_dir / ".." / "lib" / kLibName};
+  /* copybara:insert(In internal tests the library is in a different place)
+  potential_paths.emplace_back("@@LIB_ORBIT_USER_SPACE_INSTRUMENTATION_PATH@@");
+  */
   for (const auto& path : potential_paths) {
     if (std::filesystem::exists(path)) {
       return path;
