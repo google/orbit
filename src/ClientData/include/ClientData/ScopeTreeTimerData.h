655f35f6e8c64ba0503fa72ab422db1d75c54a9f
Pablo Zimmermann <karupayun@google.com>
2021-09-15 11:01:14 +0200

    Create ScopeTreeTimerData (#2760)
    We are creating ScopeTreeTimerData which will represent the timers inside
    a ThreadTrack based on ScopeTree data structure. For now, it's a temporal
    class because we are trying to query the timers for ThreadTrack. In the future
    we will do the same for other TimerTracks and we might eventually merge it with
    TrackPaneData.
    
    As currently, we are having two containers, TrackPaneData and ScopeTree.
    In the future we might use ScopeTree while capturing (with some
    modifications to erase bunch of timers), and the other container will be
    used when stopping a capture.
    
    The current functions are AddTimer(), which will replace OnTimer and
    ProcessTimer for ThreadTrack, BuildScopeTreeFromTrackData(), which will
    be used to construct the ScopeTree when loading a capture (legacy), and
    GetAllTimers(min_tick, max_tick) which will return all timers in that
    range. Aditionally we have the methods used in TrackPaneData and
    GetLeft/Right/Top/Down.

---
+++ src/ClientData/include/ClientData/ScopeTreeTimerData.h
@@ -0,0 +1,61 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CLIENT_DATA_SCOPE_TREE_TIMER_DATA_PROVIDER_H_
+#define CLIENT_DATA_SCOPE_TREE_TIMER_DATA_PROVIDER_H_
+
+#include <absl/synchronization/mutex.h>
+
+#include <vector>
+
+#include "Containers/ScopeTree.h"
+#include "TrackPaneData.h"
+
+namespace orbit_client_data {
+
+// Stores all the timers from a particular ThreadId. Provides queries to get timers in a certain
+// range as well as metadata from them.
+class ScopeTreeTimerData final {
+ public:
+  enum class ScopeTreeUpdateType { kAlways, kOnCaptureComplete, kNever };
+  explicit ScopeTreeTimerData(int64_t thread_id = -1, ScopeTreeUpdateType scope_tree_update_type =
+                                                          ScopeTreeUpdateType::kAlways)
+      : thread_id_(thread_id), scope_tree_update_type_(scope_tree_update_type){};
+
+  [[nodiscard]] int64_t GetThreadId() const { return thread_id_; }
+  [[nodiscard]] bool IsEmpty() const { return GetNumberOfTimers() == 0; }
+  [[nodiscard]] size_t GetNumberOfTimers() const;
+  [[nodiscard]] uint64_t GetMinTime() const { return track_pane_data_.GetMinTime(); }
+  [[nodiscard]] uint64_t GetMaxTime() const { return track_pane_data_.GetMaxTime(); }
+  [[nodiscard]] uint32_t GetMaxDepth() const;
+
+  const orbit_client_protos::TimerInfo& AddTimer(orbit_client_protos::TimerInfo timer_info);
+  // Build ScopeTree from timer chains, when we are loading a capture.
+  void BuildScopeTreeFromTrackData();
+
+  [[nodiscard]] std::vector<const orbit_client_protos::TimerInfo*> GetTimers(
+      uint64_t min_tick = std::numeric_limits<uint64_t>::min(),
+      uint64_t max_tick = std::numeric_limits<uint64_t>::max()) const;
+  [[nodiscard]] const orbit_client_protos::TimerInfo& GetLeft(
+      const orbit_client_protos::TimerInfo& timer) const;
+  [[nodiscard]] const orbit_client_protos::TimerInfo& GetRight(
+      const orbit_client_protos::TimerInfo& timer) const;
+  [[nodiscard]] const orbit_client_protos::TimerInfo& GetUp(
+      const orbit_client_protos::TimerInfo& timer) const;
+  [[nodiscard]] const orbit_client_protos::TimerInfo& GetDown(
+      const orbit_client_protos::TimerInfo& timer) const;
+
+ private:
+  const int64_t thread_id_;
+  mutable absl::Mutex scope_tree_mutex_;
+  orbit_containers::ScopeTree<const orbit_client_protos::TimerInfo> scope_tree_
+      GUARDED_BY(scope_tree_mutex_);
+  ScopeTreeUpdateType scope_tree_update_type_;
+
+  TrackPaneData track_pane_data_;
+};
+
+}  // namespace orbit_client_data
+
+#endif  // CLIENT_DATA_SCOPE_TREE_TIMER_DATA_PROVIDER_H_
