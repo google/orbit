53d7740df1a5aa542acc26b493074764e85db149
Pierric Gimmig <pierric@google.com>
2021-10-06 07:29:49 -0700

    Add PdbFileDia to load pdb files using the DIA SDK
    We are running into issues when loading certain pdb files with LLVM.
    Add our own DIA based pdb parser, PdbFileDia. Add PdbFileDiaTest
    and modified existing tests to be reusable by different implementations
    of PdbFile.

---
+++ src/ObjectUtils/PdbFileDia.cpp
@@ -0,0 +1,136 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "PdbFileDia.h"
+
+#include <diacreate.h>
+#include <llvm/Demangle/Demangle.h>
+#include <winerror.h>
+
+#include "OrbitBase/ExecutablePath.h"
+#include "OrbitBase/Logging.h"
+
+using orbit_grpc_protos::ModuleSymbols;
+using orbit_grpc_protos::SymbolInfo;
+
+namespace orbit_object_utils {
+
+static ErrorMessageOr<CComPtr<IDiaDataSource>> CreateDiaDataSource() {
+  // Initializes the COM library for the current thread.
+  thread_local HRESULT com_initialize_result = CoInitialize(NULL);
+  if (com_initialize_result != S_OK) {
+    return ErrorMessage{absl::StrFormat("CoInitialize failed (%u)", com_initialize_result)};
+  }
+
+  // Create instance of dia data source.
+  CComPtr<IDiaDataSource> dia_data_source = nullptr;
+  constexpr const wchar_t* kDiaDllFileName = L"msdia140.dll";
+  HRESULT cocreate_result = NoRegCoCreate(kDiaDllFileName, CLSID_DiaSource, IID_IDiaDataSource,
+                                          reinterpret_cast<LPVOID*>(&dia_data_source));
+  if (cocreate_result != S_OK) {
+    return ErrorMessage{absl::StrFormat("NoRegCoCreate failed (%u)", cocreate_result)};
+  }
+
+  return dia_data_source;
+}
+
+PdbFileDia::PdbFileDia(std::filesystem::path file_path, const ObjectFileInfo& object_file_info)
+    : file_path_(std::move(file_path)), object_file_info_(object_file_info) {}
+
+ErrorMessageOr<void> PdbFileDia::LoadDataForPDB() {
+  OUTCOME_TRY(dia_data_source_, CreateDiaDataSource());
+
+  // Open and prepare a program database (.pdb) file as a debug data source.
+  HRESULT result = dia_data_source_->loadDataFromPdb(file_path_.wstring().c_str());
+  if (result != S_OK) {
+    return ErrorMessage{
+        absl::StrFormat("loadDataFromPdb failed for %s (%u)", file_path_.string(), result)};
+  }
+
+  // Open DIA session.
+  result = dia_data_source_->openSession(&dia_session_);
+  if (result != S_OK) {
+    return ErrorMessage{
+        absl::StrFormat("openSession failed for %s (%u)", file_path_.string(), result)};
+  }
+
+  // Retrieve a reference to the global scope.
+  result = dia_session_->get_globalScope(&dia_global_scope_symbol_);
+  if (result != S_OK) {
+    return ErrorMessage{
+        absl::StrFormat("get_globalScope failed for %s (%u)", file_path_.string(), result)};
+  }
+
+  // Get age.
+  DWORD age = 0;
+  result = dia_global_scope_symbol_->get_age(&age);
+  if (result != S_OK) {
+    return ErrorMessage{absl::StrFormat("get_age failed for %s (%u)", file_path_.string(), result)};
+  }
+  age_ = age;
+
+  // Get guid.
+  GUID guid = {};
+  result = dia_global_scope_symbol_->get_guid(&guid);
+  if (result != S_OK) {
+    return ErrorMessage{
+        absl::StrFormat("get_guid failed for %s (%u)", file_path_.string(), result)};
+  }
+  static_assert(sizeof(guid_) == sizeof(GUID));
+  std::memcpy(guid_.data(), &guid, sizeof(GUID));
+
+  return outcome::success();
+}
+
+ErrorMessageOr<orbit_grpc_protos::ModuleSymbols> PdbFileDia::LoadDebugSymbols() {
+  HRESULT result = 0;
+  CComPtr<IDiaEnumSymbols> dia_enum_symbols;
+  result = dia_global_scope_symbol_->findChildren(SymTagFunction, NULL, nsNone, &dia_enum_symbols);
+  if (result != S_OK) {
+    return ErrorMessage{
+        absl::StrFormat("findChildren failed for %s (%u)", file_path_.string(), result)};
+  }
+
+  ModuleSymbols module_symbols;
+  module_symbols.set_load_bias(object_file_info_.load_bias);
+  module_symbols.set_symbols_file_path(file_path_.string());
+  CComPtr<IDiaSymbol> dia_symbol;
+  ULONG celt = 0;
+
+  while (SUCCEEDED(dia_enum_symbols->Next(1, &dia_symbol, &celt)) && (celt == 1)) {
+    SymbolInfo symbol_info;
+
+    BSTR function_name = {};
+    if (dia_symbol->get_name(&function_name) != S_OK) continue;
+    std::wstring name(function_name);
+    symbol_info.set_name(std::string(name.begin(), name.end()));
+    symbol_info.set_demangled_name(llvm::demangle(symbol_info.name()));
+    SysFreeString(function_name);
+
+    DWORD relative_virtual_address = 0;
+    if (dia_symbol->get_relativeVirtualAddress(&relative_virtual_address) != S_OK) continue;
+    symbol_info.set_address(relative_virtual_address + object_file_info_.load_bias);
+
+    ULONGLONG length = 0;
+    if (dia_symbol->get_length(&length) != S_OK) continue;
+    symbol_info.set_size(length);
+
+    *module_symbols.add_symbol_infos() = std::move(symbol_info);
+  }
+
+  return module_symbols;
+}
+
+ErrorMessageOr<std::unique_ptr<PdbFile>> PdbFileDia::CreatePdbFile(
+    const std::filesystem::path& file_path, const ObjectFileInfo& object_file_info) {
+  auto pdb_file_dia = std::make_unique<PdbFileDia>(file_path, object_file_info);
+  auto result = pdb_file_dia->LoadDataForPDB();
+  if (result.has_error()) {
+    return ErrorMessage(absl::StrFormat("Unable to load PDB file %s with error: %s",
+                                        file_path.string(), result.error().message()));
+  }
+  return std::move(pdb_file_dia);
+}
+
+}  // namespace orbit_object_utils
---
+++ src/ObjectUtils/PdbFileLlvm.h
@@ -0,0 +1,71 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef OBJECT_UTILS_PDB_FILE_LLVM_H_
+#define OBJECT_UTILS_PDB_FILE_LLVM_H_
+
+#include <llvm/DebugInfo/CodeView/CVSymbolVisitor.h>
+#include <llvm/DebugInfo/CodeView/CodeView.h>
+#include <llvm/DebugInfo/CodeView/GUID.h>
+#include <llvm/DebugInfo/CodeView/SymbolDeserializer.h>
+#include <llvm/DebugInfo/CodeView/SymbolVisitorCallbackPipeline.h>
+#include <llvm/DebugInfo/CodeView/SymbolVisitorCallbacks.h>
+#include <llvm/DebugInfo/MSF/MappedBlockStream.h>
+#include <llvm/DebugInfo/PDB/Native/DbiStream.h>
+#include <llvm/DebugInfo/PDB/Native/ModuleDebugStream.h>
+#include <llvm/DebugInfo/PDB/Native/NativeSession.h>
+#include <llvm/DebugInfo/PDB/Native/PDBFile.h>
+#include <llvm/DebugInfo/PDB/PDB.h>
+#include <llvm/DebugInfo/PDB/PDBSymbolExe.h>
+#include <llvm/DebugInfo/PDB/PDBTypes.h>
+#include <llvm/Demangle/Demangle.h>
+
+#include <array>
+#include <filesystem>
+
+#include "ObjectUtils/PdbFile.h"
+#include "ObjectUtils/WindowsBuildIdUtils.h"
+#include "OrbitBase/Result.h"
+#include "symbol.pb.h"
+
+namespace orbit_object_utils {
+
+class PdbFileLlvm : public PdbFile {
+ public:
+  PdbFileLlvm(std::filesystem::path file_path, const ObjectFileInfo& object_file_info,
+              std::unique_ptr<llvm::pdb::IPDBSession> session);
+
+  [[nodiscard]] ErrorMessageOr<orbit_grpc_protos::ModuleSymbols> LoadDebugSymbols() override;
+  [[nodiscard]] const std::filesystem::path& GetFilePath() const override { return file_path_; }
+
+  [[nodiscard]] std::array<uint8_t, 16> GetGuid() const override {
+    constexpr int kGuidSize = 16;
+    static_assert(kGuidSize == sizeof(llvm::codeview::GUID));
+    std::array<uint8_t, kGuidSize> result;
+    auto global_scope = session_->getGlobalScope();
+    const llvm::codeview::GUID& guid = global_scope->getGuid();
+    std::copy(std::begin(guid.Guid), std::end(guid.Guid), std::begin(result));
+    return result;
+  }
+
+  [[nodiscard]] uint32_t GetAge() const override { return session_->getGlobalScope()->getAge(); }
+  [[nodiscard]] std::string GetBuildId() const override {
+    return ComputeWindowsBuildId(GetGuid(), GetAge());
+  }
+
+  static ErrorMessageOr<std::unique_ptr<PdbFile>> CreatePdbFile(
+      const std::filesystem::path& file_path, const ObjectFileInfo& object_file_info);
+
+ private:
+  std::filesystem::path file_path_;
+  ObjectFileInfo object_file_info_;
+  std::unique_ptr<llvm::pdb::IPDBSession> session_;
+};
+
+ErrorMessageOr<std::unique_ptr<PdbFile>> CreatePdbFileLlvm(const std::filesystem::path& file_path,
+                                                           const ObjectFileInfo& object_file_info);
+
+}  // namespace orbit_object_utils
+
+#endif  // OBJECT_UTILS_PDB_FILE_LLVM_H_
