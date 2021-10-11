84806dc5156c162e606ccf37c47f3e05f6b93fb5
Pablo Zimmermann <karupayun@google.com>
2021-10-11 15:42:12 +0000

    Create ThreadTrackDataManager
    This PR creates ThreadTrackDataManager, a temporal place (while
    ThreadTrack is the only Track who uses ScopeTree) where the data inside
    ThreadTrack will be stored. The class is really similar to
    TimerDataManager but uses instead ScopeTreeTimerData.
    
    The interface includes:
    
    - CreateScopeTreeTimerData: To create a new ScopeTreeTimerData for a new
    track. In the future might be a private function and all the logic
    related will be modified by including AddNewTimer
    (go/stadia-orbit-track-creation), who will use this
    private function when needed.
    
    - GetAllThreadTimerChains: Currently used in TimeGraph for looking for
    previous/next function call and in some other places. Should be replaced
    in the future by some queries.
    
    - OnCaptureComplete: To build the ScopeTree when loading a capture.
    
    This is a puzzle piece inside https://github.com/google/orbit/pull/2860,
    aiming to solve http://b/202110929.
    
    PS: I'm open to advices about the name of this class,
    ScopeTreeTimerDataManager seems to be incorrect since it is being used
    for ThreadTracks and we have a map based on thread_id. Actually, this
    will help us to solve the multi-threading issue written in
    go/stadia-orbit-track-creation for ThreadTracks.

---
+++ src/ClientData/include/ClientData/ThreadTrackDataManager.h
@@ -0,0 +1,62 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THREAD_TRACK_DATA_MANAGER_H_
+#define THREAD_TRACK_DATA_MANAGER_H_
+
+#include <absl/synchronization/mutex.h>
+
+#include <vector>
+
+#include "ClientData/TimerData.h"
+#include "OrbitBase/Append.h"
+#include "ScopeTreeTimerData.h"
+#include "TimerDataManager.h"
+
+namespace orbit_client_data {
+
+// Creates and stores data from Thread Tracks in a thread-safe way, using thread_id as the key.
+class ThreadTrackDataManager final {
+ public:
+  ThreadTrackDataManager(bool is_data_from_saved_capture = false)
+      : scope_tree_update_type_(is_data_from_saved_capture
+                                    ? ScopeTreeTimerData::ScopeTreeUpdateType::kOnCaptureComplete
+                                    : ScopeTreeTimerData::ScopeTreeUpdateType::kAlways){};
+
+  ScopeTreeTimerData* CreateScopeTreeTimerData(uint32_t thread_id) {
+    absl::MutexLock lock(&mutex_);
+    scope_tree_timer_data_map_.insert_or_assign(
+        thread_id, std::make_unique<ScopeTreeTimerData>(thread_id, scope_tree_update_type_));
+    return scope_tree_timer_data_map_.at(thread_id).get();
+  };
+
+  std::vector<const TimerChain*> GetAllThreadTimerChains() const {
+    absl::MutexLock lock(&mutex_);
+    std::vector<const TimerChain*> chains;
+    for (const auto& [unused_tid, scope_tree_timer_data] : scope_tree_timer_data_map_) {
+      orbit_base::Append(chains, scope_tree_timer_data->GetChains());
+    }
+    return chains;
+  }
+
+  void OnCaptureComplete() {
+    absl::MutexLock lock(&mutex_);
+    // Build ScopeTree from timer chains when we are loading a capture.
+    if (scope_tree_update_type_ == ScopeTreeTimerData::ScopeTreeUpdateType::kOnCaptureComplete) {
+      for (const auto& [unused_tid, scope_tree_timer_data] : scope_tree_timer_data_map_) {
+        scope_tree_timer_data->BuildScopeTreeFromTimerData();
+      }
+    }
+  }
+
+ private:
+  mutable absl::Mutex mutex_;
+  absl::flat_hash_map<uint32_t, std::unique_ptr<ScopeTreeTimerData>> scope_tree_timer_data_map_
+      GUARDED_BY(mutex_);
+  ScopeTreeTimerData::ScopeTreeUpdateType scope_tree_update_type_;
+};
+
+}  // namespace orbit_client_data
+
+#endif  // THREAD_TRACK_DATA_MANAGER_H_
