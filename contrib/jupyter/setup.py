94053dc2f5425b670411c0e65683cadef9ec5e2f
ronaldfw <wotzlaw@google.com>
2021-10-20 11:21:53 +0200

    Update Python orbitutils and Jupyter notebook to new capture format
    Our Python module `orbitutils` that can be used to open Orbit captures
    for processing in Jupyter notebooks was still using the old capture file
    format. This change updates the parsing code to match the new format and
    adapts the Jupyter notebook example accordingly.
    
    Tested: Unit tests.

--- contrib/jupyter/setup.py
+++ contrib/jupyter/setup.py
@@ -8,12 +8,13 @@ from setuptools import setup
 
 setup(
     name='orbitutils',
-    version='0.0.1',
+    version='1.0.0',
     author='The Orbit Authors',
     packages=['orbitutils'],
     url='http://github.com/google/orbit',
     license='BSD 2-Clause License',
     description='Orbit capture utilities',
     # This should stay in sync with the proto version we use in C++ (see conanfile.py).
+    # Make sure that your `protoc` version also matches this.
     install_requires=["protobuf==3.9.1"],
 )
