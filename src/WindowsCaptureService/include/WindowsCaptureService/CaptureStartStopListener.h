c370a2b7a3351cf8b428403af417fc83da61307c
Pierric Gimmig <pierric@google.com>
2021-10-26 16:15:08 -0700

    Add WindowsCaptureService module

---
+++ src/WindowsCaptureService/include/WindowsCaptureService/CaptureStartStopListener.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2021 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WINDOWS_CAPTURE_SERVICE_CAPTURE_START_STOP_LISTENER_H_
+#define WINDOWS_CAPTURE_SERVICE_CAPTURE_START_STOP_LISTENER_H_
+
+#include "ProducerEventProcessor/ProducerEventProcessor.h"
+#include "capture.pb.h"
+
+namespace orbit_windows_capture_service {
+
+// This interface is used to propagate requests received by CaptureServiceImpl to start and stop
+// the capture, together with the CaptureOptions and the CaptureEventBuffer where to add the
+// generated CaptureEvents.
+class CaptureStartStopListener {
+ public:
+  virtual ~CaptureStartStopListener() = default;
+
+  virtual void OnCaptureStartRequested(
+      orbit_grpc_protos::CaptureOptions capture_options,
+      orbit_producer_event_processor::ProducerEventProcessor* producer_event_processor) = 0;
+
+  // This is to be assumed blocking until the capture stop has been fully processed by the listener.
+  virtual void OnCaptureStopRequested() = 0;
+};
+
+}  // namespace orbit_windows_capture_service
+
+#endif  // WINDOWS_CAPTURE_SERVICE_CAPTURE_START_STOP_LISTENER_H_
