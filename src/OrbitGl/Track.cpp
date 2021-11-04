45b625a5cd0073c4ddc709d76273a52e160ed8d4
Florian Reichl <freichl@google.com>
2021-11-04 16:57:03 +0100

    Track include fixes

--- src/OrbitGl/Track.cpp
+++ src/OrbitGl/Track.cpp
@@ -1,7 +1,8 @@
-#include "Track.h"
 // Copyright (c) 2020 The Orbit Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+#include "Track.h"
 
 #include <math.h>
 #include <stddef.h>
@@ -15,7 +16,6 @@
 #include "TextRenderer.h"
 #include "TimeGraph.h"
 #include "TimeGraphLayout.h"
-#include "Track.h"
 #include "Viewport.h"
 
 using orbit_client_data::TimerData;
