45cbf0d5139308d7cb1b60e2914749bde11353f6
ronaldfw <wotzlaw@google.com>
2021-11-25 15:24:22 +0100

    Add initial fuzzing for PE/COFF parsing in libunwindstack
    This adds a first fuzzer for PE/COFF parsing. The fuzzer is extremely
    basic and generates poor coverage as it is not at all aware of
    the structure of PE/COFF files. This will be fixed later. For now,
    this is added to establish the structure of how we add fuzzing tests
    for libunwindstack and to make sure it shows up in our oss-fuzz
    integration. Next steps will be to add a custom mutator and/or add a
    corpus based on PE/COFF files to trigger more code paths.
    
    Tested: Ran fuzzer locally.
    Bug: http://b/192514457

---
+++ third_party/libunwindstack/tests/fuzz/PeCoffInterfaceFuzzer.cpp
@@ -0,0 +1,32 @@
+
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <inttypes.h>
+#include <cstddef>
+
+#include <unwindstack/Memory.h>
+#include <unwindstack/PeCoffInterface.h>
+
+// The most basic fuzzer for PE/COFF parsing. Generates really poor coverage as
+// it is not PE/COFF file structure aware.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::shared_ptr<unwindstack::Memory> memory =
+      unwindstack::Memory::CreateOfflineMemory(data, 0, size);
+  unwindstack::PeCoffInterface<uint64_t> pe_coff_interface(memory.get());
+  pe_coff_interface.Init();
+  return 0;
+}
