e83049349e27bb0adfefdae6dc72181811232c70
Henning Becker <43133967+beckerhe@users.noreply.github.com>
2021-12-03 14:03:10 +0100

    Rename temporary Orbit.zip to OrbitUI.zip (#3138)
    Our CI system compiles collector and UI in separate jobs and combines
    both artifacts into a single ZIP archive called Orbit.zip.
    
    It can come to problems when both the resulting artifact of the UI
    compilation job and of the combiner job are both called Orbit.zip.
    
    This fixes the glitch by renaming the output filename of the UI
    compilation job.

--- kokoro/builds/collect.sh
+++ kokoro/builds/collect.sh
@@ -4,6 +4,5 @@ set -e
 # Trivial repackaging of the build artefacts: The collector gets integrated into Orbit.zip.
 cd "$KOKORO_GFILE_DIR"
 unzip Collector.zip
-unzip Orbit.zip
-rm Orbit.zip
+unzip OrbitUI.zip
 zip -r Orbit.zip Orbit/
