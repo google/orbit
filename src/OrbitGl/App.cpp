google/orbit

commit 19c86d19c22668bf1871f9adac582212bef019ac

Remove active waiting from the CopyFileToLocal routine (#3157)
Active waiting means the waiting function will not end but rather
execute other waiting events on the event loop.

But this will increase the stack size with every actively waiting
function and eventually leads to a stack overflow.

So this commit removes active waiting and instead returns a future. That
means the caller needs to take care of waiting. In this case waiting is
actually not needed and the caller will just chain up a couple of
futures and be done.

src/OrbitGl/App.cpp
@@ -1593,9 +1593,9 @@ orbit_base::Future<ErrorMessageOr<std::filesystem::path>> OrbitApp::RetrieveModu
                                                   {absl::GetFlag(FLAGS_instance_symbols_folder)});
       });
 
-  auto download_file =
-      [this, module_file_path, scoped_status = std::move(scoped_status)](
-          ErrorMessageOr<std::string> result) mutable -> ErrorMessageOr<std::filesystem::path> {
+  auto download_file = [this, module_file_path, scoped_status = std::move(scoped_status)](
+                           ErrorMessageOr<std::string> result) mutable
+      -> orbit_base::Future<ErrorMessageOr<std::filesystem::path>> {
     if (result.has_error()) return result.error();
 
     const std::string& debug_file_path = result.value();
@@ -1608,17 +1608,24 @@ orbit_base::Future<ErrorMessageOr<std::filesystem::path>> OrbitApp::RetrieveModu
         absl::StrFormat(R"(Copying debug info file for "%s" from remote: "%s"...)",
                         module_file_path, debug_file_path));
     SCOPED_TIMED_LOG("Copying \"%s\"", debug_file_path);
-    auto scp_result = secure_copy_callback_(debug_file_path, local_debug_file_path.string());
+    orbit_base::Future<ErrorMessageOr<void>> copy_result =
+        secure_copy_callback_(debug_file_path, local_debug_file_path.string());
 
-    if (scp_result.has_error()) {
-      return ErrorMessage{absl::StrFormat("Could not copy debug info file from the remote: %s",
-                                          scp_result.error().message())};
-    }
-
-    return local_debug_file_path;
+    orbit_base::ImmediateExecutor immediate_executor{};
+    return copy_result.Then(
+        &immediate_executor,
+        [local_debug_file_path](
+            ErrorMessageOr<void> sftp_result) -> ErrorMessageOr<std::filesystem::path> {
+          if (sftp_result.has_error()) {
+            return ErrorMessage{
+                absl::StrFormat("Could not copy debug info file from the remote: %s",
+                                sftp_result.error().message())};
+          }
+          return local_debug_file_path;
+        });
   };
 
-  return check_file_on_remote.Then(main_thread_executor_, std::move(download_file));
+  return UnwrapFuture(check_file_on_remote.Then(main_thread_executor_, std::move(download_file)));
 }
 
 orbit_base::Future<void> OrbitApp::RetrieveModulesAndLoadSymbols(

src/OrbitGl/App.h
@@ -303,7 +303,7 @@ class OrbitApp final : public DataViewFactory,
     clipboard_callback_ = std::move(callback);
   }
   using SecureCopyCallback =
-      std::function<ErrorMessageOr<void>(std::string_view, std::string_view)>;
+      std::function<orbit_base::Future<ErrorMessageOr<void>>(std::string_view, std::string_view)>;
   void SetSecureCopyCallback(SecureCopyCallback callback) {
     secure_copy_callback_ = std::move(callback);
   }

src/SessionSetup/ServiceDeployManager.cpp
@@ -300,20 +300,18 @@ outcome::result<void> ServiceDeployManager::CopyOrbitServicePackage() {
   return outcome::success();
 }
 
-ErrorMessageOr<void> ServiceDeployManager::CopyFileToLocal(std::string source,
-                                                           std::string destination) {
+orbit_base::Future<ErrorMessageOr<void>> ServiceDeployManager::CopyFileToLocal(
+    std::string source, std::string destination) {
   orbit_base::Promise<ErrorMessageOr<void>> promise;
   auto future = promise.GetFuture();
 
-  DeferToBackgroundThreadAndWait(
-      this, [this, source = std::move(source), destination = std::move(destination),
-             promise = std::move(promise)]() mutable {
-        promise.SetResult(CopyFileToLocalImpl(source, destination));
-      });
+  QMetaObject::invokeMethod(this,
+                            [this, source = std::move(source), destination = std::move(destination),
+                             promise = std::move(promise)]() mutable {
+                              promise.SetResult(CopyFileToLocalImpl(source, destination));
+                            });
 
-  if (!future.IsFinished()) return ErrorMessage{"Copy operation was aborted."};
-
-  return future.Get();
+  return future;
 }
 
 ErrorMessageOr<void> ServiceDeployManager::CopyFileToLocalImpl(std::string_view source,

src/SessionSetup/include/SessionSetup/ServiceDeployManager.h
@@ -20,6 +20,7 @@
 
 #include "DeploymentConfigurations.h"
 #include "MetricsUploader/MetricsUploader.h"
+#include "OrbitBase/Future.h"
 #include "OrbitBase/Result.h"
 #include "OrbitSsh/Context.h"
 #include "OrbitSsh/Credentials.h"
@@ -49,7 +50,8 @@ class ServiceDeployManager : public QObject {
       orbit_metrics_uploader::MetricsUploader* metrics_uploader = nullptr);
 
   // This method copies remote source file to local destination.
-  ErrorMessageOr<void> CopyFileToLocal(std::string source, std::string destination);
+  orbit_base::Future<ErrorMessageOr<void>> CopyFileToLocal(std::string source,
+                                                           std::string destination);
 
   void Shutdown();
   void Cancel();

commit 8bcf6caa63899ff8d1760ec52ba4c0857713b847

Fix status message when downloading symbols
This fixes a regression that has been introduced by PR #3157.

Since the SecureCopyCallback is not blocking anymore, showing the
download message while this function is executing is pointless. It is
fixed by moving the status handler into the completion closure.

The SCOPED_TIMER_LOG macro has been replaced by two dedicated log
messages. Unfortunately the macro does not allow the underlying type to
be moved somewhere else. To support that a bigger restructuring of all
the logging methods is needed and would exceed the scope of this PR.
Hence the work around with the two log messages.

src/OrbitGl/App.cpp
@@ -1607,20 +1607,25 @@ orbit_base::Future<ErrorMessageOr<std::filesystem::path>> OrbitApp::RetrieveModu
     scoped_status.UpdateMessage(
         absl::StrFormat(R"(Copying debug info file for "%s" from remote: "%s"...)",
                         module_file_path, debug_file_path));
-    SCOPED_TIMED_LOG("Copying \"%s\"", debug_file_path);
+    const std::chrono::time_point<std::chrono::steady_clock> copy_begin =
+        std::chrono::steady_clock::now();
+    LOG("Copying \"%s\" started", debug_file_path);
     orbit_base::Future<ErrorMessageOr<void>> copy_result =
         secure_copy_callback_(debug_file_path, local_debug_file_path.string());
 
     orbit_base::ImmediateExecutor immediate_executor{};
     return copy_result.Then(
         &immediate_executor,
-        [local_debug_file_path](
-            ErrorMessageOr<void> sftp_result) -> ErrorMessageOr<std::filesystem::path> {
+        [debug_file_path, local_debug_file_path, scoped_status = std::move(scoped_status),
+         copy_begin](ErrorMessageOr<void> sftp_result) -> ErrorMessageOr<std::filesystem::path> {
           if (sftp_result.has_error()) {
             return ErrorMessage{
                 absl::StrFormat("Could not copy debug info file from the remote: %s",
                                 sftp_result.error().message())};
           }
+          const auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+              std::chrono::steady_clock::now() - copy_begin);
+          LOG("Copying \"%s\" took %.3f ms", debug_file_path, duration.count());
           return local_debug_file_path;
         });
   };