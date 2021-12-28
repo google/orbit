c56b3675a717772942f3747c2e06481d2c62d6f6
Pierric Gimmig <pierric@google.com>
2021-12-28 12:50:26 -0800

    Fix DiaSymbol leak
    Running Orbit in debug made this issue pop up. We were overwriting the
    object address in a smart pointer, so only the last object would get
    released. Fix by creating a new smart pointer for each DiaSymbol.

--- src/ObjectUtils/PdbFileDia.cpp
+++ src/ObjectUtils/PdbFileDia.cpp
@@ -93,10 +93,11 @@ ErrorMessageOr<orbit_grpc_protos::ModuleSymbols> PdbFileDia::LoadDebugSymbols() 
   ModuleSymbols module_symbols;
   module_symbols.set_load_bias(object_file_info_.load_bias);
   module_symbols.set_symbols_file_path(file_path_.string());
-  CComPtr<IDiaSymbol> dia_symbol;
+  IDiaSymbol* dia_symbol = nullptr;
   ULONG celt = 0;
 
   while (SUCCEEDED(dia_enum_symbols->Next(1, &dia_symbol, &celt)) && (celt == 1)) {
+    CComPtr<IDiaSymbol> dia_symbol_com_ptr = dia_symbol;
     SymbolInfo symbol_info;
 
     BSTR function_name = {};
