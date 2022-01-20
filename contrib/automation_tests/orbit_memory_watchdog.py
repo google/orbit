8909eee26dc71eefce19bfe1a3f7ff07fc1835fb
dpallotti <58685940+dpallotti@users.noreply.github.com>
2022-01-20 10:15:39 +0100

    Add automated test script for OrbitService's memory watchdog (#3253)
    Bug: http://b/214164984
    
    Test:
     - Ran the script locally.
     - Created nightly build and ran on the infrastructure:
       - `blaze test`: http://sponge2/e0df3991-3878-4596-a8d2-559cbbe14c9c
       - `guitar run`: http://fusion2/4101aa6d-b637-4c5b-b285-dce21f3e8869
     - Ran `orbit_instrument_function.py` locally.

---
+++ contrib/automation_tests/orbit_memory_watchdog.py
@@ -0,0 +1,43 @@
+"""
+Copyright (c) 2022 The Orbit Authors. All rights reserved.
+Use of this source code is governed by a BSD-style license that can be
+found in the LICENSE file.
+"""
+
+from absl import app
+
+from core.orbit_e2e import E2ETestSuite
+from test_cases.connection_window import FilterAndSelectFirstProcess, ConnectToStadiaInstance
+from test_cases.capture_window import CaptureAndWaitForInterruptedWarning
+from test_cases.symbols_tab import LoadSymbols, FilterAndHookFunction
+"""Test that OrbitService stops the capture when it is using too much memory.
+
+Before this script is run there needs to be a gamelet reserved and
+"call_foo_repeatedly" (can be downloaded from GCS) needs to be started.
+
+The script requires absl and pywinauto. Since pywinauto requires the bitness of
+the python installation to match the bitness of the program under test, it needs
+to be run from 64 bit python.
+
+This automation script covers a basic workflow:
+ - connect to a gamelet and select a process;
+ - instrument a high-frequency function with user space instrumentation;
+ - start a long capture;
+ - verify that the capture gets stopped by OrbitService and that the corresponding warning is shown.
+"""
+
+
+def main(argv):
+    test_cases = [
+        ConnectToStadiaInstance(),
+        FilterAndSelectFirstProcess(process_filter="call_foo_repeat"),
+        LoadSymbols(module_search_string="call_foo_repeatedly"),
+        FilterAndHookFunction(function_search_string="foo{(}{)}"),
+        CaptureAndWaitForInterruptedWarning(user_space_instrumentation=True)
+    ]
+    suite = E2ETestSuite(test_name="Memory watchdog", test_cases=test_cases)
+    suite.execute()
+
+
+if __name__ == '__main__':
+    app.run(main)
