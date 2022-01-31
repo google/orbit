cf50ea945a2950730486da59c6e064542e4742c8
antonrohr <antonrohr@google.com>
2022-01-31 18:51:11 +0000

    Add Symbols dependency to ProcessService

--- src/Symbols/include/Symbols/SymbolUtils.h
+++ src/Symbols/include/Symbols/SymbolUtils.h
@@ -13,9 +13,9 @@
 namespace orbit_symbols {
 
 // The file extensions for symbols files are .debug for elf files and .pdb for coff files. Only
-// files with following formats are considered `module.sym_ext`, `module.mod_ext.sym_ext` and
-// `module.mod_ext`. (`mod_ext` is the module file extension, usually .elf, .so, .exe or .dll;
-// `sym_ext` is either .debug or .pdb)
+// files with the following formats are considered: `module.sym_ext`, `module.mod_ext.sym_ext` and
+// `module.mod_ext` (`mod_ext` is the module file extension, usually .elf, .so, .exe or .dll;
+// `sym_ext` is either .debug or .pdb).
 [[nodiscard]] std::vector<std::filesystem::path> GetStandardSymbolFilenamesForModule(
     const std::filesystem::path& module_path,
     const orbit_grpc_protos::ModuleInfo::ObjectFileType& object_file_type);
