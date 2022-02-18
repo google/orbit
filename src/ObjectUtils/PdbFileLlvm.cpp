b30766c3ca04816ec1f50774a2de2ce6271b00e3
Florian Kübler <kuebler@google.com>
2022-02-18 15:12:24 +0100

    Add argument list to symbol's name for PdbFileLlvm (#3354)
    Pdb's `ProcSym` debug record for function information does not
    contain the argument types in their names. As a result, we can't
    differentiate overloaded functions in Orbit.
    However, the information is transitively available in the type
    info section of the Pdb file.
    
    This change attempts to retrieve the argument list from the
    type info section and appends this to the function's name.
    
    Note, this change only fixes the issue for the LLVM-based PDB
    backend. A follow up change will target the DIA SDK implementation.
    Also, there is a similar issue in the Coff file case, which will
    be also addressed later.
    
    Test: Load symbols for triangle.exe & run test
    Bug: http://b/219413222

--- src/ObjectUtils/PdbFileLlvm.cpp
+++ src/ObjectUtils/PdbFileLlvm.cpp
@@ -5,6 +5,11 @@
 #include "PdbFileLlvm.h"
 
 #include <absl/memory/memory.h>
+#include <absl/strings/str_cat.h>
+#include <llvm/DebugInfo/CodeView/LazyRandomTypeCollection.h>
+#include <llvm/DebugInfo/CodeView/TypeDeserializer.h>
+#include <llvm/DebugInfo/CodeView/TypeRecord.h>
+#include <llvm/DebugInfo/PDB/Native/TpiStream.h>
 
 #include <memory>
 
@@ -26,8 +31,14 @@ namespace {
 // all symbol info required for functions.
 class SymbolInfoVisitor : public llvm::codeview::SymbolVisitorCallbacks {
  public:
-  SymbolInfoVisitor(ModuleSymbols* module_symbols, const ObjectFileInfo& object_file_info)
-      : module_symbols_(module_symbols), object_file_info_(object_file_info) {}
+  SymbolInfoVisitor(ModuleSymbols* module_symbols, const ObjectFileInfo& object_file_info,
+                    llvm::pdb::TpiStream* type_info_stream)
+      : module_symbols_(module_symbols),
+        object_file_info_(object_file_info),
+        type_info_stream_(type_info_stream) {
+    ORBIT_CHECK(module_symbols != nullptr);
+    ORBIT_CHECK(type_info_stream != nullptr);
+  }
 
   // This is the only record type (ProcSym) we are interested in, so we only override this
   // method. Other records will simply return llvm::Error::success without any work done.
@@ -36,20 +47,88 @@ class SymbolInfoVisitor : public llvm::codeview::SymbolVisitorCallbacks {
     SymbolInfo symbol_info;
     symbol_info.set_name(proc.Name.str());
     symbol_info.set_demangled_name(llvm::demangle(proc.Name.str()));
+
+    // The ProcSym's name does not contain an argument list. However, this information is required
+    // when dealing with overloads and it is available in the type info stream. See:
+    // https://llvm.org/docs/PDB/TpiStream.html
+    llvm::StringRef argument_list = RetrieveArgumentList(proc);
+    if (!argument_list.empty()) {
+      symbol_info.set_demangled_name(
+          absl::StrCat(symbol_info.demangled_name(), argument_list.data()));
+    }
+
     // The address in PDB files is a relative virtual address (RVA), to make the address compatible
     // with how we do the computation, we need to add both the load bias (ImageBase for COFF) and
     // the offset of the executable section.
     symbol_info.set_address(proc.CodeOffset + object_file_info_.load_bias +
                             object_file_info_.executable_segment_offset);
     symbol_info.set_size(proc.CodeSize);
+
+    ORBIT_CHECK(module_symbols_ != nullptr);
     *(module_symbols_->add_symbol_infos()) = std::move(symbol_info);
 
     return llvm::Error::success();
   }
 
  private:
+  [[nodiscard]] llvm::StringRef RetrieveArgumentList(const llvm::codeview::ProcSym& proc) const {
+    ORBIT_CHECK(type_info_stream_ != nullptr);
+    llvm::codeview::LazyRandomTypeCollection& type_collection = type_info_stream_->typeCollection();
+
+    // We expect function types being either LF_PROCEDURE or LF_MFUNCTION, which are non-simple
+    // types. However, there are cases where the function type is "<no type>", which is a simple
+    // type. In those cases, we can't retrieve the argument list. Other simple types are not
+    // expected here (as they are mostly base types). However, the call to `getType` below will fail
+    // on any simple type. So we check for all simple types here, instead of only for "<no type>".
+    if (proc.FunctionType.isSimple()) {
+      llvm::StringRef function_type = type_collection.getTypeName(proc.FunctionType);
+      ORBIT_ERROR(
+          "Unable to retrieve parameter list for function \"%s\"; The function type is \"%s\"",
+          proc.Name.data(), function_type.data());
+      return "";
+    }
+
+    llvm::codeview::CVType function_type = type_info_stream_->getType(proc.FunctionType);
+    switch (function_type.kind()) {
+      case llvm::codeview::LF_PROCEDURE: {
+        llvm::codeview::ProcedureRecord procedure_record;
+        llvm::Error error =
+            llvm::codeview::TypeDeserializer::deserializeAs<llvm::codeview::ProcedureRecord>(
+                function_type, procedure_record);
+        if (error) {
+          ORBIT_ERROR(
+              "Unable to retrieve parameter list for function \"%s\"; The function is of type "
+              "\"LF_PROCEDURE\", but we can not deserialize it to a \"ProcedureRecord\".",
+              proc.Name.data());
+          return "";
+        }
+
+        llvm::StringRef parameter_list = type_collection.getTypeName(procedure_record.ArgumentList);
+        return parameter_list;
+      }
+      case llvm::codeview::LF_MFUNCTION: {
+        llvm::codeview::MemberFunctionRecord member_function_record;
+        llvm::Error error =
+            llvm::codeview::TypeDeserializer::deserializeAs<llvm::codeview::MemberFunctionRecord>(
+                function_type, member_function_record);
+        if (error) {
+          ORBIT_ERROR(
+              "Unable to retrieve parameter list for function \"%s\"; The function is of type "
+              "\"LF_MFUNCTION\", but we can not deserialize it to a \"MemberFunctionRecord\".",
+              proc.Name.data());
+          return "";
+        }
+
+        return type_collection.getTypeName(member_function_record.ArgumentList);
+      }
+      default:
+        ORBIT_UNREACHABLE();
+    }
+  }
+
   ModuleSymbols* module_symbols_;
   ObjectFileInfo object_file_info_;
+  llvm::pdb::TpiStream* type_info_stream_;
 };
 
 }  // namespace
@@ -65,7 +144,8 @@ PdbFileLlvm::PdbFileLlvm(std::filesystem::path file_path, const ObjectFileInfo& 
   module_symbols.set_load_bias(object_file_info_.load_bias);
   module_symbols.set_symbols_file_path(file_path_.string());
 
-  llvm::pdb::NativeSession* native_session = static_cast<llvm::pdb::NativeSession*>(session_.get());
+  auto* native_session = dynamic_cast<llvm::pdb::NativeSession*>(session_.get());
+  ORBIT_CHECK(native_session != nullptr);
   llvm::pdb::PDBFile& pdb_file = native_session->getPDBFile();
 
   if (!pdb_file.hasPDBDbiStream()) {
@@ -75,6 +155,13 @@ PdbFileLlvm::PdbFileLlvm(std::filesystem::path file_path, const ObjectFileInfo& 
   llvm::Expected<llvm::pdb::DbiStream&> debug_info_stream = pdb_file.getPDBDbiStream();
   // Given that we check hasPDBDbiStream above, we must get a DbiStream here.
   ORBIT_CHECK(debug_info_stream);
+
+  if (!pdb_file.hasPDBTpiStream()) {
+    return ErrorMessage("PDB file does not have a TPI stream.");
+  }
+  llvm::Expected<llvm::pdb::TpiStream&> type_info_stream = pdb_file.getPDBTpiStream();
+  // Given that we check hasPDBTpiStream above, we must get a TpiStream here.
+  ORBIT_CHECK(type_info_stream);
 
   const llvm::pdb::DbiModuleList& modules = debug_info_stream->modules();
 
@@ -102,7 +189,7 @@ PdbFileLlvm::PdbFileLlvm(std::filesystem::path file_path, const ObjectFileInfo& 
     llvm::codeview::SymbolDeserializer deserializer(nullptr,
                                                     llvm::codeview::CodeViewContainer::Pdb);
     pipeline.addCallbackToPipeline(deserializer);
-    SymbolInfoVisitor symbol_visitor(&module_symbols, object_file_info_);
+    SymbolInfoVisitor symbol_visitor(&module_symbols, object_file_info_, &type_info_stream.get());
     pipeline.addCallbackToPipeline(symbol_visitor);
     llvm::codeview::CVSymbolVisitor visitor(pipeline);
 
