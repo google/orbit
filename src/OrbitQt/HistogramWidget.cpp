a791df7ffb27327b4638da872c913bf0a84425c3
Valeriy Avanesov <avanesov@google.com>
2022-02-18 14:53:02 +0000

    [fixup] rename zero to axes_intersection

--- src/OrbitQt/HistogramWidget.cpp
+++ src/OrbitQt/HistogramWidget.cpp
@@ -52,53 +52,55 @@ static void DrawVerticalLine(QPainter& painter, const QPoint& start, int length)
   painter.drawLine(start, {start.x(), start.y() + length});
 }
 
-static void DrawHorizontalAxis(QPainter& painter, const QPoint& zero,
+static void DrawHorizontalAxis(QPainter& painter, const QPoint& axes_intersection,
                                const orbit_statistics::Histogram& histogram, int length) {
-  DrawHorizontalLine(painter, zero, length);
+  DrawHorizontalLine(painter, axes_intersection, length);
 
   const int tick_spacing_as_value =
       RoundToClosestInt(static_cast<double>(histogram.max) / kHorizontalTickCount);
   const int tick_spacing_pixels =
       RoundToClosestInt(static_cast<double>(length) / kHorizontalTickCount);
 
-  int current_tick_location = tick_spacing_pixels + zero.x();
+  int current_tick_location = tick_spacing_pixels + axes_intersection.x();
   int current_tick_value = tick_spacing_as_value;
 
   const QFontMetrics font_metrics(painter.font());
 
   for (uint32_t i = 1; i <= kHorizontalTickCount; ++i) {
-    DrawVerticalLine(painter, {current_tick_location, zero.y()}, kTickLength);
+    DrawVerticalLine(painter, {current_tick_location, axes_intersection.y()}, kTickLength);
 
     const QString tick_label = QString::fromStdString(
         orbit_display_formats::GetDisplayTime(absl::Nanoseconds(current_tick_value)));
     const QRect tick_label_bounding_rect = font_metrics.boundingRect(tick_label);
     painter.drawText(current_tick_location - tick_label_bounding_rect.width() / 2,
-                     zero.y() + kTickLength + tick_label_bounding_rect.height(), tick_label);
+                     axes_intersection.y() + kTickLength + tick_label_bounding_rect.height(),
+                     tick_label);
 
     current_tick_location += tick_spacing_pixels;
     current_tick_value += tick_spacing_as_value;
   }
 }
 
-static void DrawVerticalAxis(QPainter& painter, const QPoint& zero, int length, double max_freq) {
-  DrawVerticalLine(painter, zero, -length);
+static void DrawVerticalAxis(QPainter& painter, const QPoint& axes_intersection, int length,
+                             double max_freq) {
+  DrawVerticalLine(painter, axes_intersection, -length);
 
   const double tick_spacing_as_value = max_freq / kVerticalTickCount;
   const int tick_spacing_pixels =
       RoundToClosestInt(static_cast<double>(length) / kVerticalTickCount);
 
   double current_tick_value = tick_spacing_as_value;
-  int current_tick_location = zero.y() - tick_spacing_pixels;
+  int current_tick_location = axes_intersection.y() - tick_spacing_pixels;
 
   const QFontMetrics font_metrics(painter.font());
 
   for (uint32_t i = 1; i <= kVerticalTickCount; ++i) {
-    DrawHorizontalLine(painter, {zero.x(), current_tick_location}, -kTickLength);
+    DrawHorizontalLine(painter, {axes_intersection.x(), current_tick_location}, -kTickLength);
 
     QString tick_label = QString::fromStdString(absl::StrFormat("%.2f", current_tick_value));
 
     QRect tick_label_bounding_rect = font_metrics.boundingRect(tick_label);
-    painter.drawText(zero.x() - tick_label_bounding_rect.width() - kTickLength,
+    painter.drawText(axes_intersection.x() - tick_label_bounding_rect.width() - kTickLength,
                      current_tick_location + tick_label_bounding_rect.height() / 2, tick_label);
 
     current_tick_location -= tick_spacing_pixels;
@@ -114,7 +116,7 @@ static void DrawVerticalAxis(QPainter& painter, const QPoint& zero, int length, 
   return static_cast<double>(histogram.counts[i]) / static_cast<double>(histogram.data_set_size);
 }
 
-static void DrawHistogram(QPainter& painter, const QPoint& zero,
+static void DrawHistogram(QPainter& painter, const QPoint& axes_intersection,
                           const orbit_statistics::Histogram& histogram, int horizontal_axis_length,
                           int vertical_axis_length, double max_freq, int vertical_shift) {
   for (size_t i = 0; i < histogram.counts.size(); ++i) {
@@ -124,11 +126,14 @@ static void DrawHistogram(QPainter& painter, const QPoint& zero,
     double freq = GetFreq(histogram, i);
     if (freq > 0) {
       const QPoint top_left(
-          zero.x() + ValueToAxisLocation(bin_from, horizontal_axis_length, histogram.max),
-          zero.y() - vertical_shift - ValueToAxisLocation(freq, vertical_axis_length, max_freq));
+          axes_intersection.x() +
+              ValueToAxisLocation(bin_from, horizontal_axis_length, histogram.max),
+          axes_intersection.y() - vertical_shift -
+              ValueToAxisLocation(freq, vertical_axis_length, max_freq));
       const QPoint lower_right(
-          zero.x() + ValueToAxisLocation(bin_to, horizontal_axis_length, histogram.max),
-          zero.y() - vertical_shift);
+          axes_intersection.x() +
+              ValueToAxisLocation(bin_to, horizontal_axis_length, histogram.max),
+          axes_intersection.y() - vertical_shift);
       const QRect bar(top_left, lower_right);
       painter.fillRect(bar, Qt::cyan);
     }
@@ -162,8 +167,8 @@ void HistogramWidget::paintEvent(QPaintEvent* /*event*/) {
   const int width = painter.device()->width();
   const int height = painter.device()->height();
 
-  QPoint zero(RoundToClosestInt(width * kRelativeMargin),
-              RoundToClosestInt(height * (1 - kRelativeMargin)));
+  QPoint axes_intersection(RoundToClosestInt(width * kRelativeMargin),
+                           RoundToClosestInt(height * (1 - kRelativeMargin)));
 
   const int vertical_axis_length = Height(painter) - 2 * HeightMargin(painter);
   const int horizontal_axis_length = Width(painter) - 2 * WidthMargin(painter);
@@ -172,11 +177,11 @@ void HistogramWidget::paintEvent(QPaintEvent* /*event*/) {
       *std::max_element(std::begin(histogram_->counts), std::end(histogram_->counts));
   const double max_freq = static_cast<double>(max_count) / histogram_->data_set_size;
 
-  DrawHistogram(painter, zero, histogram_.value(), horizontal_axis_length, vertical_axis_length,
-                max_freq, axis_width);
+  DrawHistogram(painter, axes_intersection, histogram_.value(), horizontal_axis_length,
+                vertical_axis_length, max_freq, axis_width);
 
-  DrawHorizontalAxis(painter, zero, histogram_.value(), horizontal_axis_length);
-  DrawVerticalAxis(painter, zero, vertical_axis_length, max_freq);
+  DrawHorizontalAxis(painter, axes_intersection, histogram_.value(), horizontal_axis_length);
+  DrawVerticalAxis(painter, axes_intersection, vertical_axis_length, max_freq);
 
   DrawTitle(function_name_.value(), painter);
 }
