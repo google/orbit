d90719cd919ef9d043d59b6f304d884cc1f625fd
antonrohr <antonrohr@google.com>
2022-02-24 15:47:57 +0100

    Extend symbol locations e2e test for adding files
    This extends the symbol locations e2e test with the new "add file
    directly" functionality.
    
    http://b/221183473

--- contrib/automation_tests/orbit_symbol_locations.py
+++ contrib/automation_tests/orbit_symbol_locations.py
@@ -11,8 +11,8 @@ from absl import app
 from core.orbit_e2e import E2ETestSuite
 from test_cases.connection_window import FilterAndSelectFirstProcess, ConnectToStadiaInstance
 from test_cases.symbols_tab import ClearSymbolCache, LoadSymbols
-from test_cases.symbol_locations import AddSymbolLocation, ClearAllSymbolLocations
-
+from test_cases.symbol_locations import AddSymbolLocation, ClearAllSymbolLocations, AddSymbolFile
+from test_cases.main_window import EndSession
 """
 Test symbol loading from custom symbol locations.
 
@@ -32,13 +32,21 @@ This automation script covers a basic workflow:
  - verify symbol loading still fails
  - add a symbol location with a correct symbol file
  - verify symbol loading works
- - delete all symbol locations again
+ - delete all symbol locations
+ - end the session to undo the symbol loading
+ - add a stale symbol file directly
+ - verify symbol loading fails
+ - add a working symbol file directly
+ - verify that symbol loading works now
+ - delete all symbol locations (clean up)
 """
 
-stale_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                          r"testdata\symbol_location_tests\stale_symbols"))
-working_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                            r"testdata\symbol_location_tests\working_symbols"))
+stale_path = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), r"testdata\symbol_location_tests\stale_symbols"))
+working_path = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), r"testdata\symbol_location_tests\working_symbols"))
+stale_file = os.path.abspath(os.path.join(stale_path, "no_symbols_elf.debug"))
+working_file = os.path.abspath(os.path.join(working_path, "no_symbols_elf.debug"))
 
 
 def main(argv):
@@ -51,6 +59,13 @@ def main(argv):
         AddSymbolLocation(location=stale_path),
         LoadSymbols(module_search_string="no_symbols_elf", expect_fail=True),
         AddSymbolLocation(location=working_path),
+        LoadSymbols(module_search_string="no_symbols_elf"),
+        ClearAllSymbolLocations(),
+        EndSession(),
+        FilterAndSelectFirstProcess(process_filter='no_symbols_elf'),
+        AddSymbolFile(location=stale_file),
+        LoadSymbols(module_search_string="no_symbols_elf", expect_fail=True),
+        AddSymbolFile(location=working_file),
         LoadSymbols(module_search_string="no_symbols_elf"),
         ClearAllSymbolLocations()
     ]
