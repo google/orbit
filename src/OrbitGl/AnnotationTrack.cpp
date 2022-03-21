6bb31989f3c05276c169396eba6a9d9bea03dd8c
vickyliu-go4it <74234352+vickyliu-go4it@users.noreply.github.com>
2022-03-21 06:40:50 -0700

    Fix the memory warning threshold (#3442)
    * Change OrbitApp::GetMemoryWarningThresholdKb to use CaptureData
    
    OrbitApp::GetMemoryWarningThresholdKb was using the DataManager, which
    caused a check to fail as we verify whether DataManager is used from the
    main thread.
    
    With this change, we use the CaptureData to get the memory warning
    threshold value in OrbitApp::GetMemoryWarningThresholdKb.
    
    Bug: http://b/216245594
    
    * Fix the memory warning threshold position
    
    We changed the coordinate system in Orbit but the drawing position
    of memory warning threshold was not updated to use the new coordinate
    system. This change fixes the position of the memory warning threshold.
    
    Bug: http://b/216109608
    
    * [fixup] Address review comment

--- src/OrbitGl/AnnotationTrack.cpp
+++ src/OrbitGl/AnnotationTrack.cpp
@@ -8,7 +8,7 @@ namespace {
 
 const Color kWhite(255, 255, 255, 255);
 const Color kFullyTransparent(255, 255, 255, 0);
-const Color kThresholdColor(244, 67, 54, 255);
+const Color kThresholdColor(179, 0, 80, 255);
 
 }  // namespace
 
@@ -26,12 +26,12 @@ void AnnotationTrack::DrawAnnotation(Batcher& batcher, TextRenderer& text_render
   if (value_upper_bound_.has_value()) {
     std::string text = value_upper_bound_.value().first;
     float string_width = text_renderer.GetStringWidth(text.c_str(), font_size);
-    Vec2 text_box_size(string_width, layout->GetTextBoxHeight());
-    Vec2 text_box_position(content_right_x - text_box_size[0], content_bottom_y - content_height);
+    Vec2 text_box_position(content_right_x - string_width, content_bottom_y - content_height);
     text_renderer.AddText(text.c_str(), text_box_position[0], text_box_position[1], z,
-                          {font_size, kWhite, text_box_size[0]});
+                          {font_size, kWhite, string_width});
 
     if (!GetValueUpperBoundTooltip().empty()) {
+      Vec2 text_box_size(string_width, layout->GetTextBoxHeight());
       auto user_data = std::make_unique<PickingUserData>(
           nullptr, [&](PickingId /*id*/) { return this->GetValueUpperBoundTooltip(); });
       batcher.AddShadedBox(text_box_position, text_box_size, z, kFullyTransparent,
@@ -43,11 +43,10 @@ void AnnotationTrack::DrawAnnotation(Batcher& batcher, TextRenderer& text_render
   if (value_lower_bound_.has_value()) {
     std::string text = value_lower_bound_.value().first;
     float string_width = text_renderer.GetStringWidth(text.c_str(), font_size);
-    Vec2 text_box_size(string_width, layout->GetTextBoxHeight());
-    Vec2 text_box_position(content_right_x - text_box_size[0], content_bottom_y);
+    Vec2 text_box_position(content_right_x - string_width, content_bottom_y);
 
-    TextRenderer::TextFormatting formatting{font_size, kWhite, text_box_size[0]};
-    formatting.valign = TextRenderer::VAlign::Bottom;
+    TextRenderer::TextFormatting formatting{
+        font_size, kWhite, string_width, TextRenderer::HAlign::Left, TextRenderer::VAlign::Bottom};
     text_renderer.AddText(text.c_str(), text_box_position[0], text_box_position[1], z, formatting);
   }
 
@@ -60,18 +59,20 @@ void AnnotationTrack::DrawAnnotation(Batcher& batcher, TextRenderer& text_render
     if (warning_threshold <= min || warning_threshold >= max) return;
 
     double normalized_value = (warning_threshold - min) / (max - min);
-    float y = content_bottom_y + static_cast<float>(normalized_value) * content_height;
+    float y = content_bottom_y - static_cast<float>(normalized_value) * content_height;
 
     std::string text = warning_threshold_.value().first;
     float string_width = text_renderer.GetStringWidth(text.c_str(), font_size);
-    Vec2 text_box_size(string_width, layout->GetTextBoxHeight());
-    Vec2 text_box_position(track_pos[0] + layout->GetRightMargin(), y - text_box_size[1] / 2.f);
-    text_renderer.AddText(text.c_str(), text_box_position[0], text_box_position[1], z,
-                          {font_size, kThresholdColor, text_box_size[0]});
+    Vec2 text_box_position(track_pos[0] + layout->GetRightMargin(), y);
+
+    TextRenderer::TextFormatting formatting{font_size, kThresholdColor, string_width,
+                                            TextRenderer::HAlign::Left,
+                                            TextRenderer::VAlign::Middle};
+    text_renderer.AddText(text.c_str(), text_box_position[0], text_box_position[1], z, formatting);
 
     Vec2 from(track_pos[0], y);
     Vec2 to(track_pos[0] + track_size[0], y);
     batcher.AddLine(from, from + Vec2(layout->GetRightMargin() / 2.f, 0), z, kThresholdColor);
-    batcher.AddLine(Vec2(text_box_position[0] - text_box_size[0], y), to, z, kThresholdColor);
+    batcher.AddLine(Vec2(text_box_position[0] + string_width, y), to, z, kThresholdColor);
   }
 }
