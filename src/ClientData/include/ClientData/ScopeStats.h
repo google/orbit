308bfbabd1674a4eb809d0c1b488b140762cc141
Florian Kübler <kuebler@google.com>
2022-03-25 12:50:38 +0000

    fix test

--- src/ClientData/include/ClientData/ScopeStats.h
+++ src/ClientData/include/ClientData/ScopeStats.h
@@ -26,7 +26,12 @@ class ScopeStats {
   [[nodiscard]] uint64_t total_time_ns() const { return total_time_ns_; }
   void set_total_time_ns(uint64_t set_total_time_ns) { total_time_ns_ = set_total_time_ns; }
 
-  [[nodiscard]] uint64_t average_time_ns() const { return total_time_ns_ / count_; }
+  [[nodiscard]] uint64_t average_time_ns() const {
+    if (count_ == 0) {
+      return 0;
+    }
+    return total_time_ns_ / count_;
+  }
 
   [[nodiscard]] uint64_t min_ns() const { return min_ns_; }
   void set_min_ns(uint64_t min_ns) { min_ns_ = min_ns; }
