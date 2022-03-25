9186f7071d980be5a3b2ecad1a44ce9eab0c40dd
Tingwei LIU <vickyliu@google.com>
2022-03-25 14:22:20 +0100

    Add capture length to CaptureFileInfo
    With this change, we added `capture_length` into `CaptureFileInfo`.
    
    Bug: http://b/198562119

--- src/CaptureFileInfo/include/CaptureFileInfo/CaptureFileInfo.h
+++ src/CaptureFileInfo/include/CaptureFileInfo/CaptureFileInfo.h
@@ -5,6 +5,8 @@
 #ifndef CAPTURE_FILE_INFO_CAPTURE_FILE_INFO_H_
 #define CAPTURE_FILE_INFO_CAPTURE_FILE_INFO_H_
 
+#include <absl/time/time.h>
+
 #include <QDateTime>
 #include <QFileInfo>
 #include <filesystem>
@@ -22,16 +24,19 @@ class CaptureFileInfo {
 
   [[nodiscard]] QDateTime LastUsed() const { return last_used_; }
   [[nodiscard]] QDateTime Created() const { return file_info_.birthTime(); }
+  [[nodiscard]] absl::Duration CaptureLength() const { return capture_length_; }
 
   [[nodiscard]] bool FileExists() const;
 
   [[nodiscard]] uint64_t FileSize() const;
 
   void Touch() { last_used_ = QDateTime::currentDateTime(); }
+  void SetCaptureLength(absl::Duration capture_length) { capture_length_ = capture_length; }
 
  private:
   QFileInfo file_info_;
   QDateTime last_used_;
+  absl::Duration capture_length_ = absl::Nanoseconds(0);
 };
 
 }  // namespace orbit_capture_file_info
