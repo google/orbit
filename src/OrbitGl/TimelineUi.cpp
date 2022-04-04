google/orbit

commit 755f1bf6425cd983deff6efe9b2330be53a9d1c9

Don't allow labels to incercept their own tick (#3506)
* Don't allow labels to incercept their own tick

We are doing that by simply making the left padding one pixel smaller.

Test: Loading a capture. Testing the mouse label in the timeline.
Testing what happens when the screen width is really small.

http://b/226576948

* Fixup: Moving the label one pixel to the right

* Fixup: Constant name

src/OrbitGl/TimelineUi.cpp
@@ -15,6 +15,7 @@
 namespace orbit_gl {
 
 constexpr float kLabelsPadding = 4.f;
+constexpr float kPixelsBetweenMajorTicksAndLabels = 1.f;
 const Color kBackgroundColorSpecialLabels(68, 67, 69, 255);
 
 void TimelineUi::RenderLines(Batcher& batcher, uint64_t min_timestamp_ns,
@@ -46,7 +47,7 @@ void TimelineUi::RenderLabels(Batcher& batcher, TextRenderer& text_renderer,
 
   for (uint64_t tick_ns : GetTicksForNonOverlappingLabels(text_renderer, all_major_ticks)) {
     RenderLabel(batcher, text_renderer, tick_ns, GetNumDecimalsInLabels(),
-                GlCanvas::kZValueTimeBarLabel, GlCanvas::kTimeBarBackgroundColor);
+                GlCanvas::kTimeBarBackgroundColor);
   }
 }
 
@@ -64,14 +65,19 @@ void TimelineUi::RenderBackground(Batcher& batcher) const {
 }
 
 void TimelineUi::RenderLabel(Batcher& batcher, TextRenderer& text_renderer, uint64_t tick_ns,
-                             uint32_t number_of_decimal_places, float label_z,
-                             const Color background_color) const {
+                             uint32_t number_of_decimal_places, const Color background_color,
+                             bool is_mouse_label) const {
+  float label_z = is_mouse_label ? GlCanvas::kZValueTimeBarMouseLabel : GlCanvas::kZValueTimeBar;
+
+  // We add a pixel separation between the label and the major ticks so they don't intersect.
+  float extra_left_margin = is_mouse_label ? 0.f : kPixelsBetweenMajorTicksAndLabels;
+
   std::string label = GetLabel(tick_ns, number_of_decimal_places);
   float world_x = GetTickWorldXPos(tick_ns);
   Vec2 pos, size;
   float label_middle_y = GetPos()[1] + GetHeightWithoutMargin() / 2.f;
-  text_renderer.AddText(label.c_str(), world_x + kLabelsPadding, label_middle_y, label_z,
-                        /*text_formatting=*/
+  text_renderer.AddText(label.c_str(), world_x + kLabelsPadding + extra_left_margin, label_middle_y,
+                        label_z, /*text_formatting=*/
                         {layout_->GetFontSize(), Color(255, 255, 255, 255), -1.f,
                          TextRenderer::HAlign::Left, TextRenderer::VAlign::Middle},
                         /*out_text_pos=*/&pos, /*out_text_size=*/&size);
@@ -94,7 +100,7 @@ void TimelineUi::RenderMouseLabel(Batcher& batcher, TextRenderer& text_renderer,
       std::min(kMaxNumberOfDecimalDigits, GetNumDecimalsInLabels() + kNumAdditionalDecimalDigits);
 
   RenderLabel(batcher, text_renderer, mouse_tick_ns, num_decimal_places_mouse_label,
-              GlCanvas::kZValueTimeBarMouseLabel, kBackgroundColorSpecialLabels);
+              kBackgroundColorSpecialLabels, /*is_mouse_label=*/true);
 }
 
 std::string TimelineUi::GetLabel(uint64_t tick_ns, uint32_t number_of_decimal_places) const {
@@ -144,7 +150,8 @@ bool TimelineUi::WillLabelsOverlap(TextRenderer& text_renderer,
   for (auto tick_ns : tick_list) {
     float label_width = text_renderer.GetStringWidth(
         GetLabel(tick_ns, GetNumDecimalsInLabels()).c_str(), layout_->GetFontSize());
-    if (distance_between_labels < 2.f * kLabelsPadding + label_width) {
+    if (distance_between_labels <
+        2.f * kLabelsPadding + kPixelsBetweenMajorTicksAndLabels + label_width) {
       return true;
     }
   }

src/OrbitGl/TimelineUi.h
@@ -35,7 +35,8 @@ class TimelineUi : public CaptureViewElement {
   void RenderMargin(Batcher& batcher) const;
   void RenderBackground(Batcher& batcher) const;
   void RenderLabel(Batcher& batcher, TextRenderer& text_renderer, uint64_t tick_ns,
-                   uint32_t number_of_decimal_places, float label_z, Color background_color) const;
+                   uint32_t number_of_decimal_places, Color background_color,
+                   bool is_mouse_label = false) const;
   void RenderMouseLabel(Batcher& batcher, TextRenderer& text_renderer,
                         uint64_t mouse_tick_ns) const;
   [[nodiscard]] std::string GetLabel(uint64_t tick_ns, uint32_t number_of_decimal_places) const;

commit 46cf187a926feda1eba29344298d96d056e5a82d

Hide ticks behind timeline labels
Regression from https://github.com/google/orbit/pull/3506. While
refactoring I chose the wrong layer for timeline-labels.

src/OrbitGl/TimelineUi.cpp
@@ -67,7 +67,8 @@ void TimelineUi::RenderBackground(PrimitiveAssembler& primitive_assembler) const
 void TimelineUi::RenderLabel(PrimitiveAssembler& primitive_assembler, TextRenderer& text_renderer,
                              uint64_t tick_ns, uint32_t number_of_decimal_places,
                              const Color background_color, bool is_mouse_label) const {
-  float label_z = is_mouse_label ? GlCanvas::kZValueTimeBarMouseLabel : GlCanvas::kZValueTimeBar;
+  float label_z =
+      is_mouse_label ? GlCanvas::kZValueTimeBarMouseLabel : GlCanvas::kZValueTimeBarLabel;
 
   // We add a pixel separation between the label and the major ticks so they don't intersect.
   float extra_left_margin = is_mouse_label ? 0.f : kPixelsBetweenMajorTicksAndLabels;