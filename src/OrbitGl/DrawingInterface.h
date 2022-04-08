80e872279bd119ff4e2d307b3a9890bcda57803a
Pablo Zimmermann <karupayun@google.com>
2022-04-08 13:52:42 +0000

    Create DrawingInterface class
    In this PR we are creating DrawingInterface class. We will move
    GetLayers, DrawLayer and Draw methods from Batcher and TextRenderer
    there (implemented by OpenGl methods).
    
    Also, we will implement it in CaptureWindow with a new class
    DrawingCombiner which will take care of combining Layers (logic which is
    currently in CaptureWindow::Draw()

---
+++ src/OrbitGl/DrawingInterface.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_GL_DRAWING_INTERFACE_H_
+#define ORBIT_GL_DRAWING_INTERFACE_H_
+
+#include <vector>
+
+namespace orbit_gl {
+
+class DrawingInterface {
+ public:
+  [[nodiscard]] virtual std::vector<float> GetLayers() const = 0;
+  virtual void DrawLayer(float layer, bool picking) const = 0;
+  virtual void Draw(bool picking) const = 0;
+};
+
+}  // namespace orbit_gl
+
+#endif  // ORBIT_GL_DRAWING_INTERFACE_H_
