0a1d290a174ff01026a516dd3c754ce0f82c1096
Henning Becker <henning.becker@gmail.com>
2022-04-13 12:10:07 +0000

    fixup! Check if the producer side socket is already in use

--- src/ProducerSideService/BuildAndStartProducerSideServerWindows.cpp
+++ src/ProducerSideService/BuildAndStartProducerSideServerWindows.cpp
@@ -2,13 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
 #include "BuildAndStartProducerSideServerWithUri.h"
+#include "OrbitBase/Result.h"
 #include "ProducerSideChannel/ProducerSideChannel.h"
 #include "ProducerSideService/BuildAndStartProducerSideServer.h"
+#include "ProducerSideService/ProducerSideServer.h"
 
 namespace orbit_producer_side_service {
 
-BuildAndStartReturnValue BuildAndStartProducerSideServer() {
+ErrorMessageOr<std::unique_ptr<ProducerSideServer>> BuildAndStartProducerSideServer() {
   return BuildAndStartProducerSideServerWithUri(
       orbit_producer_side_channel::kProducerSideWindowsServerAddress);
 }
