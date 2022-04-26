1f7b2d322c2ac73f2c845d31a9d97c0512b7e83d
antonrohr <antonrohr@google.com>
2022-04-26 07:59:29 +0000

    Add override keyword to MockAppInterface
    Test: Compile
    Bug: no bug

--- src/DataViews/MockAppInterface.h
+++ src/DataViews/MockAppInterface.h
@@ -27,74 +27,83 @@ namespace orbit_data_views {
 // This is a mock of AppInterface which can be shared between all data view tests.
 class MockAppInterface : public AppInterface {
  public:
-  MOCK_METHOD(void, SetClipboard, (const std::string&));
-  MOCK_METHOD(std::string, GetSaveFile, (const std::string& extension), (const));
+  MOCK_METHOD(void, SetClipboard, (const std::string&), (override));
+  MOCK_METHOD(std::string, GetSaveFile, (const std::string& extension), (const, override));
 
-  MOCK_METHOD(void, SendErrorToUi, (const std::string& title, const std::string& text));
+  MOCK_METHOD(void, SendErrorToUi, (const std::string& title, const std::string& text), (override));
 
   MOCK_METHOD(orbit_base::Future<ErrorMessageOr<void>>, LoadPreset,
-              (const orbit_preset_file::PresetFile&));
-  MOCK_METHOD(PresetLoadState, GetPresetLoadState, (const orbit_preset_file::PresetFile&), (const));
-  MOCK_METHOD(void, ShowPresetInExplorer, (const orbit_preset_file::PresetFile&));
+              (const orbit_preset_file::PresetFile&), (override));
+  MOCK_METHOD(PresetLoadState, GetPresetLoadState, (const orbit_preset_file::PresetFile&),
+              (const, override));
+  MOCK_METHOD(void, ShowPresetInExplorer, (const orbit_preset_file::PresetFile&), (override));
 
-  MOCK_METHOD(bool, IsFunctionSelected, (const orbit_client_data::FunctionInfo&), (const));
-  MOCK_METHOD(bool, IsFunctionSelected, (const orbit_client_data::SampledFunction&), (const));
+  MOCK_METHOD(bool, IsFunctionSelected, (const orbit_client_data::FunctionInfo&),
+              (const, override));
+  MOCK_METHOD(bool, IsFunctionSelected, (const orbit_client_data::SampledFunction&),
+              (const, override));
 
-  MOCK_METHOD(uint64_t, GetHighlightedScopeId, (), (const));
-  MOCK_METHOD(void, SetHighlightedScopeId, (uint64_t highlighted_function_id));
-  MOCK_METHOD(void, SetVisibleScopeIds, (absl::flat_hash_set<uint64_t> visible_functions));
-  MOCK_METHOD(void, DeselectTimer, ());
-  MOCK_METHOD(bool, IsCapturing, (), (const));
-  MOCK_METHOD(void, JumpToTimerAndZoom, (uint64_t function_id, JumpToTimerMode selection_mode));
+  MOCK_METHOD(uint64_t, GetHighlightedScopeId, (), (const, override));
+  MOCK_METHOD(void, SetHighlightedScopeId, (uint64_t highlighted_function_id), (override));
+  MOCK_METHOD(void, SetVisibleScopeIds, (absl::flat_hash_set<uint64_t> visible_functions),
+              (override));
+  MOCK_METHOD(void, DeselectTimer, (), (override));
+  MOCK_METHOD(bool, IsCapturing, (), (const, override));
+  MOCK_METHOD(void, JumpToTimerAndZoom, (uint64_t function_id, JumpToTimerMode selection_mode),
+              (override));
   MOCK_METHOD(std::vector<const orbit_client_data::TimerChain*>, GetAllThreadTimerChains, (),
-              (const));
+              (const, override));
 
-  MOCK_METHOD(bool, IsFrameTrackEnabled, (const orbit_client_data::FunctionInfo&), (const));
-  MOCK_METHOD(bool, HasFrameTrackInCaptureData, (uint64_t), (const));
+  MOCK_METHOD(bool, IsFrameTrackEnabled, (const orbit_client_data::FunctionInfo&),
+              (const, override));
+  MOCK_METHOD(bool, HasFrameTrackInCaptureData, (uint64_t), (const, override));
 
-  MOCK_METHOD(bool, HasCaptureData, (), (const));
-  MOCK_METHOD(orbit_client_data::CaptureData&, GetMutableCaptureData, ());
-  MOCK_METHOD(const orbit_client_data::CaptureData&, GetCaptureData, (), (const));
-  MOCK_METHOD(const orbit_client_data::ModuleManager*, GetModuleManager, (), (const));
-  MOCK_METHOD(orbit_client_data::ModuleManager*, GetMutableModuleManager, (), ());
+  MOCK_METHOD(bool, HasCaptureData, (), (const, override));
+  MOCK_METHOD(orbit_client_data::CaptureData&, GetMutableCaptureData, (), (override));
+  MOCK_METHOD(const orbit_client_data::CaptureData&, GetCaptureData, (), (const, override));
+  MOCK_METHOD(const orbit_client_data::ModuleManager*, GetModuleManager, (), (const, override));
+  MOCK_METHOD(orbit_client_data::ModuleManager*, GetMutableModuleManager, (), (override));
 
-  MOCK_METHOD(void, OnValidateFramePointers, (std::vector<const orbit_client_data::ModuleData*>));
-  MOCK_METHOD(void, UpdateProcessAndModuleList, ());
+  MOCK_METHOD(void, OnValidateFramePointers, (std::vector<const orbit_client_data::ModuleData*>),
+              (override));
+  MOCK_METHOD(void, UpdateProcessAndModuleList, (), (override));
 
   // This needs to be called from the main thread.
-  MOCK_METHOD(bool, IsCaptureConnected, (const orbit_client_data::CaptureData&), (const));
+  MOCK_METHOD(bool, IsCaptureConnected, (const orbit_client_data::CaptureData&), (const, override));
 
-  MOCK_METHOD(const orbit_client_data::ProcessData*, GetTargetProcess, (), (const));
+  MOCK_METHOD(const orbit_client_data::ProcessData*, GetTargetProcess, (), (const, override));
 
   MOCK_METHOD(const orbit_client_data::ModuleData*, GetModuleByPathAndBuildId,
-              (const std::string&, const std::string&), (const));
+              (const std::string&, const std::string&), (const, override));
   MOCK_METHOD(orbit_client_data::ModuleData*, GetMutableModuleByPathAndBuildId,
-              (const std::string&, const std::string&));
+              (const std::string&, const std::string&), (override));
   MOCK_METHOD(orbit_base::Future<void>, RetrieveModulesAndLoadSymbols,
-              (absl::Span<const orbit_client_data::ModuleData* const>));
+              (absl::Span<const orbit_client_data::ModuleData* const>), (override));
 
-  MOCK_METHOD(void, SelectFunction, (const orbit_client_data::FunctionInfo&));
-  MOCK_METHOD(void, DeselectFunction, (const orbit_client_data::FunctionInfo&));
+  MOCK_METHOD(void, SelectFunction, (const orbit_client_data::FunctionInfo&), (override));
+  MOCK_METHOD(void, DeselectFunction, (const orbit_client_data::FunctionInfo&), (override));
 
-  MOCK_METHOD(void, EnableFrameTrack, (const orbit_client_data::FunctionInfo&));
-  MOCK_METHOD(void, DisableFrameTrack, (const orbit_client_data::FunctionInfo&));
-  MOCK_METHOD(void, AddFrameTrack, (const orbit_client_data::FunctionInfo&));
-  MOCK_METHOD(void, RemoveFrameTrack, (const orbit_client_data::FunctionInfo&));
+  MOCK_METHOD(void, EnableFrameTrack, (const orbit_client_data::FunctionInfo&), (override));
+  MOCK_METHOD(void, DisableFrameTrack, (const orbit_client_data::FunctionInfo&), (override));
+  MOCK_METHOD(void, AddFrameTrack, (const orbit_client_data::FunctionInfo&), (override));
+  MOCK_METHOD(void, RemoveFrameTrack, (const orbit_client_data::FunctionInfo&), (override));
 
-  MOCK_METHOD(void, Disassemble, (uint32_t pid, const orbit_client_data::FunctionInfo&));
-  MOCK_METHOD(void, ShowSourceCode, (const orbit_client_data::FunctionInfo&));
+  MOCK_METHOD(void, Disassemble, (uint32_t pid, const orbit_client_data::FunctionInfo&),
+              (override));
+  MOCK_METHOD(void, ShowSourceCode, (const orbit_client_data::FunctionInfo&), (override));
 
-  MOCK_METHOD(bool, IsTracepointSelected, (const orbit_grpc_protos::TracepointInfo&), (const));
-  MOCK_METHOD(void, SelectTracepoint, (const orbit_grpc_protos::TracepointInfo&));
-  MOCK_METHOD(void, DeselectTracepoint, (const orbit_grpc_protos::TracepointInfo&));
+  MOCK_METHOD(bool, IsTracepointSelected, (const orbit_grpc_protos::TracepointInfo&),
+              (const, override));
+  MOCK_METHOD(void, SelectTracepoint, (const orbit_grpc_protos::TracepointInfo&), (override));
+  MOCK_METHOD(void, DeselectTracepoint, (const orbit_grpc_protos::TracepointInfo&), (override));
 
   MOCK_METHOD(const orbit_statistics::BinomialConfidenceIntervalEstimator&,
-              GetConfidenceIntervalEstimator, (), (const));
+              GetConfidenceIntervalEstimator, (), (const, override));
 
   MOCK_METHOD(void, ShowHistogram,
               (const std::vector<uint64_t>* data, const std::string& function_name,
                uint64_t function_id),
-              ());
+              (override));
 
   MOCK_METHOD(uint64_t, ProvideScopeId, (const orbit_client_protos::TimerInfo& timer_info),
               (const));
