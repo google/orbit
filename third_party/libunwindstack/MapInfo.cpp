msft-mirror-aosp/platform.system.unwinding

Fix memory leak.
If a memory range with the same last_addr is added, one of the Memory
objects gets leaked. This should be next to impossible in normal
operation, however, bad data could cause this. Therefore, add a small
protection for this.

Found through a fuzzer.

Bug: 211570315

Test: Fuzzer case passes.
Test: All unit tests pass.
Change-Id: I250089a15512d2deba67771c4bd2e6dd2b56c1eb

libunwindstack/MapInfo.cpp
@@ -236,12 +236,15 @@ Memory* MapInfo::CreateMemory(const std::shared_ptr<Memory>& process_memory) {
   // the r-x section, which is not quite the right information.
   set_elf_start_offset(prev_real_map->offset());
 
-  MemoryRanges* ranges = new MemoryRanges;
-  ranges->Insert(new MemoryRange(process_memory, prev_real_map->start(),
-                                 prev_real_map->end() - prev_real_map->start(), 0));
-  ranges->Insert(new MemoryRange(process_memory, start(), end() - start(), elf_offset()));
-
-  return ranges;
+  std::unique_ptr<MemoryRanges> ranges(new MemoryRanges);
+  if (!ranges->Insert(new MemoryRange(process_memory, prev_real_map->start(),
+                                      prev_real_map->end() - prev_real_map->start(), 0))) {
+    return nullptr;
+  }
+  if (!ranges->Insert(new MemoryRange(process_memory, start(), end() - start(), elf_offset()))) {
+    return nullptr;
+  }
+  return ranges.release();
 }
 
 class ScopedElfCacheLock {

google/orbit

(cherry picked from commit a3417dc8674e071b24c6f95ef6da7ed5ccdae060)

third_party/libunwindstack/MapInfo.cpp
@@ -237,12 +237,15 @@ Memory* MapInfo::CreateMemory(const std::shared_ptr<Memory>& process_memory) {
   // the r-x section, which is not quite the right information.
   set_object_start_offset(prev_real_map->offset());
 
-  MemoryRanges* ranges = new MemoryRanges;
-  ranges->Insert(new MemoryRange(process_memory, prev_real_map->start(),
-                                 prev_real_map->end() - prev_real_map->start(), 0));
-  ranges->Insert(new MemoryRange(process_memory, start(), end() - start(), object_offset()));
-
-  return ranges;
+  std::unique_ptr<MemoryRanges> ranges(new MemoryRanges);
+  if (!ranges->Insert(new MemoryRange(process_memory, prev_real_map->start(),
+                                      prev_real_map->end() - prev_real_map->start(), 0))) {
+    return nullptr;
+  }
+  if (!ranges->Insert(new MemoryRange(process_memory, start(), end() - start(), object_offset()))) {
+    return nullptr;
+  }
+  return ranges.release();
 }
 
 class ScopedObjectCacheLock {