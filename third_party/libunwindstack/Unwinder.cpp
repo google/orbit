msft-mirror-aosp/platform.system.unwinding

Move MapInfo objects in to frame data.
The frame data when doing an unwind contains a lot of map_XXX
fields which were copies of the data from a MapInfo object.
Instead of doing these copies, replace those fields with a
shared pointer of the MapInfo object itself.

Remove the SetEmbeddedSoname mechanism. Since the MapInfo object
is in the frame data, move this functionality into a MapInfo
function called GetFullName() that will return the map name plus
the soname if needed. A caller needs to call this function directly,
so there is no need to avoid getting this data during the unwind.

Create a GetLoadBias() function that does not require process_memory.
This new function will work for any MapInfo object that appears
in frame data.

Adjust tests for these new changes.

Bug: 120606663

Test: Unit tests pass.
Change-Id: I59199aa344e4b44c158f2636f7f7beff53d9b99a

libunwindstack/Unwinder.cpp
@@ -62,21 +62,12 @@ void Unwinder::FillInDexFrame() {
   frame->pc = dex_pc;
   frame->sp = regs_->sp();
 
-  std::shared_ptr<MapInfo> info = maps_->Find(dex_pc);
-  if (info != nullptr) {
-    frame->map_start = info->start();
-    frame->map_end = info->end();
-    // Since this is a dex file frame, the elf_start_offset is not set
-    // by any of the normal code paths. Use the offset of the map since
-    // that matches the actual offset.
-    frame->map_elf_start_offset = info->offset();
-    frame->map_exact_offset = info->offset();
-    frame->map_load_bias = info->load_bias();
-    frame->map_flags = info->flags();
-    if (resolve_names_) {
-      frame->map_name = info->name();
-    }
-    frame->rel_pc = dex_pc - info->start();
+  frame->map_info = maps_->Find(dex_pc);
+  if (frame->map_info != nullptr) {
+    frame->rel_pc = dex_pc - frame->map_info->start();
+    // Initialize the load bias for this map so subsequent calls
+    // to GetLoadBias() will always return data.
+    frame->map_info->set_load_bias(0);
   } else {
     frame->rel_pc = dex_pc;
     warnings_ |= WARNING_DEX_PC_NOT_IN_MAP;
@@ -96,7 +87,7 @@ void Unwinder::FillInDexFrame() {
 #endif
 }
 
-FrameData* Unwinder::FillInFrame(MapInfo* map_info, Elf* elf, uint64_t rel_pc,
+FrameData* Unwinder::FillInFrame(std::shared_ptr<MapInfo>& map_info, Elf* /*elf*/, uint64_t rel_pc,
                                  uint64_t pc_adjustment) {
   size_t frame_num = frames_.size();
   frames_.resize(frame_num + 1);
@@ -111,25 +102,8 @@ FrameData* Unwinder::FillInFrame(MapInfo* map_info, Elf* elf, uint64_t rel_pc,
     return nullptr;
   }
 
-  if (resolve_names_) {
-    frame->map_name = map_info->name();
-    if (embedded_soname_ && map_info->elf_start_offset() != 0 && !frame->map_name.empty()) {
-      std::string soname = elf->GetSoname();
-      if (!soname.empty()) {
-        std::string map_with_soname;
-        map_with_soname += frame->map_name;
-        map_with_soname += '!';
-        map_with_soname += soname;
-        frame->map_name = SharedString(std::move(map_with_soname));
-      }
-    }
-  }
-  frame->map_elf_start_offset = map_info->elf_start_offset();
-  frame->map_exact_offset = map_info->offset();
-  frame->map_start = map_info->start();
-  frame->map_end = map_info->end();
-  frame->map_flags = map_info->flags();
-  frame->map_load_bias = elf->GetLoadBias();
+  frame->map_info = map_info;
+
   return frame;
 }
 
@@ -233,7 +207,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
         }
       }
 
-      frame = FillInFrame(map_info.get(), elf, rel_pc, pc_adjustment);
+      frame = FillInFrame(map_info, elf, rel_pc, pc_adjustment);
 
       // Once a frame is added, stop skipping frames.
       initial_map_names_to_skip = nullptr;
@@ -334,18 +308,19 @@ std::string Unwinder::FormatFrame(const FrameData& frame) const {
     data += android::base::StringPrintf("  #%02zu pc %016" PRIx64, frame.num, frame.rel_pc);
   }
 
-  if (frame.map_start == frame.map_end) {
+  auto map_info = frame.map_info;
+  if (map_info == nullptr) {
     // No valid map associated with this frame.
     data += "  <unknown>";
-  } else if (!frame.map_name.empty()) {
+  } else if (!map_info->name().empty()) {
     data += "  ";
-    data += frame.map_name;
+    data += map_info->GetFullName();
   } else {
-    data += android::base::StringPrintf("  <anonymous:%" PRIx64 ">", frame.map_start);
+    data += android::base::StringPrintf("  <anonymous:%" PRIx64 ">", map_info->start());
   }
 
-  if (frame.map_elf_start_offset != 0) {
-    data += android::base::StringPrintf(" (offset 0x%" PRIx64 ")", frame.map_elf_start_offset);
+  if (map_info != nullptr && map_info->elf_start_offset() != 0) {
+    data += android::base::StringPrintf(" (offset 0x%" PRIx64 ")", map_info->elf_start_offset());
   }
 
   if (!frame.function_name.empty()) {
@@ -364,7 +339,6 @@ std::string Unwinder::FormatFrame(const FrameData& frame) const {
     data += ')';
   }
 
-  std::shared_ptr<MapInfo> map_info = maps_->Find(frame.map_start);
   if (map_info != nullptr && display_build_id_) {
     std::string build_id = map_info->GetPrintableBuildID();
     if (!build_id.empty()) {
@@ -477,13 +451,7 @@ FrameData Unwinder::BuildFrameFromPcOnly(uint64_t pc, ArchEnum arch, Maps* maps,
   // Copy all the things we need into the frame for symbolization.
   frame.rel_pc = relative_pc;
   frame.pc = pc - pc_adjustment;
-  frame.map_name = map_info->name();
-  frame.map_elf_start_offset = map_info->elf_start_offset();
-  frame.map_exact_offset = map_info->offset();
-  frame.map_start = map_info->start();
-  frame.map_end = map_info->end();
-  frame.map_flags = map_info->flags();
-  frame.map_load_bias = elf->GetLoadBias();
+  frame.map_info = map_info;
 
   if (!resolve_names ||
       !elf->GetFunctionName(debug_pc, &frame.function_name, &frame.function_offset)) {

libunwindstack/include/unwindstack/MapInfo.h
@@ -74,7 +74,7 @@ class MapInfo {
   // Cached data for mapped ELF files.
   // We allocate this structure lazily since there are much fewer ELFs than maps.
   struct ElfFields {
-    ElfFields() : load_bias_(INT64_MAX), build_id_(0) {}
+    ElfFields() : load_bias_(UINT64_MAX), build_id_(0) {}
 
     std::shared_ptr<Elf> elf_;
     // The offset of the beginning of this mapping to the beginning of the
@@ -88,7 +88,7 @@ class MapInfo {
     // shared libraries into a read-only and read-execute map.
     uint64_t elf_start_offset_ = 0;
 
-    std::atomic_int64_t load_bias_;
+    std::atomic_uint64_t load_bias_;
 
     // This is a pointer to a new'd std::string.
     // Using an atomic value means that we don't need to lock and will
@@ -121,6 +121,15 @@ class MapInfo {
   //       returns nullptr.
   std::shared_ptr<MapInfo> GetNextRealMap();
 
+  // This is guaranteed to give out the Elf object associated with the
+  // object. The invariant is that once the Elf object is set under the
+  // lock in a MapInfo object it never changes and is not freed until
+  // the MapInfo object is destructed.
+  inline Elf* GetElfObj() {
+    std::lock_guard<std::mutex> guard(elf_mutex());
+    return elf().get();
+  }
+
   inline uint64_t start() const { return start_; }
   inline void set_start(uint64_t value) { start_ = value; }
 
@@ -147,8 +156,8 @@ class MapInfo {
   inline uint64_t elf_start_offset() { return GetElfFields().elf_start_offset_; }
   inline void set_elf_start_offset(uint64_t value) { GetElfFields().elf_start_offset_ = value; }
 
-  inline std::atomic_int64_t& load_bias() { return GetElfFields().load_bias_; }
-  inline void set_load_bias(int64_t value) { GetElfFields().load_bias_ = value; }
+  inline std::atomic_uint64_t& load_bias() { return GetElfFields().load_bias_; }
+  inline void set_load_bias(uint64_t value) { GetElfFields().load_bias_ = value; }
 
   inline std::atomic<SharedString*>& build_id() { return GetElfFields().build_id_; }
   inline void set_build_id(SharedString* value) { GetElfFields().build_id_ = value; }
@@ -165,7 +174,18 @@ class MapInfo {
   // This function guarantees it will never return nullptr.
   Elf* GetElf(const std::shared_ptr<Memory>& process_memory, ArchEnum expected_arch);
 
+  // Guaranteed to return the proper value if GetElf() has been called.
+  uint64_t GetLoadBias();
+
+  // Will get the proper value even if GetElf() hasn't been called.
   uint64_t GetLoadBias(const std::shared_ptr<Memory>& process_memory);
+
+  // This returns the name of the map plus the soname if this particular
+  // map represents an elf file that is contained inside of another file.
+  // The format of this soname embedded name is:
+  //   file.apk!libutils.so
+  // Otherwise, this function only returns the name of the map.
+  std::string GetFullName();
 
   Memory* CreateMemory(const std::shared_ptr<Memory>& process_memory);
 

libunwindstack/include/unwindstack/Unwinder.h
@@ -49,18 +49,7 @@ struct FrameData {
   SharedString function_name;
   uint64_t function_offset = 0;
 
-  SharedString map_name;
-  // The offset from the first map representing the frame. When there are
-  // two maps (read-only and read-execute) this will be the offset from
-  // the read-only map. When there is only one map, this will be the
-  // same as the actual offset of the map and match map_exact_offset.
-  uint64_t map_elf_start_offset = 0;
-  // The actual offset from the map where the pc lies.
-  uint64_t map_exact_offset = 0;
-  uint64_t map_start = 0;
-  uint64_t map_end = 0;
-  uint64_t map_load_bias = 0;
-  int map_flags = 0;
+  std::shared_ptr<MapInfo> map_info;
 };
 
 class Unwinder {
@@ -109,11 +98,6 @@ class Unwinder {
   // set to an empty string and the function offset being set to zero.
   void SetResolveNames(bool resolve) { resolve_names_ = resolve; }
 
-  // Enable/disable soname printing the soname for a map name if the elf is
-  // embedded in a file. This is enabled by default.
-  // NOTE: This does nothing unless resolving names is enabled.
-  void SetEmbeddedSoname(bool embedded_soname) { embedded_soname_ = embedded_soname; }
-
   void SetDisplayBuildID(bool display_build_id) { display_build_id_ = display_build_id; }
 
   void SetDexFiles(DexFiles* dex_files);
@@ -146,7 +130,8 @@ class Unwinder {
   }
 
   void FillInDexFrame();
-  FrameData* FillInFrame(MapInfo* map_info, Elf* elf, uint64_t rel_pc, uint64_t pc_adjustment);
+  FrameData* FillInFrame(std::shared_ptr<MapInfo>& map_info, Elf* elf, uint64_t rel_pc,
+                         uint64_t pc_adjustment);
 
   size_t max_frames_;
   Maps* maps_ = nullptr;
@@ -156,7 +141,6 @@ class Unwinder {
   JitDebug* jit_debug_ = nullptr;
   DexFiles* dex_files_ = nullptr;
   bool resolve_names_ = true;
-  bool embedded_soname_ = true;
   bool display_build_id_ = false;
   // True if at least one elf file is coming from memory and not the related
   // file. This is only true if there is an actual file backing up the elf.

google/orbit

(cherry picked from commit 0a2dc5be6a8770f58d3ea21e3a46bde7f7de0943)

third_party/libunwindstack/Unwinder.cpp
@@ -63,21 +63,12 @@ void Unwinder::FillInDexFrame() {
   frame->pc = dex_pc;
   frame->sp = regs_->sp();
 
-  std::shared_ptr<MapInfo> info = maps_->Find(dex_pc);
-  if (info != nullptr) {
-    frame->map_start = info->start();
-    frame->map_end = info->end();
-    // Since this is a dex file frame, the elf_start_offset is not set
-    // by any of the normal code paths. Use the offset of the map since
-    // that matches the actual offset.
-    frame->map_object_start_offset = info->offset();
-    frame->map_exact_offset = info->offset();
-    frame->map_load_bias = info->load_bias();
-    frame->map_flags = info->flags();
-    if (resolve_names_) {
-      frame->map_name = info->name();
-    }
-    frame->rel_pc = dex_pc - info->start();
+  frame->map_info = maps_->Find(dex_pc);
+  if (frame->map_info != nullptr) {
+    frame->rel_pc = dex_pc - frame->map_info->start();
+    // Initialize the load bias for this map so subsequent calls
+    // to GetLoadBias() will always return data.
+    frame->map_info->set_load_bias(0);
   } else {
     frame->rel_pc = dex_pc;
     warnings_ |= WARNING_DEX_PC_NOT_IN_MAP;
@@ -97,8 +88,8 @@ void Unwinder::FillInDexFrame() {
 #endif
 }
 
-FrameData* Unwinder::FillInFrame(MapInfo* map_info, Object* object, uint64_t rel_pc,
-                                 uint64_t pc_adjustment) {
+FrameData* Unwinder::FillInFrame(std::shared_ptr<MapInfo>& map_info, Object* /*object*/,
+                                 uint64_t rel_pc, uint64_t pc_adjustment) {
   size_t frame_num = frames_.size();
   frames_.resize(frame_num + 1);
   FrameData* frame = &frames_.at(frame_num);
@@ -112,25 +103,8 @@ FrameData* Unwinder::FillInFrame(MapInfo* map_info, Object* object, uint64_t rel
     return nullptr;
   }
 
-  if (resolve_names_) {
-    frame->map_name = map_info->name();
-    if (embedded_soname_ && map_info->object_start_offset() != 0 && !frame->map_name.empty()) {
-      std::string soname = object->GetSoname();
-      if (!soname.empty()) {
-        std::string map_with_soname;
-        map_with_soname += frame->map_name;
-        map_with_soname += '!';
-        map_with_soname += soname;
-        frame->map_name = SharedString(std::move(map_with_soname));
-      }
-    }
-  }
-  frame->map_object_start_offset = map_info->object_start_offset();
-  frame->map_exact_offset = map_info->offset();
-  frame->map_start = map_info->start();
-  frame->map_end = map_info->end();
-  frame->map_flags = map_info->flags();
-  frame->map_load_bias = object->GetLoadBias();
+  frame->map_info = map_info;
+
   return frame;
 }
 
@@ -234,7 +208,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
         }
       }
 
-      frame = FillInFrame(map_info.get(), object, rel_pc, pc_adjustment);
+      frame = FillInFrame(map_info, object, rel_pc, pc_adjustment);
 
       // Once a frame is added, stop skipping frames.
       initial_map_names_to_skip = nullptr;
@@ -335,18 +309,19 @@ std::string Unwinder::FormatFrame(const FrameData& frame) const {
     data += android::base::StringPrintf("  #%02zu pc %016" PRIx64, frame.num, frame.rel_pc);
   }
 
-  if (frame.map_start == frame.map_end) {
+  auto map_info = frame.map_info;
+  if (map_info == nullptr) {
     // No valid map associated with this frame.
     data += "  <unknown>";
-  } else if (!frame.map_name.empty()) {
+  } else if (!map_info->name().empty()) {
     data += "  ";
-    data += frame.map_name;
+    data += map_info->GetFullName();
   } else {
-    data += android::base::StringPrintf("  <anonymous:%" PRIx64 ">", frame.map_start);
+    data += android::base::StringPrintf("  <anonymous:%" PRIx64 ">", map_info->start());
   }
 
-  if (frame.map_object_start_offset != 0) {
-    data += android::base::StringPrintf(" (offset 0x%" PRIx64 ")", frame.map_object_start_offset);
+  if (map_info != nullptr && map_info->object_start_offset() != 0) {
+    data += android::base::StringPrintf(" (offset 0x%" PRIx64 ")", map_info->object_start_offset());
   }
 
   if (!frame.function_name.empty()) {
@@ -365,7 +340,6 @@ std::string Unwinder::FormatFrame(const FrameData& frame) const {
     data += ')';
   }
 
-  std::shared_ptr<MapInfo> map_info = maps_->Find(frame.map_start);
   if (map_info != nullptr && display_build_id_) {
     std::string build_id = map_info->GetPrintableBuildID();
     if (!build_id.empty()) {
@@ -478,13 +452,7 @@ FrameData Unwinder::BuildFrameFromPcOnly(uint64_t pc, ArchEnum arch, Maps* maps,
   // Copy all the things we need into the frame for symbolization.
   frame.rel_pc = relative_pc;
   frame.pc = pc - pc_adjustment;
-  frame.map_name = map_info->name();
-  frame.map_object_start_offset = map_info->object_start_offset();
-  frame.map_exact_offset = map_info->offset();
-  frame.map_start = map_info->start();
-  frame.map_end = map_info->end();
-  frame.map_flags = map_info->flags();
-  frame.map_load_bias = object->GetLoadBias();
+  frame.map_info = map_info;
 
   if (!resolve_names ||
       !object->GetFunctionName(debug_pc, &frame.function_name, &frame.function_offset)) {

third_party/libunwindstack/include/unwindstack/MapInfo.h
@@ -74,7 +74,7 @@ class MapInfo {
   // Cached data for mapped object files.
   // We allocate this structure lazily since there are much fewer objects than maps.
   struct ObjectFields {
-    ObjectFields() : load_bias_(INT64_MAX), build_id_(0) {}
+    ObjectFields() : load_bias_(UINT64_MAX), build_id_(0) {}
 
     std::shared_ptr<Object> object_;
     // The offset of the beginning of this mapping to the beginning of the
@@ -88,7 +88,7 @@ class MapInfo {
     // shared libraries into a read-only and read-execute map.
     uint64_t object_start_offset_ = 0;
 
-    std::atomic_int64_t load_bias_;
+    std::atomic_uint64_t load_bias_;
 
     // This is a pointer to a new'd std::string.
     // Using an atomic value means that we don't need to lock and will
@@ -121,6 +121,15 @@ class MapInfo {
   //       returns nullptr.
   std::shared_ptr<MapInfo> GetNextRealMap();
 
+  // This is guaranteed to give out the object file associated with the MapInfo
+  // object. The invariant is that once the object file is set under the
+  // lock in a MapInfo object it never changes and is not freed until
+  // the MapInfo object is destructed.
+  inline Object* GetCachedObj() {
+    std::lock_guard<std::mutex> guard(object_mutex());
+    return object().get();
+  }
+
   inline uint64_t start() const { return start_; }
   inline void set_start(uint64_t value) { start_ = value; }
 
@@ -149,8 +158,8 @@ class MapInfo {
     GetObjectFields().object_start_offset_ = value;
   }
 
-  inline std::atomic_int64_t& load_bias() { return GetObjectFields().load_bias_; }
-  inline void set_load_bias(int64_t value) { GetObjectFields().load_bias_ = value; }
+  inline std::atomic_uint64_t& load_bias() { return GetObjectFields().load_bias_; }
+  inline void set_load_bias(uint64_t value) { GetObjectFields().load_bias_ = value; }
 
   inline std::atomic<SharedString*>& build_id() { return GetObjectFields().build_id_; }
   inline void set_build_id(SharedString* value) { GetObjectFields().build_id_ = value; }
@@ -169,7 +178,18 @@ class MapInfo {
   // This function guarantees it will never return nullptr.
   Object* GetObject(const std::shared_ptr<Memory>& process_memory, ArchEnum expected_arch);
 
+  // Guaranteed to return the proper value if GetElf() has been called.
+  uint64_t GetLoadBias();
+
+  // Will get the proper value even if GetElf() hasn't been called.
   uint64_t GetLoadBias(const std::shared_ptr<Memory>& process_memory);
+
+  // This returns the name of the map plus the soname if this particular
+  // map represents an elf file that is contained inside of another file.
+  // The format of this soname embedded name is:
+  //   file.apk!libutils.so
+  // Otherwise, this function only returns the name of the map.
+  std::string GetFullName();
 
   Memory* CreateMemory(const std::shared_ptr<Memory>& process_memory);
 

third_party/libunwindstack/include/unwindstack/Unwinder.h
@@ -49,18 +49,7 @@ struct FrameData {
   SharedString function_name;
   uint64_t function_offset = 0;
 
-  SharedString map_name;
-  // The offset from the first map representing the frame. When there are
-  // two maps (read-only and read-execute) this will be the offset from
-  // the read-only map. When there is only one map, this will be the
-  // same as the actual offset of the map and match map_exact_offset.
-  uint64_t map_object_start_offset = 0;
-  // The actual offset from the map where the pc lies.
-  uint64_t map_exact_offset = 0;
-  uint64_t map_start = 0;
-  uint64_t map_end = 0;
-  uint64_t map_load_bias = 0;
-  int map_flags = 0;
+  std::shared_ptr<MapInfo> map_info;
 };
 
 class Unwinder {
@@ -109,11 +98,6 @@ class Unwinder {
   // set to an empty string and the function offset being set to zero.
   void SetResolveNames(bool resolve) { resolve_names_ = resolve; }
 
-  // Enable/disable soname printing the soname for a map name if the elf is
-  // embedded in a file. This is enabled by default.
-  // NOTE: This does nothing unless resolving names is enabled.
-  void SetEmbeddedSoname(bool embedded_soname) { embedded_soname_ = embedded_soname; }
-
   void SetDisplayBuildID(bool display_build_id) { display_build_id_ = display_build_id; }
 
   void SetDexFiles(DexFiles* dex_files);
@@ -146,7 +130,7 @@ class Unwinder {
   }
 
   void FillInDexFrame();
-  FrameData* FillInFrame(MapInfo* map_info, Object* object, uint64_t rel_pc,
+  FrameData* FillInFrame(std::shared_ptr<MapInfo>& map_info, Object* object, uint64_t rel_pc,
                          uint64_t pc_adjustment);
 
   size_t max_frames_;
@@ -157,7 +141,6 @@ class Unwinder {
   JitDebug* jit_debug_ = nullptr;
   DexFiles* dex_files_ = nullptr;
   bool resolve_names_ = true;
-  bool embedded_soname_ = true;
   bool display_build_id_ = false;
   // True if at least one object file is coming from memory and not the related
   // file. This is only true if there is an actual file backing up the object.