msft-mirror-aosp/platform.system.unwinding

Maps contains shared_ptr MapInfo objects now.
In order to make it easier to change the map data, make the individual
MapInfo objects shared_ptr objects.

This also changes the LocalUpdatableMaps object so that all of the
reparsing is in the object itself. This means this object can be
used with the normal unwinder unchanged and I can delete the
LocalUnwinder completely in a future cl.

Add a method to share a maps object between ThreadUnwinder objects.

Add new unit tests to cover the changes.

Bug: 120606663

Test: Unit tests pass.
Test: Did a performance run before and after to verify no
Test: performance issues.
Change-Id: I074dc925c8e5ff8b3d75779052aefe4801b2c7bf

libunwindstack/LocalUnwinder.cpp
@@ -44,8 +44,6 @@
 namespace unwindstack {
 
 bool LocalUnwinder::Init() {
-  pthread_rwlock_init(&maps_rwlock_, nullptr);
-
   // Create the maps.
   maps_.reset(new unwindstack::LocalUpdatableMaps());
   if (!maps_->Parse()) {
@@ -81,13 +79,13 @@ bool LocalUnwinder::Unwind(std::vector<LocalFrameData>* frame_info, size_t max_f
     uint64_t cur_pc = regs->pc();
     uint64_t cur_sp = regs->sp();
 
-    MapInfo* map_info = maps_->Find(cur_pc);
+    std::shared_ptr<MapInfo> map_info = maps_->Find(cur_pc);
     if (map_info == nullptr) {
       break;
     }
 
     Elf* elf = map_info->GetElf(process_memory_, arch);
-    uint64_t rel_pc = elf->GetRelPc(cur_pc, map_info);
+    uint64_t rel_pc = elf->GetRelPc(cur_pc, map_info.get());
     uint64_t step_pc = rel_pc;
     uint64_t pc_adjustment;
     if (adjust_pc) {

libunwindstack/Unwinder.cpp
@@ -24,6 +24,7 @@
 #include <unistd.h>
 
 #include <algorithm>
+#include <memory>
 
 #include <android-base/file.h>
 #include <android-base/stringprintf.h>
@@ -61,7 +62,7 @@ void Unwinder::FillInDexFrame() {
   frame->pc = dex_pc;
   frame->sp = regs_->sp();
 
-  MapInfo* info = maps_->Find(dex_pc);
+  std::shared_ptr<MapInfo> info = maps_->Find(dex_pc);
   if (info != nullptr) {
     frame->map_start = info->start();
     frame->map_end = info->end();
@@ -167,7 +168,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
     uint64_t cur_pc = regs_->pc();
     uint64_t cur_sp = regs_->sp();
 
-    MapInfo* map_info = maps_->Find(regs_->pc());
+    std::shared_ptr<MapInfo> map_info = maps_->Find(regs_->pc());
     uint64_t pc_adjustment = 0;
     uint64_t step_pc;
     uint64_t rel_pc;
@@ -190,7 +191,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
         elf_from_memory_not_file_ = true;
       }
       step_pc = regs_->pc();
-      rel_pc = elf->GetRelPc(step_pc, map_info);
+      rel_pc = elf->GetRelPc(step_pc, map_info.get());
       // Everyone except elf data in gdb jit debug maps uses the relative pc.
       if (!(map_info->flags() & MAPS_FLAGS_JIT_SYMFILE_MAP)) {
         step_pc = rel_pc;
@@ -232,7 +233,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
         }
       }
 
-      frame = FillInFrame(map_info, elf, rel_pc, pc_adjustment);
+      frame = FillInFrame(map_info.get(), elf, rel_pc, pc_adjustment);
 
       // Once a frame is added, stop skipping frames.
       initial_map_names_to_skip = nullptr;
@@ -249,7 +250,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
         // some of the speculative frames.
         in_device_map = true;
       } else {
-        MapInfo* sp_info = maps_->Find(regs_->sp());
+        auto sp_info = maps_->Find(regs_->sp());
         if (sp_info != nullptr && sp_info->flags() & MAPS_FLAGS_DEVICE_MAP) {
           // Do not stop here, fall through in case we are
           // in the speculative unwind path and need to remove
@@ -363,7 +364,7 @@ std::string Unwinder::FormatFrame(const FrameData& frame) const {
     data += ')';
   }
 
-  MapInfo* map_info = maps_->Find(frame.map_start);
+  std::shared_ptr<MapInfo> map_info = maps_->Find(frame.map_start);
   if (map_info != nullptr && display_build_id_) {
     std::string build_id = map_info->GetPrintableBuildID();
     if (!build_id.empty()) {
@@ -447,7 +448,7 @@ FrameData Unwinder::BuildFrameFromPcOnly(uint64_t pc, ArchEnum arch, Maps* maps,
                                          bool resolve_names) {
   FrameData frame;
 
-  MapInfo* map_info = maps->Find(pc);
+  std::shared_ptr<MapInfo> map_info = maps->Find(pc);
   if (map_info == nullptr || arch == ARCH_UNKNOWN) {
     frame.pc = pc;
     frame.rel_pc = pc;
@@ -456,7 +457,7 @@ FrameData Unwinder::BuildFrameFromPcOnly(uint64_t pc, ArchEnum arch, Maps* maps,
 
   Elf* elf = map_info->GetElf(process_memory, arch);
 
-  uint64_t relative_pc = elf->GetRelPc(pc, map_info);
+  uint64_t relative_pc = elf->GetRelPc(pc, map_info.get());
 
   uint64_t pc_adjustment = GetPcAdjustment(relative_pc, elf, arch);
   relative_pc -= pc_adjustment;

libunwindstack/include/unwindstack/MapInfo.h
@@ -38,8 +38,8 @@ class MemoryFileAtOffset;
 // (for example, 400 process * 400 maps * 128 bytes = 20 MB + string data).
 class MapInfo {
  public:
-  MapInfo(MapInfo* prev_map, MapInfo* prev_real_map, uint64_t start, uint64_t end, uint64_t offset,
-          uint64_t flags, SharedString name)
+  MapInfo(std::shared_ptr<MapInfo>& prev_map, std::shared_ptr<MapInfo>& prev_real_map,
+          uint64_t start, uint64_t end, uint64_t offset, uint64_t flags, SharedString name)
       : start_(start),
         end_(end),
         offset_(offset),
@@ -47,9 +47,31 @@ class MapInfo {
         name_(name),
         elf_fields_(nullptr),
         prev_map_(prev_map),
-        prev_real_map_(prev_real_map) {
-    if (prev_real_map != nullptr) prev_real_map->next_real_map_ = this;
+        prev_real_map_(prev_real_map) {}
+  MapInfo(uint64_t start, uint64_t end, uint64_t offset, uint64_t flags, SharedString name)
+      : start_(start),
+        end_(end),
+        offset_(offset),
+        flags_(flags),
+        name_(name),
+        elf_fields_(nullptr) {}
+
+  static inline std::shared_ptr<MapInfo> Create(std::shared_ptr<MapInfo>& prev_map,
+                                                std::shared_ptr<MapInfo>& prev_real_map,
+                                                uint64_t start, uint64_t end, uint64_t offset,
+                                                uint64_t flags, SharedString name) {
+    auto map_info =
+        std::make_shared<MapInfo>(prev_map, prev_real_map, start, end, offset, flags, name);
+    if (prev_real_map != nullptr) {
+      prev_real_map->next_real_map_ = map_info;
+    }
+    return map_info;
   }
+  static inline std::shared_ptr<MapInfo> Create(uint64_t start, uint64_t end, uint64_t offset,
+                                                uint64_t flags, SharedString name) {
+    return std::make_shared<MapInfo>(start, end, offset, flags, name);
+  }
+
   ~MapInfo();
 
   // Cached data for mapped ELF files.
@@ -118,14 +140,14 @@ class MapInfo {
   inline bool memory_backed_elf() { return GetElfFields().memory_backed_elf_; }
   inline void set_memory_backed_elf(bool value) { GetElfFields().memory_backed_elf_ = value; }
 
-  inline MapInfo* prev_map() const { return prev_map_; }
-  inline void set_prev_map(MapInfo* value) { prev_map_ = value; }
+  inline std::shared_ptr<MapInfo> prev_map() const { return prev_map_.lock(); }
+  inline void set_prev_map(std::shared_ptr<MapInfo>& value) { prev_map_ = value; }
 
-  inline MapInfo* prev_real_map() const { return prev_real_map_; }
-  inline void set_prev_real_map(MapInfo* value) { prev_real_map_ = value; }
+  inline std::shared_ptr<MapInfo> prev_real_map() const { return prev_real_map_.lock(); }
+  inline void set_prev_real_map(std::shared_ptr<MapInfo>& value) { prev_real_map_ = value; }
 
-  inline MapInfo* next_real_map() const { return next_real_map_; }
-  inline void set_next_real_map(MapInfo* value) { next_real_map_ = value; }
+  inline std::shared_ptr<MapInfo> next_real_map() const { return next_real_map_.lock(); }
+  inline void set_next_real_map(std::shared_ptr<MapInfo>& value) { next_real_map_ = value; }
 
   // This function guarantees it will never return nullptr.
   Elf* GetElf(const std::shared_ptr<Memory>& process_memory, ArchEnum expected_arch);
@@ -168,7 +190,7 @@ class MapInfo {
 
   std::atomic<ElfFields*> elf_fields_;
 
-  MapInfo* prev_map_ = nullptr;
+  std::weak_ptr<MapInfo> prev_map_;
   // This is the previous map that is not empty with a 0 offset. For
   // example, this set of maps:
   //  1000-2000  r--p 000000 00:00 0 libc.so
@@ -176,10 +198,10 @@ class MapInfo {
   //  3000-4000  r-xp 003000 00:00 0 libc.so
   // The last map's prev_map would point to the 2000-3000 map, while the
   // prev_real_map would point to the 1000-2000 map.
-  MapInfo* prev_real_map_ = nullptr;
+  std::weak_ptr<MapInfo> prev_real_map_;
 
   // Same as above but set to point to the next map.
-  MapInfo* next_real_map_ = nullptr;
+  std::weak_ptr<MapInfo> next_real_map_;
 };
 
 }  // namespace unwindstack

libunwindstack/tests/RegsTest.cpp
@@ -16,6 +16,8 @@
 
 #include <stdint.h>
 
+#include <memory>
+
 #include <gtest/gtest.h>
 
 #include <unwindstack/Elf.h>
@@ -169,27 +171,27 @@ TEST_F(RegsTest, rel_pc_arm) {
 }
 
 TEST_F(RegsTest, elf_invalid) {
-  MapInfo map_info(nullptr, nullptr, 0x1000, 0x2000, 0, 0, "");
+  auto map_info = MapInfo::Create(0x1000, 0x2000, 0, 0, "");
   Elf* invalid_elf = new Elf(nullptr);
-  map_info.set_elf(invalid_elf);
+  map_info->set_elf(invalid_elf);
 
-  EXPECT_EQ(0x500U, invalid_elf->GetRelPc(0x1500, &map_info));
+  EXPECT_EQ(0x500U, invalid_elf->GetRelPc(0x1500, map_info.get()));
   EXPECT_EQ(2U, GetPcAdjustment(0x500U, invalid_elf, ARCH_ARM));
   EXPECT_EQ(2U, GetPcAdjustment(0x511U, invalid_elf, ARCH_ARM));
 
-  EXPECT_EQ(0x600U, invalid_elf->GetRelPc(0x1600, &map_info));
+  EXPECT_EQ(0x600U, invalid_elf->GetRelPc(0x1600, map_info.get()));
   EXPECT_EQ(4U, GetPcAdjustment(0x600U, invalid_elf, ARCH_ARM64));
 
-  EXPECT_EQ(0x700U, invalid_elf->GetRelPc(0x1700, &map_info));
+  EXPECT_EQ(0x700U, invalid_elf->GetRelPc(0x1700, map_info.get()));
   EXPECT_EQ(1U, GetPcAdjustment(0x700U, invalid_elf, ARCH_X86));
 
-  EXPECT_EQ(0x800U, invalid_elf->GetRelPc(0x1800, &map_info));
+  EXPECT_EQ(0x800U, invalid_elf->GetRelPc(0x1800, map_info.get()));
   EXPECT_EQ(1U, GetPcAdjustment(0x800U, invalid_elf, ARCH_X86_64));
 
-  EXPECT_EQ(0x900U, invalid_elf->GetRelPc(0x1900, &map_info));
+  EXPECT_EQ(0x900U, invalid_elf->GetRelPc(0x1900, map_info.get()));
   EXPECT_EQ(8U, GetPcAdjustment(0x900U, invalid_elf, ARCH_MIPS));
 
-  EXPECT_EQ(0xa00U, invalid_elf->GetRelPc(0x1a00, &map_info));
+  EXPECT_EQ(0xa00U, invalid_elf->GetRelPc(0x1a00, map_info.get()));
   EXPECT_EQ(8U, GetPcAdjustment(0xa00U, invalid_elf, ARCH_MIPS64));
 }
 

google/orbit

Merged-In: I074dc925c8e5ff8b3d75779052aefe4801b2c7bf
(cherry picked from commit bf98d2a784c8181ba508f978e123459e9a8c610b)
(cherry picked from commit 88367cfde862f4bdd1da64d691b8a3854026f439)

third_party/libunwindstack/LocalUnwinder.cpp
@@ -44,8 +44,6 @@
 namespace unwindstack {
 
 bool LocalUnwinder::Init() {
-  pthread_rwlock_init(&maps_rwlock_, nullptr);
-
   // Create the maps.
   maps_.reset(new unwindstack::LocalUpdatableMaps());
   if (!maps_->Parse()) {
@@ -81,13 +79,13 @@ bool LocalUnwinder::Unwind(std::vector<LocalFrameData>* frame_info, size_t max_f
     uint64_t cur_pc = regs->pc();
     uint64_t cur_sp = regs->sp();
 
-    MapInfo* map_info = maps_->Find(cur_pc);
+    std::shared_ptr<MapInfo> map_info = maps_->Find(cur_pc);
     if (map_info == nullptr) {
       break;
     }
 
     Object* object = map_info->GetObject(process_memory_, arch);
-    uint64_t rel_pc = object->GetRelPc(cur_pc, map_info);
+    uint64_t rel_pc = object->GetRelPc(cur_pc, map_info.get());
     uint64_t step_pc = rel_pc;
     uint64_t pc_adjustment;
     if (adjust_pc) {

third_party/libunwindstack/Unwinder.cpp
@@ -24,6 +24,7 @@
 #include <unistd.h>
 
 #include <algorithm>
+#include <memory>
 
 #include <android-base/file.h>
 #include <android-base/stringprintf.h>
@@ -62,7 +63,7 @@ void Unwinder::FillInDexFrame() {
   frame->pc = dex_pc;
   frame->sp = regs_->sp();
 
-  MapInfo* info = maps_->Find(dex_pc);
+  std::shared_ptr<MapInfo> info = maps_->Find(dex_pc);
   if (info != nullptr) {
     frame->map_start = info->start();
     frame->map_end = info->end();
@@ -168,7 +169,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
     uint64_t cur_pc = regs_->pc();
     uint64_t cur_sp = regs_->sp();
 
-    MapInfo* map_info = maps_->Find(regs_->pc());
+    std::shared_ptr<MapInfo> map_info = maps_->Find(regs_->pc());
     uint64_t pc_adjustment = 0;
     uint64_t step_pc;
     uint64_t rel_pc;
@@ -191,7 +192,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
         object_from_memory_not_file_ = true;
       }
       step_pc = regs_->pc();
-      rel_pc = object->GetRelPc(step_pc, map_info);
+      rel_pc = object->GetRelPc(step_pc, map_info.get());
       // Everyone except elf data in gdb jit debug maps uses the relative pc.
       if (!(map_info->flags() & MAPS_FLAGS_JIT_SYMFILE_MAP)) {
         step_pc = rel_pc;
@@ -233,7 +234,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
         }
       }
 
-      frame = FillInFrame(map_info, object, rel_pc, pc_adjustment);
+      frame = FillInFrame(map_info.get(), object, rel_pc, pc_adjustment);
 
       // Once a frame is added, stop skipping frames.
       initial_map_names_to_skip = nullptr;
@@ -250,7 +251,7 @@ void Unwinder::Unwind(const std::vector<std::string>* initial_map_names_to_skip,
         // some of the speculative frames.
         in_device_map = true;
       } else {
-        MapInfo* sp_info = maps_->Find(regs_->sp());
+        auto sp_info = maps_->Find(regs_->sp());
         if (sp_info != nullptr && sp_info->flags() & MAPS_FLAGS_DEVICE_MAP) {
           // Do not stop here, fall through in case we are
           // in the speculative unwind path and need to remove
@@ -364,7 +365,7 @@ std::string Unwinder::FormatFrame(const FrameData& frame) const {
     data += ')';
   }
 
-  MapInfo* map_info = maps_->Find(frame.map_start);
+  std::shared_ptr<MapInfo> map_info = maps_->Find(frame.map_start);
   if (map_info != nullptr && display_build_id_) {
     std::string build_id = map_info->GetPrintableBuildID();
     if (!build_id.empty()) {
@@ -448,7 +449,7 @@ FrameData Unwinder::BuildFrameFromPcOnly(uint64_t pc, ArchEnum arch, Maps* maps,
                                          bool resolve_names) {
   FrameData frame;
 
-  MapInfo* map_info = maps->Find(pc);
+  std::shared_ptr<MapInfo> map_info = maps->Find(pc);
   if (map_info == nullptr || arch == ARCH_UNKNOWN) {
     frame.pc = pc;
     frame.rel_pc = pc;
@@ -457,7 +458,7 @@ FrameData Unwinder::BuildFrameFromPcOnly(uint64_t pc, ArchEnum arch, Maps* maps,
 
   Object* object = map_info->GetObject(process_memory, arch);
 
-  uint64_t relative_pc = object->GetRelPc(pc, map_info);
+  uint64_t relative_pc = object->GetRelPc(pc, map_info.get());
 
   uint64_t pc_adjustment = GetPcAdjustment(relative_pc, object, arch);
   relative_pc -= pc_adjustment;

third_party/libunwindstack/include/unwindstack/MapInfo.h
@@ -38,8 +38,8 @@ class MemoryFileAtOffset;
 // (for example, 400 process * 400 maps * 128 bytes = 20 MB + string data).
 class MapInfo {
  public:
-  MapInfo(MapInfo* prev_map, MapInfo* prev_real_map, uint64_t start, uint64_t end, uint64_t offset,
-          uint64_t flags, SharedString name)
+  MapInfo(std::shared_ptr<MapInfo>& prev_map, std::shared_ptr<MapInfo>& prev_real_map,
+          uint64_t start, uint64_t end, uint64_t offset, uint64_t flags, SharedString name)
       : start_(start),
         end_(end),
         offset_(offset),
@@ -47,9 +47,31 @@ class MapInfo {
         name_(name),
         object_fields_(nullptr),
         prev_map_(prev_map),
-        prev_real_map_(prev_real_map) {
-    if (prev_real_map != nullptr) prev_real_map->next_real_map_ = this;
+        prev_real_map_(prev_real_map) {}
+  MapInfo(uint64_t start, uint64_t end, uint64_t offset, uint64_t flags, SharedString name)
+      : start_(start),
+        end_(end),
+        offset_(offset),
+        flags_(flags),
+        name_(name),
+        object_fields_(nullptr) {}
+
+  static inline std::shared_ptr<MapInfo> Create(std::shared_ptr<MapInfo>& prev_map,
+                                                std::shared_ptr<MapInfo>& prev_real_map,
+                                                uint64_t start, uint64_t end, uint64_t offset,
+                                                uint64_t flags, SharedString name) {
+    auto map_info =
+        std::make_shared<MapInfo>(prev_map, prev_real_map, start, end, offset, flags, name);
+    if (prev_real_map != nullptr) {
+      prev_real_map->next_real_map_ = map_info;
+    }
+    return map_info;
   }
+  static inline std::shared_ptr<MapInfo> Create(uint64_t start, uint64_t end, uint64_t offset,
+                                                uint64_t flags, SharedString name) {
+    return std::make_shared<MapInfo>(start, end, offset, flags, name);
+  }
+
   ~MapInfo();
 
   // Cached data for mapped object files.
@@ -122,14 +144,14 @@ class MapInfo {
     GetObjectFields().memory_backed_object_ = value;
   }
 
-  inline MapInfo* prev_map() const { return prev_map_; }
-  inline void set_prev_map(MapInfo* value) { prev_map_ = value; }
+  inline std::shared_ptr<MapInfo> prev_map() const { return prev_map_.lock(); }
+  inline void set_prev_map(std::shared_ptr<MapInfo>& value) { prev_map_ = value; }
 
-  inline MapInfo* prev_real_map() const { return prev_real_map_; }
-  inline void set_prev_real_map(MapInfo* value) { prev_real_map_ = value; }
+  inline std::shared_ptr<MapInfo> prev_real_map() const { return prev_real_map_.lock(); }
+  inline void set_prev_real_map(std::shared_ptr<MapInfo>& value) { prev_real_map_ = value; }
 
-  inline MapInfo* next_real_map() const { return next_real_map_; }
-  inline void set_next_real_map(MapInfo* value) { next_real_map_ = value; }
+  inline std::shared_ptr<MapInfo> next_real_map() const { return next_real_map_.lock(); }
+  inline void set_next_real_map(std::shared_ptr<MapInfo>& value) { next_real_map_ = value; }
 
   // This function guarantees it will never return nullptr.
   Object* GetObject(const std::shared_ptr<Memory>& process_memory, ArchEnum expected_arch);
@@ -174,7 +196,7 @@ class MapInfo {
 
   std::atomic<ObjectFields*> object_fields_;
 
-  MapInfo* prev_map_ = nullptr;
+  std::weak_ptr<MapInfo> prev_map_;
   // This is the previous map that is not empty with a 0 offset. For
   // example, this set of maps:
   //  1000-2000  r--p 000000 00:00 0 libc.so
@@ -182,10 +204,10 @@ class MapInfo {
   //  3000-4000  r-xp 003000 00:00 0 libc.so
   // The last map's prev_map would point to the 2000-3000 map, while the
   // prev_real_map would point to the 1000-2000 map.
-  MapInfo* prev_real_map_ = nullptr;
+  std::weak_ptr<MapInfo> prev_real_map_;
 
   // Same as above but set to point to the next map.
-  MapInfo* next_real_map_ = nullptr;
+  std::weak_ptr<MapInfo> next_real_map_;
 };
 
 }  // namespace unwindstack

third_party/libunwindstack/tests/RegsTest.cpp
@@ -16,6 +16,8 @@
 
 #include <stdint.h>
 
+#include <memory>
+
 #include <gtest/gtest.h>
 
 #include <unwindstack/Elf.h>
@@ -170,27 +172,27 @@ TEST_F(RegsTest, rel_pc_arm) {
 }
 
 TEST_F(RegsTest, elf_invalid) {
-  MapInfo map_info(nullptr, nullptr, 0x1000, 0x2000, 0, 0, "");
+  auto map_info = MapInfo::Create(0x1000, 0x2000, 0, 0, "");
   Elf* invalid_elf = new Elf(nullptr);
-  map_info.set_object(invalid_elf);
+  map_info->set_object(invalid_elf);
 
-  EXPECT_EQ(0x500U, invalid_elf->GetRelPc(0x1500, &map_info));
+  EXPECT_EQ(0x500U, invalid_elf->GetRelPc(0x1500, map_info.get()));
   EXPECT_EQ(2U, GetPcAdjustment(0x500U, invalid_elf, ARCH_ARM));
   EXPECT_EQ(2U, GetPcAdjustment(0x511U, invalid_elf, ARCH_ARM));
 
-  EXPECT_EQ(0x600U, invalid_elf->GetRelPc(0x1600, &map_info));
+  EXPECT_EQ(0x600U, invalid_elf->GetRelPc(0x1600, map_info.get()));
   EXPECT_EQ(4U, GetPcAdjustment(0x600U, invalid_elf, ARCH_ARM64));
 
-  EXPECT_EQ(0x700U, invalid_elf->GetRelPc(0x1700, &map_info));
+  EXPECT_EQ(0x700U, invalid_elf->GetRelPc(0x1700, map_info.get()));
   EXPECT_EQ(1U, GetPcAdjustment(0x700U, invalid_elf, ARCH_X86));
 
-  EXPECT_EQ(0x800U, invalid_elf->GetRelPc(0x1800, &map_info));
+  EXPECT_EQ(0x800U, invalid_elf->GetRelPc(0x1800, map_info.get()));
   EXPECT_EQ(1U, GetPcAdjustment(0x800U, invalid_elf, ARCH_X86_64));
 
-  EXPECT_EQ(0x900U, invalid_elf->GetRelPc(0x1900, &map_info));
+  EXPECT_EQ(0x900U, invalid_elf->GetRelPc(0x1900, map_info.get()));
   EXPECT_EQ(8U, GetPcAdjustment(0x900U, invalid_elf, ARCH_MIPS));
 
-  EXPECT_EQ(0xa00U, invalid_elf->GetRelPc(0x1a00, &map_info));
+  EXPECT_EQ(0xa00U, invalid_elf->GetRelPc(0x1a00, map_info.get()));
   EXPECT_EQ(8U, GetPcAdjustment(0xa00U, invalid_elf, ARCH_MIPS64));
 }
 