b5e694e46b32ba2194dbbdba2846243a811b21c7
Florian Kübler <kuebler@google.com>
2022-05-05 15:14:43 +0200

    Expose frame pointer unwinding (#3669)
    This exposes frame pointer unwinding even when not in --devmode.
    
    Test: Take a capture with and without frame pointer unwinding.
    Bug: http://b/155869485

--- src/OrbitGl/App.cpp
+++ src/OrbitGl/App.cpp
@@ -1447,8 +1447,7 @@ void OrbitApp::StartCapture() {
   options.dynamic_instrumentation_method = data_manager_->dynamic_instrumentation_method();
   options.samples_per_second = data_manager_->samples_per_second();
   options.stack_dump_size = data_manager_->stack_dump_size();
-  options.unwinding_method =
-      IsDevMode() ? data_manager_->unwinding_method() : CaptureOptions::kDwarf;
+  options.unwinding_method = data_manager_->unwinding_method();
   options.max_local_marker_depth_per_command_buffer =
       data_manager_->max_local_marker_depth_per_command_buffer();
 
