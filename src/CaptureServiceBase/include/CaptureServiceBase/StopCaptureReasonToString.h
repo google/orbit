d401545bd1f7d790159c1f40e45de5dd23178eb3
Tingwei LIU <vickyliu@google.com>
2022-05-06 13:52:34 +0200

    Add StopCaptureReasonToString
    Add a `ToString` method to convert StopCaptureReason to string.
    This will be used in the CloudCollector streamz metrics.

---
+++ src/CaptureServiceBase/include/CaptureServiceBase/StopCaptureReasonToString.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CAPTURE_SERVICE_BASE_STOP_CAPTURE_REASON_TO_STRING_H_
+#define CAPTURE_SERVICE_BASE_STOP_CAPTURE_REASON_TO_STRING_H_
+
+#include <string>
+
+#include "CaptureServiceBase/CaptureServiceBase.h"
+#include "OrbitBase/Logging.h"
+
+namespace orbit_capture_service_base {
+
+[[nodiscard]] std::string StopCaptureReasonToString(
+    const OrbitCaptureBase::StopCaptureReason& stop_capture_reason) {
+  switch (stop_capture_reason) {
+    case StopCaptureReason::kClientStop:
+      return "client_stop";
+    case StopCaptureReason::kMemoryWatchdog:
+      return "memory_watchdog";
+    case StopCaptureReason::kExceededMaxDurationLimit:
+      return "exceeded_max_duration_limit";
+    case StopCaptureReason::kGuestOrcStop:
+      return "guestorc_stop";
+    case StopCaptureReason::kGuestOrcConnectionFailure:
+      return "guestorc_connection_failure";
+  }
+
+  ORBIT_UNREACHABLE();
+}
+
+}  // namespace orbit_capture_service_base
+
+#endif  // CAPTURE_SERVICE_BASE_STOP_CAPTURE_REASON_TO_STRINGE_H_
