43dd5959f9943e474274c9dd0ee73f4e721727de
Valeriy Avanesov <avanesov@google.com>
2022-05-17 19:18:41 +0000

    quotes

--- contrib/automation_tests/orbit_manual_instrumentation.py
+++ contrib/automation_tests/orbit_manual_instrumentation.py
@@ -11,7 +11,7 @@ from test_cases.connection_window import FilterAndSelectFirstProcess, ConnectToS
 from test_cases.capture_window import Capture, CheckTimers, VerifyTracksDoNotExist, VerifyTracksExist, ToggleCollapsedStateOfAllTracks, FilterTracks
 from test_cases.symbols_tab import LoadSymbols
 from test_cases.live_tab import VerifyScopeTypeAndCallCount
-"""Test Orbit's manual instrumentation.
+"""Test Orbit"s manual instrumentation.
 
 Before this script is run there needs to be a gamelet reserved and
 "OrbitTestManualInstrumentation 3 10 100000" has to be started. Note that the 
@@ -32,22 +32,22 @@ This automation script covers the following workflow:
 
 def main(argv):
     tracks = [
-        'DynamicName_0', 'DynamicName_1', 'DynamicName_2', 'DynamicName_3', 'double_var',
-        'float_var', 'int64_var', 'int_var', 'uint64_var', 'uint_var', 'ORBIT_ASYNC_TASKS',
-        'ORBIT_START_ASYNC_TEST'
+        "DynamicName_0", "DynamicName_1", "DynamicName_2", "DynamicName_3", "double_var",
+        "float_var", "int64_var", "int_var", "uint64_var", "uint_var", "ORBIT_ASYNC_TASKS",
+        "ORBIT_START_ASYNC_TEST"
     ]
-    scope_types = ['MA', 'MS', 'MS','MS','MS','MA','MS','MS','MS','MS','MS']
+    scope_types = ["MA", "MS", "MS","MS","MS","MA","MS","MS","MS","MS","MS"]
     scope_names = [
-        'ORBIT_ASYNC_TASKS', 'TestFunc', 'Sleep for two milliseconds', 
-        'Sleeping for two milliseconds with group id', 'ORBIT_START_TEST with group id',
-        'ORBIT_START_ASYNC_TEST', 'ORBIT_START_TEST', 'ORBIT_SCOPE_TEST',
-        'ORBIT_SCOPE_TEST_WITH_COLOR', 'BusyWork', 'TestFunc2'
+        "ORBIT_ASYNC_TASKS", "TestFunc", "Sleep for two milliseconds", 
+        "Sleeping for two milliseconds with group id", "ORBIT_START_TEST with group id",
+        "ORBIT_START_ASYNC_TEST", "ORBIT_START_TEST", "ORBIT_SCOPE_TEST",
+        "ORBIT_SCOPE_TEST_WITH_COLOR", "BusyWork", "TestFunc2"
     ]
 
     test_cases = [
         ConnectToStadiaInstance(),
-        FilterAndSelectFirstProcess(process_filter='OrbitTest'),
-        LoadSymbols(module_search_string='OrbitTest'),
+        FilterAndSelectFirstProcess(process_filter="OrbitTest"),
+        LoadSymbols(module_search_string="OrbitTest"),
         # Take a capture without manual instrumentation and assert that no tracks show up.
         Capture(manual_instrumentation=False),
         VerifyTracksDoNotExist(track_names=tracks),
@@ -55,9 +55,9 @@ def main(argv):
         # the timers in the thread tracks and the scopes under Live tab
         Capture(manual_instrumentation=True),
         VerifyTracksExist(track_names=tracks),
-        FilterTracks(filter_string='OrbitThread_', expected_track_count=3),
+        FilterTracks(filter_string="OrbitThread_", expected_track_count=3),
         ToggleCollapsedStateOfAllTracks(),
-        CheckTimers(track_name_filter='OrbitThread_*')
+        CheckTimers(track_name_filter="OrbitThread_*")
     ] + [ 
         VerifyScopeTypeAndCallCount(scope_name=name, scope_type=type) for name, type in zip(scope_names, scope_types)
     ] + [
@@ -65,7 +65,7 @@ def main(argv):
         Capture(manual_instrumentation=False),
         VerifyTracksDoNotExist(track_names=tracks),
     ]
-    suite = E2ETestSuite(test_name='Manual Instrumentation', test_cases=test_cases)
+    suite = E2ETestSuite(test_name="Manual Instrumentation", test_cases=test_cases)
     suite.execute()
 
 
