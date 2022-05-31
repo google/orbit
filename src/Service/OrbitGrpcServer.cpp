ed834b128b1fd5c04778a5003f5cb2e9b68aaec7
Pierric Gimmig <pierric@google.com>
2022-05-31 09:46:45 -0700

    Set Grpc server-side maximum message size
    Set the Grpc server maximum receive message size to accommodate
    for the unbounded "CaptureOptions" message which can be bigger
    than the default 4'194'304 maximum size.
    
    Tests: select thousands of function to instrument and start a capture.
    Before the change, we would get a popup with a message size error.

--- src/Service/OrbitGrpcServer.cpp
+++ src/Service/OrbitGrpcServer.cpp
@@ -71,6 +71,9 @@ bool OrbitGrpcServerImpl::Init(std::string_view server_address, bool dev_mode) {
 
   grpc::ServerBuilder builder;
 
+  // Increase maximum receive size for unbounded "CaptureOptions" message.
+  builder.SetMaxReceiveMessageSize(std::numeric_limits<int32_t>::max());
+
   builder.AddListeningPort(std::string(server_address), grpc::InsecureServerCredentials());
   builder.RegisterService(&capture_service_);
   builder.RegisterService(&process_service_);
