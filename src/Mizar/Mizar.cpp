9f684454c597981a4ee27cccf103d8ef4f910fd1
Valeriy Avanesov <avanesov@google.com>
2022-06-24 07:56:12 +0000

    Define `SFID` as an alias of `Typedef<Tag, T>`
    Tests: Unit, Manual
    Bug: http://b/237043704

--- src/Mizar/Mizar.cpp
+++ src/Mizar/Mizar.cpp
@@ -102,7 +102,7 @@ int main(int argc, char** argv) {
     const uint64_t comparison_cnt = report.GetComparisonSamplingCounts()->GetExclusiveCount(sfid);
     const double pvalue = report.GetComparisonResult(sfid).pvalue;
     if (pvalue < 0.05) {
-      ORBIT_LOG("%s %.2f %s %u %u", name, pvalue, static_cast<std::string>(sfid), baseline_cnt,
+      ORBIT_LOG("%s %.2f %u %u %u", name, pvalue, *sfid, baseline_cnt,
                 comparison_cnt);
     }
   }
