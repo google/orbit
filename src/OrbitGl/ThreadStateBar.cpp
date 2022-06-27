dec026880ee8a4af75260009436131a8d4e89b32
MahmoodDarwish <mahmooddarwish@google.com>
2022-06-27 20:03:53 +0000

    retrigger checks

--- src/OrbitGl/ThreadStateBar.cpp
+++ src/OrbitGl/ThreadStateBar.cpp
@@ -5,6 +5,7 @@
 #include "ThreadStateBar.h"
 
 #include <absl/strings/str_format.h>
+#include <absl/time/time.h>
 
 #include <memory>
 #include <string>
@@ -15,12 +16,14 @@
 #include "ClientData/CaptureData.h"
 #include "ClientData/ThreadStateSliceInfo.h"
 #include "ClientProtos/capture_data.pb.h"
+#include "CoreMath.h"
 #include "DisplayFormats/DisplayFormats.h"
 #include "Geometry.h"
 #include "GlCanvas.h"
 #include "GlUtils.h"
 #include "GrpcProtos/capture.pb.h"
 #include "OrbitBase/Logging.h"
+#include "PickingManager.h"
 #include "PrimitiveAssembler.h"
 #include "Viewport.h"
 
@@ -268,6 +271,13 @@ void ThreadStateBar::DoUpdatePrimitives(PrimitiveAssembler& primitive_assembler,
 void ThreadStateBar::OnPick(int x, int y) {
   ThreadBar::OnPick(x, y);
   app_->set_selected_thread_id(GetThreadId());
+
+/*
+  Vec2i screen_cords = Vec2i(x, y);
+  float clicked_x = viewport_->ScreenToWorld(screen_cords)[0];
+  uint64_t clicked_timestamp = timeline_info_->GetTickFromWorld(clicked_x);
+  absl::FromUnixSeconds(clicked_timestamp);
+*/
 }
 
 }  // namespace orbit_gl
