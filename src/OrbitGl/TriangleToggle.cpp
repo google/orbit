c186a38f99aec614043db481ac44007f394919c9
Florian Reichl <freichl@google.com>
2022-07-01 12:30:06 +0200

    Change position & size calculation of triangle toggle
    Previously, pos() of the triangle toggle was incorrectly set to the
    midpoint of the triangle, and the calculation did not take width /
    height into account correctly in the collapsed case (with() was always
    assumed to be the baseline of the triangle).

--- src/OrbitGl/TriangleToggle.cpp
+++ src/OrbitGl/TriangleToggle.cpp
@@ -34,31 +34,32 @@ void TriangleToggle::DoDraw(PrimitiveAssembler& primitive_assembler, TextRendere
   Color color = is_collapsible_ ? kWhite : kGrey;
 
   // Draw triangle.
-  static float half_sqrt_three = 0.5f * sqrtf(3.f);
-  float half_triangle_base_width = 0.5f * GetWidth();
-  float half_triangle_height = half_sqrt_three * 0.5f * GetHeight();
-
   const Vec2 pos = GetPos();
   if (!picking) {
-    Vec2 position(pos[0], pos[1]);
+    Vec2 midpoint = GetPos() + Vec2(GetWidth() / 2, GetHeight() / 2);
 
     Triangle triangle;
     if (is_collapsed_) {
-      triangle = Triangle(position + Vec2(-half_triangle_height, half_triangle_base_width),
-                          position + Vec2(-half_triangle_height, -half_triangle_base_width),
-                          position + Vec2(half_triangle_base_width, 0.f));
+      const float triangle_height = sqrtf(3.f) * GetWidth() * 0.5f;
+      const float half_triangle_base_width = 0.5f * GetHeight();
+      const float half_triangle_height = std::ceilf(triangle_height / 2);
+
+      triangle = Triangle(midpoint + Vec2(-half_triangle_height, half_triangle_base_width),
+                          midpoint + Vec2(-half_triangle_height, -half_triangle_base_width),
+                          midpoint + Vec2(half_triangle_height, 0.f));
     } else {
-      triangle = Triangle(position + Vec2(half_triangle_base_width, -half_triangle_height),
-                          position + Vec2(-half_triangle_base_width, -half_triangle_height),
-                          position + Vec2(0.f, half_triangle_base_width));
+      const float triangle_height = sqrtf(3.f) * GetHeight() * 0.5f;
+      const float half_triangle_base_width = 0.5f * GetWidth();
+      const float half_triangle_height = std::ceilf(triangle_height / 2);
+
+      triangle = Triangle(midpoint + Vec2(half_triangle_base_width, -half_triangle_height),
+                          midpoint + Vec2(-half_triangle_base_width, -half_triangle_height),
+                          midpoint + Vec2(0.f, half_triangle_height));
     }
     primitive_assembler.AddTriangle(triangle, z, color, shared_from_this());
   } else {
     // When picking, draw a big square for easier picking.
-    float original_width = 2 * half_triangle_base_width;
-    float large_width = 2 * original_width;
-    Quad box = MakeBox(Vec2(pos[0] - original_width, pos[1] - original_width),
-                       Vec2(large_width, large_width));
+    Quad box = MakeBox(Vec2(pos[0], pos[1]), GetSize());
     primitive_assembler.AddBox(box, z, color, shared_from_this());
   }
 }
