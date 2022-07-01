b136efb8c317da4315c711722306fab84f45c5b8
Florian Reichl <freichl@google.com>
2022-07-01 15:03:16 +0200

    Fixup

--- src/OrbitGl/TriangleToggle.cpp
+++ src/OrbitGl/TriangleToggle.cpp
@@ -34,32 +34,29 @@ void TriangleToggle::DoDraw(PrimitiveAssembler& primitive_assembler, TextRendere
   Color color = is_collapsible_ ? kWhite : kGrey;
 
   // Draw triangle.
-  const Vec2 pos = GetPos();
-  if (!picking) {
-    Vec2 midpoint = GetPos() + Vec2(GetWidth() / 2, GetHeight() / 2);
+  const Vec2 midpoint = GetPos() + Vec2(GetWidth() / 2, GetHeight() / 2);
+  const float triangle_side_length = std::min(GetWidth(), GetHeight());
+  const float triangle_height = triangle_side_length * sqrtf(3.f) * 0.5f;
 
+  const float half_triangle_height = std::ceilf(triangle_height / 2);
+  const float half_triangle_side_length = triangle_side_length / 2;
+
+  if (!picking) {
     Triangle triangle;
     if (is_collapsed_) {
-      const float triangle_height = sqrtf(3.f) * GetWidth() * 0.5f;
-      const float half_triangle_base_width = 0.5f * GetHeight();
-      const float half_triangle_height = std::ceilf(triangle_height / 2);
-
-      triangle = Triangle(midpoint + Vec2(-half_triangle_height, half_triangle_base_width),
-                          midpoint + Vec2(-half_triangle_height, -half_triangle_base_width),
+      triangle = Triangle(midpoint + Vec2(-half_triangle_height, half_triangle_side_length),
+                          midpoint + Vec2(-half_triangle_height, -half_triangle_side_length),
                           midpoint + Vec2(half_triangle_height, 0.f));
     } else {
-      const float triangle_height = sqrtf(3.f) * GetHeight() * 0.5f;
-      const float half_triangle_base_width = 0.5f * GetWidth();
-      const float half_triangle_height = std::ceilf(triangle_height / 2);
-
-      triangle = Triangle(midpoint + Vec2(half_triangle_base_width, -half_triangle_height),
-                          midpoint + Vec2(-half_triangle_base_width, -half_triangle_height),
+      triangle = Triangle(midpoint + Vec2(half_triangle_side_length, -half_triangle_height),
+                          midpoint + Vec2(-half_triangle_side_length, -half_triangle_height),
                           midpoint + Vec2(0.f, half_triangle_height));
     }
     primitive_assembler.AddTriangle(triangle, z, color, shared_from_this());
   } else {
     // When picking, draw a big square for easier picking.
-    Quad box = MakeBox(Vec2(pos[0], pos[1]), GetSize());
+    Quad box = MakeBox(midpoint - Vec2(half_triangle_side_length, half_triangle_side_length),
+                       Vec2(triangle_side_length, triangle_side_length));
     primitive_assembler.AddBox(box, z, color, shared_from_this());
   }
 }
