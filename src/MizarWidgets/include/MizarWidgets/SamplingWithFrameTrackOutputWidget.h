bae675bd1610e40d623acda93623895a60aa0ff3
Valeriy Avanesov <avanesov@google.com>
2022-07-08 16:34:04 +0200

    Implement Sampling-based report visualisation for Mizar (#3909)
    This partially implements visualisation of the report
    based on the sampling and frame track data. Namely, function name,
    exclusive percentage and exclusive active function time per frame
    are shown.
    
    Also, this templates-up SamplingWithFrameTrackComparisonReport.
    
    Tests: Unit, Manual
    Bug: http://b/238307352

---
+++ src/MizarWidgets/include/MizarWidgets/SamplingWithFrameTrackOutputWidget.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MIZAR_WIDGETS_SAMPLING_WITH_FRAME_TRACK_OUTPUT_WIDGET_H_
+#define MIZAR_WIDGETS_SAMPLING_WITH_FRAME_TRACK_OUTPUT_WIDGET_H_
+
+#include <QWidget>
+
+#include "MizarData/SamplingWithFrameTrackComparisonReport.h"
+
+namespace Ui {
+class SamplingWithFrameTrackOutputWidget;
+}
+
+namespace orbit_mizar_widgets {
+
+// The widget handles visualization of the comparison report based on sampling data with frame
+// track.
+class SamplingWithFrameTrackOutputWidget : public QWidget {
+  using Report = ::orbit_mizar_data::SamplingWithFrameTrackComparisonReport;
+
+ public:
+  explicit SamplingWithFrameTrackOutputWidget(QWidget* parent = nullptr);
+  void UpdateReport(Report report);
+
+ protected:
+  void resizeEvent(QResizeEvent* event) override;
+
+ private:
+  void ResizeReportColumns(int width) const;
+
+  std::unique_ptr<Ui::SamplingWithFrameTrackOutputWidget> ui_;
+};
+
+}  // namespace orbit_mizar_widgets
+
+#endif  // MIZAR_WIDGETS_SAMPLING_WITH_FRAME_TRACK_OUTPUT_WIDGET_H_
