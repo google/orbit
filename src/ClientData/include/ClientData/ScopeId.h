a9643b174f703d2e6d4aaffcd2b66f6184988525
Valeriy Avanesov <avanesov@google.com>
2022-07-08 15:18:35 +0000

    style

--- src/ClientData/include/ClientData/ScopeId.h
+++ src/ClientData/include/ClientData/ScopeId.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef ORBIT_CLIENT_DATA_API_SCOPE_ID_H_
 #define ORBIT_CLIENT_DATA_API_SCOPE_ID_H_
 
