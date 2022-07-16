5b44521cb7dee339e160bd1720f8a168030b5cd2
Valeriy Avanesov <avanesov@google.com>
2022-07-16 11:26:34 +0200

    Add tooltips for the input elements of Mizar GUI (#3948)
    Test: Manual
    Bug: http://b/239151213

--- src/MizarWidgets/SamplingWithFrameTrackInputWidget.cpp
+++ src/MizarWidgets/SamplingWithFrameTrackInputWidget.cpp
@@ -5,7 +5,6 @@
 #include "MizarWidgets/SamplingWithFrameTrackInputWidget.h"
 
 #include <absl/strings/str_format.h>
-#include <qcombobox.h>
 
 #include <QComboBox>
 #include <QLineEdit>
@@ -24,6 +23,12 @@ namespace orbit_mizar_widgets {
 SamplingWithFrameTrackInputWidgetBase::SamplingWithFrameTrackInputWidgetBase(QWidget* parent)
     : QWidget(parent), ui_(std::make_unique<Ui::SamplingWithFrameTrackInputWidget>()) {
   ui_->setupUi(this);
+  ui_->start_ms_->setToolTip(
+      QStringLiteral("Time in milliseconds since the capture started.\n"
+                     "Only the portion after this time will be analyzed."));
+  ui_->thread_list_->setToolTip(
+      "Only the sampling data from the selected threads will be analyzed.\n"
+      "Multiple selection is allowed.");
   QObject::connect(GetThreadList(), &QListWidget::itemSelectionChanged, this,
                    &SamplingWithFrameTrackInputWidgetBase::OnThreadSelectionChanged);
   QObject::connect(GetFrameTrackList(), qOverload<int>(&QComboBox::currentIndexChanged), this,
