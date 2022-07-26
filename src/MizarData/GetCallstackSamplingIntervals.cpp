f00741980ccca6eb9b2495d4a72f61a052d51a80
Valeriy Avanesov <avanesov@google.com>
2022-07-26 20:17:12 +0200

    Implement a function collecting intervals between stack samples (#4014)
    The function will be used for data-driven sampling rate estimation.
    
    Bug: http://b/240253740
    Test: Unit

---
+++ src/MizarData/GetCallstackSamplingIntervals.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "MizarData/GetCallstackSamplingIntervals.h"
+
+#include <optional>
+
+#include "MizarBase/ThreadId.h"
+
+namespace orbit_mizar_data {
+
+using ::orbit_client_data::CallstackEvent;
+using ::orbit_mizar_base::TID;
+
+std::vector<uint64_t> GetSamplingIntervalsNs(
+    const absl::flat_hash_set<TID>& tids, uint64_t min_timestamp, uint64_t max_timestamp,
+    const orbit_client_data::CallstackData& callstack_data) {
+  std::vector<uint64_t> result;
+
+  for (const TID tid : tids) {
+    std::optional<uint64_t> previous_timestamp = std::nullopt;
+    callstack_data.ForEachCallstackEventOfTidInTimeRange(
+        *tid, min_timestamp, max_timestamp,
+        [&result, &previous_timestamp](const CallstackEvent& event) {
+          const uint64_t timestamp = event.timestamp_ns();
+          if (previous_timestamp.has_value()) {
+            result.push_back(timestamp - previous_timestamp.value());
+          }
+          previous_timestamp = timestamp;
+        });
+  }
+  return result;
+}
+
+}  // namespace orbit_mizar_data
