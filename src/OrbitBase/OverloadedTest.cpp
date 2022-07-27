ca64b60d1f9c83eb87d7b047784b1dbbf87e8b2a
Valeriy Avanesov <avanesov@google.com>
2022-07-27 10:42:26 +0000

    comments

--- src/OrbitBase/OverloadedTest.cpp
+++ src/OrbitBase/OverloadedTest.cpp
@@ -5,6 +5,8 @@
 #include <gtest/gtest.h>
 #include <stdint.h>
 
+#include <memory>
+
 #include "OrbitBase/Overloaded.h"
 
 namespace orbit_base {
@@ -12,13 +14,12 @@ namespace orbit_base {
 constexpr std::string_view kInt = "int";
 constexpr std::string_view kString = "string";
 
-
-constexpr std::string_view kTwoInts = "two ints"; 
+constexpr std::string_view kTwoInts = "two ints";
 
 constexpr auto kFromInt = [](int /*unused*/) { return kInt; };
 constexpr auto kFromTwoInts = [](int /*unused*/, int /*unused*/) { return kTwoInts; };
 auto kFromIntMutable = [](int /*unused*/) mutable { return kInt; };
-constexpr auto kFromString = [](std::string /*unused*/) { return kString; }; 
+constexpr auto kFromString = [](std::string /*unused*/) { return kString; };
 
 std::string_view FromString(std::string /*unused*/) { return kString; }
 std::string_view FromInt(int /*unused*/) { return kInt; }
@@ -29,11 +30,33 @@ TEST(OverloadedTest, TwoLambdas) {
   EXPECT_EQ(overloaded_lambda("foo"), kString);
 }
 
+constexpr std::string_view kTwoStrings = "two strings";
+
+TEST(OverloadedTest, TwoLambdasOnePolymorphic) {
+  const auto overloaded_lambda =
+      overloaded{[](auto /*unused*/, auto /*unused*/) { return kTwoStrings; }, kFromTwoInts};
+  EXPECT_EQ(overloaded_lambda(1, 1), kTwoInts);
+  EXPECT_EQ(overloaded_lambda("foo", "bar"), kTwoStrings);
+}
+
+TEST(OverloadedTest, StackedOverloaded) {
+  const auto overloaded_lambda = overloaded{overloaded{kFromInt, kFromString}, kFromTwoInts};
+  EXPECT_EQ(overloaded_lambda(1, 1), kTwoInts);
+  EXPECT_EQ(overloaded_lambda(1), kInt);
+  EXPECT_EQ(overloaded_lambda("foo"), kString);
+}
+
+TEST(OverloadedTest, MoveOnlyArgumentLambda) {
+  const auto lambda = [](std::unique_ptr<int> /*unused*/) { return kInt; };
+  const auto overloaded_lambda = overloaded{lambda};
+  auto int_ptr = std::make_unique<int>(1);
+  EXPECT_EQ(overloaded_lambda(std::move(int_ptr)), kInt);
+}
 
 TEST(OverloadedTest, TwoLambdasWithOneAndTwoArguments) {
   const auto overloaded_lambda = overloaded{kFromInt, kFromTwoInts};
   EXPECT_EQ(overloaded_lambda(1), kInt);
-  EXPECT_EQ(overloaded_lambda(1,1), kTwoInts);
+  EXPECT_EQ(overloaded_lambda(1, 1), kTwoInts);
 }
 
 TEST(OverloadedTest, MutableAndImmutableLambdas) {
