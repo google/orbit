4c6090a159bf067cb2972ebb24bdf82087117de4
MahmoodDarwish <mahmooddarwish@google.com>
2022-08-12 11:27:57 +0000

    clang format changes

--- src/LinuxTracing/PerfEventReaders.h
+++ src/LinuxTracing/PerfEventReaders.h
@@ -34,8 +34,8 @@ uint64_t ReadThrottleUnthrottleRecordTime(PerfEventRingBuffer* ring_buffer);
 MmapPerfEvent ConsumeMmapPerfEvent(PerfEventRingBuffer* ring_buffer,
                                    const perf_event_header& header);
 
-UprobesWithStackPerfEvent ConsumeUprobeWithStackPerfEvent(
-    PerfEventRingBuffer* ring_buffer, const perf_event_header& header);
+UprobesWithStackPerfEvent ConsumeUprobeWithStackPerfEvent(PerfEventRingBuffer* ring_buffer,
+                                                          const perf_event_header& header);
 
 StackSamplePerfEvent ConsumeStackSamplePerfEvent(PerfEventRingBuffer* ring_buffer,
                                                  const perf_event_header& header);
