56afe8c19bc706ff6d9c5e0552135a806d781d2f
Pablo Zimmermann <karupayun@google.com>
2022-08-25 10:54:23 +0200

    Move NextPixel free function to the interface (#4117)
    * Move NextPixel free function to the interface
    
    GetNextPixelBoundaryTimeNs free function will be needed for any
    implementation of TimerDataInterface (GetTimersAtDepthDiscretized will
    use it to get the boundary of the pixels), so in this PR we are moving
    this function to the common interface.
    
    I also considered creating an utility file, but I think that it makes
    sense to have it in TimerDataInterface.
    
    * Fixup: Erase nodiscard

---
+++ src/ClientData/TimerDataInterface.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ClientData/TimerDataInterface.h"
+
+namespace orbit_client_data {
+
+uint64_t GetNextPixelBoundaryTimeNs(uint64_t current_timestamp_ns, uint32_t resolution,
+                                    uint64_t start_ns, uint64_t end_ns) {
+  uint64_t current_ns_from_start = current_timestamp_ns - start_ns;
+  uint64_t total_ns = end_ns - start_ns;
+
+  // Given a resolution of 4000 pixels, we can capture for 53 days without overflowing.
+  uint64_t current_pixel = (current_ns_from_start * resolution) / total_ns;
+  uint64_t next_pixel = current_pixel + 1;
+
+  // To calculate the timestamp of a pixel boundary, we round to the left similar to how it works in
+  // other parts of Orbit.
+  uint64_t next_pixel_ns_from_min = total_ns * next_pixel / resolution;
+
+  // Border case when we have a lot of pixels who have the same timestamp (because the number of
+  // pixels is less than the nanoseconds in screen). In this case, as we've already drawn in the
+  // current_timestamp, the next pixel to draw should have the next timestamp.
+  if (next_pixel_ns_from_min == current_ns_from_start) {
+    next_pixel_ns_from_min = current_ns_from_start + 1;
+  }
+
+  return start_ns + next_pixel_ns_from_min;
+}
+
+}  // namespace orbit_client_data
