7319c5ec2c3350547defce92e51d5dca7178511e
Florian Kübler <kuebler@google.com>
2022-08-31 09:55:24 +0200

    Parse PDB's public symbols using LLVM (#4131)
    In a PDB file, public symbols are not a real subset of
    the module info stream's symbols. There are symbols for
    functions (like RtUserThreadStart) that are only present
    in the public symbols.
    
    This change adds support for reading public symbols
    using LLVM.
    
    Unlike DIA, LLVM is not extrapolating the size information
    for the public symbols for us. Therefore, we try to look-up
    the missing size information in the section contribution
    stream. As a fallback, we also try to deduce the
    size information from the distance to the next symbol.
    
    Test: Run test + Check on a couple of Windows dlls.
    Bug: http://b/221968448

--- src/ObjectUtils/PdbFileDiaTest.cpp
+++ src/ObjectUtils/PdbFileDiaTest.cpp
@@ -36,31 +36,3 @@ TEST(PdbFileDiaTest, PdbFileProperlyUninitializesComLibrary) {
   ASSERT_EQ(result, S_OK);
   CoUninitialize();
 }
-
-TEST(PdbFileDiaTest, LoadsFunctionsOnlyInPublicSymbols) {
-  std::filesystem::path file_path_pdb = orbit_test::GetTestdataDir() / "libomp.dll.pdb";
-
-  ErrorMessageOr<std::unique_ptr<PdbFile>> pdb_file_result =
-      PdbFileDia::CreatePdbFile(file_path_pdb, ObjectFileInfo{0});
-  ASSERT_THAT(pdb_file_result, HasNoError());
-  std::unique_ptr<orbit_object_utils::PdbFile> pdb_file = std::move(pdb_file_result.value());
-  auto symbols_result = pdb_file->LoadDebugSymbols();
-  ASSERT_THAT(symbols_result, HasNoError());
-
-  auto symbols = std::move(symbols_result.value());
-
-  absl::flat_hash_map<uint64_t, const SymbolInfo*> symbol_infos_by_address;
-  for (const SymbolInfo& symbol_info : symbols.symbol_infos()) {
-    symbol_infos_by_address.emplace(symbol_info.address(), &symbol_info);
-  }
-
-  ASSERT_EQ(symbol_infos_by_address.size(), 6868);
-
-  {
-    const SymbolInfo* symbol = symbol_infos_by_address[0x0F187B];
-    ASSERT_NE(symbol, nullptr);
-    EXPECT_EQ(symbol->demangled_name(), "FormatMessageW");
-    EXPECT_EQ(symbol->address(), 0xF187B);
-    EXPECT_EQ(symbol->size(), 6);
-  }
-}
