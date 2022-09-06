d44bda47a7ce9a3a968f34bb661dee698938e88d
vickyliu-go4it <74234352+vickyliu-go4it@users.noreply.github.com>
2022-09-06 17:21:19 +0200

    Add StadiaSymbolStoreSymbolProviderTest (#4157)
    * Add MockSymbolCacheInterface
    
    Add MockSymbolCacheInterface that will be used in
    StadiaSymbolStoreSymbolProviderTest.
    
    Bug: http://b/239167821
    
    * Add MockDownloadmanagerInterface
    
    Add MockDownloadmanagerInterface that will be used in
    StadiaSymbolStoreSymbolProviderTest.
    
    Bug: http://b/239167821
    
    * Add StadiaSymbolStoreSymbolProviderTest
    
    Bug: http://b/239167821
    
    * [fixup] Address review comments

---
+++ src/Http/include/Http/MockDownloadManager.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef HTTP_MOCK_DOWNLOAD_MANAGER_H_
+#define HTTP_MOCK_DOWNLOAD_MANAGER_H_
+
+#include <gmock/gmock.h>
+
+#include "Http/DownloadManagerInterface.h"
+
+namespace orbit_http {
+class MockDownloadManager : public DownloadManagerInterface {
+ public:
+  MOCK_METHOD(orbit_base::Future<ErrorMessageOr<orbit_base::CanceledOr<void>>>, Download,
+              (std::string, std::filesystem::path, orbit_base::StopToken), (override));
+};
+}  // namespace orbit_http
+
+#endif  // HTTP_MOCK_DOWNLOAD_MANAGER_H_
---
+++ src/Symbols/include/Symbols/MockSymbolCache.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SYMBOLS_MOCK_SYMBOL_CACHE_H_
+#define SYMBOLS_MOCK_SYMBOL_CACHE_H_
+
+#include <gmock/gmock.h>
+
+#include "Symbols/SymbolCacheInterface.h"
+
+namespace orbit_symbols {
+class MockSymbolCache : public SymbolCacheInterface {
+ public:
+  MOCK_METHOD(std::filesystem::path, GenerateCachedFileName, (const std::filesystem::path&),
+              (const, override));
+};
+}  // namespace orbit_symbols
+
+#endif  // SYMBOLS_MOCK_SYMBOL_CACHE_H_
