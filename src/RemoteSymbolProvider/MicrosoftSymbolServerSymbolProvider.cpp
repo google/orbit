eb80bd0708b77b947495ca23e6907fd41dfb4070
Tingwei LIU <vickyliu@google.com>
2022-09-06 11:13:06 +0200

    Add MicrosoftSymbolServerSymbolProvider
    Add the SymbolProvider implementation for the Microsoft public symbol server.
    
    Bug: http://b/239167727

---
+++ src/RemoteSymbolProvider/MicrosoftSymbolServerSymbolProvider.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "RemoteSymbolProvider/MicrosoftSymbolServerSymbolProvider.h"
+
+#include <absl/strings/substitute.h>
+
+using orbit_base::CanceledOr;
+using orbit_base::Future;
+
+namespace orbit_remote_symbol_provider {
+
+MicrosoftSymbolServerSymbolProvider::MicrosoftSymbolServerSymbolProvider(
+    orbit_symbols::SymbolCacheInterface* symbol_cache,
+    orbit_http::DownloadManagerInterface* download_manager)
+    : symbol_cache_(symbol_cache),
+      download_manager_(download_manager),
+      main_thread_executor_(orbit_qt_utils::MainThreadExecutorImpl::Create()) {
+  ORBIT_CHECK(symbol_cache != nullptr);
+  ORBIT_CHECK(download_manager != nullptr);
+}
+
+std::string MicrosoftSymbolServerSymbolProvider::GetDownloadUrl(
+    const orbit_symbol_provider::ModuleIdentifier& module_id) const {
+  constexpr std::string_view kUrlToSymbolServer = "https://msdl.microsoft.com/download/symbols";
+  std::filesystem::path module_path(module_id.file_path);
+  return absl::Substitute("$0/$1/$2/$1", kUrlToSymbolServer, module_path.filename().string(),
+                          module_id.build_id);
+}
+
+Future<ErrorMessageOr<CanceledOr<std::filesystem::path>>>
+MicrosoftSymbolServerSymbolProvider::RetrieveSymbols(
+    const orbit_symbol_provider::ModuleIdentifier& module_id, orbit_base::StopToken stop_token) {
+  std::filesystem::path save_file_path = symbol_cache_->GenerateCachedFileName(module_id.file_path);
+  std::string url = GetDownloadUrl(module_id);
+
+  return download_manager_->Download(std::move(url), save_file_path, std::move(stop_token))
+      .ThenIfSuccess(main_thread_executor_.get(),
+                     [save_file_path = std::move(save_file_path)](
+                         CanceledOr<void> download_result) -> CanceledOr<std::filesystem::path> {
+                       if (orbit_base::IsCanceled(download_result)) return {orbit_base::Canceled{}};
+                       return CanceledOr<std::filesystem::path>{save_file_path};
+                     });
+}
+
+}  // namespace orbit_remote_symbol_provider
---
+++ src/RemoteSymbolProvider/include/RemoteSymbolProvider/MicrosoftSymbolServerSymbolProvider.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTE_SYMBOL_PROVIDER_MICROSOFT_SYMBOL_SERVER_SYMBOL_PROVIDER_H_
+#define REMOTE_SYMBOL_PROVIDER_MICROSOFT_SYMBOL_SERVER_SYMBOL_PROVIDER_H_
+
+#include "Http/HttpDownloadManager.h"
+#include "QtUtils/MainThreadExecutorImpl.h"
+#include "SymbolProvider/SymbolProvider.h"
+#include "Symbols/SymbolCacheInterface.h"
+
+namespace orbit_remote_symbol_provider {
+
+class MicrosoftSymbolServerSymbolProvider : public orbit_symbol_provider::SymbolProvider {
+ public:
+  explicit MicrosoftSymbolServerSymbolProvider(
+      orbit_symbols::SymbolCacheInterface* symbol_cache,
+      orbit_http::DownloadManagerInterface* download_manager);
+
+  [[nodiscard]] orbit_base::Future<ErrorMessageOr<orbit_base::CanceledOr<std::filesystem::path>>>
+  RetrieveSymbols(const orbit_symbol_provider::ModuleIdentifier& module_id,
+                  orbit_base::StopToken stop_token) override;
+
+ private:
+  [[nodiscard]] std::string GetDownloadUrl(
+      const orbit_symbol_provider::ModuleIdentifier& module_id) const;
+
+  orbit_symbols::SymbolCacheInterface* symbol_cache_;
+  orbit_http::DownloadManagerInterface* download_manager_;
+  std::shared_ptr<orbit_base::MainThreadExecutor> main_thread_executor_;
+};
+
+}  // namespace orbit_remote_symbol_provider
+
+#endif  // REMOTE_SYMBOL_PROVIDER_MICROSOFT_SYMBOL_SERVER_SYMBOL_PROVIDER_H_
