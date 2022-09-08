a7e4279c6b9fc9365b7e61167f6c07e49d99679e
vickyliu-go4it <74234352+vickyliu-go4it@users.noreply.github.com>
2022-09-08 09:48:56 +0200

    Add unit tests for Microsoft symbol provider (#4161)
    * Add unit tests for Microsoft symbol provider
    
    Bug: http://b/239167727
    
    * [fixup] Address review comments

--- src/RemoteSymbolProvider/MicrosoftSymbolServerSymbolProviderTest.cpp
+++ src/RemoteSymbolProvider/MicrosoftSymbolServerSymbolProviderTest.cpp
@@ -13,7 +13,7 @@
 #include "OrbitBase/StopSource.h"
 #include "OrbitGgp/MockClient.h"
 #include "QtUtils/MainThreadExecutorImpl.h"
-#include "RemoteSymbolProvider/StadiaSymbolStoreSymbolProvider.h"
+#include "RemoteSymbolProvider/MicrosoftSymbolServerSymbolProvider.h"
 #include "Symbols/MockSymbolCache.h"
 #include "TestUtils/TestUtils.h"
 
@@ -36,15 +36,14 @@ const std::string kValidModuleName{"valid_module_name"};
 const std::string kValidModuleBuildId{"ABCD12345678"};
 const ModuleIdentifier kValidModuleId{absl::StrFormat("module/path/to/%s", kValidModuleName),
                                       kValidModuleBuildId};
+const std::string kValidModuleDownloadUrl{
+    absl::StrFormat("https://msdl.microsoft.com/download/symbols/%s/%s/%s", kValidModuleName,
+                    kValidModuleBuildId, kValidModuleName)};
 
-const std::string kSymbolsNotFoundMsg{"Symbols not found"};
-const std::string kFailedToDownloadMsg{"Failed to download"};
-const std::string kGgpTimeoutMsg{"Timeout"};
-
-class StadiaSymbolStoreSymbolProviderTest : public testing::Test {
+class MicrosoftSymbolServerSymbolProviderTest : public testing::Test {
  public:
-  StadiaSymbolStoreSymbolProviderTest()
-      : symbol_provider_{&symbol_cache_, &download_manager_, &ggp_client_},
+  MicrosoftSymbolServerSymbolProviderTest()
+      : symbol_provider_{&symbol_cache_, &download_manager_},
         executor_(orbit_qt_utils::MainThreadExecutorImpl::Create()) {
     EXPECT_CALL(symbol_cache_, GenerateCachedFileName)
         .WillRepeatedly([](const std::filesystem::path& module_file_path) {
@@ -53,37 +52,17 @@ class StadiaSymbolStoreSymbolProviderTest : public testing::Test {
         });
   }
 
-  enum class GgpClientState { kWorking, kTimeout };
-  void SetUpGgpClient(GgpClientState ggp_client_state) {
-    EXPECT_CALL(ggp_client_, GetSymbolDownloadInfoAsync(_))
-        .Times(1)
-        .WillOnce([ggp_client_state](const std::vector<SymbolDownloadQuery>& download_queries)
-                      -> Future<ErrorMessageOr<std::vector<SymbolDownloadInfo>>> {
-          // Stadia symbol provider queries only one module each time when making the ggp call.
-          ORBIT_CHECK(download_queries.size() == 1);
-
-          if (ggp_client_state == GgpClientState::kTimeout) return {ErrorMessage{kGgpTimeoutMsg}};
-
-          if (download_queries.front().module_name != kValidModuleName ||
-              download_queries.front().build_id != kValidModuleBuildId) {
-            return {ErrorMessage{kSymbolsNotFoundMsg}};
-          }
-
-          SymbolDownloadInfo download_info;
-          download_info.file_id = QString::fromStdString(
-              absl::StrFormat("symbolFiles/%s/%s", kValidModuleBuildId, kValidModuleName));
-          download_info.url = "valid_url_for_symbol";
-          return {std::vector<SymbolDownloadInfo>{download_info}};
-        });
-  }
-
   enum class DownloadResultState { kSuccess, kCanceled, kError };
-  void SetUpDownloadManager(DownloadResultState expected_state, std::string error_msg = "") {
+  void SetUpDownloadManager(DownloadResultState expected_state, std::string expected_url,
+                            std::string error_msg = "") {
     EXPECT_CALL(download_manager_, Download)
         .Times(1)
-        .WillOnce([expected_state, error_msg = std::move(error_msg)](
-                      std::string /*url*/, std::filesystem::path /*save_file_path*/,
+        .WillOnce([expected_state, expected_url = std::move(expected_url),
+                   error_msg = std::move(error_msg)](
+                      std::string url, std::filesystem::path /*save_file_path*/,
                       orbit_base::StopToken /*token*/) -> Future<ErrorMessageOr<CanceledOr<void>>> {
+          EXPECT_EQ(url, expected_url);
+
           switch (expected_state) {
             case DownloadResultState::kSuccess:
               return {outcome::success()};
@@ -99,18 +78,16 @@ class StadiaSymbolStoreSymbolProviderTest : public testing::Test {
 
  protected:
   orbit_symbols::MockSymbolCache symbol_cache_;
-  StadiaSymbolStoreSymbolProvider symbol_provider_;
+  MicrosoftSymbolServerSymbolProvider symbol_provider_;
   std::shared_ptr<orbit_qt_utils::MainThreadExecutorImpl> executor_;
 
  private:
   orbit_http::MockDownloadManager download_manager_;
-  orbit_ggp::MockClient ggp_client_;
 };
 }  // namespace
 
-TEST_F(StadiaSymbolStoreSymbolProviderTest, RetrieveModuleSuccess) {
-  SetUpGgpClient(GgpClientState::kWorking);
-  SetUpDownloadManager(DownloadResultState::kSuccess);
+TEST_F(MicrosoftSymbolServerSymbolProviderTest, RetrieveModuleSuccess) {
+  SetUpDownloadManager(DownloadResultState::kSuccess, kValidModuleDownloadUrl);
 
   orbit_base::StopSource stop_source{};
   symbol_provider_.RetrieveSymbols(kValidModuleId, stop_source.GetStopToken())
@@ -126,9 +103,8 @@ TEST_F(StadiaSymbolStoreSymbolProviderTest, RetrieveModuleSuccess) {
   QCoreApplication::exec();
 }
 
-TEST_F(StadiaSymbolStoreSymbolProviderTest, RetrieveModuleCanceled) {
-  SetUpGgpClient(GgpClientState::kWorking);
-  SetUpDownloadManager(DownloadResultState::kCanceled);
+TEST_F(MicrosoftSymbolServerSymbolProviderTest, RetrieveModuleCanceled) {
+  SetUpDownloadManager(DownloadResultState::kCanceled, kValidModuleDownloadUrl);
 
   // Here we use the mock downloader manager rather than the stop token to simulate the canceled
   // case.
@@ -144,43 +120,18 @@ TEST_F(StadiaSymbolStoreSymbolProviderTest, RetrieveModuleCanceled) {
   QCoreApplication::exec();
 }
 
-TEST_F(StadiaSymbolStoreSymbolProviderTest, RetrieveModuleDownloadError) {
-  SetUpGgpClient(GgpClientState::kWorking);
-  SetUpDownloadManager(DownloadResultState::kError, kFailedToDownloadMsg);
-
-  orbit_base::StopSource stop_source{};
-  symbol_provider_.RetrieveSymbols(kValidModuleId, stop_source.GetStopToken())
-      .Then(executor_.get(), [](ErrorMessageOr<CanceledOr<std::filesystem::path>> result) {
-        EXPECT_THAT(result, HasError(kFailedToDownloadMsg));
-
-        QCoreApplication::exit();
-      });
-
-  QCoreApplication::exec();
-}
-
-TEST_F(StadiaSymbolStoreSymbolProviderTest, RetrieveModuleNotFound) {
-  SetUpGgpClient(GgpClientState::kWorking);
-
+TEST_F(MicrosoftSymbolServerSymbolProviderTest, RetrieveModuleNotFound) {
   const ModuleIdentifier module_id{"module/path/to/some_module_name", "some_build_id"};
+  const std::string expected_url{
+      "https://msdl.microsoft.com/download/symbols/some_module_name/some_build_id/"
+      "some_module_name"};
+  const std::string error_msg{"Symbols not found"};
+  SetUpDownloadManager(DownloadResultState::kError, expected_url, error_msg);
+
   orbit_base::StopSource stop_source{};
   symbol_provider_.RetrieveSymbols(module_id, stop_source.GetStopToken())
-      .Then(executor_.get(), [](ErrorMessageOr<CanceledOr<std::filesystem::path>> result) {
-        EXPECT_THAT(result, HasError(kSymbolsNotFoundMsg));
-
-        QCoreApplication::exit();
-      });
-
-  QCoreApplication::exec();
-}
-
-TEST_F(StadiaSymbolStoreSymbolProviderTest, RetrieveModuleTimeout) {
-  SetUpGgpClient(GgpClientState::kTimeout);
-
-  orbit_base::StopSource stop_source{};
-  symbol_provider_.RetrieveSymbols(kValidModuleId, stop_source.GetStopToken())
-      .Then(executor_.get(), [](ErrorMessageOr<CanceledOr<std::filesystem::path>> result) {
-        EXPECT_THAT(result, HasError(kGgpTimeoutMsg));
+      .Then(executor_.get(), [error_msg](ErrorMessageOr<CanceledOr<std::filesystem::path>> result) {
+        EXPECT_THAT(result, HasError(error_msg));
 
         QCoreApplication::exit();
       });
