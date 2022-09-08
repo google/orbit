d7f76b66410ffabf040d42e018a72ada460d261d
vickyliu-go4it <74234352+vickyliu-go4it@users.noreply.github.com>
2022-09-08 10:47:59 +0200

    Update src/Http/include/Http/DownloadManager.h
    Co-authored-by: dpallotti <58685940+dpallotti@users.noreply.github.com>

--- src/Http/include/Http/DownloadManager.h
+++ src/Http/include/Http/DownloadManager.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef HTTP__DOWNLOAD_MANAGER_H_
-#define HTTP__DOWNLOAD_MANAGER_H_
+#ifndef HTTP_DOWNLOAD_MANAGER_H_
+#define HTTP_DOWNLOAD_MANAGER_H_
 
 #include "OrbitBase/CanceledOr.h"
 #include "OrbitBase/Result.h"
