5ad774b830302d0481efb71a5dd9b321307dbb0c
Pablo Zimmermann <karupayun@google.com>
2022-10-11 13:16:00 +0000

    Create a utility class to iterate over pixels in ClientData
    This PR is just creating a utility class. Later it will include
    conversion between timestamps and position.
    
    It will be used for TimerData, ScopeTreeTimerData and CallstackData to
    iterate over pixels instead of going through all events.

---
+++ src/ClientData/include/ClientData/Utils.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CLIENT_DATA_UTILS_H_
+#define CLIENT_DATA_UTILS_H_
+
+namespace orbit_client_data {
+
+// Free Function that will be used for any implementation of
+// TimerDataInterface::GetTimersAtDepthDiscretized to get the next pixel timestamp. The query
+// assumes a closed-open interval [start_ns, end_ns), so end_ns is not a visible timestamp.
+[[nodiscard]] uint64_t GetNextPixelBoundaryTimeNs(uint64_t current_timestamp_ns,
+                                                  uint32_t resolution, uint64_t start_ns,
+                                                  uint64_t end_ns);
+
+}  // namespace orbit_client_data
+
+#endif  // CLIENT_DATA_UTILS_H_
