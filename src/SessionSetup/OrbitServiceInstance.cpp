08795a273b5452ea7703c23a3422d5cbcfe5523a
antonrohr <antonrohr@google.com>
2022-10-28 11:05:37 +0200

    Add SessionSetup::OrbitServiceInstance class (#4316)
    Add SessionSetup::OrbitServiceInstance class

---
+++ src/SessionSetup/OrbitServiceInstance.cpp
@@ -0,0 +1,138 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "SessionSetup/OrbitServiceInstance.h"
+
+#include <absl/strings/str_format.h>
+
+#include <QCoreApplication>
+#include <QMetaEnum>
+#include <QProcess>
+#include <filesystem>
+#include <memory>
+#include <string>
+
+#include "OrbitBase/Logging.h"
+#include "OrbitBase/Result.h"
+
+namespace orbit_session_setup {
+
+constexpr int kWaitTimeInMilliSeconds = 2000;
+
+class OrbitServiceInstanceImpl : public OrbitServiceInstance {
+ public:
+  explicit OrbitServiceInstanceImpl(const QString& program, const QStringList& arguments);
+  ~OrbitServiceInstanceImpl() override;
+
+  [[nodiscard]] bool IsRunning() const override;
+  [[nodiscard]] ErrorMessageOr<void> Shutdown() override;
+
+  [[nodiscard]] ErrorMessageOr<void> Start();
+
+ private:
+  [[nodiscard]] std::string ReadStateErrorAndOutput();
+
+  QProcess process_;
+  QMetaObject::Connection finished_connection_;
+};
+
+ErrorMessageOr<std::unique_ptr<OrbitServiceInstance>> OrbitServiceInstance::Create(
+    const QString& program, const QStringList& arguments) {
+  auto instance = std::make_unique<OrbitServiceInstanceImpl>(program, arguments);
+  OUTCOME_TRY(instance->Start());
+
+  return std::move(instance);
+}
+
+ErrorMessageOr<std::unique_ptr<OrbitServiceInstance>> OrbitServiceInstance::CreatePrivileged() {
+  const QString orbit_service_path = QCoreApplication::applicationDirPath() + "/OrbitService";
+  return OrbitServiceInstance::Create("pkexec", {orbit_service_path});
+}
+
+OrbitServiceInstanceImpl::OrbitServiceInstanceImpl(const QString& program,
+                                                   const QStringList& arguments) {
+  process_.setProgram(program);
+  process_.setArguments(arguments);
+}
+
+OrbitServiceInstanceImpl::~OrbitServiceInstanceImpl() {
+  if (!IsRunning()) return;
+
+  ErrorMessageOr<void> shutdown_result = Shutdown();
+
+  if (shutdown_result.has_error()) {
+    ORBIT_ERROR("OrbitService shutdown error: %s", shutdown_result.error().message());
+  }
+}
+
+bool OrbitServiceInstanceImpl::IsRunning() const {
+  return process_.state() == QProcess::ProcessState::Running;
+}
+
+ErrorMessageOr<void> OrbitServiceInstanceImpl::Shutdown() {
+  if (!IsRunning()) {
+    return ErrorMessage{"Unable to shutdown OrbitService, process is not running."};
+  }
+
+  QObject::disconnect(finished_connection_);
+
+  // This sends EOF, which signals to OrbitService to shut itself down.
+  process_.closeWriteChannel();
+
+  bool wait_finished_result = process_.waitForFinished(kWaitTimeInMilliSeconds);
+  if (!wait_finished_result) {
+    return ErrorMessage{"The OrbitService shutdown attempt failed."};
+  }
+  return outcome::success();
+}
+
+ErrorMessageOr<void> OrbitServiceInstanceImpl::Start() {
+  if (IsRunning()) {
+    return ErrorMessage{"Unable to start OrbitService, process is already running."};
+  }
+
+  process_.start();
+
+  bool wait_started_result = process_.waitForStarted(kWaitTimeInMilliSeconds);
+  if (!wait_started_result || !IsRunning()) {
+    return ErrorMessage{
+        absl::StrFormat("Unable to start OrbitService. Details:\n%s", ReadStateErrorAndOutput())};
+  }
+
+  QObject::connect(&process_, &QProcess::errorOccurred, this,
+                   [this](const QProcess::ProcessError& error) {
+                     emit ErrorOccurred(
+                         QString("OrbitService process error occurred, description: %1")
+                             .arg(QMetaEnum::fromType<QProcess::ProcessError>().valueToKey(error)));
+                   });
+
+  finished_connection_ = QObject::connect(
+      &process_, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), this,
+      [this](int exit_code, QProcess::ExitStatus exit_status) {
+        // If the process crashed, QProcess::errorOccurred is emitted. Hence a crash does not need
+        // to be handled here.
+        if (exit_status == QProcess::CrashExit) return;
+
+        emit ErrorOccurred(
+            QString("OrbitService process ended unexpectedly. exit code: %1").arg(exit_code));
+      });
+
+  QObject::connect(&process_, &QProcess::readyReadStandardOutput, this,
+                   [this]() { ORBIT_LOG("%s", process_.readAllStandardOutput().toStdString()); });
+
+  QObject::connect(&process_, &QProcess::readyReadStandardError, this,
+                   [this]() { ORBIT_LOG("%s", process_.readAllStandardError().toStdString()); });
+
+  return outcome::success();
+}
+
+std::string OrbitServiceInstanceImpl::ReadStateErrorAndOutput() {
+  return absl::StrFormat("Process state: %s\nProcess error: %s\nstdout: %s\nstderr: %s\n",
+                         QMetaEnum::fromType<QProcess::ProcessState>().valueToKey(process_.state()),
+                         QMetaEnum::fromType<QProcess::ProcessError>().valueToKey(process_.error()),
+                         process_.readAllStandardOutput().toStdString(),
+                         process_.readAllStandardError().toStdString());
+}
+
+}  // namespace orbit_session_setup
---
+++ src/SessionSetup/include/SessionSetup/OrbitServiceInstance.h
@@ -0,0 +1,43 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SESSION_SETUP_ORBIT_SERVICE_INSTANCE_H_
+#define SESSION_SETUP_ORBIT_SERVICE_INSTANCE_H_
+
+#include <QObject>
+#include <QString>
+#include <filesystem>
+#include <memory>
+
+#include "OrbitBase/Result.h"
+
+namespace orbit_session_setup {
+
+// Abstracts a continuously running QProcess for using OrbitService on the client.
+class OrbitServiceInstance : public QObject {
+  Q_OBJECT;
+
+ public:
+  virtual ~OrbitServiceInstance() = default;
+  [[nodiscard]] virtual bool IsRunning() const = 0;
+  // Sends EOF to OrbitService and blocks until OrbitService ended.
+  [[nodiscard]] virtual ErrorMessageOr<void> Shutdown() = 0;
+
+  // Starts an OrbitService that is located next to client executable via pkexec. Blocks until
+  // pkexec has started.
+  [[nodiscard]] static ErrorMessageOr<std::unique_ptr<OrbitServiceInstance>> CreatePrivileged();
+  // Creates and starts `program` with `arguments`. Used for testing. Blocks until `program` has
+  // started.
+  [[nodiscard]] static ErrorMessageOr<std::unique_ptr<OrbitServiceInstance>> Create(
+      const QString& program, const QStringList& arguments);
+
+ signals:
+  // ErrorOccurred is emitted when an error with the process occurred _or_ when the process ends
+  // unexpected.
+  void ErrorOccurred(QString message);
+};
+
+}  // namespace orbit_session_setup
+
+#endif  // SESSION_SETUP_ORBIT_SERVICE_INSTANCE_H_
