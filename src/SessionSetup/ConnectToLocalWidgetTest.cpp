446610f1f85128b5f590f33767e8bb35bda63d7e
antonrohr <antonrohr@google.com>
2022-11-10 15:59:59 +0000

    Add ConnectToLocalWidget
    This adds a widget for connecting to OrbitService and starting
    OrbitService (if its not done manually). The ConnectToLocalWidgetTest
    tests some functionality, but is more of a smoke test, since the grpc
    connections can't be mocked currently.

---
+++ src/SessionSetup/ConnectToLocalWidgetTest.cpp
@@ -0,0 +1,118 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gmock/gmock-spec-builders.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <QFrame>
+#include <QLabel>
+#include <QMessageBox>
+#include <QMetaObject>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QSignalSpy>
+#include <QTest>
+#include <memory>
+
+#include "SessionSetup/ConnectToLocalWidget.h"
+#include "SessionSetup/OrbitServiceInstance.h"
+
+namespace orbit_session_setup {
+
+class MockOrbitServiceInstance : public OrbitServiceInstance {
+ public:
+  MOCK_METHOD(bool, IsRunning, (), (override, const));
+  MOCK_METHOD(ErrorMessageOr<void>, Shutdown, (), (override));
+};
+
+class ConnectToLocalWidgetTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    start_orbit_service_button_ = widget_.findChild<QPushButton*>("startOrbitServiceButton");
+    ASSERT_NE(start_orbit_service_button_, nullptr);
+    status_label_ = widget_.findChild<QLabel*>("statusLabel");
+    ASSERT_NE(status_label_, nullptr);
+    content_frame_ = widget_.findChild<QFrame*>("contentFrame");
+    ASSERT_NE(content_frame_, nullptr);
+    radio_button_ = widget_.findChild<QRadioButton*>("radioButton");
+    ASSERT_NE(radio_button_, nullptr);
+
+    EXPECT_FALSE(start_orbit_service_button_->isEnabled());
+  }
+
+ protected:
+  ConnectToLocalWidget widget_;
+  QPushButton* start_orbit_service_button_;
+  QLabel* status_label_;
+  QFrame* content_frame_;
+  QRadioButton* radio_button_;
+};
+
+TEST_F(ConnectToLocalWidgetTest, IsActiveSetActive) {
+  EXPECT_TRUE(widget_.IsActive());
+  EXPECT_TRUE(content_frame_->isEnabled());
+  EXPECT_TRUE(radio_button_->isEnabled());
+  EXPECT_TRUE(radio_button_->isChecked());
+
+  widget_.SetActive(false);
+  EXPECT_FALSE(content_frame_->isEnabled());
+  EXPECT_TRUE(radio_button_->isEnabled());
+  EXPECT_FALSE(radio_button_->isChecked());
+
+  QSignalSpy spy(&widget_, &ConnectToLocalWidget::Activated);
+
+  QTest::mouseClick(radio_button_, Qt::LeftButton);
+  QCoreApplication::processEvents();
+  EXPECT_EQ(spy.count(), 1);
+}
+
+TEST_F(ConnectToLocalWidgetTest, OrbitServiceStartedSuccessfullyThenStopped) {
+  bool lambda_called = false;
+  widget_.SetOrbitServiceInstanceCreateFunction(
+      [&lambda_called]() -> ErrorMessageOr<std::unique_ptr<OrbitServiceInstance>> {
+        lambda_called = true;
+        return std::make_unique<MockOrbitServiceInstance>();
+      });
+
+  EXPECT_TRUE(start_orbit_service_button_->isEnabled());
+
+  constexpr int kWaitTime = 500;  // Double of the timer interval
+
+  QTest::qWait(kWaitTime);
+  EXPECT_EQ(status_label_->text(), "Waiting for OrbitService");
+
+  QTest::mouseClick(start_orbit_service_button_, Qt::LeftButton);
+
+  EXPECT_TRUE(lambda_called);
+
+  QTest::qWait(kWaitTime);
+  EXPECT_EQ(status_label_->text(), "Connecting... to OrbitService");
+}
+
+TEST_F(ConnectToLocalWidgetTest, OrbitServiceStartError) {
+  bool lambda_called = false;
+  widget_.SetOrbitServiceInstanceCreateFunction(
+      [&lambda_called]() -> ErrorMessageOr<std::unique_ptr<OrbitServiceInstance>> {
+        lambda_called = true;
+        return ErrorMessage{"error"};
+      });
+
+  EXPECT_TRUE(start_orbit_service_button_->isEnabled());
+
+  QMetaObject::invokeMethod(
+      &widget_,
+      [&]() {
+        auto* message_box = widget_.findChild<QMessageBox*>();
+        ASSERT_NE(message_box, nullptr);
+        message_box->close();
+      },
+      Qt::QueuedConnection);
+
+  QTest::mouseClick(start_orbit_service_button_, Qt::LeftButton);
+
+  EXPECT_TRUE(lambda_called);
+}
+
+}  // namespace orbit_session_setup
---
+++ src/SessionSetup/include/SessionSetup/ConnectToLocalWidget.h
@@ -0,0 +1,71 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SESSION_SETUP_CONNECT_TO_LOCAL_WIDGET_H_
+#define SESSION_SETUP_CONNECT_TO_LOCAL_WIDGET_H_
+
+#include <grpcpp/channel.h>
+
+#include <QWidget>
+#include <functional>
+#include <memory>
+
+#include "GrpcProtos/process.pb.h"
+#include "OrbitBase/Result.h"
+#include "SessionSetup/Connections.h"
+#include "SessionSetup/OrbitServiceInstance.h"
+
+namespace Ui {
+class ConnectToLocalWidget;
+}
+
+namespace orbit_session_setup {
+
+using OrbitServiceInstanceCreator =
+    std::function<ErrorMessageOr<std::unique_ptr<OrbitServiceInstance>>()>;
+
+// ConnectToLocalWidget provides a UI to connect to a running OrbitService and start an OrbitService
+// instance. ConnectToLocalWidget sets up a gRPC channel to localhost with port `--grpc_port` and
+// checks every 250ms whether this channel is connected to OrbitService. If the user clicks the
+// "Start OrbitService" button, a OrbitServiceInstance is set up, to which the gRPC channel can
+// connect to.
+class ConnectToLocalWidget : public QWidget {
+  Q_OBJECT
+  Q_PROPERTY(bool active READ IsActive WRITE SetActive)
+
+ public:
+  explicit ConnectToLocalWidget(QWidget* parent = nullptr);
+  ~ConnectToLocalWidget() override;
+
+  [[nodiscard]] bool IsActive() const;
+
+  void SetOrbitServiceInstanceCreateFunction(OrbitServiceInstanceCreator&& creator);
+
+  void SetConnection(LocalConnection&& connection) { local_connection_ = std::move(connection); }
+  [[nodiscard]] LocalConnection&& TakeConnection() { return std::move(local_connection_); }
+
+  [[nodiscard]] const std::shared_ptr<grpc::Channel>& GetGrpcChannel() const {
+    return local_connection_.GetGrpcChannel();
+  }
+
+ public slots:
+  void SetActive(bool value);
+
+ signals:
+  void Activated();
+  void Connected();
+  void Disconnected();
+
+ private:
+  std::unique_ptr<Ui::ConnectToLocalWidget> ui_;
+  OrbitServiceInstanceCreator orbit_service_instance_creator_ = nullptr;
+  LocalConnection local_connection_;
+  QTimer check_connection_timer_;
+
+  void OnStartOrbitServiceButtonClicked();
+};
+
+}  // namespace orbit_session_setup
+
+#endif  // SESSION_SETUP_CONNECT_TO_LOCAL_WIDGET_H_
