google/orbit

commit 4b3867c716dfbe528d54c8491544a56aa6822ae8

Create tests and optimize rendering of AllThreadsCallstackBar (#4296)
* Create tests and optimize rendering of AllThreadsCallstackBar

In this PR we are optimizing the rendering of AllThreadsCallstackBar.

In the previous PR (https://github.com/google/orbit/pull/4271), we skip going through and rendering events from the same pixel in a particular thread_id but we were still going once per pixel per thread_id. In this PR we are still iterating through pixels but stopping looking for new callstacks when we have already found some on the current_pixel.

For getting the pixel number I created a method in FastRenderingUtils class, but I also used in CallstackThreadBar since is way more precise. Ideally we should fix these functions (http://b/242973688), but I won't work on that for now. It also makes sense to use GetPixelNumber when we want to render vertical lines.

I found this issue when creating tests for CallstackData (there were no tests before for this method but I should have created them anyway), so I'm including the tests here as well.

Rendering all_thread Track for a 5 min-capture takes:
- Before (https://github.com/google/orbit/pull/4271) - >150ms
- After (https://github.com/google/orbit/pull/4271) - ~13ms
- Now - ~0.950ms

Rendering callstacks for all Tracks takes 8.6% instead of 11.9% (and before https://github.com/google/orbit/pull/4271 - 66%) . I think that this is enough for our goal.

Test: Load a capture, rendering looks fine. Run Unit tests.
Bugs: http://b/252998254 (GetPixelNumber) & http://b/253414641 (Callstack optimization itself)

* Fixup: Davide's comments

* Fixup: Big refactor based on comments

* Fixup: kAllEvents

* Fixup: Follow C++ recommendations

src/OrbitGl/CallstackThreadBar.cpp
@@ -15,6 +15,7 @@
 #include "ClientData/CallstackInfo.h"
 #include "ClientData/CallstackType.h"
 #include "ClientData/CaptureData.h"
+#include "ClientData/FastRenderingUtils.h"
 #include "FormatCallstackForTooltip.h"
 #include "Geometry.h"
 #include "GlCanvas.h"
@@ -31,6 +32,7 @@ using orbit_client_data::CallstackEvent;
 using orbit_client_data::CallstackInfo;
 using orbit_client_data::CallstackType;
 using orbit_client_data::CaptureData;
+using orbit_client_data::GetPixelNumber;
 using orbit_client_data::ThreadID;
 
 namespace orbit_gl {
@@ -116,7 +118,8 @@ void CallstackThreadBar::DoUpdatePrimitives(PrimitiveAssembler& primitive_assemb
     auto action_on_callstack_events = [&](const CallstackEvent& event) {
       const uint64_t time = event.timestamp_ns();
       ORBIT_CHECK(time >= min_tick && time <= max_tick);
-      Vec2 pos(timeline_info_->GetWorldFromTick(time), GetPos()[1]);
+      const Vec2 pos(GetPixelNumber(event.timestamp_ns(), resolution_in_pixels, min_tick, max_tick),
+                     GetPos()[1]);
       Color color = kWhite;
       if (capture_data_->GetCallstackData().GetCallstack(event.callstack_id())->type() !=
           CallstackType::kComplete) {
@@ -137,7 +140,8 @@ void CallstackThreadBar::DoUpdatePrimitives(PrimitiveAssembler& primitive_assemb
     auto action_on_selected_callstack_events = [&](const CallstackEvent& event) {
       const uint64_t time = event.timestamp_ns();
       ORBIT_CHECK(time >= min_tick && time <= max_tick);
-      Vec2 pos(timeline_info_->GetWorldFromTick(event.timestamp_ns()), GetPos()[1]);
+      const Vec2 pos(GetPixelNumber(event.timestamp_ns(), resolution_in_pixels, min_tick, max_tick),
+                     GetPos()[1]);
       primitive_assembler.AddVerticalLine(pos, track_height, z, kGreenSelection);
     };
     const orbit_client_data::CallstackData& selection_callstack_data =
@@ -159,8 +163,10 @@ void CallstackThreadBar::DoUpdatePrimitives(PrimitiveAssembler& primitive_assemb
     auto action_on_callstack_events = [&, this](const CallstackEvent& event) {
       const uint64_t time = event.timestamp_ns();
       ORBIT_CHECK(time >= min_tick && time <= max_tick);
-      Vec2 pos(timeline_info_->GetWorldFromTick(time) - kPickingBoxOffset, GetPos()[1]);
-      Vec2 size(kPickingBoxWidth, track_height);
+      const Vec2 pos(
+          GetPixelNumber(time, resolution_in_pixels, min_tick, max_tick) - kPickingBoxOffset,
+          GetPos()[1]);
+      const Vec2 size(kPickingBoxWidth, track_height);
       auto user_data = std::make_unique<PickingUserData>(
           nullptr, [this, &primitive_assembler](PickingId id) -> std::string {
             return GetSampleTooltip(primitive_assembler, id);

src/ClientData/CallstackDataTest.cpp
@@ -14,12 +14,21 @@
 #include "ClientData/CallstackEvent.h"
 #include "ClientData/CallstackInfo.h"
 #include "ClientData/CallstackType.h"
+#include "absl/functional/bind_front.h"
+
+using ::testing::AnyOfArray;
+using ::testing::Pointwise;
+using ::testing::TestParamInfo;
+using ::testing::TestWithParam;
+using ::testing::ValuesIn;
 
 using orbit_client_data::CallstackEvent;
 using orbit_client_data::CallstackInfo;
 using orbit_client_data::CallstackType;
 
 namespace orbit_client_data {
+
+namespace {
 
 MATCHER(CallstackEventEq, "") {
   const CallstackEvent& a = std::get<0>(arg);
@@ -111,39 +120,40 @@ TEST(CallstackData, FilterCallstackEventsBasedOnMajorityStart) {
 
   EXPECT_THAT(callstack_data.GetCallstackEventsOfTidInTimeRange(
                   tid, 0, std::numeric_limits<uint64_t>::max()),
-              testing::Pointwise(CallstackEventEq(), std::vector<CallstackEvent>{
-                                                         event1, event2, event3, event4, event5}));
+              Pointwise(CallstackEventEq(),
+                        std::vector<CallstackEvent>{event1, event2, event3, event4, event5}));
 
   EXPECT_THAT(callstack_data.GetCallstackEventsOfTidInTimeRange(
                   tid_with_no_complete, 0, std::numeric_limits<uint64_t>::max()),
-              testing::Pointwise(CallstackEventEq(), std::vector<CallstackEvent>{event6, event7}));
+              Pointwise(CallstackEventEq(), std::vector<CallstackEvent>{event6, event7}));
 
-  EXPECT_THAT(
-      callstack_data.GetCallstackEventsOfTidInTimeRange(tid_without_supermajority, 0,
-                                                        std::numeric_limits<uint64_t>::max()),
-      testing::Pointwise(CallstackEventEq(), std::vector<CallstackEvent>{event8, event9, event10}));
+  EXPECT_THAT(callstack_data.GetCallstackEventsOfTidInTimeRange(
+                  tid_without_supermajority, 0, std::numeric_limits<uint64_t>::max()),
+              Pointwise(CallstackEventEq(), std::vector<CallstackEvent>{event8, event9, event10}));
 }
+
+constexpr uint32_t kTid = 42;
+constexpr uint32_t kAnotherTid = 43;
+
+static inline constexpr uint64_t kCallstackId1 = 12;
+constexpr uint64_t kCallstackId2 = 13;
+
+constexpr uint64_t kCloneAddress = 0x10;
+constexpr uint64_t kBrokenAddress = 0x30;
+constexpr uint64_t kFunctionToStopUnwindingAtAddress = 0x40;
 
 TEST(CallstackData, FilterCallstackEventsBasedOnMajorityStartExcludesFunctionToStopUnwindingAt) {
   CallstackData callstack_data;
 
-  constexpr uint32_t kTid = 42;
-
-  constexpr uint64_t kCloneAddress = 0x10;
-  constexpr uint64_t kBrokenAddress = 0x30;
-  constexpr uint64_t kFunctionToStopUnwindingAtAddress = 0x40;
-
-  const uint64_t cs1_id = 12;
   const uint64_t cs1_outer = kCloneAddress;
   const uint64_t cs1_inner = 0x11;
   CallstackInfo cs1{{cs1_inner, cs1_outer}, CallstackType::kComplete};
-  callstack_data.AddUniqueCallstack(cs1_id, std::move(cs1));
+  callstack_data.AddUniqueCallstack(kCallstackId1, std::move(cs1));
 
-  const uint64_t cs2_id = 13;
   const uint64_t cs2_outer = kCloneAddress;
   const uint64_t cs2_inner = 0x21;
   CallstackInfo cs2{{cs2_inner, cs2_outer}, CallstackType::kComplete};
-  callstack_data.AddUniqueCallstack(cs2_id, std::move(cs2));
+  callstack_data.AddUniqueCallstack(kCallstackId2, std::move(cs2));
 
   const uint64_t broken_cs_id = 81;
   const uint64_t broken_cs_outer = kBrokenAddress;
@@ -161,7 +171,7 @@ TEST(CallstackData, FilterCallstackEventsBasedOnMajorityStartExcludesFunctionToS
                                     std::move(function_to_stop_unwinding_at_cs));
 
   const uint64_t time1 = 142;
-  CallstackEvent event1{time1, cs1_id, kTid};
+  CallstackEvent event1{time1, kCallstackId1, kTid};
   callstack_data.AddCallstackEvent(event1);
 
   const uint64_t time2 = 242;
@@ -169,11 +179,11 @@ TEST(CallstackData, FilterCallstackEventsBasedOnMajorityStartExcludesFunctionToS
   callstack_data.AddCallstackEvent(event2);
 
   const uint64_t time3 = 342;
-  CallstackEvent event3{time3, cs2_id, kTid};
+  CallstackEvent event3{time3, kCallstackId2, kTid};
   callstack_data.AddCallstackEvent(event3);
 
   const uint64_t time4 = 442;
-  CallstackEvent event4{time4, cs1_id, kTid};
+  CallstackEvent event4{time4, kCallstackId1, kTid};
   callstack_data.AddCallstackEvent(event4);
 
   const uint64_t time5 = 542;
@@ -181,23 +191,147 @@ TEST(CallstackData, FilterCallstackEventsBasedOnMajorityStartExcludesFunctionToS
   callstack_data.AddCallstackEvent(event5);
 
   const uint64_t time6 = 642;
-  CallstackEvent event6{time6, cs2_id, kTid};
+  CallstackEvent event6{time6, kCallstackId2, kTid};
   callstack_data.AddCallstackEvent(event6);
 
   callstack_data.UpdateCallstackTypeBasedOnMajorityStart({{kFunctionToStopUnwindingAtAddress, 10}});
 
-  EXPECT_EQ(callstack_data.GetCallstack(cs1_id)->type(), CallstackType::kComplete);
-  EXPECT_EQ(callstack_data.GetCallstack(cs2_id)->type(), CallstackType::kComplete);
+  EXPECT_EQ(callstack_data.GetCallstack(kCallstackId1)->type(), CallstackType::kComplete);
+  EXPECT_EQ(callstack_data.GetCallstack(kCallstackId2)->type(), CallstackType::kComplete);
   EXPECT_EQ(callstack_data.GetCallstack(broken_cs_id)->type(),
             CallstackType::kFilteredByMajorityOutermostFrame);
   EXPECT_EQ(callstack_data.GetCallstack(function_to_stop_unwinding_at_cs_id)->type(),
             CallstackType::kComplete);
 
-  EXPECT_THAT(
-      callstack_data.GetCallstackEventsOfTidInTimeRange(kTid, 0,
-                                                        std::numeric_limits<uint64_t>::max()),
-      testing::Pointwise(CallstackEventEq(), std::vector<CallstackEvent>{event1, event2, event3,
-                                                                         event4, event5, event6}));
+  EXPECT_THAT(callstack_data.GetCallstackEventsOfTidInTimeRange(
+                  kTid, 0, std::numeric_limits<uint64_t>::max()),
+              Pointwise(CallstackEventEq(), std::vector<CallstackEvent>{event1, event2, event3,
+                                                                        event4, event5, event6}));
 }
+
+const std::vector<uint32_t> kTids = {kTid, kTid, kAnotherTid, kTid};
+const std::vector<uint64_t> kTimestamps = {142, 242, 342, 442};
+const std::vector<CallstackEvent> kAllEvents = [] {
+  std::vector<CallstackEvent> events;
+  absl::c_transform(kTids, kTimestamps, std::back_inserter(events),
+                    [](uint32_t tid, uint64_t timestamp) {
+                      return CallstackEvent{timestamp, kCallstackId1, tid};
+                    });
+  return events;
+}();
+
+constexpr auto kGetTestName = [](const auto& info) { return info.param.test_name; };
+
+std::unique_ptr<CallstackData> kCallstackDataWithEvents = [] {
+  auto result = std::make_unique<CallstackData>();
+  CallstackInfo cs{{0x11, 0x10}, CallstackType::kComplete};
+  result->AddUniqueCallstack(kCallstackId1, std::move(cs));
+  absl::c_for_each(kAllEvents, absl::bind_front(&CallstackData::AddCallstackEvent, result.get()));
+  return result;
+}();
+
+template <typename T>
+[[nodiscard]] static std::vector<T> Slice(const std::vector<T>& v, std::vector<int> indices) {
+  std::vector<T> slice;
+  absl::c_transform(indices, std::back_inserter(slice), [&](int i) { return v[i]; });
+  return slice;
+}
+
+struct FormattingForEachCallstackEventOfTidInTimeRangeDiscretizedTest {
+  std::string test_name;
+  uint32_t tid;
+  uint64_t start_ns;
+  uint64_t end_ns;
+  uint32_t resolution;
+  std::vector<int> expected_event_ids;
+};
+
+using ForEachCallstackEventOfTidInTimeRangeDiscretizedTest =
+    TestWithParam<FormattingForEachCallstackEventOfTidInTimeRangeDiscretizedTest>;
+
+TEST_P(ForEachCallstackEventOfTidInTimeRangeDiscretizedTest, IterationIsCorrect) {
+  const FormattingForEachCallstackEventOfTidInTimeRangeDiscretizedTest& test_case = GetParam();
+  const CallstackData* callstack_data = kCallstackDataWithEvents.get();
+
+  std::vector<CallstackEvent> visited_callstack_list;
+  auto visit_callstack = [&](const CallstackEvent& event) {
+    visited_callstack_list.push_back(event);
+  };
+  callstack_data->ForEachCallstackEventOfTidInTimeRangeDiscretized(
+      test_case.tid, test_case.start_ns, test_case.end_ns, test_case.resolution, visit_callstack);
+  EXPECT_THAT(visited_callstack_list,
+              Pointwise(CallstackEventEq(), Slice(kAllEvents, test_case.expected_event_ids)));
+}
+
+constexpr uint64_t kStartNs = 0;
+constexpr uint64_t kEndNs = 1000;
+constexpr uint64_t kMaxNs = std::numeric_limits<uint64_t>::max();
+constexpr uint32_t kResolution = 2000;
+
+INSTANTIATE_TEST_SUITE_P(
+    ForEachCallstackEventOfTidInTimeRangeDiscretizedTests,
+    ForEachCallstackEventOfTidInTimeRangeDiscretizedTest,
+    ValuesIn<FormattingForEachCallstackEventOfTidInTimeRangeDiscretizedTest>({
+        {"NormalTimeRange", kTid, kStartNs, kEndNs, kResolution, {0, 1, 3}},
+        {"DifferentTid", kAnotherTid, kStartNs, kEndNs, kResolution, {2}},
+        {"SmallTimeRange", kTid, kStartNs, kTimestamps[2] - 1, kResolution, {0, 1}},
+        // When max_timestamp is std::numeric_limits<uint64_t>::max(), each callstack will be drawn
+        // in the first pixel, and therefore only one will be visible.
+        {"InfiniteTimeRange", kTid, kStartNs, kMaxNs, kResolution, {0}},
+        // With one pixel on the screen we should only see one event.
+        {"OnePixel", kTid, kStartNs, kEndNs, /*resolution=*/1, {0}},
+    }),
+    kGetTestName);
+
+constexpr auto kExpectAll = [](const auto& actual, const auto& expected) {
+  EXPECT_THAT(actual, Pointwise(CallstackEventEq(), expected));
+};
+
+constexpr auto kExpectAny = [](const auto& actual, const auto& expected) {
+  EXPECT_EQ(actual.size(), 1);
+  EXPECT_THAT(actual[0], AnyOfArray(expected));
+};
+
+struct FormattingForEachCallstackEventInTimeRangeDiscretizedTest {
+  std::string test_name;
+  absl::FunctionRef<void(std::vector<CallstackEvent>, std::vector<CallstackEvent>)> expect;
+  uint64_t start_ns;
+  uint64_t end_ns;
+  uint32_t resolution;
+  std::vector<int> expected_event_ids;
+};
+
+using ForEachCallstackEventInTimeRangeDiscretizedTest =
+    TestWithParam<FormattingForEachCallstackEventInTimeRangeDiscretizedTest>;
+TEST_P(ForEachCallstackEventInTimeRangeDiscretizedTest, IterationIsCorrect) {
+  const FormattingForEachCallstackEventInTimeRangeDiscretizedTest& test_case = GetParam();
+  const CallstackData* callstack_data = kCallstackDataWithEvents.get();
+
+  std::vector<CallstackEvent> visited_callstack_list;
+  auto visit_callstack = [&](const CallstackEvent& event) {
+    visited_callstack_list.push_back(event);
+  };
+  callstack_data->ForEachCallstackEventInTimeRangeDiscretized(
+      test_case.start_ns, test_case.end_ns, test_case.resolution, visit_callstack);
+  test_case.expect(visited_callstack_list, Slice(kAllEvents, test_case.expected_event_ids));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ForEachCallstackEventInTimeRangeDiscretizedTests,
+    ForEachCallstackEventInTimeRangeDiscretizedTest,
+    ValuesIn<FormattingForEachCallstackEventInTimeRangeDiscretizedTest>({
+        {"NormalTimeRange", kExpectAll, kStartNs, kEndNs, kResolution, {0, 1, 2, 3}},
+        {"SmallTimeRange", kExpectAll, kStartNs, kTimestamps[2] - 1, kResolution, {0, 1}},
+        // When max_timestamp is std::numeric_limits<uint64_t>::max(), each callstack should be draw
+        // in the first pixel, and therefore only one will be visible. It should be the first of
+        // some of the threads.
+        {"InfiniteTimeRange", kExpectAny, kStartNs, kMaxNs, kResolution, {0, 2}},
+        // With one pixel on the screen we should only see one event. It should be the first of some
+        // of the threads.
+        {"OnePixel", kExpectAny, kStartNs, kEndNs, /*resolution=*/1, {0, 2}},
+    }),
+    kGetTestName);
+
+}  // namespace
 
 }  // namespace orbit_client_data

src/ClientData/FastRenderingUtils.cpp
@@ -6,13 +6,18 @@
 
 namespace orbit_client_data {
 
-uint64_t GetNextPixelBoundaryTimeNs(uint64_t current_timestamp_ns, uint32_t resolution,
-                                    uint64_t start_ns, uint64_t end_ns) {
-  uint64_t current_ns_from_start = current_timestamp_ns - start_ns;
+uint32_t GetPixelNumber(uint64_t timestamp_ns, uint32_t resolution, uint64_t start_ns,
+                        uint64_t end_ns) {
+  uint64_t current_ns_from_start = timestamp_ns - start_ns;
   uint64_t total_ns = end_ns - start_ns;
 
   // Given a resolution of 4000 pixels, we can capture for 53 days without overflowing.
-  uint64_t current_pixel = (current_ns_from_start * resolution) / total_ns;
+  return (current_ns_from_start * resolution) / total_ns;
+}
+
+uint64_t GetNextPixelBoundaryTimeNs(uint64_t current_timestamp_ns, uint32_t resolution,
+                                    uint64_t start_ns, uint64_t end_ns) {
+  uint64_t current_pixel = GetPixelNumber(current_timestamp_ns, resolution, start_ns, end_ns);
   uint64_t next_pixel = current_pixel + 1;
 
   // Calculates `ceil(dividend / divisor)` only using integers assuming dividend and divisor are not
@@ -21,6 +26,7 @@ uint64_t GetNextPixelBoundaryTimeNs(uint64_t current_timestamp_ns, uint32_t reso
     return 1 + (dividend - 1) / divisor;
   };
 
+  uint64_t total_ns = end_ns - start_ns;
   // To calculate the timestamp of a pixel boundary, we make a cross-multiplication rounding_up to
   // be consistent to how we calculate current_pixel.
   uint64_t next_pixel_ns_from_min = rounding_up_division(total_ns * next_pixel, resolution);

src/ClientData/FastRenderingUtilsTest.cpp
@@ -10,9 +10,29 @@ namespace orbit_client_data {
 
 constexpr uint64_t kStartNs = 100;
 constexpr uint64_t kEndNs = 200;
-static std::vector<uint32_t> kPixelResolutionsInTest = {1, 20, 30, 50, 100};
 
-TEST(GetNextPixelBoundaryTimeNs, TimestampAreInRange) {
+using GetPixelNumberTest = ::testing::TestWithParam<uint32_t>;
+using GetNextPixelBoundaryTimeNsTest = ::testing::TestWithParam<uint32_t>;
+
+TEST_P(GetPixelNumberTest, FirstPixel) {
+  const uint32_t resolution = GetParam();
+  EXPECT_EQ(GetPixelNumber(kStartNs, resolution, kStartNs, kEndNs), 0);
+
+  const uint32_t kLastNsForFirstPixel = kStartNs + (kEndNs - kStartNs - 1) / resolution;
+  EXPECT_EQ(GetPixelNumber(kLastNsForFirstPixel, resolution, kStartNs, kEndNs), 0);
+  EXPECT_EQ(GetPixelNumber(kLastNsForFirstPixel + 1, resolution, kStartNs, kEndNs), 1);
+}
+
+TEST_P(GetPixelNumberTest, LastPixel) {
+  const uint32_t resolution = GetParam();
+  EXPECT_EQ(GetPixelNumber(kEndNs - 1, resolution, kStartNs, kEndNs), resolution - 1);
+  EXPECT_EQ(GetPixelNumber(kEndNs, resolution, kStartNs, kEndNs), resolution);
+}
+
+INSTANTIATE_TEST_SUITE_P(GetPixelNumberTests, GetPixelNumberTest, testing::Values(1, 20, 30, 100));
+
+TEST_P(GetNextPixelBoundaryTimeNsTest, TimestampAreInRange) {
+  const uint32_t resolution = GetParam();
   constexpr uint64_t visible_ns = kEndNs - kStartNs;  // 100
 
   // Calculates `ceil(dividend / divisor)` only using integers assuming dividend and divisor are not
@@ -21,35 +41,33 @@ TEST(GetNextPixelBoundaryTimeNs, TimestampAreInRange) {
     return 1 + (dividend - 1) / divisor;
   };
 
-  for (uint32_t resolution : kPixelResolutionsInTest) {
-    // The max number of nanoseconds per pixel can be calculated using a ceil function.
-    const uint64_t max_nanoseconds_per_pixel = rounding_up_division(visible_ns, resolution);
-    for (uint64_t timestamp_ns = kStartNs; timestamp_ns < kEndNs; timestamp_ns++) {
-      const uint64_t next_pixel_ns =
-          GetNextPixelBoundaryTimeNs(timestamp_ns, resolution, kStartNs, kEndNs);
-      // The timestamp of the next pixel should be between the current one and the current plus the
-      // maximum number of nanoseconds per pixel.
-      EXPECT_GT(next_pixel_ns, timestamp_ns);
-      EXPECT_LE(next_pixel_ns, timestamp_ns + max_nanoseconds_per_pixel);
-    }
+  // The max number of nanoseconds per pixel can be calculated using a ceil function.
+  const uint64_t max_nanoseconds_per_pixel = rounding_up_division(visible_ns, resolution);
+  for (uint64_t timestamp_ns = kStartNs; timestamp_ns < kEndNs; timestamp_ns++) {
+    const uint64_t next_pixel_ns =
+        GetNextPixelBoundaryTimeNs(timestamp_ns, resolution, kStartNs, kEndNs);
+    // The timestamp of the next pixel should be between the current one and the current plus the
+    // maximum number of nanoseconds per pixel.
+    EXPECT_GT(next_pixel_ns, timestamp_ns);
+    EXPECT_LE(next_pixel_ns, timestamp_ns + max_nanoseconds_per_pixel);
   }
 }
 
-TEST(GetNextPixelBoundaryTimeNs, NumIterations) {
+TEST_P(GetNextPixelBoundaryTimeNsTest, NumIterations) {
+  const uint32_t resolution = GetParam();
+
   // Iterating through visible pixels using GetNextPixelBoundaryTimeNs should go once per pixel.
-  for (uint32_t resolution : kPixelResolutionsInTest) {
-    uint32_t num_iterations = 0;
-    uint64_t current_timestamp_ns = kStartNs;
-    while (current_timestamp_ns < kEndNs) {
-      ++num_iterations;
-      current_timestamp_ns =
-          GetNextPixelBoundaryTimeNs(current_timestamp_ns, resolution, kStartNs, kEndNs);
-    }
-    EXPECT_EQ(num_iterations, resolution);
+  uint32_t num_iterations = 0;
+  uint64_t current_timestamp_ns = kStartNs;
+  while (current_timestamp_ns < kEndNs) {
+    ++num_iterations;
+    current_timestamp_ns =
+        GetNextPixelBoundaryTimeNs(current_timestamp_ns, resolution, kStartNs, kEndNs);
   }
+  EXPECT_EQ(num_iterations, resolution);
 }
 
-TEST(GetNextPixelBoundaryTimeNs, ExtremeZoomInBorderCase) {
+TEST(GetNextPixelBoundaryTimeNsTest, ExtremeZoomInBorderCase) {
   constexpr uint64_t visible_ns = kEndNs - kStartNs;
 
   // If there are more visible pixels than visible timestamps, we will have several pixels with the
@@ -57,5 +75,8 @@ TEST(GetNextPixelBoundaryTimeNs, ExtremeZoomInBorderCase) {
   // greater than the one queried.
   EXPECT_EQ(GetNextPixelBoundaryTimeNs(kStartNs, visible_ns * 10, kStartNs, kEndNs), kStartNs + 1);
 }
+
+INSTANTIATE_TEST_SUITE_P(GetNextPixelBoundaryTimeNsTests, GetNextPixelBoundaryTimeNsTest,
+                         testing::Values(1, 20, 30, 100));
 
 }  // namespace orbit_client_data

src/ClientData/include/ClientData/CallstackData.h
@@ -83,9 +83,34 @@ class CallstackData {
   template <typename Action>
   void ForEachCallstackEventInTimeRangeDiscretized(uint64_t min_timestamp, uint64_t max_timestamp,
                                                    uint32_t resolution, Action&& action) const {
-    for (const auto& [tid, unused_events] : callstack_events_by_tid_) {
-      ForEachCallstackEventOfTidInTimeRangeDiscretized(tid, min_timestamp, max_timestamp,
-                                                       resolution, action);
+    auto get_next_callstack = [&](uint64_t timestamp) -> std::optional<CallstackEvent> {
+      std::optional<CallstackEvent> next_callstack;
+      const uint32_t current_pixel =
+          GetPixelNumber(timestamp, resolution, min_timestamp, max_timestamp);
+      for (const auto& [unused_tid, events] : callstack_events_by_tid_) {
+        auto next_callstack_of_tid = events.lower_bound(timestamp);
+        if (next_callstack_of_tid == events.end() ||
+            (next_callstack.has_value() &&
+             next_callstack.value().timestamp_ns() <= next_callstack_of_tid->second.timestamp_ns()))
+          continue;
+
+        // If this callstack will be drawn in the current_pixel, we don't need to search for more of
+        // them. Otherwise there could be a callstack in another thread_id that will be draw before,
+        // so we need to keep looking.
+        if (GetPixelNumber(next_callstack_of_tid->first, resolution, min_timestamp,
+                           max_timestamp) == current_pixel) {
+          return next_callstack_of_tid->second;
+        }
+        next_callstack = next_callstack_of_tid->second;
+      }
+      return next_callstack;
+    };
+
+    for (std::optional<CallstackEvent> next_callstack = get_next_callstack(min_timestamp);
+         next_callstack.has_value() && next_callstack.value().timestamp_ns() < max_timestamp;
+         next_callstack = get_next_callstack(GetNextPixelBoundaryTimeNs(
+             next_callstack.value().timestamp_ns(), resolution, min_timestamp, max_timestamp))) {
+      std::invoke(action, next_callstack.value());
     }
   }
 

src/ClientData/include/ClientData/FastRenderingUtils.h
@@ -14,6 +14,11 @@ namespace orbit_client_data {
                                                   uint32_t resolution, uint64_t start_ns,
                                                   uint64_t end_ns);
 
+// Free Function that returns the number of the pixel of a particular timestamp. The query
+// assumes a closed-open interval [start_ns, end_ns), so end_ns is not a visible timestamp.
+[[nodiscard]] uint32_t GetPixelNumber(uint64_t timestamp_ns, uint32_t resolution, uint64_t start_ns,
+                                      uint64_t end_ns);
+
 }  // namespace orbit_client_data
 
 #endif  // CLIENT_DATA_FAST_RENDERING_UTILS_H_

commit da7601a5f00074df53897abe6d61a82cd0a4b333

Fix samples being drawn in pixel coordinates
This PR if fixing a regression from
https://github.com/google/orbit/pull/4296.

In that PR we started to use GetPixelNumber because is more precise when
drawing lines (based on imprecision of GetWorldFromTick) but we have the
issue that we are not converting to world coordinates, so in this PR we
are reverting and using the new created method that is used in
SchedulerTrack, ThreadTrack and ThreadStates to draw boxes to get the
position of the line.

Test: Load a capture, navigate.

src/OrbitGl/CallstackThreadBar.cpp
@@ -140,9 +140,8 @@ void CallstackThreadBar::DoUpdatePrimitives(PrimitiveAssembler& primitive_assemb
     auto action_on_selected_callstack_events = [&](const CallstackEvent& event) {
       const uint64_t time = event.timestamp_ns();
       ORBIT_CHECK(time >= min_tick && time <= max_tick);
-      const Vec2 pos(GetPixelNumber(event.timestamp_ns(), resolution_in_pixels, min_tick, max_tick),
-                     GetPos()[1]);
-      primitive_assembler.AddVerticalLine(pos, track_height, z, kGreenSelection);
+      const auto& [pos_x, unused_size_x] = timeline_info_->GetBoxPosXAndWidthFromTicks(time, time);
+      primitive_assembler.AddVerticalLine({pos_x, GetPos()[1]}, track_height, z, kGreenSelection);
     };
     const orbit_client_data::CallstackData& selection_callstack_data =
         capture_data_->selection_callstack_data();