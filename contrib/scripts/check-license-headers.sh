c792ede1bf6b43265c47cefde645670e5ed8c344
Florian Kübler <kuebler@google.com>
2022-11-14 15:07:31 +0000

    Report missing header files as check comments
    Test: CI

--- contrib/scripts/check-license-headers.sh
+++ contrib/scripts/check-license-headers.sh
@@ -14,20 +14,21 @@ MERGE_BASE="$(git merge-base $REFERENCE HEAD)" # Merge base is the commit on mai
 LICENSE_HEADER_MISSED=""
 
 echo -e "The following files are missing a license/copyright header:\n"
+touch "${GITHUB_WORKSPACE}/missing_license_headers.txt"
 while read file; do
   if [ ! -f "$file" ]; then
     continue
   fi
 
   if ! grep -E 'Copyright \(c\) [0-9]{4} The Orbit Authors\. All rights reserved\.' "$file" > /dev/null; then
-    LICENSE_HEADER_MISSED="yes"
     echo "$file"
+    echo "$file" >> "${GITHUB_WORKSPACE}/missing_license_headers.txt"
   fi
 
 done <<< $(git diff -U0 --no-color --relative --name-only --diff-filter=r $MERGE_BASE \
 | grep -v third_party/ \
 | egrep '\.cpp$|\.h$|CMakeLists\.txt$|\.js$|\.proto$|\.py$')
 
-if [ -n "$LICENSE_HEADER_MISSED" ]; then
+if [ -s "${GITHUB_WORKSPACE}/missing_license_headers.txt" ]; then
   exit 1
 fi
