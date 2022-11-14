c15ab73a2cc8744dd45b84896ad52b0176d50b9d
Florian Kübler <kuebler@google.com>
2022-11-14 12:52:40 +0000

    test

--- src/Api/LockFreeApiEventProducer.cpp
+++ src/Api/LockFreeApiEventProducer.cpp
@@ -1,7 +1,3 @@
-// Copyright (c) 2021 The Orbit Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 #include "LockFreeApiEventProducer.h"
 
 namespace orbit_api {
