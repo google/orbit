baded0fe3d705235d2937c727b6b8d396b46f26c
Henning Becker <henning.becker@gmail.com>
2022-11-16 08:17:36 +0000

    Add CaptureWindowDebugWidget
    This widget is combining all the debug information for a single
    CaptureWindow (either the general capture window or the introspection
    window).
    
    It integrates the controls to change time graph layout values and it
    shows debug output, similar to how the ImGui based debug UI did it.

---
+++ src/OrbitQt/CaptureWindowDebugWidget.h
@@ -0,0 +1,55 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ORBIT_QT_CAPTURE_WINDOW_DEBUG_WIDGET_H_
+#define ORBIT_QT_CAPTURE_WINDOW_DEBUG_WIDGET_H_
+
+#include <QTimer>
+#include <QWidget>
+#include <memory>
+
+#include "CaptureWindowDebugInterface.h"
+#include "TimeGraphLayout.h"
+
+namespace Ui {
+class CaptureWindowDebugWidget;
+}
+
+namespace orbit_qt {
+
+// The capture window debug widget has 2 purposes:
+// 1. It integrates a TimeGraphLayoutWidget and exposes its interface by the GetTimeGraphLayout
+// member function.
+// 2. It takes a CaptureWindowDebugInterface and presents its debug information.
+//
+// It's used in Orbit's debug tab and targets Orbit developers.
+class CaptureWindowDebugWidget : public QWidget {
+  Q_OBJECT
+
+ public:
+  explicit CaptureWindowDebugWidget(QWidget* parent = nullptr);
+  ~CaptureWindowDebugWidget() override;
+
+  // Be aware the caller is responsible to keep `capture_window_debug_interface` alive until the
+  // widget's lifetime ends or `ResetCaptureWindowDebugInterface` is called.
+  void SetCaptureWindowDebugInterface(
+      const orbit_gl::CaptureWindowDebugInterface* capture_window_debug_interface);
+  void ResetCaptureWindowDebugInterface();
+
+  [[nodiscard]] TimeGraphLayout* GetTimeGraphLayout() const;
+
+ private:
+  QTimer update_timer;
+  void UpdateUiElements();
+
+  std::unique_ptr<Ui::CaptureWindowDebugWidget> ui_;
+  const orbit_gl::CaptureWindowDebugInterface* capture_window_debug_interface_ = nullptr;
+
+ signals:
+  void AnyLayoutPropertyChanged();
+};
+
+}  // namespace orbit_qt
+
+#endif  // ORBIT_QT_CAPTURE_WINDOW_DEBUG_WIDGET_H_
