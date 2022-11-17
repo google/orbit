3e760ba0d6de01e8c9f75d8be83f854a71c1af27
Florian Kübler <kuebler@google.com>
2022-11-17 13:00:01 +0000

    removed even more unused stuff

--- src/ApiUtils/include/ApiUtils/EncodedEvent.h
+++ src/ApiUtils/include/ApiUtils/EncodedEvent.h
@@ -13,65 +13,6 @@
 
 namespace orbit_api {
 
-constexpr uint8_t kVersion = 1;
-
-enum EventType : uint8_t {
-  kNone = 0,
-  kScopeStart = 1,
-  kScopeStop = 2,
-  kScopeStartAsync = 3,
-  kScopeStopAsync = 4,
-  kTrackInt = 5,
-  kTrackInt64 = 6,
-  kTrackUint = 7,
-  kTrackUint64 = 8,
-  kTrackFloat = 9,
-  kTrackDouble = 10,
-  kString = 11,
-};
-
-constexpr size_t kMaxEventStringSize = 34;
-struct Event {
-  uint8_t version;                 // 1
-  uint8_t type;                    // 1
-  char name[kMaxEventStringSize];  // 34
-  orbit_api_color color;           // 4
-  uint64_t data;                   // 8
-};
-
-// EncodedEvent is used for encoding an Orbit API event into the 6 integer register of the Linux
-// x64 ABI. This is useful for the version of the manual instrumentation API that relies on uprobes.
-union EncodedEvent {
-  EncodedEvent() = default;
-  EncodedEvent(orbit_api::EventType type, const char* name = nullptr, uint64_t data = 0,
-               orbit_api_color color = kOrbitColorAuto) {
-    static_assert(sizeof(EncodedEvent) == 48, "orbit_api::EncodedEvent should be 48 bytes.");
-    static_assert(sizeof(Event) == 48, "orbit_api::Event should be 48 bytes.");
-    event.version = kVersion;
-    event.type = static_cast<uint8_t>(type);
-    memset(event.name, 0, kMaxEventStringSize);
-    if (name != nullptr) {
-      std::strncpy(event.name, name, kMaxEventStringSize - 1);
-      event.name[kMaxEventStringSize - 1] = 0;
-    }
-    event.data = data;
-    event.color = color;
-  }
-
-  EncodedEvent(uint64_t a0, uint64_t a1, uint64_t a2, uint64_t a3, uint64_t a4, uint64_t a5) {
-    args[0] = a0;
-    args[1] = a1;
-    args[2] = a2;
-    args[3] = a3;
-    args[4] = a4;
-    args[5] = a5;
-  }
-  orbit_api::EventType Type() const { return static_cast<orbit_api::EventType>(event.type); }
-
-  Event event;
-  uint64_t args[6];
-};
-
 template <typename Dest, typename Source>
 inline Dest Encode(const Source& source) {
   static_assert(sizeof(Source) <= sizeof(Dest), "orbit_api::Encode destination type is too small");
