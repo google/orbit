7aef6a947402ef0f4fd023f4ad32ad73ec7e1a9f
Florian Kübler <kuebler@google.com>
2022-11-21 08:17:00 +0000

    Add Windows CI
    This adds an MSCV2019 Release build to the
    build and test job. The job uses conan
    for dependencies other than qt.
    
    We might want to consider also moving to
    conan for the linux build at some later
    point in time.
    
    Note, Ninja is needed to support ccache.
    Also note, ccache does not support "-Zi",
    so there is no relwithdebinfo build.
    
    Test: Run CI

--- third_party/conan/configs/install.ps1
+++ third_party/conan/configs/install.ps1
@@ -2,8 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-$conan = Get-Command conan -ErrorAction Stop
+$py = Get-Command py -ErrorAction Stop
 
 # Installs conan config (build settings, public remotes, conan profiles, conan options). Have a look in \third_party\conan\configs\windows for more information.
-$process = Start-Process $conan.Path -Wait -NoNewWindow -ErrorAction Stop -PassThru -ArgumentList "config","install","$PSScriptRoot\windows"
+$process = Start-Process $py.Path -Wait -NoNewWindow -ErrorAction Stop -PassThru -ArgumentList "-3","-m","conans.conan","config","install","$PSScriptRoot\windows"
 if ($process.ExitCode -ne 0) { Throw "Error while installing conan config." }
