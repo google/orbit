6bcfff5fb40f34472e84553d81c9f7e25ba80bca
Henning Becker <43133967+beckerhe@users.noreply.github.com>
2022-11-21 11:03:42 +0100

    Add CaptureWindowDebugWidget (#4423)
    This widget is combining all the debug information for a single
    CaptureWindow (either the general capture window or the introspection
    window).
    
    It integrates the controls to change time graph layout values and it
    shows debug output, similar to how the ImGui based debug UI did it.

---
+++ src/OrbitQt/CaptureWindowDebugWidget.cpp
@@ -0,0 +1,76 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "CaptureWindowDebugWidget.h"
+
+#include <QWidget>
+#include <chrono>
+#include <memory>
+
+#include "TimeGraphLayoutWidget.h"
+#include "ui_CaptureWindowDebugWidget.h"
+
+namespace orbit_qt {
+
+CaptureWindowDebugWidget::CaptureWindowDebugWidget(QWidget* parent)
+    : QWidget{parent}, ui_{std::make_unique<Ui::CaptureWindowDebugWidget>()} {
+  ui_->setupUi(this);
+
+  QObject::connect(ui_->layoutPropertiesWidget,
+                   &TimeGraphLayoutWidget::AnyRegisteredPropertyChangedValue, this,
+                   [this]() { emit AnyLayoutPropertyChanged(); });
+}
+
+// This needs to be here to allow Ui::CaptureWindowDebugWidget to be incomplete in the header.
+CaptureWindowDebugWidget::~CaptureWindowDebugWidget() = default;
+
+void CaptureWindowDebugWidget::SetCaptureWindowDebugInterface(
+    const orbit_gl::CaptureWindowDebugInterface* capture_window_debug_interface) {
+  capture_window_debug_interface_ = capture_window_debug_interface;
+
+  if (capture_window_debug_interface_ != nullptr) {
+    constexpr std::chrono::milliseconds kDebugDataUpdateInterval{200};
+    update_timer.start(kDebugDataUpdateInterval);
+    UpdateUiElements();
+  } else {
+    update_timer.stop();
+  }
+
+  QObject::connect(&update_timer, &QTimer::timeout, this,
+                   &CaptureWindowDebugWidget::UpdateUiElements, Qt::UniqueConnection);
+}
+
+void CaptureWindowDebugWidget::ResetCaptureWindowDebugInterface() {
+  update_timer.stop();
+  capture_window_debug_interface_ = nullptr;
+}
+
+TimeGraphLayout* CaptureWindowDebugWidget::GetTimeGraphLayout() const {
+  return ui_->layoutPropertiesWidget;
+}
+
+void CaptureWindowDebugWidget::UpdateUiElements() {
+  if (capture_window_debug_interface_ == nullptr) return;
+
+  QString capture_info = QString::fromStdString(capture_window_debug_interface_->GetCaptureInfo());
+
+  if (ui_->captureInfoTextEdit->toPlainText() != capture_info) {
+    ui_->captureInfoTextEdit->setPlainText(capture_info);
+  }
+
+  QString performance_info =
+      QString::fromStdString(capture_window_debug_interface_->GetPerformanceInfo());
+
+  if (ui_->performanceTextEdit->toPlainText() != performance_info) {
+    ui_->performanceTextEdit->setPlainText(performance_info);
+  }
+
+  QString selection_summary =
+      QString::fromStdString(capture_window_debug_interface_->GetSelectionSummary());
+
+  if (ui_->selectionSummaryTextEdit->toPlainText() != selection_summary) {
+    ui_->selectionSummaryTextEdit->setPlainText(selection_summary);
+  }
+}
+}  // namespace orbit_qt
---
+++ src/OrbitQt/CaptureWindowDebugWidgetTest.cpp
@@ -0,0 +1,113 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <QApplication>
+#include <QPlainTextEdit>
+#include <QTest>
+
+#include "CaptureWindowDebugInterface.h"
+#include "CaptureWindowDebugWidget.h"
+
+namespace {
+class MockCaptureWindowDebugInterface : public orbit_gl::CaptureWindowDebugInterface {
+ public:
+  MOCK_METHOD(std::string, GetCaptureInfo, (), (const, override));
+  MOCK_METHOD(std::string, GetPerformanceInfo, (), (const, override));
+  MOCK_METHOD(std::string, GetSelectionSummary, (), (const, override));
+};
+}  // namespace
+
+TEST(CaptureWindowDebugWidget, Simple) {
+  MockCaptureWindowDebugInterface debug_interface{};
+  EXPECT_CALL(debug_interface, GetCaptureInfo())
+      .WillRepeatedly(testing::Return("This is the capture info."));
+  EXPECT_CALL(debug_interface, GetPerformanceInfo())
+      .WillRepeatedly(testing::Return("This is the performance info."));
+  EXPECT_CALL(debug_interface, GetSelectionSummary())
+      .WillRepeatedly(testing::Return("This is the selection summary."));
+
+  orbit_qt::CaptureWindowDebugWidget widget{};
+  widget.SetCaptureWindowDebugInterface(&debug_interface);
+
+  const auto* capture_info_text_edit = widget.findChild<QPlainTextEdit*>("captureInfoTextEdit");
+  ASSERT_NE(capture_info_text_edit, nullptr);
+  EXPECT_THAT(capture_info_text_edit->toPlainText().toStdString(),
+              testing::HasSubstr("capture info"));
+
+  const auto* performance_info_text_edit = widget.findChild<QPlainTextEdit*>("performanceTextEdit");
+  ASSERT_NE(performance_info_text_edit, nullptr);
+  EXPECT_THAT(performance_info_text_edit->toPlainText().toStdString(),
+              testing::HasSubstr("performance info"));
+
+  const auto* selection_summary_text_edit =
+      widget.findChild<QPlainTextEdit*>("selectionSummaryTextEdit");
+  ASSERT_NE(selection_summary_text_edit, nullptr);
+  EXPECT_THAT(selection_summary_text_edit->toPlainText().toStdString(),
+              testing::HasSubstr("selection summary"));
+}
+
+TEST(CaptureWindowDebugWidget, DynamicUpdates) {
+  MockCaptureWindowDebugInterface debug_interface{};
+  EXPECT_CALL(debug_interface, GetCaptureInfo()).WillRepeatedly(testing::Return(""));
+  EXPECT_CALL(debug_interface, GetPerformanceInfo()).WillRepeatedly(testing::Return(""));
+  EXPECT_CALL(debug_interface, GetSelectionSummary()).WillRepeatedly(testing::Return(""));
+
+  orbit_qt::CaptureWindowDebugWidget widget{};
+  widget.SetCaptureWindowDebugInterface(&debug_interface);
+
+  const auto* capture_info_text_edit = widget.findChild<QPlainTextEdit*>("captureInfoTextEdit");
+  ASSERT_NE(capture_info_text_edit, nullptr);
+  EXPECT_TRUE(capture_info_text_edit->toPlainText().isEmpty());
+
+  const auto* performance_info_text_edit = widget.findChild<QPlainTextEdit*>("performanceTextEdit");
+  ASSERT_NE(performance_info_text_edit, nullptr);
+  EXPECT_TRUE(performance_info_text_edit->toPlainText().isEmpty());
+
+  const auto* selection_summary_text_edit =
+      widget.findChild<QPlainTextEdit*>("selectionSummaryTextEdit");
+  ASSERT_NE(selection_summary_text_edit, nullptr);
+  EXPECT_TRUE(selection_summary_text_edit->toPlainText().isEmpty());
+
+  // After the first initial check, we change the returned results of the debug interface and expect
+  // the widget update after some time.
+  EXPECT_CALL(debug_interface, GetCaptureInfo())
+      .WillRepeatedly(testing::Return("This is the capture info."));
+  EXPECT_CALL(debug_interface, GetPerformanceInfo())
+      .WillRepeatedly(testing::Return("This is the performance info."));
+  EXPECT_CALL(debug_interface, GetSelectionSummary())
+      .WillRepeatedly(testing::Return("This is the selection summary."));
+
+  // The widget guarantees to update every 200ms (roughly), so we check after 500ms to avoid any
+  // timing problems. Note that qWait runs an event loop in the background, it's not a sleep!
+  QTest::qWait(500);
+
+  EXPECT_THAT(capture_info_text_edit->toPlainText().toStdString(),
+              testing::HasSubstr("capture info"));
+  EXPECT_THAT(performance_info_text_edit->toPlainText().toStdString(),
+              testing::HasSubstr("performance info"));
+  EXPECT_THAT(selection_summary_text_edit->toPlainText().toStdString(),
+              testing::HasSubstr("selection summary"));
+}
+
+// Use command line options `--gtest_filter=CaptureWindowDebugWidget.DISABLED_Demo
+// --gtest_also_run_disabled_tests` to run this demo.
+TEST(CaptureWindowDebugWidget, DISABLED_Demo) {
+  MockCaptureWindowDebugInterface debug_interface{};
+  EXPECT_CALL(debug_interface, GetCaptureInfo())
+      .WillRepeatedly(testing::Return("This is the capture info."));
+  EXPECT_CALL(debug_interface, GetPerformanceInfo())
+      .WillRepeatedly(testing::Return("This is the performance info."));
+  EXPECT_CALL(debug_interface, GetSelectionSummary())
+      .WillRepeatedly(testing::Return("This is the selection summary."));
+
+  orbit_qt::CaptureWindowDebugWidget widget{};
+  widget.SetCaptureWindowDebugInterface(&debug_interface);
+  widget.show();
+
+  QApplication::exec();
+  SUCCEED();
+}
