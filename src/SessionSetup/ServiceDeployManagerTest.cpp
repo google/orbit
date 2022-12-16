0886e534626034909de0ce6689b268f5e9d3d7d6
Henning Becker <43133967+beckerhe@users.noreply.github.com>
2022-12-16 09:28:32 +0100

    Add tests for ServiceDeployManager (#4635)
    This is adding tests for all 3 deployment methods of
    ServiceDeployManager.
    
    Two of them need to upload some artifacts to a remote machine that behave
    like a (packaged) OrbitService. These don't need to be the real
    artifacts. The SSH server containers that we use for testing are
    prepared to work with simple fakes, like shell scripts instead of a
    binary OrbitService.

---
+++ src/SessionSetup/ServiceDeployManagerTest.cpp
@@ -0,0 +1,139 @@
+// Copyright (c) 2022 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <QSignalSpy>
+#include <QTest>
+
+#include "OrbitBase/Result.h"
+#include "OrbitSsh/Context.h"
+#include "OrbitSsh/Credentials.h"
+#include "OrbitSshQt/Session.h"
+#include "OrbitSshQt/Task.h"
+#include "QtTestUtils/WaitFor.h"
+#include "SessionSetup/DeploymentConfigurations.h"
+#include "SessionSetup/ServiceDeployManager.h"
+#include "SshQtTestUtils/KillProcessListeningOnTcpPort.h"
+#include "SshQtTestUtils/ParsePortNumberFromSocatOutput.h"
+#include "SshQtTestUtils/SshSessionTest.h"
+#include "Test/Path.h"
+#include "TestUtils/TestUtils.h"
+
+namespace orbit_session_setup {
+using orbit_qt_test_utils::WaitFor;
+using orbit_qt_test_utils::YieldsResult;
+using orbit_ssh_qt_test_utils::KillProcessListeningOnTcpPort;
+using orbit_test_utils::HasNoError;
+
+using ServiceDeployManagerTest = orbit_ssh_qt_test_utils::SshSessionTest;
+
+struct ServiceDeployManagerSigningTest : orbit_ssh_qt_test_utils::SshSessionTest {
+  constexpr static std::string_view kSigningSshServerEnvironmentVariableName =
+      "ORBIT_TESTING_SSH_SERVER_SIGNING_ADDRESS";
+  ServiceDeployManagerSigningTest()
+      : orbit_ssh_qt_test_utils::SshSessionTest{
+            std::string{kSigningSshServerEnvironmentVariableName}} {}
+};
+
+TEST_F(ServiceDeployManagerTest, NoDeployment) {
+  auto context = orbit_ssh::Context::Create();
+  ASSERT_THAT(context, orbit_test_utils::HasValue());
+
+  orbit_ssh_qt::Session helper_session{&context.value()};
+  EXPECT_THAT(WaitFor(helper_session.ConnectToServer(GetCredentials())),
+              YieldsResult(HasNoError()));
+
+  // This is emulating OrbitService - It's important that we don't hard code any ports because all
+  // tests usually share the same network namespace and they might run concurrently.
+  orbit_ssh_qt::Task orbit_service_task{&helper_session,
+                                        "socat -dd TCP-LISTEN:0,fork exec:'/bin/cat'"};
+  std::string socat_output{};
+  orbit_ssh_qt::ScopedConnection output_connection{
+      QObject::connect(&orbit_service_task, &orbit_ssh_qt::Task::readyReadStdErr,
+                       &orbit_service_task, [&orbit_service_task, &socat_output]() {
+                         socat_output.append(orbit_service_task.ReadStdErr());
+                       })};
+  EXPECT_THAT(WaitFor(orbit_service_task.Start()), YieldsResult(HasNoError()));
+
+  std::optional<ErrorMessageOr<int>> port_or_error{};
+  std::ignore = QTest::qWaitFor([&]() {
+    port_or_error = orbit_ssh_qt_test_utils::ParsePortNumberFromSocatOutput(socat_output);
+    return port_or_error.has_value();
+  });
+  ASSERT_TRUE(port_or_error.has_value()) << "The socat output was: " << socat_output;
+  ASSERT_THAT(port_or_error.value(), HasNoError());
+  const ServiceDeployManager::GrpcPort grpc_port{static_cast<uint16_t>(port_or_error->value())};
+
+  // We need the signal spy have longer lifetime than the ServiceDeployManager because we wanna
+  // ensure that the ServiceDeployManager's destructor does not emit any signals.
+  qRegisterMetaType<std::error_code>("std::error_code");
+  std::optional<QSignalSpy> socket_error_signal{};
+  {
+    DeploymentConfiguration deployment_config{NoDeployment{}};
+    ServiceDeployManager sdm{&deployment_config, &context.value(), GetCredentials(), grpc_port};
+    socket_error_signal.emplace(&sdm, &ServiceDeployManager::socketErrorOccurred);
+    EXPECT_THAT(sdm.Exec(), orbit_test_utils::HasNoError());
+  }
+  EXPECT_THAT(*socket_error_signal, testing::IsEmpty());
+
+  EXPECT_THAT(KillProcessListeningOnTcpPort(&helper_session, grpc_port.grpc_port), HasNoError());
+  EXPECT_THAT(WaitFor(orbit_service_task.Stop()), YieldsResult(HasNoError()));
+  EXPECT_THAT(WaitFor(helper_session.Disconnect()), YieldsResult(HasNoError()));
+}
+
+TEST_F(ServiceDeployManagerTest, BareExecutableAndRootPassword) {
+  auto context = orbit_ssh::Context::Create();
+  ASSERT_THAT(context, orbit_test_utils::HasValue());
+
+  orbit_ssh_qt::Session helper_session{&context.value()};
+  EXPECT_THAT(WaitFor(helper_session.ConnectToServer(GetCredentials())),
+              YieldsResult(HasNoError()));
+
+  // We need the signal spy have longer lifetime than the ServiceDeployManager because we wanna
+  // ensure that the ServiceDeployManager's destructor does not emit any signals.
+  qRegisterMetaType<std::error_code>("std::error_code");
+  std::optional<QSignalSpy> socket_error_signal{};
+  {
+    DeploymentConfiguration deployment_config{BareExecutableAndRootPasswordDeployment{
+        orbit_test::GetTestdataDir() / "deployments" / "BareExecutableAndRootPassword" / "bin" /
+            "emulate_orbit_service.sh",
+        "loginpassword"}};
+    ServiceDeployManager sdm{&deployment_config, &context.value(), GetCredentials(),
+                             ServiceDeployManager::GrpcPort{44765}};
+    socket_error_signal.emplace(&sdm, &ServiceDeployManager::socketErrorOccurred);
+    EXPECT_THAT(sdm.Exec(), orbit_test_utils::HasNoError());
+  }
+  EXPECT_THAT(*socket_error_signal, testing::IsEmpty());
+
+  EXPECT_THAT(WaitFor(helper_session.Disconnect()), YieldsResult(HasNoError()));
+}
+
+TEST_F(ServiceDeployManagerSigningTest, SignedDebianPackageDeployment) {
+  auto context = orbit_ssh::Context::Create();
+  ASSERT_THAT(context, orbit_test_utils::HasValue());
+
+  orbit_ssh_qt::Session helper_session{&context.value()};
+  EXPECT_THAT(WaitFor(helper_session.ConnectToServer(GetCredentials())),
+              YieldsResult(HasNoError()));
+
+  // We need the signal spy have longer lifetime than the ServiceDeployManager because we wanna
+  // ensure that the ServiceDeployManager's destructor does not emit any signals.
+  qRegisterMetaType<std::error_code>("std::error_code");
+  std::optional<QSignalSpy> socket_error_signal{};
+  {
+    DeploymentConfiguration deployment_config{SignedDebianPackageDeployment{
+        orbit_test::GetTestdataDir() / "deployments" / "SignedDebianPackage" / "OrbitService",
+        orbit_test::GetTestdataDir() / "deployments" / "SignedDebianPackage" / "OrbitService.asc"}};
+    ServiceDeployManager sdm{&deployment_config, &context.value(), GetCredentials(),
+                             ServiceDeployManager::GrpcPort{44765}};
+    socket_error_signal.emplace(&sdm, &ServiceDeployManager::socketErrorOccurred);
+    EXPECT_THAT(sdm.Exec(), orbit_test_utils::HasNoError());
+  }
+  EXPECT_THAT(*socket_error_signal, testing::IsEmpty());
+
+  EXPECT_THAT(WaitFor(helper_session.Disconnect()), YieldsResult(HasNoError()));
+}
+}  // namespace orbit_session_setup
