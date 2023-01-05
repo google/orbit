2cfa4f1daffced0b65739a1dd68a019e2a8b85d8
Tori Baker <vwbaker@google.com>
2023-01-05 16:03:00 +0100

    Remove bar selection for --time_range_selection (#4680)
    CallstackThreadBar selection is not meant to work alongside time range
    selection (currently hidden behind --time_range_selection flag). Check
    if the flag is set and do nothing in this case.

--- src/OrbitGl/CallstackThreadBar.cpp
+++ src/OrbitGl/CallstackThreadBar.cpp
@@ -6,6 +6,8 @@
 
 #include <GteVector.h>
 #include <absl/container/flat_hash_map.h>
+#include <absl/flags/flag.h>
+#include <absl/flags/internal/flag.h>
 #include <absl/strings/str_format.h>
 
 #include <memory>
@@ -18,6 +20,7 @@
 #include "ClientData/CallstackInfo.h"
 #include "ClientData/CallstackType.h"
 #include "ClientData/CaptureData.h"
+#include "ClientFlags/ClientFlags.h"
 #include "OrbitBase/Logging.h"
 #include "OrbitBase/ThreadConstants.h"
 #include "OrbitGl/BatcherInterface.h"
@@ -85,19 +88,20 @@ void CallstackThreadBar::DoDraw(PrimitiveAssembler& primitive_assembler,
   primitive_assembler.AddLine(pos, Vec2(x1, y0), event_bar_z, color, shared_from_this());
   primitive_assembler.AddLine(Vec2(x1, y1), Vec2(x0, y1), event_bar_z, color, shared_from_this());
 
-  if (picked_) {
-    Vec2& from = mouse_pos_last_click_;
-    Vec2& to = mouse_pos_cur_;
+  // This is being replaced by time range selection and the two should not be active together.
+  if (!picked_ || absl::GetFlag(FLAGS_time_range_selection)) return;
 
-    x0 = from[0];
-    y0 = pos[1];
-    x1 = to[0];
-    y1 = y0 + GetHeight();
+  Vec2& from = mouse_pos_last_click_;
+  Vec2& to = mouse_pos_cur_;
 
-    Color picked_color(0, 128, 255, 128);
-    Quad picked_box = MakeBox(Vec2(x0, y0), Vec2(x1 - x0, GetHeight()));
-    primitive_assembler.AddBox(picked_box, GlCanvas::kZValueUi, picked_color, shared_from_this());
-  }
+  x0 = from[0];
+  y0 = pos[1];
+  x1 = to[0];
+  y1 = y0 + GetHeight();
+
+  Color picked_color(0, 128, 255, 128);
+  Quad picked_box = MakeBox(Vec2(x0, y0), Vec2(x1 - x0, GetHeight()));
+  primitive_assembler.AddBox(picked_box, GlCanvas::kZValueUi, picked_color, shared_from_this());
 }
 
 void CallstackThreadBar::DoUpdatePrimitives(PrimitiveAssembler& primitive_assembler,
@@ -188,7 +192,10 @@ void CallstackThreadBar::DoUpdatePrimitives(PrimitiveAssembler& primitive_assemb
 
 void CallstackThreadBar::OnRelease() {
   CaptureViewElement::OnRelease();
-  SelectCallstacks();
+  // This is being replaced by time range selection and the two should not be active together.
+  if (!absl::GetFlag(FLAGS_time_range_selection)) {
+    SelectCallstacks();
+  }
 }
 
 void CallstackThreadBar::OnPick(int x, int y) {
