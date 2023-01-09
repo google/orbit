627c2345af3c218a3bdf583e4071dfdb8c36dc21
Henning Becker <43133967+beckerhe@users.noreply.github.com>
2023-01-09 14:28:49 +0100

    Add utilties to find the user's SSH config (#4676)
    There is a new function that produces an instance of `orbit_ssh::Credentials`
    based on the private keys available in the users .ssh config directory.

--- src/OrbitSsh/include/OrbitSsh/Credentials.h
+++ src/OrbitSsh/include/OrbitSsh/Credentials.h
@@ -8,6 +8,7 @@
 #include <filesystem>
 #include <string>
 
+#include "OrbitBase/Result.h"
 #include "OrbitSsh/AddrAndPort.h"
 
 namespace orbit_ssh {
@@ -18,6 +19,18 @@ struct Credentials {
   std::filesystem::path known_hosts_path;
   std::vector<std::filesystem::path> key_paths;
 };
+
+// Returns the path to the `~/.ssh` directory inside the user's home directory. Returns an error if
+// the directory doesn't exist, but if it does no further checks are performed. Use
+// `FindUsersCredentials` below to check whether the `.ssh` directory contains the necessary files.
+ErrorMessageOr<std::filesystem::path> GetUsersSshConfigDirectory();
+
+// Constructs an instance of `Credentials` with the given address, port and username, and all the
+// supported SSH keys found in `ssh_config_directory`. Returns an error if no supported key was
+// found. It also requires a `known_hosts` file to exist since we currently don't support writing to
+// or creating known_hosts files. This might change in the future.
+ErrorMessageOr<Credentials> FindUsersCredentials(const std::filesystem::path& ssh_config_directory,
+                                                 AddrAndPort addr_and_port, std::string username);
 
 }  // namespace orbit_ssh
 
