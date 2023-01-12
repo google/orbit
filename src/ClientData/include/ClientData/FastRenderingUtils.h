400fee2f5c7108d9086923c3441f26feecf1640d
Pablo Zimmermann <karupayun@google.com>
2023-01-12 14:19:24 +0100

    Inline methods from FastRenderingUtils (#4693)
    * Inline methods from FastRenderingUtils
    
    As they are called very often and are part of the bottleneck when
    zooming-out in long captures, we are inlining both methods.
    
    Test: Load a capture.
    
    * Fixup: erase lambda function

--- src/ClientData/include/ClientData/FastRenderingUtils.h
+++ src/ClientData/include/ClientData/FastRenderingUtils.h
@@ -9,17 +9,34 @@
 
 namespace orbit_client_data {
 
+// Free Function that returns the number of the pixel of a particular timestamp. The query
+// assumes a closed-open interval [start_ns, end_ns), so end_ns is not a visible timestamp.
+[[nodiscard]] inline uint64_t GetPixelNumber(uint64_t timestamp_ns, uint32_t resolution,
+                                             uint64_t start_ns, uint64_t end_ns) {
+  uint64_t current_ns_from_start = timestamp_ns - start_ns;
+  uint64_t total_ns = end_ns - start_ns;
+
+  // Given a resolution of 4000 pixels, we can capture for 53 days without overflowing.
+  return (current_ns_from_start * resolution) / total_ns;
+}
+
 // Free Function that will be used for any implementation of
 // TimerDataInterface::GetTimersAtDepthDiscretized to get the next pixel timestamp. The query
 // assumes a closed-open interval [start_ns, end_ns), so end_ns is not a visible timestamp.
-[[nodiscard]] uint64_t GetNextPixelBoundaryTimeNs(uint64_t current_timestamp_ns,
-                                                  uint32_t resolution, uint64_t start_ns,
-                                                  uint64_t end_ns);
+[[nodiscard]] inline uint64_t GetNextPixelBoundaryTimeNs(uint64_t current_timestamp_ns,
+                                                         uint32_t resolution, uint64_t start_ns,
+                                                         uint64_t end_ns) {
+  uint64_t current_pixel = GetPixelNumber(current_timestamp_ns, resolution, start_ns, end_ns);
+  uint64_t next_pixel = current_pixel + 1;
 
-// Free Function that returns the number of the pixel of a particular timestamp. The query
-// assumes a closed-open interval [start_ns, end_ns), so end_ns is not a visible timestamp.
-[[nodiscard]] uint64_t GetPixelNumber(uint64_t timestamp_ns, uint32_t resolution, uint64_t start_ns,
-                                      uint64_t end_ns);
+  uint64_t total_ns = end_ns - start_ns;
+  // To calculate the timestamp of a pixel boundary, we make a cross-multiplication rounding_up to
+  // be consistent to how we calculate current_pixel. We use that `ceil(a/b) = 1 + (a - 1) / b`,
+  // assuming a, b are integers greater than 0.
+  uint64_t next_pixel_ns_from_min = 1 + (total_ns * next_pixel - 1) / resolution;
+
+  return start_ns + next_pixel_ns_from_min;
+}
 
 }  // namespace orbit_client_data
 
