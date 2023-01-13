38bf86db599dc9ab0d5b846cc3e815d96f3a83ca
Henning Becker <43133967+beckerhe@users.noreply.github.com>
2023-01-13 14:25:06 +0100

    Add SingleThreadExecutor (#4705)
    This is adding a new type of `Executor` where tasks will be scheduled on
    a single background thread. The Executor is meant for code that
    interacts with Qt. The background thread is created and managed by a
    `QThread` which makes it interact well with Qt slots that should also
    run on the same background QThread.

---
+++ src/QtUtils/SingleThreadExecutor.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2023 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "QtUtils/SingleThreadExecutor.h"
+
+#include <QMetaObject>
+#include <Qt>
+#include <utility>
+
+#include "ApiInterface/Orbit.h"
+#include "OrbitBase/Action.h"
+
+namespace orbit_qt_utils {
+
+SingleThreadExecutor::SingleThreadExecutor(QObject* parent) : QObject{parent} {
+  thread_.start();
+  context_.moveToThread(&thread_);
+}
+
+SingleThreadExecutor::~SingleThreadExecutor() {
+  thread_.quit();
+  thread_.wait();
+}
+
+void SingleThreadExecutor::ScheduleImpl(std::unique_ptr<Action> action) {
+  QMetaObject::invokeMethod(
+      &context_,
+      [action = std::move(action)]() {
+        ORBIT_SCOPE("SingleThreadExecutor Action");
+        action->Execute();
+      },
+      Qt::QueuedConnection);
+}
+
+}  // namespace orbit_qt_utils
---
+++ src/QtUtils/include/QtUtils/SingleThreadExecutor.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2023 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef QT_UTILS_SINGLE_THREAD_EXECUTOR_H_
+#define QT_UTILS_SINGLE_THREAD_EXECUTOR_H_
+
+#include <QObject>
+#include <QThread>
+#include <memory>
+
+#include "OrbitBase/Action.h"
+#include "OrbitBase/Executor.h"
+
+namespace orbit_qt_utils {
+
+// An implementation of an Executor that schedules tasks on a background thread. Only use this
+// executor when you need your tasks to run on a `QThread`. If not, using the global instance of
+// `orbit_base::ThreadPool` is the better option.
+class SingleThreadExecutor : public QObject, public orbit_base::Executor {
+  Q_OBJECT
+
+ public:
+  explicit SingleThreadExecutor(QObject* parent = nullptr);
+  ~SingleThreadExecutor() override;
+
+  [[nodiscard]] const QThread* GetThread() const { return &thread_; }
+  [[nodiscard]] QThread* GetThread() { return &thread_; }
+
+  [[nodiscard]] Handle GetExecutorHandle() const override { return executor_handle_.Get(); }
+
+ private:
+  void ScheduleImpl(std::unique_ptr<Action> action) override;
+
+  QThread thread_{};
+  QObject context_{};
+  ScopedHandle executor_handle_{this};
+};
+
+}  // namespace orbit_qt_utils
+
+#endif  // QT_UTILS_SINGLE_THREAD_EXECUTOR_H_
