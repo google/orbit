ad82ec648e6bb2628a0c119b16dd4b1a38754591
reichlfl <freichl@google.com>
2023-01-18 11:25:09 +0000

    Add class comments in BatchRenderGroup.h

--- src/OrbitGl/include/OrbitGl/BatchRenderGroup.h
+++ src/OrbitGl/include/OrbitGl/BatchRenderGroup.h
@@ -12,12 +12,10 @@
 
 namespace orbit_gl {
 
-struct StencilConfig {
-  bool enabled = false;
-  std::array<float, 2> pos = {0, 0};
-  std::array<float, 2> size = {0, 0};
-};
-
+// A BatchRenderGroupId identifies a group of primitives that can be rendered together in a single
+// draw call. It is conceptually similar to a layer z-value, but it also includes a name that can be
+// used to manually separate elements into different groups. It is used by the Batcher and
+// TextRenderer to group elements into draw calls.
 struct BatchRenderGroupId {
   static const std::string kGlobalGroup;
 
@@ -55,10 +53,26 @@ struct hash<orbit_gl::BatchRenderGroupId> {
 
 namespace orbit_gl {
 
+// Group property: This defines if all rendering should be restricted to a bounding box, and if so,
+// stores the extents of this box
+struct StencilConfig {
+  bool enabled = false;
+  std::array<float, 2> pos = {0, 0};
+  std::array<float, 2> size = {0, 0};
+};
+
+// Collection of all properties that are associated with a BatchRenderGroup and that will influence
+// how the group is rendered.
 struct BatchRenderGroupState {
   StencilConfig stencil;
 };
 
+// Maps render group names to a collection of properties that influence rendering. This decouples
+// the state of render groups from the Batcher, as an instance of this class will be shared by
+// multiple Batcher and TextRenderer instances (usually all that use the same canvas).
+// Note that the mapping is done by group name only, not by the full group ID. Conceptually, this
+// means that groups with the same name share common properties, and the "layer" part of the group
+// ID defines a sub-ordering underneath this group name to ensure correct rendering order.
 class BatchRenderGroupStateManager {
  public:
   [[nodiscard]] BatchRenderGroupState GetGroupState(const std::string& group_name) const;
