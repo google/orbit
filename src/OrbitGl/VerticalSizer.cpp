288ea75464a40bcc62ee2cf1ac114a329e4ab1d8
Pierric Gimmig <pierric.gimmig@gmail.com>
2023-01-23 08:39:09 -0800

    Add VerticalSizer for track header (#4752)
    Add vertical sizer class to resize new track header. The cursor
    changes to horizontal arrows when the mouse is in the sizer.
    We keep a minimum width equal to the height of the timeline
    for symmetry, to keep the triangle toggles visible, and to keep
    the track moving feature available.
    
    Also, set right margin to 0, but keep logic just in case, and tweak
    distance between timeline and tracks for more stream lined look.

---
+++ src/OrbitGl/VerticalSizer.cpp
@@ -0,0 +1,57 @@
+// Copyright (c) 2023 The Orbit Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "OrbitGl/VerticalSizer.h"
+
+#include <QCursor>
+#include <QGuiApplication>
+
+#include "OrbitGl/GlCanvas.h"
+
+namespace orbit_gl {
+
+VerticalSizer::VerticalSizer(CaptureViewElement* parent, const orbit_gl::Viewport* viewport,
+                             const TimeGraphLayout* layout,
+                             std::function<void(int /*x*/, int /*y*/)> on_drag_callback)
+    : CaptureViewElement(parent, viewport, layout),
+      on_drag_callback_(std::move(on_drag_callback)) {}
+
+void VerticalSizer::SetHeight(float height) {
+  if (height == height_) return;
+  height_ = height;
+  RequestUpdate(RequestUpdateScope::kDraw);
+}
+
+void VerticalSizer::DoDraw(PrimitiveAssembler& primitive_assembler, TextRenderer& text_renderer,
+                           const DrawContext& draw_context) {
+  CaptureViewElement::DoDraw(primitive_assembler, text_renderer, draw_context);
+  Quad box = MakeBox(GetPos(), GetSize());
+  primitive_assembler.AddBox(box, GlCanvas::kZValueMargin, GlCanvas::kBackgroundColor,
+                             shared_from_this());
+}
+
+CaptureViewElement::EventResult VerticalSizer::OnMouseEnter() {
+  EventResult event_result = CaptureViewElement::OnMouseEnter();
+  if (QGuiApplication::instance() != nullptr) {
+    QGuiApplication::setOverrideCursor(Qt::SizeHorCursor);
+  }
+  return event_result;
+}
+
+CaptureViewElement::EventResult VerticalSizer::OnMouseLeave() {
+  EventResult event_result = CaptureViewElement::OnMouseLeave();
+  if (QGuiApplication::instance() != nullptr) {
+    QGuiApplication::restoreOverrideCursor();
+  }
+  return event_result;
+}
+
+void VerticalSizer::OnDrag(int x, int y) { on_drag_callback_(x, y); }
+
+std::unique_ptr<orbit_accessibility::AccessibleInterface>
+VerticalSizer::CreateAccessibleInterface() {
+  return nullptr;
+}
+
+}  // namespace orbit_gl
