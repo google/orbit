cf7d49853c2beb806617de98ddc072d7c3aae136
Pierric Gimmig <pierric.gimmig@gmail.com>
2023-01-24 20:34:55 -0800

    Update src/SessionSetup/ConnectToLocalWidget.cpp
    clang-format
    
    Co-authored-by: orbitprofiler-bot <orbitprofiler-bot@google.com>

--- src/SessionSetup/ConnectToLocalWidget.cpp
+++ src/SessionSetup/ConnectToLocalWidget.cpp
@@ -126,7 +126,7 @@ void ConnectToLocalWidget::SetupProcessListUpdater() {
           std::vector<orbit_grpc_protos::ProcessInfo> process_list) {
         if (self == nullptr) return;
         emit self->ProcessListUpdated(
-	    QVector<orbit_grpc_protos::ProcessInfo>::fromStdVector(process_list));
+            QVector<orbit_grpc_protos::ProcessInfo>::fromStdVector(process_list));
       });
 }
 
